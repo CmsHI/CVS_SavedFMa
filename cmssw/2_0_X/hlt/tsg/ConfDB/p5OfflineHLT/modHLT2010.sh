d5895d46fd53fcf7d34ce980c93e291e6ccd0596
frankma <>
2008-11-06 16:05:31 +0000

    added a default output name

--- cmssw/2_0_X/hlt/tsg/ConfDB/p5OfflineHLT/modHLT2010.sh
+++ cmssw/2_0_X/hlt/tsg/ConfDB/p5OfflineHLT/modHLT2010.sh
@@ -5,6 +5,8 @@ if [ $# -eq 0 ]; then
    echo "   20X menus (2_0_10+) to cmsRun in P5 2_0_10 + online tags"
    echo usage:
    echo "   $0 <inputHLTName> <outputHLTName> [maxEvents]"
+elif [ $# -eq 1 ]; then
+   sed 's/string fedColl = "rawDataCollector"/InputTag inputLabel = rawDataCollector/' $1 > "my$1"
 elif [ $# -eq 2 ]; then
    sed 's/string fedColl = "rawDataCollector"/InputTag inputLabel = rawDataCollector/' $1 > $2
 elif [ $# -eq 3 ]; then
