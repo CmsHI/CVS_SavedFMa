748d7618c915f9bc046fcd43c1e4f0174461c748
frankma <>
2008-11-07 12:57:02 +0000

    a shell script to check frontier global tag records in 20X

---
+++ bin/online_globaltag.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "frontier://(proxyurl=http://localhost:3128)(serverurl=http://frontier1.cms:8000/FrontierOnProd)(serverurl=http://frontier2.cms:8000/FrontierOnProd)(retrieve-ziplevel=0)/CMS_COND_20X_GLOBALTAG" -T $1 
+if [ $# -eq 0 ]; then
+   cmscond_tagtree_list -c "frontier://(proxyurl=http://localhost:3128)(serverurl=http://frontier1.cms:8000/FrontierOnProd)(serverurl=http://frontier2.cms:8000/FrontierOnProd)(retrieve-ziplevel=0)/CMS_COND_20X_GLOBALTAG" 
+   else
+   cmscond_tagtree_list -c "frontier://(proxyurl=http://localhost:3128)(serverurl=http://frontier1.cms:8000/FrontierOnProd)(serverurl=http://frontier2.cms:8000/FrontierOnProd)(retrieve-ziplevel=0)/CMS_COND_20X_GLOBALTAG" -T $1 
+fi
+
