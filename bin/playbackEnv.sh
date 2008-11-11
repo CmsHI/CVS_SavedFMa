1280f5a6b23967d5a5e92c513dd29015f06d6946
frankma <>
2008-11-11 10:30:52 +0000

    made output more spacious; added check on ShM max of system

--- bin/playbackEnv.sh
+++ bin/playbackEnv.sh
@@ -1,10 +1,17 @@
-echo "===Check XDAQ installation:"
+echo "===Check XDAQ installation:==="
 env | grep XDAQ
+
+echo
 echo "===Check HOST:"
 if [ $HOST ]; then
-   echo $HOST
+    echo "Ok. HOST defined:"
+    echo $HOST
 else
-   echo "Need to define host:"
-   echo "export HOST=$(hostname)"
+    echo "Need to define host:==="
+    echo "export HOST=$(hostname)"
 fi
+
+echo
+echo "===Check Shared Memory:==="
+cat /proc/sys/kernel/shmmax
 
