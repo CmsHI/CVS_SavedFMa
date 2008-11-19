ea65ead0430dbbb3bad33e76724b908ab1208cd5
frankma <>
2008-11-19 16:50:40 +0000

    Working version used using debug session with Emilio, adding also the testGlobalNumbers analyzer

--- cmssw/cmssw_2_1_X/EventFilter/AutoBU/playback/test/dumpRawData_cfg.py
+++ cmssw/cmssw_2_1_X/EventFilter/AutoBU/playback/test/dumpRawData_cfg.py
@@ -6,7 +6,10 @@ process = cms.Process("DumpDTRaw")
 # the source
 process.source = cms.Source("PoolSource",
                             fileNames = cms.untracked.vstring(
-    'file:/data/c/cerminar/data/GlobalRun/run61642_BeamSplash/Run61642_EventNumberSkim_RAW.root'
+    #'file:/data/c/cerminar/data/GlobalRun/run61642_BeamSplash/Run61642_EventNumberSkim_RAW.root'
+    
+    'file:/export/04a/frankma/data/217/raw_skim/minbiasSummer08_IDEAL_V9_v1_Raw1k.root'
+    #'file:/export/04a/frankma/data/2011/official/officialMB_PureRaw_8k.root'
     ),
                             skipEvents = cms.untracked.uint32(0) )
 
@@ -26,9 +29,13 @@ process.MessageLogger = cms.Service("MessageLogger",
                                     )
 
 process.dump = cms.EDAnalyzer("DumpFEDRawDataProduct",
-                              feds = cms.untracked.vint32(770, 771, 772, 773, 774, 775),
+	                      #feds = cms.untracked.vint32(770, 771, 772, 773, 774, 775),
+                              feds = cms.untracked.vint32(812,813),
                               dumpPayload = cms.untracked.bool(True)
                               )
 
+# Emilio added analyzer to see event # readout of Gt FED's
+process.ana = cms.EDAnalyzer("GlobalNumbersAnalysis"
+			     )
 
-process.dtRawDump = cms.Path(process.dump)
+process.dtRawDump = cms.Path(process.ana)
