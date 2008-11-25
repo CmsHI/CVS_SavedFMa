47a7a454fbb59ce5780f6a9d9a6a387eabb14467
frankma <>
2008-11-25 13:50:16 +0000

    a complete re-working, based on the template newNote.sh

--- bin/newREADME.sh
+++ bin/newREADME.sh
@@ -1,12 +1,3 @@
 #! /bin/bash -
-newREADME=README_$1.txt
-#echo $newREADME
-if [ -e $newREADME ]; then
-#if [ -e README_.txt ]; then
-   echo file already exists!
-   exit 1
-else
-   cp $HOME/bin/templates/README_new.txt $newREADME
-   emacs -nw $newREADME
-fi
+$HOME/bin/templates/newNote.sh README $HOME/bin/templates/README_new.txt $1
 
