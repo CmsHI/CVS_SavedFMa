d0143b8e62f9c9c7f370169d4693b6b4f1113fbc
frankma <>
2008-11-28 17:23:26 +0000

    added some reference comments

--- bin/offline_global_tag.sh
+++ bin/offline_global_tag.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+#-- For available tags: --
+# https://twiki.cern.ch/twiki/bin/view/CMS/SWGuideFrontierConditions
 #defaultRecord="CMS_COND_20X_GLOBALTAG"
 defaultRecord="CMS_COND_21X_GLOBALTAG"
+
+#-- For availabe frontier or local DB Connections --
+# https://twiki.cern.ch/twiki/bin/view/CMS/SWGuideORACLEProductionAndDevelopmentDatabaseSetup#Production
 
 #-- Interation --
 #connection='frontier://(proxyurl=http://localhost:3128)(serverurl=http://cmsfrontier.cern.ch:8000/FrontierInt)(serverurl=http://cmsfrontier1.cern.ch:8000/FrontierInt)(serverurl=http://cmsfrontier2.cern.ch:8000/FrontierInt)(serverurl=http://cmsfrontier3.cern.ch:8000/FrontierInt)(retrieve-ziplevel=0)'
