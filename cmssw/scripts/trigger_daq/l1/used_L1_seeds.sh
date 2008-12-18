f92366e2a0b5e2041e444219650b6241612f2d68
frankma <>
2008-12-18 15:01:10 +0000

    added help: usage

--- cmssw/scripts/trigger_daq/l1/used_L1_seeds.sh
+++ cmssw/scripts/trigger_daq/l1/used_L1_seeds.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-grep -o L1_[[:alnum:]]* $1 | grep -v L1Extra | sort | uniq
+echo $#
+if [ $# -eq 0 ]; then
+   echo "$0 usage:"
+   echo "   $0 <cfg_file_that_uses_L1_seeds>"
+else
+   grep -o L1_[[:alnum:]]* $1 | grep -v L1Extra | sort | uniq
+fi
 
