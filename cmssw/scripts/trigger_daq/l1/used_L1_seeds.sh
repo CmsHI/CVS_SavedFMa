ed2de5331e30cc51ab7c8be2a7ef552807d20505
frankma <>
2008-12-18 15:05:30 +0000

    added more help comments

--- cmssw/scripts/trigger_daq/l1/used_L1_seeds.sh
+++ cmssw/scripts/trigger_daq/l1/used_L1_seeds.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 if [ $# -eq 0 ]; then
-   echo "$0 usage:"
+   echo "$0"
+   echo "usage:"
    echo "   $0 <cfg_file_that_uses_L1_seeds>"
+   echo "output:"
+   echo "   stdout: a (sorted, unique) list of the L1 bits used"
+   echo "   in the cfg file"
 else
    grep -o L1_[[:alnum:]]* $1 | grep -v L1Extra | sort | uniq
 fi
