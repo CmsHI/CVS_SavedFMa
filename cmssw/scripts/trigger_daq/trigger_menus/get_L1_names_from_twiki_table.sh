07eb80c470677475d0488575375b72d271725deb
frankma <>
2008-12-18 18:36:20 +0000

    modified to use the awk program from ~/bin/awk directory

--- cmssw/scripts/trigger_daq/trigger_menus/get_L1_names_from_twiki_table.sh
+++ cmssw/scripts/trigger_daq/trigger_menus/get_L1_names_from_twiki_table.sh
@@ -2,6 +2,6 @@
 awk -F 'L1' '{print $2}' $1 |
    sed 's/^[[:space:]]*/L1_/' |
    #awk -v 'OFS=_' '{ print $1,$2,$3 }' |
-   awk -f fill_space.awk |
+   awk -f ~/bin/awk/fill_space_with_underscore.awk |
    sort | uniq
 
