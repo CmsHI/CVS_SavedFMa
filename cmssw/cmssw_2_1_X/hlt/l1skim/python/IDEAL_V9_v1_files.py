ad646cfc74d23fdafd554cb72c6cd151efa6583e
frankma <>
2008-12-19 16:17:59 +0000

    List of files for the /MinBias/Summer08 IDEAL_V9 production files, moved from  test/prod/prod_digi_l1_digi2raw/test_prescaled_menu/

---
+++ cmssw/cmssw_2_1_X/hlt/l1skim/python/IDEAL_V9_v1_files.py
@@ -0,0 +1,15 @@
+fileNames = [
+       'dcache:/pnfs/cmsaf.mit.edu/t2bat/cms/store/mc/Summer08/MinBias/GEN-SIM-RAW/IDEAL_V9_v1/0029/00C2D656-7282-DD11-AC51-001C23C0F1F9.root',
+       'dcache:/pnfs/cmsaf.mit.edu/t2bat/cms/store/mc/Summer08/MinBias/GEN-SIM-RAW/IDEAL_V9_v1/0029/023B03A1-B282-DD11-8E33-00E08147F80E.root',
+       'dcache:/pnfs/cmsaf.mit.edu/t2bat/cms/store/mc/Summer08/MinBias/GEN-SIM-RAW/IDEAL_V9_v1/0029/129C350B-B482-DD11-8641-001EC94BFFEB.root',
+       'dcache:/pnfs/cmsaf.mit.edu/t2bat/cms/store/mc/Summer08/MinBias/GEN-SIM-RAW/IDEAL_V9_v1/0029/14D3240B-B482-DD11-8769-001EC94BF999.root',
+       'dcache:/pnfs/cmsaf.mit.edu/t2bat/cms/store/mc/Summer08/MinBias/GEN-SIM-RAW/IDEAL_V9_v1/0029/16CCD8C4-B482-DD11-8D9A-001EC94BA146.root',
+       'dcache:/pnfs/cmsaf.mit.edu/t2bat/cms/store/mc/Summer08/MinBias/GEN-SIM-RAW/IDEAL_V9_v1/0029/26051E99-C082-DD11-A26F-0019B9CABD37.root',
+       'dcache:/pnfs/cmsaf.mit.edu/t2bat/cms/store/mc/Summer08/MinBias/GEN-SIM-RAW/IDEAL_V9_v1/0029/2AFB0264-7182-DD11-93B8-001C23C0F175.root',
+       'dcache:/pnfs/cmsaf.mit.edu/t2bat/cms/store/mc/Summer08/MinBias/GEN-SIM-RAW/IDEAL_V9_v1/0029/2E78EFA0-B282-DD11-A509-00E08147F80E.root',
+       'dcache:/pnfs/cmsaf.mit.edu/t2bat/cms/store/mc/Summer08/MinBias/GEN-SIM-RAW/IDEAL_V9_v1/0029/30426E55-7282-DD11-96B8-001C23C107AF.root',
+       'dcache:/pnfs/cmsaf.mit.edu/t2bat/cms/store/mc/Summer08/MinBias/GEN-SIM-RAW/IDEAL_V9_v1/0029/30F2D754-B382-DD11-A86F-001EC94B6929.root',
+       'dcache:/pnfs/cmsaf.mit.edu/t2bat/cms/store/mc/Summer08/MinBias/GEN-SIM-RAW/IDEAL_V9_v1/0029/36201CAC-7082-DD11-BF0F-00E0813405F8.root',
+       'dcache:/pnfs/cmsaf.mit.edu/t2bat/cms/store/mc/Summer08/MinBias/GEN-SIM-RAW/IDEAL_V9_v1/0029/36BB7E6F-A082-DD11-851D-00E081237869.root'
+]
+
