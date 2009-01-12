1915fdd82613ca361521ef2c2a6978db60b228db
frankma <>
2009-01-12 17:28:09 +0000

    Added some lesson comments, and updated scrab env file to the latest version

--- cmssw/cmssw_2_1_X/grid/crab/scripts/setcrab.sh
+++ cmssw/cmssw_2_1_X/grid/crab/scripts/setcrab.sh
@@ -4,6 +4,10 @@
 #   working in CMSSW_2_1_7
 #   Following WorkBookRunningGrid (updated Nov 2008 for 2_1_9)
 #
+# Note:
+#   Make you you source these env scripts in the correct order, as
+#   according to the twiki instructions
+#
 
 # 1. get access to a LCG User Interface (LCG UI). It will allow you to
 #    access WLCG-affiliated resources.
@@ -16,7 +20,9 @@ cd $l1skim_area
 cmsenv
 
 # 2. Source the crab env variables
+# From WorkBookRunningGrid (updated Nov 2008 for 2_1_9):
 #source /afs/cern.ch/cms/ccs/wm/scripts/Crab/CRAB_2_4_0_Tutorial/crab.sh
+# The lastest version:
 source /afs/cern.ch/cms/ccs/wm/scripts/Crab/crab.sh
 
 cd -
