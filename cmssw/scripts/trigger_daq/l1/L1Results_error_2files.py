9d00606d7cc03e4f2a786b32f4e0f0956578696c
frankma <>
2009-01-23 15:31:37 +0000

    allows input to be more general with re.finall('^[ ]*L1Menu')

--- cmssw/scripts/trigger_daq/l1/L1Results_error_2files.py
+++ cmssw/scripts/trigger_daq/l1/L1Results_error_2files.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python
 
 import sys
+import re
 
 def usage():
    print sys.argv[0]
@@ -10,45 +11,50 @@ def usage():
 argc = len(sys.argv)
 if argc == 1:
    usage()
-else:
-   colComp = 3
-   colName = 0
-   if argc >= 3:
-      file1 = sys.argv[1]
-      file2 = sys.argv[2]
-   if argc >= 4:
-      colComp = int(sys.argv[3])
-   if argc == 5:
-      colName = int(sys.argv[4])
+   sys.exit(1)
 
-   l1=[]
-   for line1 in open(file1).xreadlines():
+# Set parameters
+colMenu = 0
+colName = 1
+colComp = 2
+if argc >= 3:
+   file1 = sys.argv[1]
+   file2 = sys.argv[2]
+if argc >= 4:
+   colComp = int(sys.argv[3])
+if argc == 5:
+   colName = int(sys.argv[4])
+
+l1=[]
+for line1 in open(file1).xreadlines():
+   if re.findall('^[ ]*L1Menu',line1):
       p1 = line1.split()
       #print p1, len(p1)
       #print p1[0]
       l1.append(p1)
-   
-   l2=[]
-   for line2 in open(file2).xreadlines():
+
+l2=[]
+for line2 in open(file2).xreadlines():
+   if re.findall('^[ ]*L1Menu',line2):
       p2 = line2.split()
       #print p2, len(p2)
       #print p2[0]
       l2.append(p2)
-   
-   #print l1
-   #print l2
-   if len(l1) == len(l2):
-      i=0
-      for l in l1:
-         #print "i:", i
-         x1 = float(l1[i][colComp])
-         x2 = float(l2[i][colComp])
-	 print int(x1), "  ", int(x2)
-         if x1 == 0:
-	    print l1[i][colName].ljust(30)
-	 else:
-	    print l1[i][colName].ljust(30), " ",str((x1-x2)/x1).rjust(20)
-         i+=1
-   else:
-      print "list lengths don't match: ", len(l1), " and ", len(l2)
+
+#print l1
+#print l2
+if len(l1) == len(l2):
+   i=0
+   for l in l1:
+      #print "i:", i
+      x1 = float(l1[i][colComp])
+      x2 = float(l2[i][colComp])
+      #print int(x1), "  ", int(x2)
+      if x1 == 0:
+	 print l1[i][colName].ljust(30), " denominator is 0"
+      else:
+	 print l1[i][colName].ljust(30), " ",str((x1-x2)/x1).rjust(20)
+      i+=1
+else:
+   print "list lengths don't match: ", len(l1), " and ", len(l2)
 
