c03b0a55a30a1620c04c9ee566647517eac2ed11
frankma <>
2009-01-29 18:37:02 +0000

    first try to add a return state in response to the tests

--- bin/playbackEnv.sh
+++ bin/playbackEnv.sh
@@ -1,5 +1,11 @@
+result=0
 echo "===Check XDAQ installation:==="
 env | grep XDAQ
+if [ $? -ne 0 ]; then
+   result=2
+   echo XDAQ env not found
+   echo try to setup scramv1 env.
+fi
 
 echo
 echo "===Check HOST:"
@@ -14,4 +20,14 @@ fi
 echo
 echo "===Check Shared Memory:==="
 cat /proc/sys/kernel/shmmax
+
+echo
+echo "Result:"
+if [ $result -eq 0 ]; then
+   echo "  Software env all set for playback"
+else
+   echo "  warning, env not setup for playback"
+fi
+
+exit $result
 
