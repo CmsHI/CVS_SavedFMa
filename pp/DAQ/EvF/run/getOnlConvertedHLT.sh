1c22eae9d3dc60365dd6975509e959035fc05dd6
frankma <>
2009-02-02 19:09:29 +0000

    added the auto stripping of the HLT part of the converter output

--- pp/DAQ/EvF/run/getOnlConvertedHLT.sh
+++ pp/DAQ/EvF/run/getOnlConvertedHLT.sh
@@ -16,8 +16,16 @@ else
    confdbPath="."
 fi
 
+# Run onlineConverter
 cd $confdbPath/test
 ./runOnlineConverter --configName $cfgPath > $rawOut
 cd -
 mv $confdbPath/test/$rawOut .
+
+# Run stripOnlConvertedHLT2.sh
+cvs co -r cmssw_2_1_11 UserCode/SavedFMa/pp/DAQ/EvF/sw/EventFilter/ConfigDB/test/
+playbackCfg=$(echo "$cfgPath" | awk -F/ '{print $(NF-1)$(NF)}'| sed 's/cmsRun/playback/')
+playbackCfg=$playbackCfg.cfg
+echo playbackCfg: $playbackCfg
+UserCode/SavedFMa/pp/DAQ/EvF/sw/EventFilter/ConfigDB/test/stripOnlConvertedHLT2.sh $rawOut $playbackCfg
 
