634ed4a5d1e76de57a739b2e0ee4d8a8da2d513a
frankma <>
2009-02-05 13:33:05 +0000

    move out of Keberos 4 into 5, move to ssh way of connecting to repository

--- bin/cmsusr/cvsinit.sh
+++ bin/cmsusr/cvsinit.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-setcms
-project CMSSW
-kinit -4 frankma@CERN.CH
-# p5 has not yet moved to Kerberos V
+#setcms
+#project CMSSW
+kinit frankma@CERN.CH
+# p5 has moved to Kerberos V
+export CVS_RSH=ssh
+#alias cvss='cvs -d ":ext:cmscvs.cern.ch:/cvs_server/repositories/CMSSW"'
+export CVSROOT=":ext:cmscvs.cern.ch:/cvs_server/repositories/CMSSW"
+# cf Eric Cano's email
+# cf http://cvs.web.cern.ch/cvs/howto.php#accessing-clients
 
