e265848cebf4eb5d9430c8fc4969a5edd6bade46
frankma <>
2009-02-09 16:18:13 +0000

    Don't check out every time, just update

--- pp/DAQ/EvF/run/setupOnlConverter.sh
+++ pp/DAQ/EvF/run/setupOnlConverter.sh
@@ -10,11 +10,17 @@ startpath=$(pwd)
 cd $evfSwPath
 echo checking out EventFilter/ConfigDB to $(pwd)
 # co and compile ConfigDB (java) code
-cvs co -r V00-26-04 EventFilter/ConfigDB 
+if [ -e EventFilter/ConfigDB ]; then
+   cd EventFilter/ConfigDB
+   cvs up
+else
+   cvs co -r V00-26-04 EventFilter/ConfigDB
+   cd EventFilter/ConfigDB/
+fi
+
 # V00-26-04 is the head as of 2009.1.28
 # cf https://twiki.cern.ch/twiki/bin/view/CMS/EvfHiltonWorkingTutorialOnlineConverter
 #    for more information regarding the OnlineConverter
-cd $evfSwPath/EventFilter/ConfigDB/
 ant
 cd $startpath
 
