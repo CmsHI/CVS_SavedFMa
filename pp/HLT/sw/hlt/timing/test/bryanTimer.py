cb94e7ca0e8ec3ba434ff4d4fa5ddaec1c1cd2f2
frankma <>
2009-02-12 18:20:17 +0000

    now Bryan's executable runs, after modifying the endpath, instead of re-defining it out of order. Note: CMSSW by itself is ok to define the path anywhere, but some how Bryan's executable has more stringent requirements

--- pp/HLT/sw/hlt/timing/test/bryanTimer.py
+++ pp/HLT/sw/hlt/timing/test/bryanTimer.py
@@ -1,5 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
+#process = cms.Process("HLTtimer")
 import hlt.tsg.myp5_cmsRun_online_timingHLTV6 as myhlt
 process = myhlt.process
 
@@ -17,5 +18,25 @@ process.hltDefaultOutput.outputCommands = ['keep HLTPerformanceInfo_*_*_*']
 print process.hltDefaultOutput.outputCommands
 del process.hltDefaultOutput.SelectEvents
 
-process.HLTAnalyzerEndpath = cms.EndPath( process.timer + process.hltL1gtTrigReport + process.hltTrigReport )
+# Print out orginal paths
+# cf
+# - http://cmslxr.fnal.gov/lxr/source/L1Trigger/Configuration/python/patchToRerunL1Emulator.py
+# - for listing all the attributes of a python object, use: dir([obj])
+#   * cf http://docs.python.org/library/functions.html
+# - for end path (python) class definition
+#   * $CMSSW_RELEASE_BASE/python/FWCore/ParameterSet/SequenceTypes.py
+print "All paths found in original cfg:"
+for iterable in process.endpaths.itervalues():
+   print "  ", iterable.label(),": ", iterable
+   #del iterable
+
+#process.HLTAnalyzerEndpath = cms.EndPath( process.timer + process.hltL1gtTrigReport + process.hltTrigReport )
+# appearantly Bryan's timer executable would not work if the order of path definition 
+# is switched ...
+process.HLTAnalyzerEndpath.replace(process.hltL1gtTrigReport,process.timer)
+
+# Print out some useful info
+myep=process.HLTAnalyzerEndpath
+print "Modified path:"
+print "  ", myep.label(),": ", myep
 
