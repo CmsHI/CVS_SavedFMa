36b3441b110395065df5c4d0a70c42bfddcfe475
frankma <>
2009-03-16 18:34:27 +0000

    Main modifications 1. added HiGenParticleProducer 2. Modified as according to /UserCode/MitHig/HeavyIonJetAnalysis/prod/jets.py r1.3   * Changed the settings to iterativeCone5HiGenJets   * Didn't add any of the calojet cfg's 3. Added comments to the mixing module "MixingNoPileUp_cff" 4. Added Schedule 5. And a few others

--- HI/jet/sw/MitHig/HeavyIonJetAnalysis/test/producer/genjets_cfg.py
+++ HI/jet/sw/MitHig/HeavyIonJetAnalysis/test/producer/genjets_cfg.py
@@ -1,25 +1,40 @@
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process("GEN")
+process = cms.Process("GENJET")
 
+# Standard includes
 process.load("Configuration.StandardSequences.Geometry_cff")
 process.load("Configuration.StandardSequences.Generator_cff")
 
+# - Run trivial mixing module without secondary source
+#   * to add mixing frame products needed by HiGenParticleProducer
+#     + cf pwd/mixNoBackground.py
+process.load("Configuration.StandardSequences.MixingNoPileUp_cff")
+del process.mix.mixObjects
+process.mix.mixObjects = cms.PSet(
+   mixHepMC = cms.PSet(process.mixHepMCProducts)
+)
+
+# Jet related includes
 process.load("RecoJets.Configuration.RecoGenJets_cff")
 process.load("RecoJets.JetProducers.IconeJetParameters_cfi")
 process.load("RecoJets.Configuration.GenJetParticles_cff")
 process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
 
-#process.load("GeneratorInterface.PyquenInterface.pyquenSourceDefault_cfi")
-#process.PyquenSource.doQuench = False
+# HiGenParticleProducer
+# - borrows the cross frame to produce association maps
+#   * which in this case is the SubEventMap
+#   * thus we need the framework of mixing
+#     + runs a trivial mixing just to get the crossing frame
+#       products (see above)
+process.load("RecoHI.HiCandidateAlgos.hiGenParticles_cfi")
 
-process.load("GeneratorInterface.HydjetInterface.hydjetSourceDefault_cfi")
-process.HydjetSource.allowEmptyEvents = cms.bool(False)
-process.HydjetSource.hydjetMode = 'kQJetsOnly'
-#process.HydjetSource.hydjetMode = 'kJetsOnly'
+process.source = cms.Source("PoolSource",
+   fileNames = cms.untracked.vstring('file:hydjet_edm.root')
+)
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(1000)
+    input = cms.untracked.int32(10)
 )
 
 process.SimpleMemoryCheck = cms.Service('SimpleMemoryCheck',
@@ -28,40 +43,38 @@ process.SimpleMemoryCheck = cms.Service('SimpleMemoryCheck',
                                         )
 
 process.iterativeCone5HiGenJets = cms.EDProducer("IterativeConeHiGenJetProducer",
-                                                 process.GenJetParameters,  
+#                                                 process.GenJetParameters,  
                                                  process.IconeJetParameters, 
-                                                 jetType = cms.untracked.string('GenJet'), 
+						 inputEtMin = cms.double(0.0),
+						 inputEMin = cms.double(0.0),
                                                  alias = cms.untracked.string('IC5HiGenJet'),
                                                  coneRadius = cms.double(0.5)
                                                  )
 
 
-process.RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService",
-                                                   sourceSeed = cms.untracked.uint32(1)
-                                                   )
-
 process.subevent = cms.EDAnalyzer('HeavyIonJetAnalyzer',
-                                                               jetSrc = cms.vstring('iterativeCone5HiGenJets')
-                                                               )
+   jetSrc = cms.vstring('iterativeCone5HiGenJets')
+)
 
 process.allevent =  cms.EDAnalyzer('HeavyIonJetAnalyzer',
-                                                                      jetSrc = cms.vstring('iterativeCone5GenJets'),
-                                                                      doParticles = cms.untracked.bool(False)
-                                                                      )
+   jetSrc = cms.vstring('iterativeCone5GenJets'),
+   doParticles = cms.untracked.bool(False)
+)
 
 process.TFileService = cms.Service('TFileService',
-                                   fileName = cms.string('genjets.root')
+                                   fileName = cms.string('genjets_tree.root')
                                    )
 
 process.output = cms.OutputModule("PoolOutputModule",
     compressionLevel = cms.untracked.int32(2),
     commitInterval = cms.untracked.uint32(1),
-    fileName = cms.untracked.string('PbPb_edm.root')
+    fileName = cms.untracked.string('genjets_edm.root')
 )
 
-process.runjets = cms.Sequence(process.GeneInfo*process.genJetParticles*process.iterativeCone5GenJets*process.iterativeCone5HiGenJets)
+process.runjets = cms.Sequence(process.GeneInfo*process.hiGenParticles*process.iterativeCone5HiGenJets)
+process.pre = cms.Path(process.mix)
+# mixing module is needed by Philip's HiGenParticleProducer
 process.p = cms.Path(process.runjets*process.subevent*process.allevent)
 process.outpath = cms.EndPath(process.output)
 
-
-
+process.schedule = cms.Schedule(process.pre,process.p,process.outpath)
