2bffba3402fd4442b36a84194eb1337f03b0418c
frankma <>
2009-05-06 13:20:29 +0000

    setup root on cgate

---
+++ analysis/root/scripts/set_root.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+#
+# Script to setup root
+# - main ideas
+#   * setup $ROOTSYS
+#   * setup $PATH and a few other related variables
+# - cf
+#   * http://root.cern.ch/drupal/content/installing-root-source
+#
+
+#--- Define variables ---
+rootpath=/app/root/x86_64/5-14-00
+
+#--- Setup Root env using script ---
+#--Setup main root env variables--
+source /opt/bin/sh/setrootpath.sh $rootpath
+#--Setup system variables and include paths--
+source $rootpath/bin/thisroot.sh
+
+
