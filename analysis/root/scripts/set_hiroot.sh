d376da550a78e87d5d9b7459254971c8c278d3b9
frankma <>
2009-05-06 13:24:47 +0000

    my 1st version

--- analysis/root/scripts/set_hiroot.sh
+++ analysis/root/scripts/set_hiroot.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
+#--- Setup Root env using script ---
+. $SavedFMa/analysis/root/scripts/set_root.sh
 
-source /opt/bin/sh/setrootpath.sh /app/root/x86_64/5-14-00
-source /opt/bin/sh/sethirootpath.sh $HOME/HiRoot/hiroot_dev
+#--- Setup hiroot env ---
+source /opt/bin/sh/sethirootpath.sh $HOME/work/hiroot
 
-export SCRAM_ARCH=slc3_ia32_gcc323
+#export SCRAM_ARCH=slc3_ia32_gcc323
 
-export HIROOT=$HOME/HiRoot/hiroot_dev
-
+export HIROOT=$HOME/work/hiroot
 export LD_LIBRARY_PATH=$HIROOT/lib:$LD_LIBRARY_PATH
