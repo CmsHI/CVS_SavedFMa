819193d820c3faeb71f0042b73dff44e766bfab4
frankma <>
2009-05-18 10:11:01 +0000

    Christof's macro to generate a HROOT tree with index tree. Taken from /macros/hictl/createHRIT.C

---
+++ HI/analysis/dijet/sw/hiroot/macros/hictl/createHRIT.C
@@ -0,0 +1,40 @@
+// $Id: createHRIT.C 2837 2006-05-28 20:57:16Z loizides $
+
+/* 
+   This macro generates a HRT with index tree
+
+   Input:
+   THIGenerator *gen:  configured generator object
+   THIGenerator *igen: configured index generator object
+   ULong_t n: number of events to generate
+   ULong_t runno: run number to use
+*/   
+
+#if !defined(__CINT__) || defined(__MAKECINT__)
+#include <Riostream.h>
+#include <THIDebug.h>
+#include <THIGenerator.h>
+#include <THIIndexGenerator.h>
+#include <THITreeWriter.h>
+#endif
+
+//________________________________________________________________________
+void createHRIT(THIGenerator *gen, THIIndexGenerator *igen, ULong_t n, ULong_t runno)
+{
+   gDebugMask = THIDebug::kTreeWriting;
+   gDebugLevel = 1; //Increase this if needed.
+
+   if(!gen) {
+      cerr << "Generator must be instantiated!" << endl;
+      return;
+   }
+
+   THITreeWriter tw(gen,igen);
+   if(gSystem->Getenv("HIROOT_TREEPREFIX")) {
+      TString pref=gSystem->Getenv("HIROOT_TREEPREFIX");
+      tw.SetNamePrefix(Form("%s_hrt",pref.Data()));
+      tw.SetIndexNamePrefix(Form("%s_hrit",pref.Data()));
+   }
+
+   tw.Generate(runno,n);
+}
