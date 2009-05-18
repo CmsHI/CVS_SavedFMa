dd4ada3bec8af37eab055cc129f1c8dd34065347
frankma <>
2009-05-18 14:43:50 +0000

    gave pt cut, also uses the factorized printing of canvases

--- HI/analysis/dijet/sw/hiroot/macros/anareco/plotLeading.C
+++ HI/analysis/dijet/sw/hiroot/macros/anareco/plotLeading.C
@@ -20,22 +20,24 @@ void plotLeading(char * infname1 = "/net/pstore01/d00/scratch/frankma/hiroot/pyt
 //   getHistos(ntpythia, "Pythia");
 
    //--- inv mass ---
-   drawTree(ntpythia, "mass>>hMassPythia","","",    "hMassPythia","Pythia: inv mass of dijet",100,0,1800,true,kRed,1,3);
-   drawTree(ntpyquen, "mass>>hMassPyquen","","same","hMassPyquen","draw Pyquen: inv mass of dijet",100,0,1800,true,kBlue,1,3);
+   drawTree(ntpythia, "mass>>hMassPythia","nlpet>1 && alpet>1","",    "hMassPythia","Pythia: inv mass of dijet",100,0,1800,true,kRed,1,3);
+   drawTree(ntpyquen, "mass>>hMassPyquen","nlpet>1 && alpet>1","same","hMassPyquen","draw Pyquen: inv mass of dijet",100,0,1800,true,kBlue,1,3);
    
    //--- Et ---
    // -near-
-   drawTree(ntpythia, "nlpet>>hNearLPartonPythia","","",    "hNearLPartonPythia","Pythia: leading partons",100,80,200,true,kRed,1,3);
-   drawTree(ntpyquen, "nlpet>>hNearLPartonPyquen","","same","hNearLPartonPyquen","Pyquen: leading partons",100,80,200,true,kBlue,1,3);
+   drawTree(ntpythia, "nlpet>>hNearLPartonPythia","nlpet>1","",    "hNearLPartonPythia","Pythia: leading partons",100,80,200,true,kRed,1,3);
+   drawTree(ntpyquen, "nlpet>>hNearLPartonPyquen","nlpet>1","same","hNearLPartonPyquen","Pyquen: leading partons",100,80,200,true,kBlue,1,3);
    // -away--
-   drawTree(ntpythia, "alpet>>hAwayLPartonPythia","","same","hAwayLPartonPythia","Pythia: leading partons",100,80,200,true,kRed,7,3);
-   drawTree(ntpyquen, "alpet>>hAwayLPartonPyquen","","same","hAwayLPartonPyquen","draw Pyquen: leading partons",100,80,200,true,kBlue,7,3);
+   drawTree(ntpythia, "alpet>>hAwayLPartonPythia","alpet>1","same","hAwayLPartonPythia","Pythia: leading partons",100,80,200,true,kRed,7,3);
+   drawTree(ntpyquen, "alpet>>hAwayLPartonPyquen","alpet>1","same","hAwayLPartonPyquen","draw Pyquen: leading partons",100,80,200,true,kBlue,7,3);
 
    //--- dphi ---
-   drawTree(ntpythia, "dphi>>hdPhiPythia","","","hdPhiPythia","Pythia: dphi of leading partons",100,0,3.14,true,kRed,1,3);
-   drawTree(ntpyquen, "dphi>>hdPhiPyquen","","same","hdPhiPyquen","draw Pyquen: dphi of leading partons",100,0,3.14,true,kBlue,1,3);
+   drawTree(ntpythia, "dphi>>hdPhiPythia","nlpet>1 && alpet>1","","hdPhiPythia","Pythia: dphi of leading partons",100,0,3.14,true,kRed,1,3);
+   drawTree(ntpyquen, "dphi>>hdPhiPyquen","nlpet>1 && alpet>1","same","hdPhiPyquen","draw Pyquen: dphi of leading partons",100,0,3.14,true,kBlue,1,3);
 
    //--- Et ratio ---
    drawTree(ntpythia, "(alpet/nlpet)>>hEtRatioPythia","","","hEtRatioPythia","Pythia: Et ratio of leading partons",100,0,1.5,true,kRed,1,3);
    drawTree(ntpyquen, "(alpet/nlpet)>>hEtRatioPyquen","","same","hEtRatioPyquen","draw Pyquen: Et ratio of leading partons",100,0,1.5,true,kBlue,1,3);
+
+   printAllCanvases();
 }
