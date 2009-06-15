0c56f8180c7c7c9891104bf74c93e9ec2bb7eded
frankma <>
2009-06-15 21:48:35 +0000

    added xi binning variable

--- HI/analysis/dijet/sw/hiroot/macros/anareco/testJetFFana/run2JetAna_Pt90to100.sh
+++ HI/analysis/dijet/sw/hiroot/macros/anareco/testJetFFana/run2JetAna_Pt90to100.sh
@@ -9,11 +9,11 @@ export rootskim=iCone5_match35_jetEtCut30_ntJetEt
 export infileName=test_50k.root
 export input0="$HIROOT_BASEURL/$job/$gen0/$rootskim"
 export input1="$HIROOT_BASEURL/$job/$gen1/$rootskim"
-#echo "input skim: $skim"
 
 #== output vars ===
 export ana=ptMin100
 export plotbase=$dijetSens/jetana/plots
+export NXIBIN=100
 
 ./runJetAnaBase.sh
 
