cc22c03ff2ba19e3a26f0c4fb31b2453702edb68
frankma <>
2009-06-22 14:40:29 +0000

    update to generalize format

--- HI/analysis/dijet/sw/hiroot/macros/anareco/testJetFFana/run3JetAna_PtScan.sh
+++ HI/analysis/dijet/sw/hiroot/macros/anareco/testJetFFana/run3JetAna_PtScan.sh
@@ -8,9 +8,11 @@ tar xf macros.tar
 
 for i in $(seq 70 $PtBin 130); do
    pytmin=$i
-   pytmax=$((i+$PtBin))
+   pytmax=$(($pytmin+$PtBin))
    pyqmin=$i
-   pyqmax=$((i+$PtBin))
+   pyqmax=$(($pyqmin+$PtBin))
    echo $pytmin $pytmax $pyqmin $pyqmax $amin $dphi
    ./runGenericJetAnaPt.sh test_300k.root $pytmin $pytmax $amin $pyqmin $pyqmax $amin $dphi
+   #./runGenericJetAnaPt.sh test_50k.root $pytmin $pytmax $amin $pyqmin $pyqmax $amin $dphi
 done
+
