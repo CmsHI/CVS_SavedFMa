91984367bb85c3455b42477f596343a9816a7cef
frankma <>
2009-06-22 09:35:51 +0000

    start to make analysis more modular. Initial goal: put cuts into a class. Further goal: put analysis into a class

---
+++ HI/analysis/dijet/sw/hiroot/macros/anareco/testJetFFana/dijetAna.h
@@ -0,0 +1,11 @@
+namespace dijetAna {
+   const char * drsgFF="hist";
+   const char * drdbFF="hist same";
+   const char * drsgFFE="E1 ";
+   const char * drdbFFE="E1 same";
+
+   Int_t HJETETBINS = 100;
+   Double_t HJETETMIN = 0;
+   Double_t HJETETMAX = 200;
+}
+
