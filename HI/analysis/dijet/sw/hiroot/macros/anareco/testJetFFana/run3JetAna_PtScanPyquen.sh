94719e555463365d0283c9feb9d394e0e8d8b415
frankma <>
2009-06-22 14:26:24 +0000

    scan over pyquen pt

---
+++ HI/analysis/dijet/sw/hiroot/macros/anareco/testJetFFana/run3JetAna_PtScanPyquen.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+PtBin=20
+amin=50
+dphi=3.0
+
+tar xf macros.tar
+
+for i in $(seq 70 $PtBin 130); do
+   pytmin=90
+   pytmax=$(($pytmin+$PtBin))
+   pyqmin=$i
+   pyqmax=$((i+$PtBin))
+   echo $pytmin $pytmax $pyqmin $pyqmax $amin $dphi
+   ./runGenericJetAnaPt.sh test_300k.root $pytmin $pytmax $amin $pyqmin $pyqmax $amin $dphi
+   #./runGenericJetAnaPt.sh test_50k.root $pytmin $pytmax $amin $pyqmin $pyqmax $amin $dphi
+done
