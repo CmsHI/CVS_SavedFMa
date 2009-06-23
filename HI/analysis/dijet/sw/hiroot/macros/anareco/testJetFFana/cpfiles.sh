dc59bc677704803df649a9abf4b30b8a35b14040
frankma <>
2009-06-23 19:13:58 +0000

    add condor submission files

--- HI/analysis/dijet/sw/hiroot/macros/anareco/testJetFFana/cpfiles.sh
+++ HI/analysis/dijet/sw/hiroot/macros/anareco/testJetFFana/cpfiles.sh
@@ -1,9 +1,15 @@
+# macros
 cp /net/hisrv0001/home/frankma/work/hiroot/test/dijet/jetana/runAna/rootlogon.C .
 cp /net/hisrv0001/home/frankma/work/hiroot/test/dijet/jetana/runAna/dijetAna.h .
 cp /net/hisrv0001/home/frankma/work/hiroot/test/dijet/jetana/runAna/plotJetLeading.C .
 cp /net/hisrv0001/home/frankma/work/hiroot/test/dijet/jetana/runAna/plotFF.C .
 cp /net/hisrv0001/home/frankma/work/hiroot/test/dijet/jetana/runAna/plotFinal.C .
+# run scripts
 cp /net/hisrv0001/home/frankma/work/hiroot/test/dijet/jetana/runAna/runJetAnaBase.sh .
 cp /net/hisrv0001/home/frankma/work/hiroot/test/dijet/jetana/runAna/runGenericJetAnaPt.sh .
 cp /net/hisrv0001/home/frankma/work/hiroot/test/dijet/jetana/runAna/run3JetAna_PtScan.sh .
 cp /net/hisrv0001/home/frankma/work/hiroot/test/dijet/jetana/runAna/run3JetAna_PtScanPyquen.sh .
+# condor scripts
+cp /net/hisrv0001/home/frankma/work/hiroot/test/dijet/job/scanPyquen/run3JetAna_PtScanPyquen.sh .
+cp /net/hisrv0001/home/frankma/work/hiroot/test/dijet/job/scanPyquen/run3JetAna_DPhiScan.sh .
+
