fa1ddb7863fcaf5aec8c614ccf5f8c2bd810277d
frankma <>
2009-06-23 17:51:07 +0000

    Record: dev source area

---
+++ HI/analysis/dijet/sw/hiroot/anareco/cpfiles.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -
+. /net/hisrv0001/home/frankma/UserCode/SavedFMa/bin/cgate/set_hiroot.sh
+cp $HIROOT/anareco/anarecoLinkDef.h .
+cp $HIROOT/anareco/anareco.mk .
+cp $HIROOT/anareco/THIDiJetTruthAnaMod.cxx .
+cp $HIROOT/anareco/THIDiJetTruthAnaMod.h .
