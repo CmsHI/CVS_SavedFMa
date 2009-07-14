4ebdb94c5ac32533645c9b7dfe0e91888d1aeac4
frankma <>
2009-07-14 12:17:10 +0000

    add option to specify destination file name

--- bin/newGetFiles.sh
+++ bin/newGetFiles.sh
@@ -1,5 +1,15 @@
 #!/bin/bash -
-fileName=${1##/*/}
-destination=`pwd`
-echo "cp -v $1 $destination/$fileName" > getfiles.sh
+if [ $# -eq 0 ]; then
+   echo "Usage:"
+   echo "  $0 <source_file> [destination_file]"
+fi
+if [ $# -eq 1 ]; then
+   fileName=${1##/*/}
+   destination=`pwd`/$fileName
+fi
+if [ $# -gt 1 ]; then
+   destination=`pwd`/$2
+fi
+
+echo "cp -v $1 $destination" > getfiles.sh
 chmod +x ./getfiles.sh
