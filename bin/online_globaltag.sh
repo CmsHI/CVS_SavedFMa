f2fd9990d2bb5cdb44fcf3a7f4cb0e417f982cd0
frankma <>
2009-07-16 16:12:29 +0000

    update default record to 31X

--- bin/online_globaltag.sh
+++ bin/online_globaltag.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #defaultRecord="CMS_COND_20X_GLOBALTAG"
-defaultRecord="CMS_COND_21X_GLOBALTAG"
+#defaultRecord="CMS_COND_21X_GLOBALTAG"
+defaultRecord="CMS_COND_31X_GLOBALTAG"
 
 connection='frontier://(proxyurl=http://localhost:3128)(serverurl=http://frontier1.cms:8000/FrontierOnProd)(serverurl=http://frontier2.cms:8000/FrontierOnProd)(retrieve-ziplevel=0)'
 
