c3473189feeef07be5e9abbcd4d9f70ce48b80b8
frankma <>
2009-07-22 15:05:42 +0000

    added version option

--- HI/HLT/scripts/getOfflHLT.sh
+++ HI/HLT/scripts/getOfflHLT.sh
@@ -5,13 +5,25 @@
 #   * CMSSW
 #
 
+dbCfg=/user/frankma/HI/cmssw311/devHIon/V3
+
 if [ $# -eq 0 ]; then
    echo usage:
    echo "  $0 <file:raw.root>"
+   echo "versions:"
+   edmConfigFromDB --configName $dbCfg --versions
    exit 1
 fi
-dbCfg=/user/frankma/HI/cmssw311/devHIon
-name=${dbCfg##/*/}
+
+if echo $dbCfg | grep -q 'V[[:alnum:]]$'; then
+   name=`echo $dbCfg | awk -F/ '{print $(NF-1) $(NF)}'`
+else
+   name=${dbCfg##/*/}
+fi
+
 echo === $dbCfg: ===
 edmConfigFromDB --configName $dbCfg --versions
+echo output: $name
+
+# run
 edmConfigFromDB --configName $dbCfg --input $1 > ${name}_cfg.py
