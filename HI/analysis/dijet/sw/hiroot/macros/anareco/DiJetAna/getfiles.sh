8988162f54a708e3b0c6ccfc8fef9b38a2cc7cd9
frankma <>
2009-07-24 16:39:21 +0000

    added ana cut classes

--- HI/analysis/dijet/sw/hiroot/macros/anareco/DiJetAna/getfiles.sh
+++ HI/analysis/dijet/sw/hiroot/macros/anareco/DiJetAna/getfiles.sh
@@ -1,4 +1,6 @@
 cp -v /net/hisrv0001/home/frankma/work/hiroot/test/dijet/jetana/DiJetAna/DiJets.h DiJets.h
 cp -v /net/hisrv0001/home/frankma/work/hiroot/test/dijet/jetana/DiJetAna/dijetsDriver.C .
 cp -v /net/hisrv0001/home/frankma/work/hiroot/test/dijet/jetana/DiJetAna/runGenericJetAnaPt.sh .
+cp -v /net/hisrv0001/home/frankma/work/hiroot/test/dijet/jetana/DiJetAna/AnaCuts.h .
+cp -v /net/hisrv0001/home/frankma/work/hiroot/test/dijet/jetana/DiJetAna/anacutDriver.C .
 
