7e61d3771b435387b01b336f93071291ef6aa594
frankma <>
2009-07-25 23:20:25 +0000

    added TreeData class

--- HI/analysis/dijet/sw/hiroot/macros/anareco/DiJetAna/getfiles.sh
+++ HI/analysis/dijet/sw/hiroot/macros/anareco/DiJetAna/getfiles.sh
@@ -3,4 +3,5 @@ cp -v /net/hisrv0001/home/frankma/work/hiroot/test/dijet/jetana/DiJetAna/dijetsD
 cp -v /net/hisrv0001/home/frankma/work/hiroot/test/dijet/jetana/DiJetAna/runGenericJetAnaPt.sh .
 cp -v /net/hisrv0001/home/frankma/work/hiroot/test/dijet/jetana/DiJetAna/AnaCuts.h .
 cp -v /net/hisrv0001/home/frankma/work/hiroot/test/dijet/jetana/DiJetAna/anacutDriver.C .
+cp -v /net/hisrv0001/home/frankma/work/hiroot/test/dijet/jetana/DiJetAna/TreeData.h .
 
