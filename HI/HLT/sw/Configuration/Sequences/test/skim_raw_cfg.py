c6ff58f0cde13413d8e2a321da86ace83f29c81d
frankma <>
2009-07-28 11:18:55 +0000

    example cfg to run the skim raw template

---
+++ HI/HLT/sw/Configuration/Sequences/test/skim_raw_cfg.py
@@ -0,0 +1,25 @@
+import FWCore.ParameterSet.Config as cms
+# os var's
+from  os import getenv
+cvsdir=getenv("SavedFMa")
+import sys
+sys.path.append(cvsdir)
+# sys path
+import sys
+cfgpath=cvsdir+'/HI/HLT/sw/Configuration/Sequences/python'
+sys.path.append(cfgpath)
+
+# main cfg
+from skim_raw_template_cfg import *
+
+# dataset
+process.load("Local_source_cff")
+process.source.fileNames = ['file:MinBias_cfi_GEN_SIM_DIGI_L1_DIGI2RAW.root']
+
+# n of events to run
+process.maxEvents.input = 100
+
+# output file
+process.output.fileName = 'MinBias_raw322patch2.root'
+print process.output.fileName
+print process.out_step
