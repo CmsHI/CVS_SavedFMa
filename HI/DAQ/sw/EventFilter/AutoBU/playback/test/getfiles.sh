065d9d542125289f1bb72fd2c0eb57e010ea6602
frankma <>
2009-07-30 16:01:00 +0000

    - after the first round of digi2raw fixes in 31X   * cf http://www.cmsaf.mit.edu/twiki/bin/view/CmsHi/SavedFrankHiDaq31XLog#2009_7_30_Thur

---
+++ HI/DAQ/sw/EventFilter/AutoBU/playback/test/getfiles.sh
@@ -0,0 +1,3 @@
+#cp -v /nfshome0/frankma/work/HI/DAQ/sw/devCMSSW_3_1_1_onlpatch1_ONLINEp2-7/src/EventFilter/AutoBU/playback/initialize /nfshome0/frankma/UserCode/SavedFMa/HI/DAQ/sw/EventFilter/AutoBU/playback/initialize
+cp -v /nfshome0/frankma/scratch/HI/DAQ/proCMSSW_3_1_1_onlpatch3_ONLINEp2-7/initialize initialize
+cp -v /nfshome0/frankma/scratch/HI/DAQ/proCMSSW_3_1_1_onlpatch3_ONLINEp2-7/runFF.sh .
---
+++ HI/DAQ/sw/EventFilter/AutoBU/playback/test/runFF.sh
@@ -0,0 +1,4 @@
+# 2009/7/30
+# $@: -playback raw.root, for now
+# sm has some errors ..., currently running without
+./initialize -python -noSM "$@"
