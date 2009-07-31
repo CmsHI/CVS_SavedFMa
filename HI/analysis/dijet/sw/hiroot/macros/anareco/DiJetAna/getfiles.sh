cf16b9cff0e7f71e1fe0d1f12a037b98537e7111
frankma <>
2009-07-31 01:24:38 +0000

    new files after hastily ran plot macros using the new classes. Also beginning to add a tree event data class

--- HI/analysis/dijet/sw/hiroot/macros/anareco/DiJetAna/getfiles.sh
+++ HI/analysis/dijet/sw/hiroot/macros/anareco/DiJetAna/getfiles.sh
@@ -1,7 +1,19 @@
+# c++
 cp -v /net/hisrv0001/home/frankma/work/hiroot/test/dijet/jetana/DiJetAna/DiJets.h DiJets.h
 cp -v /net/hisrv0001/home/frankma/work/hiroot/test/dijet/jetana/DiJetAna/dijetsDriver.C .
-cp -v /net/hisrv0001/home/frankma/work/hiroot/test/dijet/jetana/DiJetAna/runGenericJetAnaPt.sh .
 cp -v /net/hisrv0001/home/frankma/work/hiroot/test/dijet/jetana/DiJetAna/AnaCuts.h .
 cp -v /net/hisrv0001/home/frankma/work/hiroot/test/dijet/jetana/DiJetAna/anacutDriver.C .
 cp -v /net/hisrv0001/home/frankma/work/hiroot/test/dijet/jetana/DiJetAna/TreeData.h .
+cp -v /net/hisrv0001/home/frankma/work/hiroot/test/dijet/jetana/DiJetAna/TreeDiJetEventData.h .
+cp -v /net/hisrv0001/home/frankma/work/hiroot/test/dijet/jetana/DiJetAna/treedijeteventdataDriver.C .
 
+# macros
+cp -v /net/hisrv0001/home/frankma/work/hiroot/test/dijet/jetana/DiJetAna/rootlogon.C .
+cp -v /net/hisrv0001/home/frankma/work/hiroot/test/dijet/jetana/DiJetAna/plotFF.C .
+cp -v /net/hisrv0001/home/frankma/work/hiroot/test/dijet/jetana/DiJetAna/plotJetLeading.C .
+cp -v /net/hisrv0001/home/frankma/work/hiroot/test/dijet/jetana/DiJetAna/plotFinal.C .
+
+
+# scripts
+cp -v /net/hisrv0001/home/frankma/work/hiroot/test/dijet/jetana/DiJetAna/runGenericJetAnaPt.sh .
+cp -v /net/hisrv0001/home/frankma/work/hiroot/test/dijet/jetana/DiJetAna/runJetAnaBase.sh .
