2c194e7d3635fc2376aeac5d746a832de6e8c759
frankma <>
2009-08-01 02:20:35 +0000

    - Implemented the tree set branch function   * A little disappointing that we can't use root 5-22, b/c the version does not     yet install tam in the mit root dir   * So we can't use vectors for branches :(   * But use array for now

--- HI/analysis/dijet/sw/hiroot/macros/anareco/DiJetAna/TreeDiJetEventData.h
+++ HI/analysis/dijet/sw/hiroot/macros/anareco/DiJetAna/TreeDiJetEventData.h
@@ -11,6 +11,7 @@
 
 namespace DiJetAna
 {
+   const Int_t MAXNPARTICLES = 10000;
    //
    // DiJet ana Event Data Tree definition
    //
@@ -19,61 +20,62 @@ namespace DiJetAna
       public:
 	 // --- DiJet Event variables ---
 	 //  --event level--
-	 ULong_t		    run;
-	 ULong_t		    event;
-	 Double_t		    mass;
-	 Double_t		    cmeta;
+	 ULong_t		    run_;
+	 ULong_t		    event_;
+	 Double_t		    mass_;
+	 Double_t		    cmeta_;
 
 	 //  -- parton info --
-	 Double_t		    nlpid;
-	 Double_t		    nlpstat;
-	 Double_t		    nlpet;
-	 Double_t		    nlpetsm;
-	 Double_t		    nlpeta;
-	 Double_t		    nlpphi;
+	 Int_t	  		    nlpid_;
+	 Int_t	  		    nlpstat_;
+	 Double_t		    nlpet_;
+	 Double_t		    nlpetsm_;
+	 Double_t		    nlpeta_;
+	 Double_t		    nlpphi_;
 
-	 Double_t		    alpid;
-	 Double_t		    alpstat;
-	 Double_t		    alpet;
-	 Double_t		    alpetsm;
-	 Double_t		    alpeta;
-	 Double_t		    alpphi;
+	 Int_t	  		    alpid_;
+	 Int_t	  		    alpstat_;
+	 Double_t		    alpet_;
+	 Double_t		    alpetsm_;
+	 Double_t		    alpeta_;
+	 Double_t		    alpphi_;
 
-	 Double_t		    pdphi;
+	 Double_t		    pdphi_;
 
 	 //  -- jet info --
-	 Double_t		    nljet;
-	 Double_t		    nljetsm;
-	 Double_t		    nljeta;
-	 Double_t		    nljphi;
-	 ULong_t		    nljnp;
+	 Double_t		    nljet_;
+	 Double_t		    nljetsm_;
+	 Double_t		    nljeta_;
+	 Double_t		    nljphi_;
+	 ULong_t		    nljnp_;
 
-	 Double_t		    aljet;
-	 Double_t		    aljetsm;
-	 Double_t		    aljeta;
-	 Double_t		    aljphi;
-	 ULong_t		    aljnp;
+	 Double_t		    aljet_;
+	 Double_t		    aljetsm_;
+	 Double_t		    aljeta_;
+	 Double_t		    aljphi_;
+	 ULong_t		    aljnp_;
 
-	 Double_t		    jdphi;
+	 Double_t		    jdphi_;
 
 	 // -- particle info --
-	 std::vector<Double_t>     	    ppid;
-	 std::vector<Double_t>     	    ppt;
-	 std::vector<Double_t>     	    peta;
-	 std::vector<Double_t>     	    pphi;
+	 UInt_t			    evtnp_;
+	 Int_t		  	    ppid_[MAXNPARTICLES];
+	 Double_t	      	    ppt_[MAXNPARTICLES];
+	 Double_t		    peta_[MAXNPARTICLES];
+	 Double_t		    pphi_[MAXNPARTICLES];
 
-	 std::vector<Double_t>     	    pndphi;
-	 std::vector<Double_t>     	    pndeta;
-	 std::vector<Double_t>     	    pndr;
-	 std::vector<Double_t>     	    pndrbg;
+	 Double_t	  	    pndphi_[MAXNPARTICLES];
+	 Double_t	    	    pndeta_[MAXNPARTICLES];
+	 Double_t	   	    pndr_[MAXNPARTICLES];
+	 Double_t		    pndrbg_[MAXNPARTICLES];
 
-	 std::vector<Double_t>     	    padphi;
-	 std::vector<Double_t>     	    padeta;
-	 std::vector<Double_t>     	    padr;
-	 std::vector<Double_t>     	    padrbg;
+	 Double_t        	    padphi_[MAXNPARTICLES];
+	 Double_t       	    padeta_[MAXNPARTICLES];
+	 Double_t       	    padr_[MAXNPARTICLES];
+	 Double_t       	    padrbg_[MAXNPARTICLES];
 
-	 std::vector<Double_t>     	    zn;
-	 std::vector<Double_t>     	    za;
+	 Double_t       	    zn_[MAXNPARTICLES];
+	 Double_t       	    za_[MAXNPARTICLES];
 	 
 
 	 // --- tree branch names ---
@@ -90,11 +92,16 @@ namespace DiJetAna
 	 TString tNJPDR_;
 	 TString tAJPDR_;
 
-	 TreeDiJetEventData();
+	 // === Functions ===
+	 TreeDiJetEventData(TTree * tree);
+	 void SetBranches();
+
+      private:
+	 TTree*                             tree_;
    };
 
    // constructor
-   TreeDiJetEventData::TreeDiJetEventData() :
+   TreeDiJetEventData::TreeDiJetEventData(TTree * tree) :
       //
       // We will call the default constructor of the event variables   
       //
@@ -111,6 +118,69 @@ namespace DiJetAna
       tNJPDR_("pndr"),
       tAJPDR_("padr")
    {
+      tree_ = tree;
+   }
+
+   // set brances
+   void TreeDiJetEventData::SetBranches()
+   {
+      // --event level--
+      tree_->Branch("run", &(this->run_), "run/l");
+      tree_->Branch("event", &(this->event_), "event/l");
+      tree_->Branch("mass", &(this->mass_), "mass/D");
+      tree_->Branch("cmeta", &(this->cmeta_), "cmeta/D");
+
+      // --parton info--
+      tree_->Branch("nlpid", &(this->nlpid_), "nlpid/I");
+      tree_->Branch("nlpstat", &(this->nlpstat_), "nlpstat/I");
+      tree_->Branch("nlpet", &(this->nlpet_), "nlpet/D");
+      tree_->Branch("nlpetsm", &(this->nlpetsm_), "nlpetsm/D");
+      tree_->Branch("nlpeta", &(this->nlpeta_), "nlpeta/D");
+      tree_->Branch("nlpphi", &(this->nlpphi_), "nlpphi/D");
+
+      tree_->Branch("alpid", &(this->alpid_), "alpid/I");
+      tree_->Branch("alpstat", &(this->alpstat_), "alpstat/I");
+      tree_->Branch("alpet", &(this->alpet_), "alpet/D");
+      tree_->Branch("alpetsm", &(this->alpetsm_), "alpetsm/D");
+      tree_->Branch("alpeta", &(this->alpeta_), "alpeta/D");
+      tree_->Branch("alpphi", &(this->alpphi_), "alpphi/D");
+
+      tree_->Branch("pdphi", &(this->pdphi_), "pdphi/D");
+
+      // -- jet info --
+      tree_->Branch("nljet", &(this->nljet_), "nljet/D");
+      tree_->Branch("nljetsm", &(this->nljetsm_), "nljetsm/D");
+      tree_->Branch("nljeta", &(this->nljeta_), "nljeta/D");
+      tree_->Branch("nljphi", &(this->nljphi_), "nljphi/D");
+      tree_->Branch("nljnp", &(this->nljnp_), "nljnp/l");
+
+      tree_->Branch("aljet", &(this->aljet_), "aljet/D");
+      tree_->Branch("aljetsm", &(this->aljetsm_), "aljetsm/D");
+      tree_->Branch("aljeta", &(this->aljeta_), "aljeta/D");
+      tree_->Branch("aljphi", &(this->aljphi_), "aljphi/D");
+      tree_->Branch("aljnp", &(this->aljnp_), "aljnp/l");
+
+      tree_->Branch("jdphi", &(this->jdphi_), "jdphi/D");
+
+      // -- particle info --
+      tree_->Branch("evtnp",&(this->evtnp_),"evtnp/i");
+      tree_->Branch("ppid",this->ppid_,"ppid[evtnp]/I");
+      tree_->Branch("ppt",this->ppt_,"ppt[evtnp]/D");
+      tree_->Branch("peta",this->peta_,"peta[evtnp]/D");
+      tree_->Branch("pphi",this->pphi_,"pphi[evtnp]/D");
+
+      tree_->Branch("pndphi",this->pndphi_,"pndphi[evtnp]/D");
+      tree_->Branch("pndeta",this->pndeta_,"pndeta[evtnp]/D");
+      tree_->Branch("pndr",this->pndr_,"pndr[evtnp]/D");
+      tree_->Branch("pndrbg",this->pndrbg_,"pndrbg[evtnp]/D");
+
+      tree_->Branch("padphi",this->padphi_,"padphi[evtnp]/D");
+      tree_->Branch("padeta",this->padeta_,"padeta[evtnp]/D");
+      tree_->Branch("padr",this->padr_,"padr[evtnp]/D");
+      tree_->Branch("padrbg",this->padrbg_,"padrbg[evtnp]/D");
+
+      tree_->Branch("zn",this->zn_,"zn[evtnp]/D");
+      tree_->Branch("za",this->za_,"za[evtnp]/D");
    }
 }
 
--- HI/analysis/dijet/sw/hiroot/macros/anareco/DiJetAna/treedijeteventdataDriver.C
+++ HI/analysis/dijet/sw/hiroot/macros/anareco/DiJetAna/treedijeteventdataDriver.C
@@ -1,9 +1,86 @@
 #include "TreeDiJetEventData.h"
+#include "TFile.h"
+#include "TTree.h"
+#include <vector>
 using namespace DiJetAna;
 
 void treedijeteventdataDriver()
 {
-   TreeDiJetEventData td;
-   printf("%f\n",td.nljet);
-   printf("%d\n",td.ppid.size());
+   TFile f("eventdata.root","RECREATE","dijet event data");
+
+   TTree * evtt = new TTree("evtTree","dijet event data tree");
+
+   TreeDiJetEventData td(evtt);
+   printf("%f\n",td.nljet_);
+
+   // set branch
+   td.SetBranches();
+
+   //  --event level--
+   td.run_=77;
+   td.event_=77;
+   td.mass_=77;
+   td.cmeta_=77;
+
+   //  -- parton info --
+   td.nlpid_=77;
+   td.nlpstat_=77;
+   td.nlpet_=77;
+   td.nlpetsm_=77;
+   td.nlpeta_=77;
+   td.nlpphi_=77;
+
+   td.alpid_=77;
+   td.alpstat_=77;
+   td.alpet_=77;
+   td.alpetsm_=77;
+   td.alpeta_=77;
+   td.alpphi_=77;
+
+   td.pdphi_=77;
+
+   //  -- jet info --
+   td.nljet_=77;
+   td.nljetsm_=77;
+   td.nljeta_=77;
+   td.nljphi_=77;
+   td.nljnp_=77;
+
+   td.aljet_=77;
+   td.aljetsm_=77;
+   td.aljeta_=77;
+   td.aljphi_=77;
+   td.aljnp_=77;
+
+   td.jdphi_=77;
+
+   // -- particle info --
+   int evtnp = 0;
+   for(int i=0; i<10; ++i) {
+      ++evtnp;
+      td.ppid_[i] = 3;
+      td.ppt_[i] = 3;
+      td.peta_[i] = 3;
+      td.pphi_[i] = 3;
+
+      td.pndphi_[i] = 3;
+      td.pndeta_[i] = 3;
+      td.pndr_[i] = 3;
+      td.pndrbg_[i] = 3;
+
+      td.padphi_[i] = 3;
+      td.padeta_[i] = 3;
+      td.padr_[i] = 3;
+      td.padrbg_[i] = 3;
+
+      td.zn_[i] = 4;
+      td.za_[i] = 4;
+   }
+   td.evtnp_ = evtnp;
+
+
+   evtt->Fill();
+
+   // save
+   f.Write();
 }
