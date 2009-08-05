dae083c5b6358c6a4101a5247500c97185d412cf
frankma <>
2009-08-05 13:09:01 +0000

    First checked version of Ana using TreeDiJetEventData

--- HI/analysis/dijet/sw/hiroot/macros/anareco/DiJetAna/plotJetLeading.C
+++ HI/analysis/dijet/sw/hiroot/macros/anareco/DiJetAna/plotJetLeading.C
@@ -7,19 +7,8 @@
 #include "TROOT.h"
 #include "TString.h"
 #include "ana/savedfrankTools.C"
-
-const Int_t ETAMIN=-4;
-const Int_t ETAMAX=4;
-const UInt_t NBIN=100;
-
-const char * drsgE="E1";
-const char * drdbE="E1 same";
-const char * drsg="hist";
-//const char * drdb="E1 hist same";
-const char * drdb="hist same";
-TString drsg2("colz");
-TString drsg2nc("BOX");
-TString drdb2nc("same");
+#include "DiJets.h"
+using namespace DiJetAna;
 
 void plotJetLeading(bool doCheck=true,
 	         char * infname1 = "/net/pstore01/d00/scratch/frankma/hiroot/pythia100_5k_dijet_000777.root",
@@ -29,65 +18,62 @@ void plotJetLeading(bool doCheck=true,
    printf("%s\n",infname1);
    printf("%s\n",infname2);
    TFile * infile = new TFile(infname1);
-//   TNtuple * ntPythia = findTree(infile,"NTLPartons");
-//   TNtuple * ntPythia = findTree(infile,"NTPartonLeading");
-   TTree * ntPythia = findTree(infile,"evtTreeJetFF");
+//   TNtuple * trJetPythia = findTree(infile,"NTPartonLeading");
+   TTree * trJetPythia = findTree(infile,"evtTreeJetFF");
    TFile * infile2 = new TFile(infname2);
-//   TNtuple * ntPyquen = findTree(infile2,"NTPartonLeading");
-//   TNtuple * ntPyquen = findTree(infile2,"NTJetLeading");
-   TTree * ntPyquen = findTree(infile2,"evtTreeJetFF");
+//   TNtuple * trJetPyquen = findTree(infile2,"NTPartonLeading");
+   TTree * trJetPyquen = findTree(infile2,"evtTreeJetFF");
    TFile * outfile = new TFile("LeadingHistos.root","RECREATE");
 
    //--- If do checks ---
    if (doCheck) {
    //--- inv mass ---
-//   drawTree(ntPythia, "mass>>hMassPythia","nlpet>1 && alpet>1",drsg,    "hMassPythia","Pythia: inv mass of dijet",100,0,1800,true,kRed,1,3);
-//   drawTree(ntPyquen, "mass>>hMassPyquen","nlpet>1 && alpet>1",drdb,"hMassPyquen","draw Pyquen: inv mass of dijet",100,0,1800,true,kBlue,1,3);
-      drawTree2(ntPythia, "nljet:nlpet>>hNearJEtPEtPythia","",drsg2,"hNearJEtPEtPythia",";Parton Et [GeV];Jet Et [GeV]",NBIN,0,200,NBIN,0,200,2);
-      drawTree2(ntPythia, "nljphi:nlpphi>>hNearJPhiPPhiPythia","",drsg2,"hNearJPhiPPhiPythia",";Parton Phi [GeV];Jet Phi [GeV]",NBIN,-1*PI,PI,NBIN,-1*PI,PI,2);
-      drawTree2(ntPythia, "nljeta:nlpeta>>hNearJEtaPEtaPythia","",drsg2,"hNearJEtaPEtaPythia",";Parton Eta [GeV];Jet Eta [GeV]",NBIN,-1*ETAMAX,ETAMAX,NBIN,-1*ETAMAX,ETAMAX,2);
+//   drawTree(trJetPythia, "mass>>hMassPythia","nlpet>1 && alpet>1",drsg,    "hMassPythia","Pythia: inv mass of dijet",100,0,1800,true,kRed,1,3);
+//   drawTree(trJetPyquen, "mass>>hMassPyquen","nlpet>1 && alpet>1",drdb,"hMassPyquen","draw Pyquen: inv mass of dijet",100,0,1800,true,kBlue,1,3);
+      drawTree2(trJetPythia, "nljet:nlpet>>hNearJEtPEtPythia","",drsg2,"hNearJEtPEtPythia",";Parton Et [GeV];Jet Et [GeV]",NBIN,0,200,NBIN,0,200,2);
+      drawTree2(trJetPythia, "nljphi:nlpphi>>hNearJPhiPPhiPythia","",drsg2,"hNearJPhiPPhiPythia",";Parton Phi [GeV];Jet Phi [GeV]",NBIN,-1*PI,PI,NBIN,-1*PI,PI,2);
+      drawTree2(trJetPythia, "nljeta:nlpeta>>hNearJEtaPEtaPythia","",drsg2,"hNearJEtaPEtaPythia",";Parton Eta [GeV];Jet Eta [GeV]",NBIN,-1*ETAMAX,ETAMAX,NBIN,-1*ETAMAX,ETAMAX,2);
       drawNormHist(infile, "hJetEtDist",drsg,"","Jet Et [GeV]","#",1,true,2,7,3,1,1);
-      drawTree(ntPythia, "nljet>>hCheckNearLJetPythia","",drdb,    "hCheckNearLJetPythia",";Et [GeV];",100,0,200,true,kRed,1,3,1,8);
-      drawTree(ntPyquen, "nljet>>hCheckNearLJetPyquen","",drdb,"hCheckNearLJetPyquen",";Et [GeV];",100,0,200,true,kRed-2,1,3,1,8);
-      drawTree(ntPythia, "nljet>>hCheckCutNearLJetPythia","nljet>70",drdbE,    "hCheckCutNearLJetPythia",";Et [GeV];",100,0,200,true,kBlack,1,3,1,8);
-      drawTree(ntPyquen, "nljet>>hCheckCutNearLJetPyquen","nljet>70",drdbE,"hCheckCutNearLJetPyquen",";Et [GeV];",100,0,200,true,kBlue,1,3,1,8);
+      drawTree(trJetPythia, "nljet>>hCheckNearLJetPythia","",drdb,    "hCheckNearLJetPythia",";Et [GeV];",100,0,200,true,kRed,1,3,1,8);
+      drawTree(trJetPyquen, "nljet>>hCheckNearLJetPyquen","",drdb,"hCheckNearLJetPyquen",";Et [GeV];",100,0,200,true,kRed-2,1,3,1,8);
+      drawTree(trJetPythia, "nljet>>hCheckCutNearLJetPythia","nljet>70",drdbE,    "hCheckCutNearLJetPythia",";Et [GeV];",100,0,200,true,kBlack,1,3,1,8);
+      drawTree(trJetPyquen, "nljet>>hCheckCutNearLJetPyquen","nljet>70",drdbE,"hCheckCutNearLJetPyquen",";Et [GeV];",100,0,200,true,kBlue,1,3,1,8);
    }
    
    //--- Et ---
    // -near-
-   drawTree(ntPythia, "nlpet>>hNearLPartonPythia","",drsg,    "hNearLPartonPythia",";(near jet) Et [GeV];",100,0,200,true,kRed,1,3);
-   drawTree(ntPythia, "nljet>>hNearLJetPythia","",drdbE,    "hNearLJetPythia",";Et [GeV];",100,0,200,true,kBlack,1,3,1,8);
-   drawTree(ntPyquen, "nlpet>>hNearLPartonPyquen","",drdb,"hNearLPartonPyquen",";Et [GeV];",100,0,200,true,kRed-2,1,3);
-   drawTree(ntPyquen, "nljet>>hNearLJetPyquen","",drdbE,"hNearLJetPyquen",";Et [GeV];",100,0,200,true,kBlue,1,3,1,8);
-//   // -away--
-   drawTree(ntPythia, "alpet>>hAwayLPartonPythia","",drsg,"hAwayLPartonPythia",";(away jet) Et [GeV];",100,0,200,true,kRed,1,3);
-   drawTree(ntPythia, "aljet>>hAwayLJetPythia","",drdbE,"hAwayLJetPythia",";Et [GeV];",100,0,200,true,kBlack,1,3,1,8);
-   drawTree(ntPyquen, "alpet>>hAwayLPartonPyquen","",drdb,"hAwayLPartonPyquen",";Et [GeV];",100,0,200,true,kRed-2,1,3);
-   drawTree(ntPyquen, "aljet>>hAwayLJetPyquen","",drdbE,"hAwayLJetPyquen",";Et [GeV];",100,0,200,true,kBlue,1,3,1,8);
-//
-//   //--- dphi ---
-//   drawTree(ntPythia, "dphi>>hdPhiPythia","nlpet>1 && alpet>1",drsg,"hdPhiPythia","Pythia: dphi of leading partons",100,0,3.14,true,kRed,1,3);
-//   drawTree(ntPyquen, "dphi>>hdPhiPyquen","nlpet>1 && alpet>1",drdb,"hdPhiPyquen","draw Pyquen: dphi of leading partons",100,0,3.14,true,kBlue,1,3);
-//
-//   //--- near/away Et ratio ---
-   drawTree(ntPythia, "(alpet/nlpet)>>hPEtRatioPythia","",drsg,"hPEtRatioPythia",";(away Et)/(near Et);",100,0,1.5,true,kRed,1,3);
-   drawTree(ntPyquen, "(alpet/nlpet)>>hPEtRatioPyquen","",drdb,"hPEtRatioPyquen",";(away Et)/(near Et);",100,0,1.5,true,kRed-2,1,3);
+   drawTree(trJetPythia, "nlpet>>hNearLPartonPythia","",drsg,    "hNearLPartonPythia",";(near jet) Et [GeV];",100,0,200,true,kRed,1,3);
+   drawTree(trJetPythia, "nljet>>hNearLJetPythia","",drdbE,    "hNearLJetPythia",";Et [GeV];",100,0,200,true,kBlack,1,3,1,8);
+   drawTree(trJetPyquen, "nlpet>>hNearLPartonPyquen","",drdb,"hNearLPartonPyquen",";Et [GeV];",100,0,200,true,kRed-2,1,3);
+   drawTree(trJetPyquen, "nljet>>hNearLJetPyquen","",drdbE,"hNearLJetPyquen",";Et [GeV];",100,0,200,true,kBlue,1,3,1,8);
+   // -away--
+   drawTree(trJetPythia, "alpet>>hAwayLPartonPythia","",drsg,"hAwayLPartonPythia",";(away jet) Et [GeV];",100,0,200,true,kRed,1,3);
+   drawTree(trJetPythia, "aljet>>hAwayLJetPythia","",drdbE,"hAwayLJetPythia",";Et [GeV];",100,0,200,true,kBlack,1,3,1,8);
+   drawTree(trJetPyquen, "alpet>>hAwayLPartonPyquen","",drdb,"hAwayLPartonPyquen",";Et [GeV];",100,0,200,true,kRed-2,1,3);
+   drawTree(trJetPyquen, "aljet>>hAwayLJetPyquen","",drdbE,"hAwayLJetPyquen",";Et [GeV];",100,0,200,true,kBlue,1,3,1,8);
+
+   //--- dphi ---
+//   drawTree(trJetPythia, "dphi>>hdPhiPythia","nlpet>1 && alpet>1",drsg,"hdPhiPythia","Pythia: dphi of leading partons",100,0,3.14,true,kRed,1,3);
+//   drawTree(trJetPyquen, "dphi>>hdPhiPyquen","nlpet>1 && alpet>1",drdb,"hdPhiPyquen","draw Pyquen: dphi of leading partons",100,0,3.14,true,kBlue,1,3);
+
+   //--- near/away Et ratio ---
+   drawTree(trJetPythia, "(alpet/nlpet)>>hPEtRatioPythia","",drsg,"hPEtRatioPythia",";(away Et)/(near Et);",100,0,1.5,true,kRed,1,3);
+   drawTree(trJetPyquen, "(alpet/nlpet)>>hPEtRatioPyquen","",drdb,"hPEtRatioPyquen",";(away Et)/(near Et);",100,0,1.5,true,kRed-2,1,3);
    //--- Away/Near Jet Leading ---
-   drawTree(ntPythia, "(aljet/nljet)>>hJEtRatioPythia","",drdb,"hJEtRatioPythia",";(away Et)/(near Et);",100,0,1.5,true,kBlack,1,3);
-   drawTree(ntPyquen, "(aljet/nljet)>>hJEtRatioPyquen","",drdb,"hJEtRatioPyquen",";(away Et)/(near Et);",100,0,1.5,true,kBlue,1,3);
+   drawTree(trJetPythia, "(aljet/nljet)>>hJEtRatioPythia","",drdb,"hJEtRatioPythia",";(away Et)/(near Et);",100,0,1.5,true,kBlack,1,3);
+   drawTree(trJetPyquen, "(aljet/nljet)>>hJEtRatioPyquen","",drdb,"hJEtRatioPyquen",";(away Et)/(near Et);",100,0,1.5,true,kBlue,1,3);
 
    //=== jet/parton Et ratio ===
    // -near-
-   drawTree(ntPythia, "(nljet/nlpet)>>hNJEtPEtRatioPythia","",drsgE,"hNJEtPEtRatioPythia",";E_{t}^{Jet}/E_{t}^{Parton};",100,0,1.5,true,kBlack,1,3,1,8);
-   drawTree(ntPyquen, "(nljet/nlpet)>>hNJEtPEtRatioPyquen","",drdbE,"hNJEtPEtRatioPyquen",";E_{t}^{Jet}/E_{t}^{Parton};",100,0,1.5,true,kBlue,1,3,1,8);
+   drawTree(trJetPythia, "(nljet/nlpet)>>hNJEtPEtRatioPythia","",drsgE,"hNJEtPEtRatioPythia",";E_{t}^{Jet}/E_{t}^{Parton};",100,0,1.5,true,kBlack,1,3,1,8);
+   drawTree(trJetPyquen, "(nljet/nlpet)>>hNJEtPEtRatioPyquen","",drdbE,"hNJEtPEtRatioPyquen",";E_{t}^{Jet}/E_{t}^{Parton};",100,0,1.5,true,kBlue,1,3,1,8);
    // -away-
-   drawTree(ntPythia, "(aljet/alpet)>>hAJEtPEtRatioPythia","",drdbE,"hAJEtPEtRatioPythia",";(away) E_{t}^{Jet}/E_{t}^{Parton};",100,0,1.5,true,kBlack,1,3,1,4);
-   drawTree(ntPyquen, "(aljet/alpet)>>hAJEtPEtRatioPyquen","",drdbE,"hAJEtPEtRatioPyquen",";E_{t}^{Jet}/E_{t}^{Parton};",100,0,1.5,true,kBlue,1,3,1,4);
-  //--- as a function of parton Et---
+   drawTree(trJetPythia, "(aljet/alpet)>>hAJEtPEtRatioPythia","",drdbE,"hAJEtPEtRatioPythia",";(away) E_{t}^{Jet}/E_{t}^{Parton};",100,0,1.5,true,kBlack,1,3,1,4);
+   drawTree(trJetPyquen, "(aljet/alpet)>>hAJEtPEtRatioPyquen","",drdbE,"hAJEtPEtRatioPyquen",";E_{t}^{Jet}/E_{t}^{Parton};",100,0,1.5,true,kBlue,1,3,1,4);
+
+   //--- as a function of parton Et---
    // -near-
-   drawTree2(ntPythia, "(nljet/nlpet):nlpet>>hNearJEtPEtRatPythia2","",drsg2,"hNearJEtPEtRatPythia2",";Parton Et [GeV];E_{t}^{Jet}/E_{t}^{Parton}",NBIN,80,200,NBIN,0,1.5,2);
-   //=== Now plot the FF ===
-   //plotFF(infname1,infname2);
+   drawTree2(trJetPythia, "(nljet/nlpet):nlpet>>hNearJEtPEtRatPythia2","",drsg2,"hNearJEtPEtRatPythia2",";Parton Et [GeV];E_{t}^{Jet}/E_{t}^{Parton}",NBIN,80,200,NBIN,0,1.5,2);
 
    //-------------- Done Save Plots----------------------
    printAllCanvases(plotdir);
--- HI/analysis/dijet/sw/hiroot/macros/anareco/DiJetAna/runGenericJetAnaPt.sh
+++ HI/analysis/dijet/sw/hiroot/macros/anareco/DiJetAna/runGenericJetAnaPt.sh
@@ -4,6 +4,8 @@
 if [ $# -eq 0 ]; then
    echo "Usage: "
    echo "  $0 <infile_name> <pyq_nljetmin> <pyq_nljetmax> <pyq_aljetmin> <pyq_nljetmin> <pyq_nljetmax> <pyq_aljetmin> <jdphimin> <tag_name>"
+   echo "Eg:"
+   echo "  ./runGenericJetAnaPt.sh dijet_000001.root 90 110 50 90 110 50 2.8 121"
    exit 1;
 fi
 
@@ -13,7 +15,7 @@ echo "$@"
 export job=prodPtHat50
 export gen0=pythia50
 export gen1=pyquen50
-export rootskim=iCone5_match35_jetEtCut30_Mod_120
+export rootskim=iCone5_match35_jetEtCut30_Mod_121
 export infileName=dijet_000001.root
 export infileName=$1
 export input0="$HIROOT_BASEURL/$job/$gen0/$rootskim"
