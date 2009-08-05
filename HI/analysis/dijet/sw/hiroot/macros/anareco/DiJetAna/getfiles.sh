54eb26aca6cb6345b79dd309734440264bf78f68
frankma <>
2009-08-05 16:31:44 +0000

    added runJFPythia.C

--- HI/analysis/dijet/sw/hiroot/macros/anareco/DiJetAna/getfiles.sh
+++ HI/analysis/dijet/sw/hiroot/macros/anareco/DiJetAna/getfiles.sh
@@ -7,6 +7,10 @@ cp -v /net/hisrv0001/home/frankma/work/hiroot/test/dijet/jetana/DiJetAna/TreeDat
 cp -v /net/hisrv0001/home/frankma/work/hiroot/test/dijet/jetana/DiJetAna/TreeDiJetEventData.h .
 cp -v /net/hisrv0001/home/frankma/work/hiroot/test/dijet/jetana/DiJetAna/treedijeteventdataDriver.C .
 
+# run hiroot anamod
+cp -v /net/hisrv0001/home/frankma/work/hiroot/test/dijet/job/DiJetAna/runJFPythia.C .
+cp -v /net/hisrv0001/home/frankma/work/hiroot/test/dijet/job/DiJetAna/runICone5_JetEt30_Mod.sh runMod_ICone5_JetEt30.sh
+
 # macros
 cp -v /net/hisrv0001/home/frankma/work/hiroot/test/dijet/jetana/DiJetAna/rootlogon.C .
 cp -v /net/hisrv0001/home/frankma/work/hiroot/test/dijet/jetana/DiJetAna/plotFF.C .
@@ -18,6 +22,5 @@ cp -v /net/hisrv0001/home/frankma/work/hiroot/test/dijet/jetana/DiJetAna/plotFin
 cp -v /net/hisrv0001/home/frankma/work/hiroot/test/dijet/jetana/DiJetAna/runGenericJetAnaPt.sh .
 cp -v /net/hisrv0001/home/frankma/work/hiroot/test/dijet/jetana/DiJetAna/runJetAnaBase.sh .
 # job
-cp -v /net/hisrv0001/home/frankma/work/hiroot/test/dijet/job/DiJetAna/runICone5_JetEt30_Mod.sh runMod_ICone5_JetEt30.sh
 #cp -v /net/hisrv0001/home/frankma/work/hiroot/test/dijet/job/DiJetAna/runJob_ICone5_JetEt30.sh .
 
