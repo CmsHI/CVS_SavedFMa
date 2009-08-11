36a4f420d05017eaf0b9657d3fb8f5d99d6d95ea
frankma <>
2009-08-11 16:56:52 +0000

    updated string info

--- bin/online_globaltag.sh
+++ bin/online_globaltag.sh
@@ -4,6 +4,9 @@
 defaultRecord="CMS_COND_31X_GLOBALTAG"
 
 connection='frontier://(proxyurl=http://localhost:3128)(serverurl=http://frontier1.cms:8000/FrontierOnProd)(serverurl=http://frontier2.cms:8000/FrontierOnProd)(retrieve-ziplevel=0)'
+# the record of the online connection strings changed to:
+#connection='frontier://(proxyurl=http://localhost:3128)(serverurl=http://localhost:8000/FrontierOnProd)(serverurl=http://localhost:8000/FrontierOnProd)(retrieve-ziplevel=0)'
+# But the global tag seems to still use the old string ...
 
 echo "$connection/$defaultRecord" -T $1 
 if [ $# -eq 0 ]; then
