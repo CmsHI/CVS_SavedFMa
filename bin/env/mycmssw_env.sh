77aefeb7a1481f23e019101fdff133a9f4ef6498
frankma <>
2009-08-11 15:25:06 +0000

    added check on cluster, we don't need advanced dbs tools at .cms network

--- bin/env/mycmssw_env.sh
+++ bin/env/mycmssw_env.sh
@@ -1,6 +1,10 @@
 #alias mycmsenv='eval `scramv1 runtime -sh`'
 export PYTHONPATH=$PYTHONPATH:$SavedFMa/cmssw
-cd $data/DBS/Clients/Python
-. setup.sh
-cd -
+
+nslookup `hostname` | grep Name | grep '\.cms$'
+if [ $? -ne 0 ]; then
+   cd $data/DBS/Clients/Python
+   . setup.sh
+   cd -
+fi
 
