95beb5f48b786dbcf1c251e110c99490d8a74524
frankma <>
2009-08-12 16:21:17 +0000

    delete the mu path from OM selection

--- cmssw/MyConfiguration/Sequences/python/custom_p5_online.py
+++ cmssw/MyConfiguration/Sequences/python/custom_p5_online.py
@@ -3,8 +3,15 @@ def customise_p5_online(process):
    process.MessageLogger.destinations = ['warnings','errors','infos','debugs','cout','cerr','log4cplus']
    process.HLTHIRecopixelvertexingSequence = cms.Sequence( process.hltHIPixelTracks )
    del process.HLT_HIDoubleMu
+   newsel=[]
+   for i in process.hltDefaultOutputWithFEDs.SelectEvents.SelectEvents:
+      if (i!="HLT_HIDoubleMu"):
+	 newsel.append(i)
+   process.hltDefaultOutputWithFEDs.SelectEvents.SelectEvents=newsel
    return(process)
 
 # redefine process
 process=customise_p5_online(process)
 
+# check
+print process.hltDefaultOutputWithFEDs.SelectEvents.SelectEvents
