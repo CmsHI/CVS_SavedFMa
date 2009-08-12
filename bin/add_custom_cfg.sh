673791ce82a393832c6b24763447b5b2c050d4c9
frankma <>
2009-08-12 15:30:44 +0000

    allow adding multiple py's more managable

--- bin/add_custom_cfg.sh
+++ bin/add_custom_cfg.sh
@@ -5,23 +5,38 @@ if [ $# -eq 0 ]; then
    exit 1
 fi
 
+# set var's
 mainCfg=$1
 cusCfg=$2
+
+# save copy
+cp $mainCfg tmp.py
 
 mainCfgName=${mainCfg##*/}
 mainCfgName=${mainCfgName%.py}
 cusCfgName=${cusCfg##*/}
 cusCfgName=${cusCfgName%.py}
-out=${mainCfgName}_${cusCfgName}.py
+#out=${mainCfgName}_${cusCfgName}.py
+pref="custom"
+echo $mainCfgName | grep $pref
+if [ $? -ne 0 ]; then
+   out=${pref}_${mainCfgName}.py
+   cmd="cat $mainCfg $cusCfg >> $out"
+else
+   out=$mainCfg
+   cmd="cat tmp.py $cusCfg >> $out"
+fi
 
-cmd="cat $mainCfg $cusCfg >> $out"
 echo $cmd
 
 # run
 echo "#cmdline: $cmd" > $out
 eval $cmd
 
+# clean up
+rm -f tmp.py
+
 # add output of cfg summary
-if [ $SavedFMa/cmssw/MyConfiguration/Sequences/python/output_cfg_summary.py ]; then
-   cat $SavedFMa/cmssw/MyConfiguration/Sequences/python/output_cfg_summary.py >> $out
-fi
+#if [ $SavedFMa/cmssw/MyConfiguration/Sequences/python/output_cfg_summary.py ]; then
+#   cat $SavedFMa/cmssw/MyConfiguration/Sequences/python/output_cfg_summary.py >> $out
+#fi
