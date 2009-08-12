680a787f9b9e44f91af2e72061cb840c87c5e52f
frankma <>
2009-08-12 09:12:43 +0000

    added dbs tools path for python

--- bin/env/mycmssw_env.sh
+++ bin/env/mycmssw_env.sh
@@ -1,5 +1,6 @@
 #alias mycmsenv='eval `scramv1 runtime -sh`'
 export PYTHONPATH=$PYTHONPATH:$SavedFMa/cmssw
+export PYTHONPATH=$PYTHONPATH:$SavedFMa/data
 
 nslookup `hostname` | grep Name | grep '\.cms$'
 if [ $? -ne 0 ]; then
