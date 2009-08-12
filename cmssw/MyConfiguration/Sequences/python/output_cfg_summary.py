a7a5dded7df46fb9ab1a3910020f2423d7f611c7
frankma <>
2009-08-12 14:03:22 +0000

    better formatting for pahts/endpaths

--- cmssw/MyConfiguration/Sequences/python/output_cfg_summary.py
+++ cmssw/MyConfiguration/Sequences/python/output_cfg_summary.py
@@ -1,13 +1,16 @@
 def output_cfg_summary(process):
    print "======================= Configuration Summary ===================="
-   print "  === source ==="
-   print process.source
-   print "  === verbosity ==="
+   print "=== source ==="
+   print "  ", process.source
+   print "=== verbosity ==="
    print "  categories:", process.MessageLogger.categories
    print "  destinations:", process.MessageLogger.destinations
-   print "  === paths: ==="
-   print process.paths
-   print "  === endpaths: ==="
-   print process.endpaths
+   # --Using the itervalues() in dict to step through the dict container--
+   print "===All paths found in cfg:==="
+   for iterable in process.paths.itervalues():
+      print "  ", iterable.label(),": ", iterable
+   print "\n===All endpaths found in  cfg:==="
+   for iterable in process.endpaths.itervalues():
+      print "  ", iterable.label(),": ", iterable
 
 output_cfg_summary(process)
