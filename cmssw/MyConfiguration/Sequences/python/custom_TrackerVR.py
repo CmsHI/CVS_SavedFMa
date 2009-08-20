89a9391f6d9311f35485b00790e5e6eee432611e
frankma <>
2009-08-20 20:47:56 +0000

    tried to not compress, but apparently did not work

--- cmssw/MyConfiguration/Sequences/python/custom_TrackerVR.py
+++ cmssw/MyConfiguration/Sequences/python/custom_TrackerVR.py
@@ -27,6 +27,19 @@ def custom_TrackerVR(process):
    print "=== With process.newSiStripDigiToRaw: ==="
    print process.newSiStripDigiToRaw.dumpPython()
    process.digi2raw_step._replace(process.SiStripDigiToRaw,process.newSiStripDigiToRaw)
+
+   # turn of compression in OM
+   process.output = cms.OutputModule("PoolOutputModule",
+      splitLevel = cms.untracked.int32(0),
+      outputCommands = process.RAWSIMEventContent.outputCommands,
+      fileName = cms.untracked.string('raw_DIGI_L1_DIGI2RAW.root'),
+      dataset = cms.untracked.PSet(
+	 dataTier = cms.untracked.string('DIGI-RAW'),
+	 filterName = cms.untracked.string('')
+	 ),
+      #use_compression = cms.untracked.bool(False)
+      compression_level = cms.untracked.int32( -1 )
+   )
    return(process)
 
 custom_TrackerVR(process)
