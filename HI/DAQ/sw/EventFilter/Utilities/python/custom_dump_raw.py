3c415d02a317cd48c63e110e5a28d60baf3fea2d
frankma <>
2009-08-31 13:49:10 +0000

    fixed typo

--- HI/DAQ/sw/EventFilter/Utilities/python/custom_dump_raw.py
+++ HI/DAQ/sw/EventFilter/Utilities/python/custom_dump_raw.py
@@ -2,7 +2,7 @@ def local_mod(process):
    # sistrip min: 50
    # ecal min: 600
    # dttf min: 780
-   process.dump.feds = [54,605,780]]
+   process.dump.feds = [54,605,780]
    process.dtRawDump = cms.Path(process.dump)
    return (process)
 
