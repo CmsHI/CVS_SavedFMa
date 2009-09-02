40ede686225d3bc155d6e799f3533ff837092c35
frankma <>
2009-09-02 16:09:19 +0000

    added: totnevt parameter, modification of cfg.py for job

--- cmssw/grid/crab/scripts/create_crab_cfg.sh
+++ cmssw/grid/crab/scripts/create_crab_cfg.sh
@@ -3,22 +3,30 @@
 #   * assumes crab env has been already set
 if [ $# -eq 0 ]; then
    echo "Usage:"
-   echo ". create_crab_cfg.sh <job_cfg.py>"
+   echo ". create_crab_cfg.sh <job_cfg.py> <totnevt>"
    return 1
 fi
 
 dbspath=/Hydjet_MinBias_4TeV/Summer09-MC_31X_V3-GaussianVtx_312_ver1/GEN-SIM-RAW
-jobcfg=$1
-totnevt=5000
+pycfg=$1
+totnevt=$2
 nperjob=500
 outfile=hydjetMinBias325DigiToRawFixEcalTag.root
 outdir=/d01/frankma/scratch/data/simCMSSW_3_2_5_DigiToRawFix_EcalTag/raw
 
 if [ ! -e $outdir ]; then mkdir $outdir; fi
 
+# edit python
+jobpycfg=job_${pycfg##*/}
+cp $pycfg $jobpycfg
+echo '# now customization for crab' >> $jobpycfg
+echo "process.output.fileName = '$outfile'" >> $jobpycfg
+echo "print 'crab job output file:', process.output.fileName" >> $jobpycfg
+
+
 cat $CRABPATH/full_crab.cfg | \
 sed 's;^datasetpath=.*;datasetpath='$dbspath';' | \
-sed 's;^pset=.*;pset='$jobcfg';' | \
+sed 's;^pset=.*;pset='$jobpycfg';' | \
 sed 's;^output_file =.*;output_file = '$outfile';' | \
 sed 's;^#outputdir=.*;outputdir= '$outdir';' | \
 sed 's;^total_number_of_events.*;total_number_of_events='$totnevt';' | \
