59fe8f64c09958cbb6c9e74d9e2a59bc0d82edb8
frankma <>
2009-09-02 15:38:34 +0000

    added adjustable parameters

--- cmssw/grid/crab/scripts/create_crab_cfg.sh
+++ cmssw/grid/crab/scripts/create_crab_cfg.sh
@@ -7,16 +7,21 @@ if [ $# -eq 0 ]; then
    return 1
 fi
 
+dbspath=/Hydjet_MinBias_4TeV/Summer09-MC_31X_V3-GaussianVtx_312_ver1/GEN-SIM-RAW
 jobcfg=$1
 totnevt=5000
 nperjob=500
+outfile=hydjetMinBias325DigiToRawFixEcalTag.root
+outdir=/d01/frankma/scratch/data/simCMSSW_3_2_5_DigiToRawFix_EcalTag/raw
+
+if [ ! -e $outdir ]; then mkdir $outdir; fi
 
 cat $CRABPATH/full_crab.cfg | \
-sed 's;^datasetpath=.*;datasetpath=/Hydjet_MinBias_4TeV/Summer09-MC_31X_V3-GaussianVtx_312_ver1/GEN-SIM-RAW;' | \
+sed 's;^datasetpath=.*;datasetpath='$dbspath';' | \
 sed 's;^pset=.*;pset='$jobcfg';' | \
-sed 's;^output_file =.*;output_file = hydjet325DigiToRawFixEcalTag.root;' | \
-sed 's;^#outputdir=.*;outputdir= /d01/frankma/scratch/data/simCMSSW_3_2_5_DigiToRawFix_EcalTag/raw;' | \
+sed 's;^output_file =.*;output_file = '$outfile';' | \
+sed 's;^#outputdir=.*;outputdir= '$outdir';' | \
 sed 's;^total_number_of_events.*;total_number_of_events='$totnevt';' | \
-sed 's;^#events_per_job.*;events_per_job = '$number_of_jobs';' | \
+sed 's;^#events_per_job.*;events_per_job = '$nperjob';' | \
 sed 's;^number_of_jobs;#number_of_jobs;' \
 > crab.cfg
