011eb08517b07878a59504d6c1d21d07023b7b49
frankma <>
2009-09-07 15:08:09 +0000

    new default to hydjet data

--- cmssw/MyConfiguration/Sequences/python/add_source_dbs_cff.py
+++ cmssw/MyConfiguration/Sequences/python/add_source_dbs_cff.py
@@ -1,7 +1,7 @@
 #process.load("MyConfiguration/Sequences/python/Local_source_cff")
 from dbs.python.DBSFetch import DBSFetch
 def add_source_from_dbs_cff(process):
-   process.source.fileNames = DBSFetch.getFiles('/RelValQCD_Pt_80_120/CMSSW_3_1_2-MC_31X_V3-v1/GEN-SIM-DIGI-RAW-HLTDEBUG')
+   process.source.fileNames = DBSFetch.getFiles('/RelValHydjetQ_MinBias_4TeV/CMSSW_3_2_4-MC_31X_V3-v1/GEN-SIM-RAW')
    return(process)
 
 process = add_source_from_dbs_cff(process)
