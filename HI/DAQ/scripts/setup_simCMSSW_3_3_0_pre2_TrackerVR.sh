fd5d0cf534bd2aa99854d457be77983fcdb20369
frankma <>
2009-09-07 15:09:11 +0000

    setup script for produing tracker VR in 330pre2

---
+++ HI/DAQ/scripts/setup_simCMSSW_3_3_0_pre2_TrackerVR.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -
+scramv1 project -n simCMSSW_3_3_0_pre2_TrackerVR CMSSW CMSSW_3_3_0_pre2
+cd simCMSSW_3_3_0_pre2_TrackerVR/src
+cmsenv
+
+addpkg EventFilter/SiStripRawToDigi
+
+# dataformat
+# this should work for 330pre2 b/c Ivan just used 2009/9/4
+cp -r /d00/icali/CMSSW_3_1_0/src/SimTracker .
+
+# db
+cp /d00/icali/CMSSW_3_1_0/src/dbfile.db .
+
+# run!
+scramv1 b
