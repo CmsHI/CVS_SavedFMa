f156fb53e11ec24f9edbf39cb001676da09e6f2a
frankma <>
2009-09-08 15:49:20 +0000

    custom MessageLogger

---
+++ cmssw/MyConfiguration/Sequences/python/custom_MessageLogger.py
@@ -0,0 +1,33 @@
+#
+# custom message logger
+#
+# - cf
+#   * http://cmssw.cvs.cern.ch/cgi-bin/cmssw.cgi/CMSSW/FWCore/MessageLogger/examples/timingFilteredMLexample_cfg.py?view=log
+#   * http://cmssw.cvs.cern.ch/cgi-bin/cmssw.cgi/CMSSW/HLTrigger/Configuration/python/customL1THLT_Options.py?view=markup
+#   * https://hypernews.cern.ch/HyperNews/CMS/get/swDevelopment/1836/1.html
+#   * https://twiki.cern.ch/twiki/bin/view/CMS/SWGuideMessageLogger
+
+def custom_messagelogger(process):
+   #process.load("FWCore.MessageService.MessageLogger_cfi")
+   process.MessageLogger.categories.append('GenParticleProducer')
+   process.MessageLogger.categories.append('InputGenJetsParticleSelector')
+   process.MessageLogger.categories.append('SubEventGenJetProducer')
+   process.MessageLogger.debugModules = ['*']
+   process.MessageLogger.cout = cms.untracked.PSet(
+      threshold = cms.untracked.string('DEBUG'),
+      default = cms.untracked.PSet(
+	 limit = cms.untracked.int32(0)
+	 ),
+      GenParticleProducer = cms.untracked.PSet(
+	 limit = cms.untracked.int32(-1)
+	 ),
+      InputGenJetsParticleSelector = cms.untracked.PSet(
+	 limit = cms.untracked.int32(-1)
+	 ),
+      SubEventGenJetProducer = cms.untracked.PSet(
+	 limit = cms.untracked.int32(-1)
+	 )
+      )
+   return(process)
+
+custom_messagelogger(process)
