21dcdbf59859b998b1a3b9b78763d93243dbffd5
frankma <>
2009-09-09 16:44:46 +0000

    forgot to move CmsHi

--- HI/jet/scripts/setup/setup_subegenjetCMSSW_3_3_0_pre2.sh
+++ HI/jet/scripts/setup/setup_subegenjetCMSSW_3_3_0_pre2.sh
@@ -8,7 +8,7 @@ cd subegenjetCMSSW_3_3_0_pre2/src
 # cvs
 cvscms
 # Need the HiGenParticleProducer class
-cvs co CmsHi/Utilities
+cvs co UserCode/CmsHi/Utilities
 # Need head version for the HiGenParticleProducer class to compile
 cvs co V04-00-00 SimGeneral/TrackingAnalysis # HEAD as of 2009.9.9
 # - No other code needed
@@ -16,5 +16,7 @@ cvs co V04-00-00 SimGeneral/TrackingAnalysis # HEAD as of 2009.9.9
 #   * using the normal PhysicsTools/HepMCCandAlgos hepmc converter
 #     with the normal DataFormats/HepMCCandidate for now
 #     until it is fixed in the Development_3xy_HeavyIons branch
+mv UserCode/CmsHi .
+rm -r CmsHi
 
 scramv1 b
