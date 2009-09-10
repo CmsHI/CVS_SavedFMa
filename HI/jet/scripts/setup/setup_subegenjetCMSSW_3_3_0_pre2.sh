e693f13a3d9fde9435d7a147efc3f0f931b3fbde
frankma <>
2009-09-10 11:57:01 +0000

    move to 326 for now to be able to use CRAB

--- HI/jet/scripts/setup/setup_subegenjetCMSSW_3_3_0_pre2.sh
+++ HI/jet/scripts/setup/setup_subegenjetCMSSW_3_3_0_pre2.sh
@@ -1,22 +1,36 @@
 #!/bin/bash -
-# to be sourced
+
+#
+# - setup set genjet code with tag 330pre2 using subevent map
+# - to be sourced
+#
 
 # setup project
-scramv1 project -n subegenjetCMSSW_3_3_0_pre2 CMSSW CMSSW_3_3_0_pre2
-cd subegenjetCMSSW_3_3_0_pre2/src
+rel=CMSSW_3_2_6
+pname=semgenjet$rel
+scramv1 project -n $pname CMSSW $rel
+cd $pname/src
 
 # cvs
 cvscms
+# First co the new jet code taged in 330pre2
+cvs co -r CMSSW_3_3_0_pre2 RecoJets/JetProducers
+cvs co -r CMSSW_3_3_0_pre2 RecoJets/JetAlgorithms
+cvs co -r CMSSW_3_3_0_pre2 RecoJets/Configuration
+# HI jet reco sequences
+cvs co -r CMSSW_3_3_0_pre2 RecoHI/HiJetAlgos
+
 # Need the HiGenParticleProducer class
 cvs co UserCode/CmsHi/Utilities
 # Need head version for the HiGenParticleProducer class to compile
-cvs co V04-00-00 SimGeneral/TrackingAnalysis # HEAD as of 2009.9.9
+cvs co -r V04-00-00 SimGeneral/TrackingAnalysis # HEAD as of 2009.9.9
+
 # - No other code needed
 #   * hi jet reco code is the new structure in 330pre2
 #   * using the normal PhysicsTools/HepMCCandAlgos hepmc converter
 #     with the normal DataFormats/HepMCCandidate for now
 #     until it is fixed in the Development_3xy_HeavyIons branch
 mv UserCode/CmsHi .
-rm -r CmsHi
+rm -rf CmsHi
 
 scramv1 b
