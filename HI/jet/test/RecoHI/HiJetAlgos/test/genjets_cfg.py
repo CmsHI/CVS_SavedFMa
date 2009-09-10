734e3d997d2c109eadd3aa29f562c938fdde8063
frankma <>
2009-09-10 17:16:50 +0000

    temporarily revert back to SubEventMap way. Also added an analyzer for quick checks

--- HI/jet/test/RecoHI/HiJetAlgos/test/genjets_cfg.py
+++ HI/jet/test/RecoHI/HiJetAlgos/test/genjets_cfg.py
@@ -2,12 +2,17 @@ import FWCore.ParameterSet.Config as cms
 
 process = cms.Process('GENJETRECO')
 
-# === import of 33X configurations ===
+# === import configurations ===
 process.load('Configuration/StandardSequences/Services_cff')
 process.load('FWCore/MessageService/MessageLogger_cfi')
 process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
+# hi jet cfg
 process.load("RecoHI.HiJetAlgos.IterativeCone5HiGenJets_cff")
 process.load("RecoHI.HiJetAlgos.RecoHiJets_EventContent_cff")
+# subevent map
+process.load("CmsHi.Utilities.HiGenParticles_cfi")
+# quick check analyzer
+process.load("CmsHi.JetAnalysis.HiJetAnalysisModules_cff")
 
 
 # === standard configurations ===
@@ -26,7 +31,7 @@ process.source = cms.Source("PoolSource",
 process.output = cms.OutputModule("PoolOutputModule",
     splitLevel = cms.untracked.int32(0),
     outputCommands = process.RecoHiJetsFEVT.outputCommands,
-    fileName = cms.untracked.string('genjets330pre2.root'),
+    fileName = cms.untracked.string('semgenjets326.root'),
     dataset = cms.untracked.PSet(
         dataTier = cms.untracked.string('RECO'),
         filterName = cms.untracked.string('')
@@ -40,16 +45,16 @@ process.GlobalTag.globaltag = 'MC_31X_V3::All'
 
 
 # === Genjet specific configuration ===
-# First produce a GenParticle collection with subevent record
-hiGenParticles = cms.EDProducer('GenParticleProducer',
-   doSubEvent = cms.untracked.bool(True),
-   #srcVector = cms.vstring("hiSignal","generator")
-   srcVector = cms.vstring("generator") # for now no mixing
+# for now only one HepMC collection, no mixing yet
+process.hiGenParticles.src = ["generator"]
+
+process.genevent.jetSrc = cms.InputTag("iterativeCone5HiGenJets")
+
+process.TFileService = cms.Service('TFileService',
+   fileName = cms.string('treefile.root')
 )
-process.hiGenParticles = hiGenParticles
 
 
 # === Executable ===
-#process.reco_step = cms.Path(process.hiGenParticles*process.hiGenParticlesForJets*process.iterativeCone5HiGenJets)
-process.reco_step = cms.Path(process.hiGenParticles*process.hiGenParticlesForJets)
+process.reco_step = cms.Path(process.hiGenParticles*process.hiGenParticlesForJets*process.iterativeCone5HiGenJets*process.genevent)
 process.out_step = cms.EndPath(process.output)
