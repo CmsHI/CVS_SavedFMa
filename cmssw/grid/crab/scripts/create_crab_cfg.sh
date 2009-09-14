1232266fbb6e4ea146e3b80dd169620e0c67a3b8
frankma <>
2009-09-14 18:01:49 +0000

    made switch for different output storage cases

--- cmssw/grid/crab/scripts/create_crab_cfg.sh
+++ cmssw/grid/crab/scripts/create_crab_cfg.sh
@@ -6,9 +6,14 @@
 #   * cf https://twiki.cern.ch/twiki/bin/view/CMS/SWGuideCrabHowTo#4_Stage_out_in_your_own_director
 #
 
-if [ $# -eq 0 ]; then
+if [ $# -eq 0 -o $# -lt 4 ]; then
    echo "Usage:"
-   echo ". create_crab_cfg.sh <job_cfg.py> <totnevt>"
+   echo ". create_crab_cfg.sh <job_cfg.py> <totnevt> <output_storage> <storage_path_base/subdir>"
+   echo "output_storage:"
+   echo "  castor,mit,local"
+   echo "Eg:"
+   echo '. create_crab_cfg.sh custom_genjets_cfg.py 10 local $data/semgenjetCMSSW_3_2_6/tree'
+   echo '. create_crab_cfg.sh custom_DIGI_RAW_MC_31X_V3_driver.py castor simCMSSW_3_2_5_DigiToRawFix_EcalTag/raw'
    return 1
 fi
 
@@ -20,13 +25,10 @@ pycfg=$1
 totnevt=$2
 nperjob=250
 # output
-outfile=hydjetB0_325DigiToRawFixEcalTag.root
-release=simCMSSW_3_2_5_DigiToRawFix_EcalTag
-# - return local -
-#outbase=$data/simCMSSW_3_2_5_DigiToRawFix_EcalTag_hyjetB0Job
-outdir=
-# - storage element -
-sedir=/user/f/frankma/data/cmssw/$release
+outfile=output.root
+storage=$3
+sbasepath=${4%/*}
+spath=$4
 
 # ===== Execute =====
 
@@ -48,25 +50,31 @@ sed 's;^number_of_jobs;#number_of_jobs;' \
 > tmpcrab.cfg
 
 # Finally, output
-if [ $outdir ]; then
-   echo "Will output to local dir: $outdir"
-   if [ ! -e $outbase ]; then mkdir $outbase; fi
-   if [ ! -e $outdir ]; then mkdir $outdir; fi
-   sed 's;^#outputdir=.*;outputdir= '$outdir';' tmpcrab.cfg > crab.cfg
-else
+case $storage in
+   local)
+   echo "Will output to local dir: $spath"
+   if [ ! -e $sbasepath ]; then mkdir $sbasepath; fi
+   if [ ! -e $spath ]; then mkdir $spath; fi
+   sed 's;^#outputdir=.*;outputdir= '$spath';' tmpcrab.cfg > crab.cfg
+   ;;
+
+   castor)
+   suserpath=/user/f/frankma/data/cmssw/$spath
    sed 's/^return_data = 1/return_data = 0/' tmpcrab.cfg | \
    sed 's/^copy_data = 0/copy_data = 1/' | \
    sed 's/^#storage_element=srm-cms.cern.ch/storage_element=srm-cms.cern.ch/' | \
    # The order of parameter is important!
-   sed 's;^#storage_path=/srm/managerv2?SFN=/castor/cern.ch/;storage_path=/srm/managerv2?SFN=/castor/cern.ch/\nuser_remote_dir='$sedir';' \
+   sed 's;^#storage_path=/srm/managerv2?SFN=/castor/cern.ch/;storage_path=/srm/managerv2?SFN=/castor/cern.ch/\nuser_remote_dir='$suserpath';' \
    > crab.cfg
    # check castor dir
-   castordir=/castor/cern.ch$sedir
-   rfdir $castordir
-   if [ ! $? -eq 0 ]; then rfmkdir $castordir; fi
-   rfchmod 775 $castordir
-   rfdir $castordir
-fi
+   for i in $sbasepath $spath; do
+      cdir=/castor/cern.ch/user/f/frankma/data/cmssw/$i
+      rfdir $cdir | tail
+      if [ ! $? -eq 0 ]; then rfmkdir $cdir; fi
+      rfchmod 775 $cdir
+   done
+   ;;
+esac
 
 # clean up
 rm -f tmpcrab.cfg
