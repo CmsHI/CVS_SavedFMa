25c1614caae8a4c8766e046bd6b43857a3750a65
frankma <>
2009-09-19 21:39:29 +0000

    Make co more factorized, and only checkout direct tags

--- HI/HLT/scripts/setup/setup_openhlt.sh
+++ HI/HLT/scripts/setup/setup_openhlt.sh
@@ -1,13 +1,23 @@
-project=openhlt
+#!/bin/bash -
+
+project=HIOpenHLT_1_0
 release=CMSSW_3_2_4
-pname=${project}${release}
+pname=${project}_${release}
 scramv1 project -n $pname CMSSW $release
 cd $pname/src
 cmsenv
 
+cvscms
+
+# fix for hi mu hlt reco in 324
+cvs co -r V01-00-04 RecoHI/HiTracking
+cvs co -r V00-03-08 RecoHIMuon/HiMuTracking
+cvs co -r V00-02-02 RecoHIMuon/HiMuPropagator
+cvs co -r V00-03-02 RecoHIMuon/HiMuSeed
+
 # christof's open hlt code so far in 324
-cvs co UserCode/HIHLTStudy
-mv UserCode/HIHLTStudy/* .
+cvs co -r $project UserCode/HIHLTStudy/HLTrigger
+mv UserCode/HIHLTStudy/HLTrigger .
 
 # genjet 32x code
 cvs co -r cmshi_32X UserCode/CmsHi/JetAnalysis
@@ -18,6 +28,8 @@ cvs co -r V00-01-02 DataFormats/HeavyIonEvent
 
 # move to compile directory structure
 mv UserCode/CmsHi .
+
+return 0
 
 # build
 scramv1 b
