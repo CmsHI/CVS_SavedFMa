0b88e77f22b55e29c499c7575680e7c14af4206b
frankma <>
2009-09-24 22:44:32 +0000

    === First Working Version === - Calculate L1 efficiency based on whether the L1 bit fired or not - Futuer improvements   * No fake rate for L1 for now, b/c not clear what is the l1 object and in what eta range

--- HI/HLT/sw/HLTrigger/HLTanalyzers/test/RateEff/macros/plotL1Eff.C
+++ HI/HLT/sw/HLTrigger/HLTanalyzers/test/RateEff/macros/plotL1Eff.C
@@ -10,7 +10,7 @@
 #include "/home/frankma/UserCode/SavedFMa/analysis/root/macros/savedfrankTools.C"
 using namespace std;
 
-void calcEffFake(TTree * ntlead, vector<Double_t> vthresh, TH1D* hnum, TH1D* hden, TH1D* heff, TH1D* hfake,Color_t * vc)
+void calcEffFake(TTree * ntlead, vector<Int_t> vthresh, TH1D* hnum, TH1D* hden, TH1D* heff, TH1D* hfake,Color_t * vc)
 {
    // Ana Parameters
    Double_t etaAcc = 3;
@@ -26,8 +26,7 @@ void calcEffFake(TTree * ntlead, vector<Double_t> vthresh, TH1D* hnum, TH1D* hde
    TCanvas * cpt = new TCanvas("cpt","cpt");
    cpt->SetLogy();
    TCanvas * ceff = new TCanvas("ceff","ceff",500,500);
-   TCanvas * cfake = new TCanvas("cfake","cfake",500,500);
-   TLegend *legeff = new TLegend(0.19,0.79,0.5,0.92);
+   TLegend *legeff = new TLegend(0.55,0.24,0.87,0.37);
    legeff->SetFillColor(0);
    legeff->SetTextSize(0.035);
    //
@@ -40,13 +39,13 @@ void calcEffFake(TTree * ntlead, vector<Double_t> vthresh, TH1D* hnum, TH1D* hde
    for (UInt_t i=0; i<vthresh.size(); ++i) {
       // Efficiency
       cpt->cd();
-      effcut=genjetcut + Form(" && abs(receta)<%f && recpt>%f",etaAcc,vthresh[i]);
+      effcut=genjetcut + Form(" && l1j%d==1",vthresh[i]);
       cout << "effcut: " << effcut << endl;
       ntlead->Draw("genpt>>hden",genjetcut,"hist");
       ntlead->Draw("genpt>>hnum",effcut,"same hist");
       //
       TGraphAsymmErrors *gEfficiency = new TGraphAsymmErrors();
-      gEfficiency->SetMarkerStyle(kFullCircle);
+      gEfficiency->SetMarkerStyle(kFullTriangleUp);
       gEfficiency->SetMarkerSize(1.2);
       gEfficiency->SetMarkerColor(vc[i]);
       //
@@ -58,35 +57,21 @@ void calcEffFake(TTree * ntlead, vector<Double_t> vthresh, TH1D* hnum, TH1D* hde
 	 heff->Draw();
       gEfficiency->Draw("pz");
 
-      legeff->AddEntry(gEfficiency,Form("HLT_HIJet%dU",(Int_t)vthresh[i]),"p");
-
-      // Fakes
-      cpt->cd();
-      ntlead->Draw("recpt>>hden",caljetcut,"hist");
-      fakecut=caljetcut + Form(" && abs(recdr)>%f && abs(recdrgen2)>%f",dRMax,dRMax);
-      cout << "fakecut: " << fakecut << endl;
-      ntlead->Draw("recpt>>hnum",fakecut,"same hist");
-      gFake->BayesDivide(hnum,hden);
-      clearXErrorBar(gFake);
-
-      cfake->cd();
-      if (i==0)
-	 hfake->Draw();
-      gFake->Draw("pz");
+      legeff->AddEntry(gEfficiency,Form("L1_SingleJet%d",(Int_t)vthresh[i]),"p");
    }
 
    ceff->cd();
    legeff->Draw();
 }
 
-void plotEffFake(char * infile = "MyEffHist_0.root")
+void plotL1Eff(char * infile = "MyEffHist_0.root")
 {
    // Top variables
    Int_t NBIN=30;
    Int_t MAXPT=150;
-   vector<Double_t> vthresh;
+   vector<Int_t> vthresh;
+   vthresh.push_back(15);
    vthresh.push_back(30);
-   vthresh.push_back(35);
    vthresh.push_back(50);
    Color_t colors[4] = {kGreen-2, kBlue, kMagenta-4, kRed+2};
 
@@ -116,25 +101,15 @@ void plotEffFake(char * infile = "MyEffHist_0.root")
    hfake->SetLineColor(kBlue);
 
    // === Do checks ===
-   TCanvas * c0 = new TCanvas("cdphi","c0");
-   // here we see that quite often the leading cal jet corresponds to the
-   // away side of the gen jet
-   // However it does not add up to the unmatched distribution, so we are
-   // still missing some.
-   ntlead->Draw("abs(recdphi)","recpt>0");
-   ntlead->Draw("abs(recdphigen2)","recpt>0","same");
-   TCanvas * c1 = new TCanvas("cdr","c1");
-   ntlead->Draw("abs(recdr)","recpt>0");
-   ntlead->Draw("abs(recdrgen2)","recpt>0","same");
-   TCanvas * c2 = new TCanvas("cmatch","c2");
-   // Though at high pt most leading cal jets match
-   ntlead->Draw("genpt","recpt>0 && genpt>40");
-   ntlead->Draw("genpt","recpt>0 && abs(recdr<0.5) && genpt>40","same");
-   ntlead->Draw("genpt","recpt>0 && abs(recdrgen2<0.5) && genpt>40","same");
+   TCanvas * c2 = new TCanvas("cptl1cut","c2");
+   ntlead->Draw("genpt","genpt>0 && genpt>0");
+   ntlead->Draw("genpt","genpt>0 && l1j15==1","same");
+   ntlead->Draw("genpt","genpt>0 && l1j30==1","same");
+   ntlead->Draw("genpt","genpt>0 && l1j50==1","same");
 
    // === Do Calculations ===
    calcEffFake(ntlead,vthresh,hnum,hden,heff,hfake, colors);
 
    // Print Canvases
-   printAllCanvases("./results/hlt");
+   printAllCanvases("./results/l1");
 }
