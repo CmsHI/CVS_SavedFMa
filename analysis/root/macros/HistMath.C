0383961bce226a1f875ebd716013ce974e0e33f2
frankma <>
2009-09-24 15:53:47 +0000

    moved from SavedFMa/HI/analysis/dijet/sw/hiroot/macros/ana/

---
+++ analysis/root/macros/HistMath.C
@@ -0,0 +1,26 @@
+#ifndef HISTMATH_H
+#define HISTMATH_H
+#include "TH1D.h"
+
+void HistMath() {;}
+
+TH1D * integrateHist(TH1 * hin)
+{
+   printf("Want to integrate: %s\n",hin->GetName());
+   TH1D* hout = (TH1D*)hin->Clone(Form("%s_Integ",hin->GetName()));
+   //TH1D * hout = dynamic_cast<TH1D*>(hin->Clone(Form("%s_Integ",hin->GetName())));
+   hout->SetTitle(Form("%s Integrated",hin->GetTitle()));
+
+   Float_t sum = 0.;
+   for (Int_t i=1; i<=hin->GetNbinsX(); ++i) {
+      // calc the integral up to this bin
+      sum += (hin->GetBinContent(i) * hin->GetBinWidth(i));
+      // set the integral up to this bin
+      hout->SetBinContent(i,sum);
+      // set the error
+      hout->SetBinError(i,0);
+   }
+   return hout;
+}
+
+#endif // HISTMATH_H
