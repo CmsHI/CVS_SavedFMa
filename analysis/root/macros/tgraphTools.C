db20351a4e491bced84f3945eba13c6dce589c49
frankma <>
2009-09-24 22:21:02 +0000

    GetN() actually returns Int_t

--- analysis/root/macros/tgraphTools.C
+++ analysis/root/macros/tgraphTools.C
@@ -5,7 +5,7 @@ void tgraphTools() {}
 
 void clearXErrorBar(TGraphAsymmErrors * gr)
 {
-   for (UInt_t i=0; i< gr->GetN(); ++i) {
+   for (Int_t i=0; i< gr->GetN(); ++i) {
       gr->SetPointEXhigh(i,0);
       gr->SetPointEXlow(i,0);
    }
