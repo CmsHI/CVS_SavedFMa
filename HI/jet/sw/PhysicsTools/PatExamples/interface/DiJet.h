36b46e07e35b1f11316d0e650a49dcec95996591
frankma <>
2009-10-13 12:58:35 +0000

    === First Implementation of DiJet === - Lessons   * careful of the BuildFile for linking   * executables built from test dir are in $CMSSW_BASE/test/$SCRAM_ARCH

--- HI/jet/sw/PhysicsTools/PatExamples/interface/DiJet.h
+++ HI/jet/sw/PhysicsTools/PatExamples/interface/DiJet.h
@@ -1,12 +1,55 @@
-#include "TLorentzVector.h"
+#ifndef HiJetAna_DiJet_h
+#define HiJetAna_DiJet_h
+
+//
+// based on http://cmssw.cvs.cern.ch/cgi-bin/cmssw.cgi/CMSSW/RecoJets/JetAlgorithms/interface/ProtoJet.h?hideattic=0&view=markup
+//
+
+#include <iostream>
+#include "DataFormats/Math/interface/LorentzVector.h"
+#include "DataFormats/Math/interface/deltaPhi.h"
 
 class DiJet
 {
   public:
+    typedef math::XYZTLorentzVector LorentzVector;
+
     DiJet();
+    void SetNearJet(LorentzVector nj) {nj_ = nj;};
+    void SetAwayJet(LorentzVector aj) {
+      aj_ = aj;
+      cm_ = nj_+aj_;
+      dphi_ = reco::deltaPhi(aj_.phi(), nj_.phi());
+    };
+
+    // Standard quantities derived from the dijet Lorentz vector
+    /** Returns the modulus of the momentum of the dijet */
+    double p() const {return cm_.P();}
+    /** Returns the transverse momentum of the dijet*/
+    double pt() const {return cm_.Pt();}
+    /** Returns the transverse energy of the dijet*/
+    double et() const {return cm_.Et();}
+    /** Returns the dijet mass of the dijet*/
+    double m() const {return cm_.M();}
+    /** Returns the azimuthal angle of the dijet, Phi*/
+    double phi() const {return cm_.Phi();}
+    /** Returns the pseudorapidity of the dijet*/
+    double eta() const {return cm_.Eta();}
+    /** Returns the rapidity of the dijet*/
+    double y() const {return p() > 0 ? cm_.Rapidity() : 0;}
+
+    // Freind Functions
+    friend std::ostream& operator <<(std::ostream& outs, const DiJet& dj);
+
 
   private:
-    TLorentzVector j1_;
-    TLorentzVector j2_;
-    Double_t dphi_;
+    // Near jet
+    LorentzVector nj_;
+    // Away jet
+    LorentzVector aj_;
+    // dijet properties
+    double dphi_;
+    LorentzVector cm_;
 };
+
+#endif //HiJetAna_DiJet_h
--- HI/jet/sw/PhysicsTools/PatExamples/src/DiJet.cc
+++ HI/jet/sw/PhysicsTools/PatExamples/src/DiJet.cc
@@ -1,6 +1,16 @@
+#include <iostream>
 #include "PhysicsTools/PatExamples/interface/DiJet.h"
+using namespace std;
 
 DiJet::DiJet()
+  : dphi_(-1)
 {
-  printf("construct dijet\n");
+  // empty
+}
+
+// === Friend Functions ===
+ostream& operator <<(ostream& os, const DiJet& dj)
+{
+  os << "near: " << dj.nj_ << "  away: " << dj.aj_ << "  cm: " << dj.cm_ << "  dphi: " << dj.dphi_ << "  mass:" << dj.cm_.M();
+  return os;
 }
