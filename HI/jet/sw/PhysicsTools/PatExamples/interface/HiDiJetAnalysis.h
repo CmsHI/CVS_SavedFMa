e8ef2089d406f27d81c73866a11972da28456efa
frankma <>
2009-10-28 17:40:18 +0000

    remember to destroy allocated memory when the class calls destructor

--- HI/jet/sw/PhysicsTools/PatExamples/interface/HiDiJetAnalysis.h
+++ HI/jet/sw/PhysicsTools/PatExamples/interface/HiDiJetAnalysis.h
@@ -24,6 +24,7 @@ namespace jetana
     public:
       // constructor
       HiDiJetAnalysis();
+      ~HiDiJetAnalysis() { delete tree_; }
 
       // analysis methods
       void CalcJetVars(const AnaDiJetCollection & dijets);
