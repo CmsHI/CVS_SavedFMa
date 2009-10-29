350d67a720f9dc5d60aa5fa72e1223d911b585b9
frankma <>
2009-10-29 22:47:14 +0000

    ok was not using hadd correctly

--- data/scripts/transfer/merge_from_list.sh
+++ data/scripts/transfer/merge_from_list.sh
@@ -1,19 +1,27 @@
 #/bin/bash -
 if [ $# -lt 3 ]; then
   echo Usage:
-  echo "  get_castor_from_list <destination.root> <flist> <n> [pref]"
+  echo "  get_castor_from_list <tag> <flist> <n> [pref]"
   exit 1
 fi
 
-dest=$1
+# set vars
+tag=$1
 flist=$2
 n=$3
 pref=$4
 
-for i in `cat $flist | head -n $n`; do
-   # todo: add a check of already merged ones from previous list
-   cmd="hadd $dest ${pref}$i"
+ncol=2
+
+# run
+for i in `seq 1 $ncol $n`; do
+   end=$((i + ncol -1))
+   dest=${tag}_$i.root
+   curlist=`sed -n "$i,${end}p" $flist | sed "s;^;$pref;"`
+   cmd="hadd $dest $curlist"
    echo $cmd
    eval $cmd
 done
 
+if [ ! -e final ]; then mkdir final; fi
+hadd final/${tag}.root ${tag}_*.root
