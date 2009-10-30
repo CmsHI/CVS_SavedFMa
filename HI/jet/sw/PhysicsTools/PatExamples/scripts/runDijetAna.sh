09962c9bd6b1e8001bd2d89198a15a948c92a262
frankma <>
2009-10-30 11:29:34 +0000

    improves name scheme. Also fixes a typo

--- HI/jet/sw/PhysicsTools/PatExamples/scripts/runDijetAna.sh
+++ HI/jet/sw/PhysicsTools/PatExamples/scripts/runDijetAna.sh
@@ -1,15 +1,19 @@
 if [ $# -eq 0 ]; then
   echo Usage
-  echo "  $0 <jetType> <trackType>"
+  echo "  $0 <jetType> <trackType> [tag]"
   exit 1
 fi
 
 jetType=$1
 trackType=$2
+anatag=$3
 input=/d01/frankma/scratch/data/pat/cmssw331/hydjetNoCollElossFix02/merged/all_50.root
+
 infname=${input##/*/}
 inname=${infname%.root}
-cmd="DiJetFWLiteAnalyzer inputFiles=/d01/frankma/scratch/data/pat/cmssw331/hydjetNoCollElossFix02/merged/all_50.root jetType=$jetType trackType=$trackType tag=$inname outputevery=1 >& log/${inname}_j${jetType}_t${trackTyep}.txt &"
+tag=${inname}_${anatag}
+
+cmd="DiJetFWLiteAnalyzer inputFiles=/d01/frankma/scratch/data/pat/cmssw331/hydjetNoCollElossFix02/merged/all_50.root jetType=$jetType trackType=$trackType tag=$tag outputevery=1 >& log/${tag}_j${jetType}_t${trackType}.txt &"
 
 echo $cmd
 eval "time $cmd"
