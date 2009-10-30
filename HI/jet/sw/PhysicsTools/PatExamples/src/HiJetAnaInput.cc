0718ece1b1f43986351a3c7073d182b016781339
frankma <>
2009-10-30 00:37:01 +0000

    fix silly copy error with the patjet handle, also add track handle

--- HI/jet/sw/PhysicsTools/PatExamples/src/HiJetAnaInput.cc
+++ HI/jet/sw/PhysicsTools/PatExamples/src/HiJetAnaInput.cc
@@ -33,66 +33,100 @@ bool HiJetAnaInput::isParton(const reco::GenParticle & p)
   return result;
 }
 
-bool HiJetAnaInput::passBasicJetKin(const AnaInputItem & cand)
+bool HiJetAnaInput::passBasicJetKin(const InputItem & cand)
 {
   if (cand.pt()<20) return false;
   if (fabs(cand.eta())>3.0) return false;
 
   return true;
 }
+bool HiJetAnaInput::passBasicTrackKin(const AnaInputItem & cand)
+{
+  if (cand.pt()<1.0) return false;
+  if (fabs(cand.eta())>2.5) return false;
 
+  return true;
+}
+
+// Main Methods ============================================
 void HiJetAnaInput::LoadJets(JetType jetType, bool corrected)
 {
-  gSystem->Load( "libFWCoreFWLite" );
-  AutoLibraryLoader::enable();
+  // - cf http://msdn.microsoft.com/en-us/library/2dzy4k6e%28VS.80%29.aspx
+  //   * for example of using enum switch
+  switch (jetType) {
+    // - cf instantiation in case
+    //   * http://docs.freebsd.org/info/g++FAQ/g++FAQ.info.jump_crosses_initialization.html
+    case PARTON:
+      {
+	cout << "load parton" << endl;
+	// fwlight::Handle to gen particles
+	fwlite::Handle<std::vector<reco::GenParticle> > particles;
+	particles.getByLabel(*eventCont_, "hiGenParticles");
+	for (unsigned ip=0; ip<particles->size(); ++ip) {
+	  // select partons
+	  if ( isParton((*particles)[ip]) && passBasicJetKin((*particles)[ip].p4()) )
+	    jets_.push_back((*particles)[ip].p4());
+	}
+	break;
+      }
+    case GENJET:
+      {
+	cout << "load genjet" << endl;
+	// fwlite::Handle to genjet collection
+	fwlite::Handle<std::vector<reco::GenJet> > gjets;
+	gjets.getByLabel(*eventCont_,"iterativeCone5HiGenJets");
+	// loop genjet collection
+	for (unsigned j=0; j<gjets->size(); ++j) {
+	  if ( passBasicJetKin((*gjets)[j].p4()) )
+	    jets_.push_back((*gjets)[j].p4());
+	}
+	break;
+      }
+    case PATJET: 
+      {
+	cout << "load patjet" << endl;
+	fwlite::Handle<std::vector<pat::Jet> > jets;
+	jets.getByLabel(*eventCont_, "selectedLayer1Jets");
+	for(unsigned j=0; j<jets->size(); ++j){
+	  // select jets
+	  if ( passBasicJetKin((*jets)[j].p4()) ) {
+	    if (!corrected)
+	      jets_.push_back((*jets)[j].p4());
+	    // implement else!
+	  }
+	}
+	break;
+      }
+    default:
+      cout << "bad jet type: " << jetType  << endl;
+  }
+}
 
-    fwlite::Handle<std::vector<pat::Jet> > jets;
-    // - cf http://msdn.microsoft.com/en-us/library/2dzy4k6e%28VS.80%29.aspx
-    //   * for example of using enum switch
-    switch (jetType) {
-      // - cf instantiation in case
-      //   * http://docs.freebsd.org/info/g++FAQ/g++FAQ.info.jump_crosses_initialization.html
-      case PARTON:
-	{
-	  cout << "load parton" << endl;
-	  // fwlight::Handle to gen particles
-	  fwlite::Handle<std::vector<reco::GenParticle> > particles;
-	  particles.getByLabel(*eventCont_, "hiGenParticles");
-	  for (unsigned ip=0; ip<particles->size(); ++ip) {
-	    // select partons
-	    if ( isParton((*particles)[ip]) && passBasicJetKin((*particles)[ip].p4()) )
-	      jets_.push_back((*particles)[ip].p4());
-	  }
-	  break;
+void HiJetAnaInput::LoadTracks(TrackType trackType)
+{
+  // - cf http://msdn.microsoft.com/en-us/library/2dzy4k6e%28VS.80%29.aspx
+  //   * for example of using enum switch
+  switch (trackType) {
+    // - cf instantiation in case
+    //   * http://docs.freebsd.org/info/g++FAQ/g++FAQ.info.jump_crosses_initialization.html
+    case PARTICLE:
+      {
+	cout << "load track" << endl;
+	// fwlight::Handle to gen particles
+	fwlite::Handle<std::vector<reco::GenParticle> > particles;
+	particles.getByLabel(*eventCont_, "hiGenParticles");
+	for (unsigned ip=0; ip<particles->size(); ++ip) {
+	  // select particles
+	  //if ( isParton((*particles)[ip]) && passBasicJetKin((*particles)[ip].p4()) )
 	}
-      case GENJET:
-	{
-	  cout << "load genjet" << endl;
-	  // fwlite::Handle to genjet collection
-	  fwlite::Handle<std::vector<reco::GenJet> > gjets;
-	  gjets.getByLabel(*eventCont_,"iterativeCone5HiGenJets");
-	  // loop genjet collection
-	  for (unsigned j=0; j<gjets->size(); ++j) {
-	    if ( passBasicJetKin((*gjets)[j].p4()) )
-	      jets_.push_back((*gjets)[j].p4());
-	  }
-	  break;
-	}
-      case PATJET: 
-	{
-	  cout << "load patjet" << endl;
-	  jets.getByLabel(*eventCont_, "selectedLayer1Jets");
-	  for(unsigned j=0; j<jets->size(); ++j){
-	    // select jets
-	    if ( passBasicJetKin((*jets)[j].p4()) ) {
-	      if (!corrected)
-		jets_.push_back((*jets)[j].p4());
-	      // implement else!
-	    }
-	  }
-	  break;
-	}
-      default:
-	cout << "jet type: " << jetType  << endl;
-    }
+	break;
+      }
+    case TRACK:
+      {
+	fwlite::Handle<std::vector<reco::Track> > tracks;
+	break;
+      }
+    default:
+      cout << "bad track type: " << trackType  << endl;
+  }
 }
