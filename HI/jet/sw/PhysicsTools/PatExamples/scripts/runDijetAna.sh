2417021e514e40719659eee37cb9bdc09519677d
frankma <>
2009-10-31 10:56:46 +0000

    realized the input was hard-coded

--- HI/jet/sw/PhysicsTools/PatExamples/scripts/runDijetAna.sh
+++ HI/jet/sw/PhysicsTools/PatExamples/scripts/runDijetAna.sh
@@ -7,13 +7,14 @@ fi
 jetType=$1
 trackType=$2
 anatag=$3
-input=/d01/frankma/scratch/data/pat/cmssw331/hydjetNoCollElossFix02/merged/all_50.root
+#input=/d01/frankma/scratch/data/pat/cmssw331/hydjetNoCollElossFix02/merged/all_50.root
+input=rfio:/castor/cern.ch/user/f/frankma/data/HI/jet/pat/cmssw331/all_300.root
 
-infname=${input##/*/}
+infname=${input##*/}
 inname=${infname%.root}
 tag=${inname}_${anatag}
 
-cmd="DiJetFWLiteAnalyzer inputFiles=/d01/frankma/scratch/data/pat/cmssw331/hydjetNoCollElossFix02/merged/all_50.root jetType=$jetType trackType=$trackType tag=$tag outputevery=1 >& log/${tag}_j${jetType}_t${trackType}.txt &"
+cmd="DiJetFWLiteAnalyzer inputFiles=$input jetType=$jetType trackType=$trackType tag=$tag outputevery=1 >& log/${tag}_j${jetType}_t${trackType}.txt &"
 
 echo $cmd
 eval "time $cmd"
