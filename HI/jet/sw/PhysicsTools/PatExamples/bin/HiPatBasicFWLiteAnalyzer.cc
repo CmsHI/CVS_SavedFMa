fbee882e3e9e7c5bf7897ffcb5814e0deba8cedb
frankma <>
2009-10-31 17:32:23 +0000

    do all with this pat exe, b/c the collection lable names have to be changed in any case for pp

--- HI/jet/sw/PhysicsTools/PatExamples/bin/HiPatBasicFWLiteAnalyzer.cc
+++ HI/jet/sw/PhysicsTools/PatExamples/bin/HiPatBasicFWLiteAnalyzer.cc
@@ -60,7 +60,26 @@ int main(int argc, char* argv[])
   // event as well as to store histograms, etc.
   fwlite::EventContainer eventCont (parser);
 
-  // Book those histograms!
+  // book histograms for basic objects
+  eventCont.add( new TH1D("genjetPt", "pt",    100,  0.,150.) );
+  eventCont.add( new TH1D("genjetEta","eta",   100, -5.,  5.) );
+  eventCont.add( new TH1D("genjetPhi","phi",   100, -5.,  5.) );
+  eventCont.add( new TH1D("rawjetPt", "pt",    100,  0.,150.) );
+  eventCont.add( new TH1D("jetPt", "pt",    100,  0.,150.) );
+  eventCont.add( new TH1D("jetEta","eta",   100, -5.,  5.) );
+  eventCont.add( new TH1D("jetPhi","phi",   100, -5.,  5.) );
+  eventCont.add( new TH2D("matjetDR", "dR",    100,   0, 6,100,0,100) ); 
+  eventCont.add( new TH2D("jetDR", "dR",    100,   0, 6, 100,0,100) ); 
+  eventCont.add( new TH2D("jetPtCorrel", "pt gen vs calo",    100,  0.,150., 100,0,150) );
+  
+  eventCont.add( new TH1D("partlPt", "pt",    100,  0.,50.) );
+  eventCont.add( new TH1D("partlEta","eta",   100, -5.,  5.) );
+  eventCont.add( new TH1D("partlPhi","phi",   100, -5.,  5.) );
+
+  eventCont.add( new TH1D("trackPt", "pt",    100,  0.,50.) );
+  eventCont.add( new TH1D("trackEta","eta",   100, -5.,  5.) );
+  eventCont.add( new TH1D("trackPhi","phi",   100, -5.,  5.) );
+  // Book those histograms for HI Event
   eventCont.add( new TH1F( "genb", "genb", 50, 0, 20) );
   eventCont.add( new TH1F( "gennpart", "gennpart", 100, 0, 300) );
   eventCont.add( new TH1F( "genncoll", "genncoll", 100, 0, 600) );
@@ -86,6 +105,79 @@ int main(int argc, char* argv[])
     //////////////////////////////////
     // Take What We Need From Event //
     //////////////////////////////////
+
+    // fwlite::Handle to to jet collection
+    fwlite::Handle<std::vector<pat::Jet> > jets;
+    jets.getByLabel(eventCont, "selectedLayer1Jets");
+    cout << "got pat jets: " << jets->size() << endl;
+    
+    // fwlite::Handle to genjet collection
+    fwlite::Handle<std::vector<reco::GenJet> > gjets;
+    gjets.getByLabel(eventCont,"iterativeCone5HiGenJets");
+    //gjets.getByLabel(eventCont,"hiGenJetCleaner");
+    cout << "got gen jets: " << gjets->size() << endl;
+
+    // loop jet collection and fill histograms
+    for(unsigned i=0; i<jets->size(); ++i){
+      eventCont.hist("rawjetPt")->Fill( (*jets)[i].correctedJet("raw").pt()  );
+      eventCont.hist("jetPt")->Fill( (*jets)[i].pt()  );
+      eventCont.hist("jetEta")->Fill( (*jets)[i].eta() );
+      eventCont.hist("jetPhi")->Fill( (*jets)[i].phi() );
+      printf("cor caljet et|eta|phi: %f|%f|%f\n",
+	  (*jets)[i].pt(), (*jets)[i].eta(), (*jets)[i].phi());
+      printf("raw caljet et|eta|phi: %f|%f|%f\n",
+	  (*jets)[i].correctedJet("raw").pt(),
+	  (*jets)[i].correctedJet("raw").eta(),
+	  (*jets)[i].correctedJet("raw").phi());
+
+      // If there is a matched genjet to the caljet
+      const reco::GenJet * genjet = (*jets)[i].genJet();
+      if ( genjet != NULL ) {
+	Double_t jetDR = reco::deltaR((*jets)[i],*genjet);
+	printf("genjet et|eta|phi: %f|%f|%f, dR: %f\n",
+	    genjet->pt(),genjet->eta(),genjet->phi(),jetDR);
+	eventCont.hist("matjetDR")->Fill(jetDR,genjet->pt());
+	eventCont.hist("jetPtCorrel")->Fill((*jets)[i].pt(),genjet->pt());
+      }
+
+      // loop genjet collection
+      for (unsigned j=0; j<gjets->size(); ++j) {
+	Double_t jetDR = reco::deltaR((*jets)[i],(*gjets)[j]);
+	eventCont.hist("jetDR")->Fill(jetDR,(*gjets)[j].pt());
+      }  
+    }
+
+    // loop over gen jets
+    for (unsigned j=0; j<gjets->size(); ++j) {
+      eventCont.hist("genjetPt")->Fill( (*gjets)[j].pt()  );
+      eventCont.hist("genjetEta")->Fill( (*gjets)[j].eta() );
+      eventCont.hist("genjetPhi")->Fill( (*gjets)[j].phi() );
+    }  
+    
+
+    // fwlight::Handle to gen particles
+    fwlite::Handle<std::vector<reco::GenParticle> > particles;
+    particles.getByLabel(eventCont, "hiGenParticles");
+    // fwlight::Handle to tracks
+    fwlite::Handle<std::vector<reco::Track> > tracks;
+    tracks.getByLabel(eventCont, "hiSelectedTracks");
+
+    // loop particle collection and fill
+    for (unsigned ip=0; ip<particles->size(); ++ip) {
+      reco::GenParticle p = (*particles)[ip];
+      if (p.status() == 1 && p.charge()!=0 && p.pt()>1) {
+	eventCont.hist("partlPt") ->Fill( (*particles)[ip].pt()  );
+	eventCont.hist("partlEta")->Fill( (*particles)[ip].eta() );
+	eventCont.hist("partlPhi")->Fill( (*particles)[ip].phi() );
+      }
+    }
+
+    // loop track collection and fill
+    for (unsigned it=0; it<tracks->size(); ++it) {
+      eventCont.hist("trackPt") ->Fill( (*tracks)[it].pt()  );
+      eventCont.hist("trackEta")->Fill( (*tracks)[it].eta() );
+      eventCont.hist("trackPhi")->Fill( (*tracks)[it].phi() );
+    }
     // fwlite::Handle to to HI info collections
     fwlite::Handle<pat::HeavyIon> hievt;
     hievt.getByLabel(eventCont,"heavyIon");
