fe2764ca81ac986307231244beb8986c268fb95f
frankma <>
2009-11-02 14:45:44 +0000

    for gen level info, use corrected thresholds

--- HI/jet/sw/PhysicsTools/PatExamples/scripts/runAll.sh
+++ HI/jet/sw/PhysicsTools/PatExamples/scripts/runAll.sh
@@ -1,4 +1,4 @@
 /home/frankma/work/HI/jet/sw/pat/patanaCMSSW_3_3_1/src/PhysicsTools/PatExamples/scripts/runFullList.sh 33 sw44 2 1 1
 /home/frankma/work/HI/jet/sw/pat/patanaCMSSW_3_3_1/src/PhysicsTools/PatExamples/scripts/runFullList.sh 33 sw44 2 1 0
-/home/frankma/work/HI/jet/sw/pat/patanaCMSSW_3_3_1/src/PhysicsTools/PatExamples/scripts/runFullList.sh 33 sw44 1 0 1
-/home/frankma/work/HI/jet/sw/pat/patanaCMSSW_3_3_1/src/PhysicsTools/PatExamples/scripts/runFullList.sh 33 sw44 0 0 1
+/home/frankma/work/HI/jet/sw/pat/patanaCMSSW_3_3_1/src/PhysicsTools/PatExamples/scripts/runFullList.sh 33 sw44 1 0 0
+/home/frankma/work/HI/jet/sw/pat/patanaCMSSW_3_3_1/src/PhysicsTools/PatExamples/scripts/runFullList.sh 33 sw44 0 0 0
