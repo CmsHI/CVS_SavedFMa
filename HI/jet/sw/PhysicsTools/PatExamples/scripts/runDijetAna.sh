4f14fedf18f9fe9e926444868e025c2f27290a84
frankma <>
2009-11-05 21:00:51 +0000

    make name scheme uniform: jetType, correction, trackType

--- HI/jet/sw/PhysicsTools/PatExamples/scripts/runDijetAna.sh
+++ HI/jet/sw/PhysicsTools/PatExamples/scripts/runDijetAna.sh
@@ -1,31 +1,38 @@
 #!/bin/bash -
 if [ $# -eq 0 ]; then
   echo Usage
-  echo "  $0 <tag> [jetType] [trackType] [jetCorrection?] <filename> [indir]"
+  echo "  $0 <tag> [jetType] [jetCorrection?] [trackType] <filename> [indir]"
   echo "Eg:"
-  echo "  $0 sw45 2 1 1 pyquen_unquendijet_nocoll_pt120to170_d20091025_runs1to40.root /castor/cern.ch/user/f/frankma/data/pat/CMSSW_3_3_1_fix03/Hydjet_MinBias_noColl_4TeV/pyquen_unquendijet_nocoll_pt120to170_d20091025"
+  echo "  $0 sw45 2 1 1 pyquen_unquendijet_nocoll_pt120to170_d20091025_runs1to40.root rfio:/castor/cern.ch/user/f/frankma/data/pat/CMSSW_3_3_1_fix03/Hydjet_MinBias_noColl_4TeV/pyquen_unquendijet_nocoll_pt120to170_d20091025"
   exit 1
 fi
 
 anatag=$1
 jetType=$2
-trackType=$3
-jetCorrec=$4
+jetCorrec=$3
+trackType=$4
 fileName=$5
 indir=$6
+
+# genType: pp(0), HI(1)
+genType=0
+
+# HI pat files
 #input=/d01/frankma/scratch/data/pat/cmssw331/hydjetNoCollElossFix02/merged/all_50.root
 #input=rfio:/castor/cern.ch/user/f/frankma/data/HI/jet/pat/cmssw331/all_300.root
 #input=rfio:/castor/cern.ch/user/f/frankma/data/pat/CMSSW_3_3_1_fix03/Hydjet_MinBias_noColl_4TeV/RECO_runs1to40.root
+# pp pat files
+input=/d01/frankma/scratch/data/pat/CMSSW_3_3_1_fix04/SignalOnly/Hydjet_MinBias_noColl_4TeV/pyquen_unquendijet_nocoll_pt120to170_d20091025/467CB4EC-B6A0-DE11-A0DA-00E08133FF64_numEvent10546.root
 if [ -z $indir ]; then indir=/castor/cern.ch/user/f/frankma/data/pat/CMSSW_3_3_1_fix03/Hydjet_MinBias_noColl_4TeV/pyquen_unquendijet_nocoll_pt120to170_d20091025; fi
 if [ -z $fileName ]; then fileName=pyquen_unquendijet_nocoll_pt120to170_d20091025_runs1to40.root; fi
-input=rfio:$indir/$fileName
+input=$indir/$fileName
 
 infname=${input##*/}
 inname=${infname%.root}
 tag=${inname}_${anatag}
 
 if [ ! -e log ]; then mkdir log; fi
-cmd="DiJetFWLiteAnalyzer inputFiles=$input jetType=$jetType jetCorrec=$jetCorrec trackType=$trackType tag=$tag outputevery=1 >& log/${tag}_j${jetType}_${jetCorrec}_t${trackType}.txt"
+cmd="DiJetFWLiteAnalyzer inputFiles=$input genType=$genType jetType=$jetType jetCorrec=$jetCorrec trackType=$trackType tag=$tag outputevery=1 >& log/${tag}_j${jetType}_${jetCorrec}_t${trackType}.txt"
 
 echo $cmd
 eval "time $cmd"
--- HI/jet/sw/PhysicsTools/PatExamples/scripts/runFullList.sh
+++ HI/jet/sw/PhysicsTools/PatExamples/scripts/runFullList.sh
@@ -1,17 +1,17 @@
 if [ $# -eq 0 ]; then
   echo Usage
-  echo "  <n> <tag> <jtype> <ptype> <cor> [indir]"
+  echo "  <n> <tag> <jtype> <cor> <trackType> [indir]"
   exit 1
 fi
 
 n=$1
 tag=$2
 jtype=$3
-ptype=$4
-cor=$5
+cor=$4
+ptype=$5
 indir=$6
 if [ -z $indir ]; then indir=/castor/cern.ch/user/f/frankma/data/pat/CMSSW_3_3_1_fix03/Hydjet_MinBias_noColl_4TeV/; fi
 
 for file in `rfdir $indir | awk '{print $NF}' | head -n $n`; do
-  /home/frankma/work/HI/jet/sw/pat/patanaCMSSW_3_3_1/src/PhysicsTools/PatExamples/scripts/runDijetAna.sh $tag $jtype $ptype $cor $file $indir
+  /home/frankma/work/HI/jet/sw/pat/patanaCMSSW_3_3_1/src/PhysicsTools/PatExamples/scripts/runDijetAna.sh $tag $jtype $cor $ptype $file $indir
 done
