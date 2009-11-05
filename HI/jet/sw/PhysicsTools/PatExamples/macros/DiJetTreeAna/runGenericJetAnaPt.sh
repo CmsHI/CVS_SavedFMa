a37da189f45ce678e7bb9e4c95683b82c4626df8
frankma <>
2009-11-05 03:25:44 +0000

    unquenched pt 120 to 170 run

--- HI/jet/sw/PhysicsTools/PatExamples/macros/DiJetTreeAna/runGenericJetAnaPt.sh
+++ HI/jet/sw/PhysicsTools/PatExamples/macros/DiJetTreeAna/runGenericJetAnaPt.sh
@@ -11,8 +11,10 @@ echo "$@"
 
 #== input vars==
 tag=$1
-indir=/d01/frankma/scratch/data/ana/CMSSW_3_3_1_fix03/Hydjet_MinBias_noColl_4TeV/pyquen_unquendijet_nocoll_pt120to170_d20091025/$tag
-export plotbase=plots/CMSSW_3_3_1_fix03/Hydjet_MinBias_noColl_4TeV/pyquen_unquendijet_nocoll_pt120to170_d20091025/$tag
+#signal=pyquen_unquendijet_nocoll_pt120to170_d20091025
+signal=pyquen_quendijet_nocoll_pt120to170_d20091024
+indir=/d01/frankma/scratch/data/ana/CMSSW_3_3_1_fix03/Hydjet_MinBias_noColl_4TeV/$signal/$tag
+export plotbase=plots/CMSSW_3_3_1_fix03/Hydjet_MinBias_noColl_4TeV/$signal/$tag
 
 AnaNJetEtMin=100
 AnaNJetEtMax=300
@@ -20,6 +22,6 @@ AnaAJetEtMin=80
 JDPhiMin=2.85
 
 for anatype in j2_1_t1 j2_0_t1 j1_1_t0 j0_1_t0; do
-  ./runJetAnaBase.sh tree_djana_${anatype}_pyquen_unquendijet_nocoll_pt120to170_d20091025_runs*to*.root  $AnaNJetEtMin $AnaNJetEtMax $AnaAJetEtMin $JDPhiMin ${tag}_${anatype} $indir
+  ./runJetAnaBase.sh tree_djana_${anatype}_${signal}_runs*to*.root  $AnaNJetEtMin $AnaNJetEtMax $AnaAJetEtMin $JDPhiMin ${tag}_${anatype} $indir
 done
 
