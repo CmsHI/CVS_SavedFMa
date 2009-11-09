e036d19eca4e27cbb797e26bef3893bec964aa37
frankma <>
2009-11-09 15:07:52 +0000

    adjust pt cut b/c used only pt_hat of 120 to 170 events. fix03: pyquen mixed. fix04: pyquen signal only

--- HI/jet/sw/PhysicsTools/PatExamples/macros/DiJetTreeAna/runGenericJetAnaPt.sh
+++ HI/jet/sw/PhysicsTools/PatExamples/macros/DiJetTreeAna/runGenericJetAnaPt.sh
@@ -12,16 +12,28 @@ echo "$@"
 #== input vars==
 tag=$1
 #signal=pyquen_unquendijet_nocoll_pt120to170_d20091025
-signal=pyquen_quendijet_nocoll_pt120to170_d20091024
-indir=/d01/frankma/scratch/data/ana/CMSSW_3_3_1_fix03/Hydjet_MinBias_noColl_4TeV/$signal/$tag
-export plotbase=plots/CMSSW_3_3_1_fix03/Hydjet_MinBias_noColl_4TeV/$signal/$tag
 
-AnaNJetEtMin=100
+AnaNJetEtMin=135
 AnaNJetEtMax=300
-AnaAJetEtMin=80
+AnaAJetEtMin=100
 JDPhiMin=2.85
 
-for anatype in j2_1_t1 j2_0_t1 j1_1_t0 j0_1_t0; do
-  ./runJetAnaBase.sh tree_djana_${anatype}_${signal}_runs*to*.root  $AnaNJetEtMin $AnaNJetEtMax $AnaAJetEtMin $JDPhiMin ${tag}_${anatype} $indir
+cmssw=CMSSW_3_3_1_fix03
+indir=/d01/frankma/scratch/data/ana/$cmssw/Hydjet_MinBias_noColl_4TeV
+for signal in pyquen_quendijet_nocoll_pt120to170_d20091024 pyquen_unquendijet_nocoll_pt120to170_d20091025; do
+  for anatype in j2_1_t1 j2_0_t1 j1_1_t0 j0_1_t0; do
+    #for anatype in j2_1_t1; do
+    export plotbase=plots/$cmssw/Hydjet_MinBias_noColl_4TeV/$signal/$tag
+    ./runJetAnaBase.sh tree_djana_${anatype}_${signal}_runs*to*_${tag}.root  $AnaNJetEtMin $AnaNJetEtMax $AnaAJetEtMin $JDPhiMin ${tag}_${anatype} $indir
+  done
 done
 
+cmssw=CMSSW_3_3_1_fix04
+indir=/d01/frankma/scratch/data/ana/$cmssw/SignalOnly
+for signal in  pyquen_unquendijet_nocoll_pt120to170_d20091025; do
+  for anatype in j2_1_t1 j2_0_t1 j1_1_t0 j0_1_t0; do
+    #for anatype in j2_1_t1; do
+    export plotbase=plots/$cmssw/SignalOnly/$signal/$tag
+    ./runJetAnaBase.sh tree_djana_${anatype}_${signal}_runs*to*_${tag}.root  $AnaNJetEtMin $AnaNJetEtMax $AnaAJetEtMin $JDPhiMin ${tag}_${anatype} $indir
+  done
+done
