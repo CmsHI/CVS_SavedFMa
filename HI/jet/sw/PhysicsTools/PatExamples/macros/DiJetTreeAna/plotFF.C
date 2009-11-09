37cde424fdaa227f580aed30a235ca8b930d1695
frankma <>
2009-11-09 15:09:28 +0000

    Add checks for distributions in different near-away-Et-correlation regions. Also make histograms for background xi distribution

--- HI/jet/sw/PhysicsTools/PatExamples/macros/DiJetTreeAna/plotFF.C
+++ HI/jet/sw/PhysicsTools/PatExamples/macros/DiJetTreeAna/plotFF.C
@@ -14,15 +14,16 @@ using namespace std;
 
 void plotFF(//char * infname1 = "/d01/frankma/scratch/HI/jet/pat/patanaCmssw331/hydjetNoCollElossFix03/tree_djana_j2_1_t1_RECO_runs1to40_sw43.root",
     //char * infname1 = "/d01/frankma/scratch/data/ana/hydjet_noColl_dijet_2009_10_1/merged/back/tree_djana_j2_1_t1_RECO_all40k_sw44.root",
-    char * infname1 = "/d01/frankma/scratch/data/ana/hydjet_noColl_dijet_2009_10_1/tree_djana_j2_1_t1_RECO_*to*_sw44.root",
+    //char * infname1 = "/d01/frankma/scratch/data/ana/hydjet_noColl_dijet_2009_10_1/tree_djana_j2_1_t1_RECO_*to*_sw44.root",
+    char * infname1 = "/d01/frankma/scratch/data/ana/CMSSW_3_3_1_fix03/Hydjet_MinBias_noColl_4TeV/tree_djana_j2_1_t1_pyquen_quendijet_nocoll_pt120to170_d20091024_runs161to200_sw47_2.root",
     // -pythia-
-    Double_t NJetEtMin = 65,
-    Double_t NJetEtMax = 200,
-    Double_t AJetEtMin = 30,
-    Double_t JDPhiMin = 3.14-0.5,
-    char * plotdir = "plots1",
-    const Int_t NXIBIN = 30,
-    const Double_t XIMAX = 6,
+    Double_t NJetEtMin = 120,
+    Double_t NJetEtMax = 300,
+    Double_t AJetEtMin = 80,
+    Double_t JDPhiMin = 3.14-0.25,
+    char * plotdir = "testplots",
+    const Int_t NXIBIN = 25,
+    const Double_t XIMAX = 7,
     const Double_t XIYMAX = 10,
     Bool_t check = kTRUE
     )
@@ -56,7 +57,7 @@ void plotFF(//char * infname1 = "/d01/frankma/scratch/HI/jet/pat/patanaCmssw331/
     // make cut
     vgen[ig]->SetDPhiMin(JDPhiMin);
     vgen[ig]->SetJetPartlDRMax(0.5);
-    vgen[ig]->SetPartlPtMin(2.);
+    vgen[ig]->SetPartlPtMin(1); // history: 2.
     vgen[ig]->CreateCuts();
     // dijet cuts
     vgen[ig]->cut_.AndJetCut("lajmul",1,"equ");
@@ -82,7 +83,23 @@ void plotFF(//char * infname1 = "/d01/frankma/scratch/HI/jet/pat/patanaCmssw331/
   if (check) {
     printf("\n============================Do Checks======================\n");
     printf("%s\n",infname1);
-    //---- Check dijet properties ----
+    // === Analysis checks ===
+    // A/N diag corr vs no corr
+    drawTree2(djTree, "aljet:nljet","(aljet/nljet)>0.8","colz","hNJetEtvsAJetEtDiag",";near Jet E_{t} [GeV];away Jet E_{t} [GeV]",HJETETBINS,HJETETMIN,HJETETMAX,HJETETBINS,HJETETMIN,HJETETMAX,1);
+    drawTree2(djTree, "aljet:nljet","aljet<80","colz","hNJetEtvsAJetEtHori",";near Jet E_{t} [GeV];away Jet E_{t} [GeV]",HJETETBINS,HJETETMIN,HJETETMAX,HJETETBINS,HJETETMIN,HJETETMAX,1);
+    // -- dphi --
+    drawTree(djTree, "jdphi","",drsgFF,"hANEtCorrRefJDPhi",";Dijet d#phi;",40,3.14/2,3.14,0,nrColor,nrStyle,3,1,8);
+    drawTree(djTree, "jdphi","(aljet/nljet)>0.8",drdbFF,"hANEtCorrDiagJDPhi",";Dijet d#phi;",40,3.14/2,3.14,0,kRed,nrStyle,3,1,8);
+    drawTree(djTree, "jdphi","aljet<80",drdbFF,"hANEtCorrHoriJDPhi",";Dijet d#phi;",40,3.14/2,3.14,0,kBlue,nrStyle,3,1,8);
+    // -- eta --
+    drawTree(djTree, "nljeta","",drsgFF,"hANEtCorrRefnljeta",";Near Jet Eta;",ETANBIN,ETAMIN,ETAMAX,0,nrColor,nrStyle,3,1,8);
+    drawTree(djTree, "nljeta","(aljet/nljet)>0.8",drdbFF,"hANEtCorrDiagnljeta",";Near Eta;",ETANBIN,ETAMIN,ETAMAX,0,kRed,nrStyle,3,1,8);
+    drawTree(djTree, "nljeta","aljet<80",drdbFF,"hANEtCorrHorinljeta",";Near Jet Eta;",ETANBIN,ETAMIN,ETAMAX,0,kBlue,nrStyle,3,1,8);
+    //  -away-
+    drawTree(djTree, "aljeta","",drsgFF,"hANEtCorrRefaljeta",";Away Jet Eta;",ETANBIN,ETAMIN,ETAMAX,0,nrColor,nrStyle,3,1,8);
+    drawTree(djTree, "aljeta","(aljet/nljet)>0.8",drdbFF,"hANEtCorrDiagaljeta",";;",ETANBIN,ETAMIN,ETAMAX,0,kRed,nrStyle,3,1,8);
+    drawTree(djTree, "aljeta","aljet<80",drdbFF,"hANEtCorrHorialjeta",";;",ETANBIN,ETAMIN,ETAMAX,0,kBlue,nrStyle,3,1,8);
+
     //--- inv mass ---
     //drawTree(djTree, "mass","",drsgFF,"hUncutMass","Dijet Inv Mass [GeV];",100,0,1800,true,kBlack,1,3);
     // --from jet leading--
@@ -109,11 +126,11 @@ void plotFF(//char * infname1 = "/d01/frankma/scratch/HI/jet/pat/patanaCmssw331/
   drawTree(djTree, "nljet",treeana.GetCut().GetDiJetCut().Data(),drdbFF,"hNJetEt",";Jet Et [GeV];",HJETETBINS,HJETETMIN,HJETETMAX,true,nrColor,nrStyle,3,1,8);
   drawTree(djTree, "aljet",treeana.GetCut().GetDiJetCut().Data(),drdbFF,"hAJetEt",";Jet Et [GeV];",HJETETBINS,HJETETMIN,HJETETMAX,true,awColor,awStyle,3,1,4);
 
-  // Dijets
+  // === Dijet properties ===
   drawTree(djTree, "jdphi","",drsgFF,"hUncutJDPhi",";Dijet d#phi;",40,3.14/2,3.14,0,nrColor,nrStyle,1,1,8);
   drawTree(djTree, "jdphi","",drsgFF,"hJDPhi",";Dijet d#phi;",40,3.14/2,3.14,0,nrColor,nrStyle,1,1,8);
 
-  // ratio
+  // Et correlation
   drawTree2(djTree, "aljet:nljet","","colz","hUncutNJetEtvsAJetEt",";near Jet E_{t} [GeV];away Jet E_{t} [GeV]",HJETETBINS,HJETETMIN,HJETETMAX,HJETETBINS,HJETETMIN,HJETETMAX,1);
   drawTree2(djTree, "aljet:nljet",treeana.GetCut().GetDiJetCut().Data(),"colz","hNJetEtvsAJetEt",";near Jet E_{t} [GeV];away Jet E_{t} [GeV]",HJETETBINS,HJETETMIN,HJETETMAX,HJETETBINS,HJETETMIN,HJETETMAX,1);
   // rato vs b
@@ -134,6 +151,14 @@ void plotFF(//char * infname1 = "/d01/frankma/scratch/HI/jet/pat/patanaCmssw331/
   drawTree(djTree, "log(1/zn)",treeana.GetCut().GetNJetPartlCut().Data(),drsgFFE,"hXiNearJet",";#xi=ln(E_{t}^{Jet}/E_{t}^{Particle});#frac{1}{N_{jet}} #frac{dN}{d#xi}",NXIBIN,0,XIMAX,log,nrColor,nrStyle,3,2,kFullDotLarge,nJetNorm,XIYMAX);
   // -away-
   drawTree(djTree, "log(1/za)",treeana.GetCut().GetAJetPartlCut().Data(),drdbFFE,"hXiAwayJet",";#xi=ln(E_{t}^{Jet}/E_{t}^{Particle});#frac{1}{N_{jet}} #frac{dN}{d#xi}",NXIBIN,0,XIMAX,log,awColor,awStyle,3,2,kCircle,nJetNorm,XIYMAX);
+
+  // --- background ---
+  // -near-
+  printf("  - and background particle cut:\n  %s\n  %s\n",vgen[0]->GetCut().GetNewNJetPartlCut("pndr","pndrbg").Data(),
+      vgen[0]->GetCut().GetNewAJetPartlCut("padr","padrbg").Data());
+  drawTree(djTree, "log(1/zn)",treeana.GetCut().GetNewNJetPartlCut("pndr","pndrbg").Data(),drsgFFE,"hBgXiNearJet",";#xi=ln(E_{t}^{Jet}/E_{t}^{Particle});#frac{1}{N_{jet}} #frac{dN}{d#xi}",NXIBIN,0,XIMAX,log,nrColor,nrStyle,3,2,kFullDotLarge,nJetNorm,XIYMAX);
+  // -away-
+  drawTree(djTree, "log(1/za)",treeana.GetCut().GetNewAJetPartlCut("padr","padrbg").Data(),drdbFFE,"hBgXiAwayJet",";#xi=ln(E_{t}^{Jet}/E_{t}^{Particle});#frac{1}{N_{jet}} #frac{dN}{d#xi}",NXIBIN,0,XIMAX,log,awColor,awStyle,3,2,kCircle,nJetNorm,XIYMAX);
 
   //=== Save and exit ===
   printAllCanvases(plotdir);
