817391ffeb8deb9bae0bcfce559f0d8069f99787
frankma <>
2009-11-09 15:06:11 +0000

    run on many signals at once

--- HI/jet/sw/PhysicsTools/PatExamples/scripts/runAll.sh
+++ HI/jet/sw/PhysicsTools/PatExamples/scripts/runAll.sh
@@ -1,9 +1,11 @@
 #signal=pyquen_unquendijet_nocoll_pt120to170_d20091025
-signal=pyquen_quendijet_nocoll_pt120to170_d20091024
-indir=rfio:/castor/cern.ch/user/f/frankma/data/pat/CMSSW_3_3_1_fix03/Hydjet_MinBias_noColl_4TeV/$signal
+#signal=pyquen_quendijet_nocoll_pt120to170_d20091024
+tag=sw47_2
 n=2
-tag=sw46_2
-/home/frankma/work/HI/jet/sw/pat/patanaCMSSW_3_3_1/src/PhysicsTools/PatExamples/scripts/runFullList.sh $n $tag 2 1 1 $indir
-/home/frankma/work/HI/jet/sw/pat/patanaCMSSW_3_3_1/src/PhysicsTools/PatExamples/scripts/runFullList.sh $n $tag 2 0 1 $indir
-/home/frankma/work/HI/jet/sw/pat/patanaCMSSW_3_3_1/src/PhysicsTools/PatExamples/scripts/runFullList.sh $n $tag 1 1 0 $indir
-/home/frankma/work/HI/jet/sw/pat/patanaCMSSW_3_3_1/src/PhysicsTools/PatExamples/scripts/runFullList.sh $n $tag 0 1 0 $indir
+for signal in pyquen_quendijet_nocoll_pt120to170_d20091024 pyquen_unquendijet_nocoll_pt120to170_d20091025; do
+  indir=/castor/cern.ch/user/f/frankma/data/pat/CMSSW_3_3_1_fix03/Hydjet_MinBias_noColl_4TeV/$signal
+  /home/frankma/work/HI/jet/sw/pat/patanaCMSSW_3_3_1/src/PhysicsTools/PatExamples/scripts/runFullList.sh $n $tag 2 1 1 $indir
+  /home/frankma/work/HI/jet/sw/pat/patanaCMSSW_3_3_1/src/PhysicsTools/PatExamples/scripts/runFullList.sh $n $tag 2 0 1 $indir
+  /home/frankma/work/HI/jet/sw/pat/patanaCMSSW_3_3_1/src/PhysicsTools/PatExamples/scripts/runFullList.sh $n $tag 1 1 0 $indir
+  /home/frankma/work/HI/jet/sw/pat/patanaCMSSW_3_3_1/src/PhysicsTools/PatExamples/scripts/runFullList.sh $n $tag 0 1 0 $indir
+done
