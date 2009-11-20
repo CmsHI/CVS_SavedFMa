62c214b5b6601cea1e49b1977384ec6add00dda3
frankma <>
2009-11-20 11:52:08 +0000

    make member data public

--- pp/dNdEta/EventSelection/macros/SelectionData.h
+++ pp/dNdEta/EventSelection/macros/SelectionData.h
@@ -4,14 +4,15 @@
 
 class SelectionData
 {
-  // trigger info
-  // size: N
-  std::vector<TString> trigName;
-  std::vector<std::vector<Double_t> > trigEff;
+  public:
+    // trigger info
+    // size: N
+    std::vector<TString> trigName;
+    std::vector<std::vector<Double_t> > trigEff;
 
-  // equations
-  // size: N choose s
-  std::vector<TMatrixD> coeff;
-  std::vector<TMatrixD> rhs;
-  std::vector<TMatrixD> sol;
+    // equations
+    // size: N choose s
+    std::vector<TMatrixD> coeff;
+    std::vector<TMatrixD> rhs;
+    std::vector<TMatrixD> sol;
 };
