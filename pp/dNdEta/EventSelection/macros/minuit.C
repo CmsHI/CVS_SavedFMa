847ff033b14ed211bd737eda262b96c6feb0fc91
frankma <>
2009-11-23 18:30:25 +0000

    some more cleaning and factor minuitFunction to the main function

--- pp/dNdEta/EventSelection/macros/minuit.C
+++ pp/dNdEta/EventSelection/macros/minuit.C
@@ -5,55 +5,33 @@
 #include "TFile.h"
 #include "TF1.h"
 #include "TCanvas.h"
+
+#include "SelectionData.h"
 using namespace std;
 
-void minuitFunction(int& nDim, double* gout, double& result, double par[], int flg){
-
-  // return chi2;
-  //result = _folder->convolute(par[0],par[1],par[2]);
-  double sd=par[0];
-  double nsd=par[1];
-  // 900 900
-  //double trigA = 0.534*sd+0.92*nsd - 0.834;
-  //double trigB = 0.761*sd+0.983*nsd - 0.933;
-  // 2.2 900
-  //double trigA = 0.551*sd+0.92*nsd - 0.834;
-  //double trigB = 0.74*sd+0.98*nsd - 0.933;
-  // 7 900
-  double trigA = 0.57*sd+0.94*nsd - 0.834;
-  double trigB = 0.717*sd+0.98*nsd - 0.933;
-  result = trigA*trigA + trigB*trigB;
-}
-
+void minuitFunction(int& nDim, double* gout, double& result, double par[], int flg);
 
 void minuit(){
+  TFitter* minimizer = new TFitter(NTYPES);
+  {
+    double p1 = -1;
+    minimizer->ExecuteCommand("SET PRINTOUT",&p1,1);
+  }
 
-   TH1::SetDefaultSumw2();
+  // Minimize convolution by Minuit
 
-   const int NTYPES=2;
-   TFitter* minimizer = new TFitter(NTYPES);
-   {
-      double p1 = -1;
-      minimizer->ExecuteCommand("SET PRINTOUT",&p1,1);
-   }
+  minimizer->SetFCN(minuitFunction);
+  minimizer->SetParameter(0,"SD",0.2,0.2,0,0); //P index, P label, initial value, expected distance, nevermind, nevermind
+  minimizer->SetParameter(1,"NSD",0.77,0.2,0,0);
 
-   //_folder = new Folder();
-   //_folder->initialize();
+  minimizer->ExecuteCommand("SIMPLEX",0,0);
+  minimizer->ExecuteCommand("MIGRAD",0,0);
 
-   // Minimize convolution by Minuit
+  double bestSD = minimizer->GetParameter(0);
+  double errorSD = minimizer->GetParError(0);
+  double bestNSD = minimizer->GetParameter(1);
+  double errorNSD = minimizer->GetParError(1);
 
-   minimizer->SetFCN(minuitFunction);
-   minimizer->SetParameter(0,"SD",0.2,0.2,0,0); //P index, P label, initial value, expected distance, nevermind, nevermind
-   minimizer->SetParameter(1,"NSD",0.77,0.2,0,0);
-
-   minimizer->ExecuteCommand("SIMPLEX",0,0);
-   minimizer->ExecuteCommand("MIGRAD",0,0);
-
-   double bestSD = minimizer->GetParameter(0);
-   double errorSD = minimizer->GetParError(0);
-   double bestNSD = minimizer->GetParameter(1);
-   double errorNSD = minimizer->GetParError(1);
-
-   cout<<"SD = "<<bestSD<< " error: " << errorSD << endl;
-   cout<<"NSD = "<<bestNSD<< " error: " << errorNSD << endl;
+  cout<<"SD = "<<bestSD<< " error: " << errorSD << endl;
+  cout<<"NSD = "<<bestNSD<< " error: " << errorNSD << endl;
 }
