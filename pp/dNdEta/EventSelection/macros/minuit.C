1cb44423b9a3e6b651823c929940ff308a4ab798
frankma <>
2009-11-24 00:52:10 +0000

    added scan uncertainty function

--- pp/dNdEta/EventSelection/macros/minuit.C
+++ pp/dNdEta/EventSelection/macros/minuit.C
@@ -1,5 +1,4 @@
 #include <iostream>
-#include "TFitter.h"
 #include "TH2D.h"
 #include "TH1D.h"
 #include "TFile.h"
@@ -11,27 +10,37 @@ using namespace std;
 
 void minuitFunction(int& nDim, double* gout, double& result, double par[], int flg);
 
-void minuit(FitData & ft){
-  TFitter* minimizer = new TFitter(NTYPES);
-  {
-    double p1 = -1;
-    minimizer->ExecuteCommand("SET PRINTOUT",&p1,1);
-  }
+void minuit(FitData & ft)
+{
+  // make it quiet
+  TFitter* minimizer_ = new TFitter(NTYPES);
+  cout << "allocated minimizer" << endl;
+  double p1 = -1;
+  minimizer_->ExecuteCommand("SET PRINTOUT",&p1,1);
 
-  // Minimize convolution by Minuit
+  // Minimize chi2 by Minuit
+  minimizer_->SetFCN(minuitFunction);
+  minimizer_->SetParameter(0,"SD",ft.bestX_,ft.searchDist_,0,0); //P index, P label, initial value, expected distance, nevermind, nevermind
+  minimizer_->SetParameter(1,"NSD",ft.bestY_,ft.searchDist_,0,0);
 
-  minimizer->SetFCN(minuitFunction);
-  minimizer->SetParameter(0,"SD",ft.bestX_,ft.searchDist_,0,0); //P index, P label, initial value, expected distance, nevermind, nevermind
-  minimizer->SetParameter(1,"NSD",ft.bestY_,ft.searchDist_,0,0);
+  minimizer_->ExecuteCommand("SIMPLEX",0,0);
+  minimizer_->ExecuteCommand("MIGRAD",0,0);
 
-  minimizer->ExecuteCommand("SIMPLEX",0,0);
-  minimizer->ExecuteCommand("MIGRAD",0,0);
-
-  ft.bestX_ = minimizer->GetParameter(0);
-  ft.errorX_ = minimizer->GetParError(0);
-  ft.bestY_ = minimizer->GetParameter(1);
-  ft.errorY_ = minimizer->GetParError(1);
+  ft.bestX_ = minimizer_->GetParameter(0);
+  ft.errorX_ = minimizer_->GetParError(0);
+  ft.bestY_ = minimizer_->GetParameter(1);
+  ft.errorY_ = minimizer_->GetParError(1);
 
   cout<<"SD = "<<ft.bestX_<< " error: " << ft.errorX_ << endl;
   cout<<"NSD = "<<ft.bestY_<< " error: " << ft.errorY_ << endl;
+}
+
+void scanUncert(FitData & ft)
+{
+  // make it quiet
+  TFitter* minimizer_ = new TFitter(NTYPES);
+  cout << "allocated minimizer" << endl;
+  double p1 = -1;
+  minimizer_->ExecuteCommand("SET PRINTOUT",&p1,1);
+
 }
