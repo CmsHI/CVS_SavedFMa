6157e75ca4c2e3fe5df2a32067925f0a9d2df8db
frankma <>
2009-11-29 20:54:42 +0000

    setup for running new openhlt with technical bits using HTLBitAnalyzer

--- pp/HLT/sw/HLTrigger/HLTanalyzers/setup.sh
+++ pp/HLT/sw/HLTrigger/HLTanalyzers/setup.sh
@@ -1,12 +1,19 @@
 cvsdir=UserCode/SavedFMa/pp/HLT/sw/HLTrigger/HLTanalyzers
 localdir=HLTrigger/HLTanalyzers
+
+# setup project area
 cd $CMSSW_BASE/src
 eval `scramv1 runtime -sh`
-cvs co -r CMSSW_3_3_4 HLTrigger/HLTanalyzers
+
+# check-out code
+cvs co HLTrigger/HLTanalyzers # head version (2009.9.29)
 cvs co $cvsdir
-cp $cvsdir/src/HLTAnalyzer.cc $localdir/src
-cp $cvsdir/interface/HLTAnalyzer.h $localdir/interface
-cp $cvsdir/python/HLTAnalyser_cfi.py $localdir/python
+
+# cp the private code into HLTrigger/HLTanalyzers
+cp $cvsdir/src/HLTAnaInfo.cc $localdir/src
+cp $cvsdir/interface/HLTAnaInfo.h $localdir/interface
+cp $cvsdir/src/HLTBitAnalyzer.cc $localdir/src
+cp $cvsdir/interface/HLTBitAnalyzer.h $localdir/interface
 cp $cvsdir/test/HLTAna.py $localdir/test
 scramv1 b
 
