08eaa31ab77b302cbe5cc9d196598f90235b8b1b
frankma <>
2009-12-07 15:47:37 +0000

    for the collision menu (https://hypernews.cern.ch/HyperNews/CMS/get/hlt/1209.html)

---
+++ pp/HLT/scripts/setup/setup_33X_HLT.sh
@@ -0,0 +1,13 @@
+# menu /online/collisions/week49/HLT/V2
+# cf - https://hypernews.cern.ch/HyperNews/CMS/get/hlt/1188/6/1/1.html
+cvs co -r V01-12-24       FastSimulation/Configuration
+cvs co -r V04-07-22-02    FastSimulation/HighLevelTrigger
+#cvs co -r V01-12-68       HLTrigger/Configuration
+
+# menu /online/collisions/week49/HLT/V14
+# cf - https://hypernews.cern.ch/HyperNews/CMS/get/hlt/1209.html
+cvs co -r V01-12-74      HLTrigger/Configuration
+cvs co -r V02-05-11      HLTrigger/HLTcore
+cvs co -r V01-05-09      HLTrigger/HLTfilters
+cvs co -r V01-07-30      HLTrigger/special
+
