7660878d4717dfec5a028e2d79e1b8591cc12e0d
frankma <>
2009-12-08 01:28:18 +0000

    add a default case: if no n option, then copy all

--- data/scripts/transfer/get_castor_from_list.sh
+++ data/scripts/transfer/get_castor_from_list.sh
@@ -1,12 +1,12 @@
 #/bin/bash -
-if [ $# -lt 2 ]; then
+if [ $# -lt 1 ]; then
   echo Usage:
-  echo "  get_castor_from_list <flist> <n>"
+  echo "  get_castor_from_list <flist> [n]"
   exit 1
 fi
 
 flist=$1
-n=$2
+if [ $2 ]; then n=$2; fi
 
 for i in `cat $flist | head -n $n`; do
   if [ ! -e ${i##/*/} ]; then
