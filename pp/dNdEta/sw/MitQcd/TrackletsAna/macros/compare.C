dbb3b4939aab5eeb5232b9a5f3d3b1cca560b6df
frankma <>
2010-01-10 09:17:13 +0000

    draw Vz also, and normalize all histograms

--- pp/dNdEta/sw/MitQcd/TrackletsAna/macros/compare.C
+++ pp/dNdEta/sw/MitQcd/TrackletsAna/macros/compare.C
@@ -14,9 +14,9 @@ using namespace std;
 
 // === Main function ===
 void compare(int evtType = 0, int doSel = 4,
-    const char * datafname="pixelTree_124022a3a4_MB_Christof_Christof_SDRelFrac1.0.root",
-    const char * mcfname="pixelTree_merge_BSC_Tuned_v1_Pythia_MinBias_D6T_900GeV_d20091210.root",
-    const char * mc2fname="pixelTree_Phojet_MinBias_900GeV_d20100104_all14_SDRelFrac1.0.root")
+    const char * datafname="PixelTree-124022-hfcuts.root",
+    const char * mcfname="pixelTree_Pythia_MinBias_D6T_900GeV_d20091229_Vertex1229.root",
+    const char * mc2fname="pixelTree_Phojet_MinBias_900GeV_d20100108.root")
 {
   // get trees
   TFile * dataFile = new TFile(datafname);
@@ -27,8 +27,8 @@ void compare(int evtType = 0, int doSel = 4,
   TTree * treeMC2; mc2File->GetObject("PixelTree",treeMC2);
 
   // trigger
-  selectionCut mcSel(1,doSel);
-  selectionCut dataSel(0,doSel);
+  selectionCut mcSel(1,doSel,124023,41,96);
+  selectionCut dataSel(0,doSel,124023,41,96);
 
   // configuation
   // sources
@@ -74,12 +74,14 @@ void compare(int evtType = 0, int doSel = 4,
     vh1.push_back(new TH1D(Form("hEaddEp_%s",source[i].Data()),";#Sigma E+Pz;",EPzNBINS,0,EPzMax));
     vh1.push_back(new TH1D(Form("hEsubEp_%s",source[i].Data()),";#Sigma E-Pz;",EPzNBINS,0,EPzMax));
     vh1.push_back(new TH2D(Form("hEPz_%s",source[i].Data()),";#Sigma E+Pz;E-Pz",EPzNBINS,0,EPzMax,EPzNBINS,0,EPzMax));
-    if (source[i]=="pythia") {
+    vh1.push_back(new TH1D(Form("hVz_%s",source[i].Data()),";vz;",100,-20,20));
+    if (source[i]=="pythia"||source[i]=="phojet") {
       for (Int_t j=0; j<etype.size(); ++j) {
 	vh1.push_back(new TH1D(Form("hEvtEta_%s_%s",source[i].Data(),etype[j].Data()),";Event #eta;",100,-5,5));
 	vh1.push_back(new TH1D(Form("hEaddEp_%s_%s",source[i].Data(),etype[j].Data()),";#Sigma E+Pz;",EPzNBINS,0,EPzMax));
 	vh1.push_back(new TH1D(Form("hEsubEp_%s_%s",source[i].Data(),etype[j].Data()),";#Sigma E-Pz;",EPzNBINS,0,EPzMax));
 	vh1.push_back(new TH2D(Form("hEPz_%s_%s",source[i].Data(),etype[j].Data()),";#Sigma E+Pz;E-Pz",EPzNBINS,0,EPzMax,EPzNBINS,0,EPzMax));
+	vh1.push_back(new TH1D(Form("hVz_%s_%s",source[i].Data(),etype[j].Data()),";vz;",100,-20,20));
       }
     }
   }
@@ -87,6 +89,12 @@ void compare(int evtType = 0, int doSel = 4,
   for (Int_t ih1=0; ih1<vh1.size(); ++ih1) {
     //cout << "hist: " << vh1[ih1]->GetName() << " title: " << vh1[ih1]->GetXaxis()->GetTitle() << endl;
     vh1[ih1]->Sumw2();
+    if (TString(vh1[ih1]->GetName()).Contains("pythia")) {
+      vh1[ih1]->SetMarkerColor(kRed);
+    }
+    if (TString(vh1[ih1]->GetName()).Contains("phojet")) {
+      vh1[ih1]->SetMarkerColor(kBlue);
+    }
   }
 
   // Fill histos
@@ -101,26 +109,29 @@ void compare(int evtType = 0, int doSel = 4,
   TCanvas * c0 = new TCanvas("c0","c0",500,500);
   TH1D * hEaddEp_pythia = (TH1D*)gDirectory->FindObject("hEaddEp_pythia");
   printf("%s: %f entries\n",hEaddEp_pythia->GetName(),hEaddEp_pythia->GetEntries());
-  hEaddEp_pythia->Scale(1./hEaddEp_pythia->GetEntries()/hEaddEp_pythia->GetBinWidth(1));
-  hEaddEp_pythia->SetMarkerColor(kRed);
-  hEaddEp_pythia->SetMarkerStyle(kOpenCircle);
-  hEaddEp_pythia->SetLineColor(kRed);
   hEaddEp_pythia->SetMinimum(0);
   hEaddEp_pythia->SetMaximum(0.006);
   hEaddEp_pythia->Draw("E");
 
   TH1D * hEaddEp_phojet = (TH1D*)gDirectory->FindObject("hEaddEp_phojet");
   printf("%s: %f entries\n",hEaddEp_phojet->GetName(),hEaddEp_phojet->GetEntries());
-  hEaddEp_phojet->Scale(1./hEaddEp_phojet->GetEntries()/hEaddEp_phojet->GetBinWidth(1));
-  hEaddEp_phojet->SetMarkerColor(kBlue);
-  hEaddEp_phojet->SetMarkerStyle(kOpenStar);
-  hEaddEp_phojet->SetLineColor(kBlue);
   hEaddEp_phojet->Draw("Esame");
 
   TH1D * hEaddEp_data = (TH1D*)gDirectory->FindObject("hEaddEp_data");
   printf("%s: %f entries\n",hEaddEp_data->GetName(),hEaddEp_data->GetEntries());
-  hEaddEp_data->Scale(1./hEaddEp_data->GetEntries()/hEaddEp_data->GetBinWidth(1));
   if (evtType==0) hEaddEp_data->Draw("Esame");
 
+
+  TCanvas * c2 = new TCanvas("c2","c2",500,500);
+  treeMC->Draw("vz[1]>>hVz_pythia",mcSel.Cut&&etypeCut[evtType],"");
+  treeMC2->Draw("vz[1]>>hVz_phojet",mcSel.Cut&&TCut(etypePhojCut[evtType]),"same");
+  treeData->Draw("vz[1]>>hVz_data",dataSel.Cut,"same");
+
+  //scale
+  for (Int_t ih1=0; ih1<vh1.size(); ++ih1) {
+    vh1[ih1]->Scale(1./vh1[ih1]->GetEntries()/vh1[ih1]->GetBinWidth(1));
+  }
+  c0->Update();
+  c2->Update();
   c0->Print("plots/compare_hEaddEp.gif");
 }
