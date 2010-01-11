809797476faa503d885d6b11943175b00fb06b57
frankma <>
2010-01-11 17:33:48 +0000

    don't erase log

--- pp/dNdEta/sw/MitQcd/TrackletsAna/macros/runFit.sh
+++ pp/dNdEta/sw/MitQcd/TrackletsAna/macros/runFit.sh
@@ -9,9 +9,10 @@ tag=$1
 log=log/fit_$tag.log
 version=V1
 
+echo start > $log
 for DataSource in data pythia phojet; do
   for MCSource in pythia phojet; do
-    root -b -q matchFrac.C+\(\"$version\",\"$DataSource\",\"$MCSource\",4,0,0,200,5\) > $log
+    root -b -q matchFrac.C+\(\"$version\",\"$DataSource\",\"$MCSource\",4,0,0,200,5\) >> $log
     root -b -q matchFrac.C+\(\"$version\",\"$DataSource\",\"$MCSource\",4,0,18,200,5\) >> $log
     root -b -q matchFrac.C+\(\"$version\",\"$DataSource\",\"$MCSource\",4,0,0,50,5\) >> $log
   done
