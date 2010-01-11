e9ad052476f060a937c7bed883399044fcc1496f
frankma <>
2010-01-11 22:01:06 +0000

    better output name. Make fit obs an input var

--- pp/dNdEta/sw/MitQcd/TrackletsAna/macros/runFit.sh
+++ pp/dNdEta/sw/MitQcd/TrackletsAna/macros/runFit.sh
@@ -1,21 +1,31 @@
 #!/bin/bash -
 if [ $# -eq 0 ]; then
   echo Usage
-  echo "  $0 <tag>"
+  echo "  $0 <Version> [tag]"
   exit 1
 fi
 
-tag=$1
-log=log/fit_$tag.log
-version=V1
-#doSel=4
-doSel=1
+version=$1
+tag=$2
+obs="EvtEta" # EaddEp, EsubEp, EvtEta
+doSel=1 # 1, 4
+anaMode=0
+log=log/ana${version}_${obs}_${anaMode}_Sel${doSel}_$tag.log
 
 echo start > $log
+
 for DataSource in data pythia phojet; do
   for MCSource in pythia phojet; do
-    root -b -q matchFrac.C+\(\"$version\",\"$DataSource\",\"$MCSource\",$doSel,0,0,200,5\) >> $log
-    root -b -q matchFrac.C+\(\"$version\",\"$DataSource\",\"$MCSource\",$doSel,0,18,200,5\) >> $log
-    root -b -q matchFrac.C+\(\"$version\",\"$DataSource\",\"$MCSource\",$doSel,0,0,50,5\) >> $log
+    # case EaddEp
+    if [ $obs == "EaddEp" ]; then
+      for EPzMax in 200 10 50; do
+	root -b -q matchFrac.C+\(\"$version\",\"$DataSource\",\"$MCSource\",\"$obs\",$doSel,$anaMode,0,$EPzMax,5\) >> $log
+      done
+    fi
+    # case Evt Eta
+    if [ $obs == "EvtEta" ]; then
+      root -b -q matchFrac.C+\(\"$version\",\"$DataSource\",\"$MCSource\",\"$obs\",$doSel,$anaMode,0,200,5\) >> $log
+    fi
   done
 done
+
