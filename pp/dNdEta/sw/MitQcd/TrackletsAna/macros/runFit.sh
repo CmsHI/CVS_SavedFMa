733324a61bec666792f936b03cbda27600f3c2f3
frankma <>
2010-01-11 20:21:14 +0000

    make selection a variable

--- pp/dNdEta/sw/MitQcd/TrackletsAna/macros/runFit.sh
+++ pp/dNdEta/sw/MitQcd/TrackletsAna/macros/runFit.sh
@@ -8,12 +8,14 @@ fi
 tag=$1
 log=log/fit_$tag.log
 version=V1
+#doSel=4
+doSel=1
 
 echo start > $log
 for DataSource in data pythia phojet; do
   for MCSource in pythia phojet; do
-    root -b -q matchFrac.C+\(\"$version\",\"$DataSource\",\"$MCSource\",4,0,0,200,5\) >> $log
-    root -b -q matchFrac.C+\(\"$version\",\"$DataSource\",\"$MCSource\",4,0,18,200,5\) >> $log
-    root -b -q matchFrac.C+\(\"$version\",\"$DataSource\",\"$MCSource\",4,0,0,50,5\) >> $log
+    root -b -q matchFrac.C+\(\"$version\",\"$DataSource\",\"$MCSource\",$doSel,0,0,200,5\) >> $log
+    root -b -q matchFrac.C+\(\"$version\",\"$DataSource\",\"$MCSource\",$doSel,0,18,200,5\) >> $log
+    root -b -q matchFrac.C+\(\"$version\",\"$DataSource\",\"$MCSource\",$doSel,0,0,50,5\) >> $log
   done
 done
