d953e147a60cd3f66e8d31e96a14f7d7e13adaa2
frankma <>
2010-01-11 22:10:17 +0000

    loop on obs

--- pp/dNdEta/sw/MitQcd/TrackletsAna/macros/runFit.sh
+++ pp/dNdEta/sw/MitQcd/TrackletsAna/macros/runFit.sh
@@ -7,25 +7,28 @@ fi
 
 version=$1
 tag=$2
-obs="EvtEta" # EaddEp, EsubEp, EvtEta
 doSel=1 # 1, 4
 anaMode=0
-log=log/ana${version}_${obs}_${anaMode}_Sel${doSel}_$tag.log
 
-echo start > $log
 
-for DataSource in data pythia phojet; do
-  for MCSource in pythia phojet; do
-    # case EaddEp
-    if [ $obs == "EaddEp" ]; then
-      for EPzMax in 200 10 50; do
-	root -b -q matchFrac.C+\(\"$version\",\"$DataSource\",\"$MCSource\",\"$obs\",$doSel,$anaMode,0,$EPzMax,5\) >> $log
-      done
-    fi
-    # case Evt Eta
-    if [ $obs == "EvtEta" ]; then
-      root -b -q matchFrac.C+\(\"$version\",\"$DataSource\",\"$MCSource\",\"$obs\",$doSel,$anaMode,0,200,5\) >> $log
-    fi
+#for obs in EaddEp; do
+#for obs in EaddEp EvtEta EsubEp; do
+for obs in EaddEp EsubEp; do
+  log=log/ana${version}_${obs}_Mode${anaMode}_Sel${doSel}_$tag.log
+  echo start > $log
+  for DataSource in data pythia phojet; do
+    for MCSource in pythia phojet; do
+      # case EaddEp
+      if [ $obs == "EaddEp" ]; then
+	for EPzMax in 200 10 50; do
+	  root -b -q matchFrac.C+\(\"$version\",\"$DataSource\",\"$MCSource\",\"$obs\",$doSel,$anaMode,0,$EPzMax,5\) >> $log
+	done
+      fi
+      # case Evt Eta
+      if [ $obs == "EvtEta" ]; then
+	root -b -q matchFrac.C+\(\"$version\",\"$DataSource\",\"$MCSource\",\"$obs\",$doSel,$anaMode,0,200,5\) >> $log
+      fi
+    done
   done
 done
 
