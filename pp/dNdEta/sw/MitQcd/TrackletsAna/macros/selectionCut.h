f172385b497a1e39df27de7d67fd30e940336b75
frankma <>
2010-01-14 16:09:33 +0000

    for HF coincidence selection put in bit 34, difference in # events passed is ~0 for phojet, 0.5% for pythia

--- pp/dNdEta/sw/MitQcd/TrackletsAna/macros/selectionCut.h
+++ pp/dNdEta/sw/MitQcd/TrackletsAna/macros/selectionCut.h
@@ -52,7 +52,7 @@ selectionCut::selectionCut(bool isMC, int sel, int runnum, int nLumiL, int nLumi
      evtSelection      = ("L1T[41]>0&&L1T[36]!=1&&L1T[37]!=1&&L1T[38]!=1&&L1T[39]!=1");
    // halo veto + Hf tower count
    else if (selType==4)
-     evtSelection      = ("nHFp>0&&nHFn>0&&L1T[36]!=1&&L1T[37]!=1&&L1T[38]!=1&&L1T[39]!=1");
+     evtSelection      = ("L1T[34]&&nHFp>0&&nHFn>0&&L1T[36]!=1&&L1T[37]!=1&&L1T[38]!=1&&L1T[39]!=1");
    // Diffractive only
    else if (selType==10)
      evtSelection      = ("L1T[34]>0&&!(nHFp>0&&nHFn>0)&&L1T[36]!=1&&L1T[37]!=1&&L1T[38]!=1&&L1T[39]!=1");
