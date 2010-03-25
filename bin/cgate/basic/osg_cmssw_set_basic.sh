9f617da1f5bc5295ac110ad0d893aa3afd41a2c4
frankma <>
2010-03-25 13:22:20 +0000

    update to slc5

--- bin/cgate/basic/osg_cmssw_set_basic.sh
+++ bin/cgate/basic/osg_cmssw_set_basic.sh
@@ -1,7 +1,9 @@
 #!/bin/sh -
 
 #-- SL4 --
-export SCRAM_ARCH=slc4_ia32_gcc345
+#export SCRAM_ARCH=slc4_ia32_gcc345
+#-- SL5 --
+export SCRAM_ARCH=slc5_ia32_gcc434
 source /osg/app/cmssoft/cms/cmsset_default.sh
 echo 'done'
 
