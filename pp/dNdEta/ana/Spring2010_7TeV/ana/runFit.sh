486bac9ae035d161bc43d4b37a1af95555ee811b
frankma <>
2010-04-14 13:12:07 +0000

    update to the new fit macro which does the factorized fitting

--- pp/dNdEta/ana/Spring2010_7TeV/ana/runFit.sh
+++ pp/dNdEta/ana/Spring2010_7TeV/ana/runFit.sh
@@ -9,12 +9,10 @@ fi
 version=$1
 
 dataFile=../pixel_trees/collbx/pixelTree_run132440_PromptReco-v7_veryloosecuts_v4.root
-#pythiaFile=../pixel_trees/mc/pixelTree_pythiaD6t_MB7TeV_356ReRecov1_1M.root
-pythiaFile=../pixel_trees/mc/pixelTree_yilmaz-MinBiasATLAS_RECO_0332_v1.root
-phojetFile=../pixel_trees/mc/
+pythiaD6tFile=../pixel_trees/mc/pixelTree_pythiaD6t_MB7TeV_356ReRecov1_1M.root
+pythiaAtlasFile=../pixel_trees/mc/pixelTree_yilmaz-MinBiasATLAS_RECO_0332_v1.root
+phojetFile=../pixel_trees/mc/pixelTree_yilmaz-MinBiasPhojet_RECO_0413_v1.root
 
-#tag=data_pythiaD6t
-tag=data_pythiaAtlas
 anaMode=0
 
 # !! check again above to match tag with input file !!
@@ -22,34 +20,27 @@ anaMode=0
 
 # === run ===
 for doSel in 1; do #1 4 10; do
-#for doSel in 1 4 10; do #1 4 10; do
-  #for obs in EvtEta EsubEp EaddEpPos EsubEpNeg; do
-  #for obs in EvtEta EaddEpPos MinEPz EsubEpNeg; do
-  for obs in EsubEpNeg; do
-  #for obs in EaddEpPos; do
-    log=log/ana${version}_${obs}_Mode${anaMode}_Sel${doSel}_$tag.log
-    echo "$@" > $log
-    for DataSource in data pythia; do
-      if [ $DataSource == data ]; then dataF=$dataFile; fi
-      if [ $DataSource == pythia ]; then dataF=$pythiaFile; fi
-      for MCSource in pythia; do
-	if [ $MCSource == pythia ]; then mcF=$pythiaFile; fi
-	# case EaddEp
-	if [ $obs != "EvtEta" ]; then
-	  for EPzMax in 100 200 300; do
-	  #for EPzMax in 125 150 250; do
-	  #for EPzMax in 200; do
-	    root -b -q matchFrac.C+\(\"$version\",\"$DataSource\",\"$dataF\",\"$MCSource\",\"$mcF\",\"$obs\",$doSel,$anaMode,0,$EPzMax,5\) >> $log
-	    #echo $cmd >> $log
-	    #eval $cmd
-	  done
-	fi
-	# case Evt Eta
-	if [ $obs == "EvtEta" ]; then
-	  root -b -q matchFrac.C+\(\"$version\",\"$DataSource\",\"$dataF\",\"$MCSource\",\"$mcF\",\"$obs\",$doSel,$anaMode,0,200,5\) >> $log
-	  #echo $cmd >> $log
-	  #eval $cmd
-	fi
+  for DataSource in data pythiaAtlas phojet pythiaD6T; do
+    if [ $DataSource == data ]; then dataF=$dataFile; fi
+    if [ $DataSource == pythiaD6T ]; then dataF=$pythiaD6tFile; fi
+    if [ $DataSource == pythiaAtlas ]; then dataF=$pythiaAtlasFile; fi
+    if [ $DataSource == phojet ]; then dataF=$phojetFile; fi
+    for MCSource in pythiaAtlas phojet pythiaD6T; do
+      if [ $MCSource == data ]; then mcF=$dataFile; fi
+      if [ $MCSource == pythiaD6T ]; then mcF=$pythiaD6tFile; fi
+      if [ $MCSource == pythiaAtlas ]; then mcF=$pythiaAtlasFile; fi
+      if [ $MCSource == phojet ]; then mcF=$phojetFile; fi
+      # output log
+      tag=${DataSource}_${MCSource}
+      log=log/ana${version}_Mode${anaMode}_Sel${doSel}-${tag}_fit.log
+      echo "input pars: $version $DataSource $dataF $MCSource $mcF $doSel $anaMode" > $log
+      # Different obs
+      root -b -q fit_shapes.C+\(\"$version\",\"$DataSource\",\"$dataF\",\"$MCSource\",\"$mcF\",\"EvtEta\",$doSel,$anaMode,0,200,5\) >> $log
+      for obs in MinEPz EaddEpPos EsubEpNeg; do
+	# different EPz ranges
+	for EPzMax in 100 200 300; do
+	  root -b -q fit_shapes.C+\(\"$version\",\"$DataSource\",\"$dataF\",\"$MCSource\",\"$mcF\",\"$obs\",$doSel,$anaMode,0,$EPzMax,5\) >> $log
+	done
       done
     done
   done
