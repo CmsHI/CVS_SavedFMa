5b135ece489c8f9cd4ca2af34632ff4d5140c567
frankma <>
2010-04-15 19:14:19 +0000

    add normalization to unity implemented bin by bin

--- analysis/root/macros/HistMath.C
+++ analysis/root/macros/HistMath.C
@@ -23,4 +23,25 @@ TH1D * integrateHist(TH1 * hin)
    return hout;
 }
 
+void normHist(TH1 * hin, Int_t type=1)
+{
+  hin->Sumw2();
+  // Normalize by area
+  if (type<10) {
+    hin->Scale(1./hin->Integral());
+  }
+
+  if (type==1) {
+    for (Int_t i=1; i<=hin->GetNbinsX(); ++i) {
+      Double_t w = hin->GetBinWidth(i);
+      Double_t h = hin->GetBinContent(i);
+      Double_t e = hin->GetBinError(i);
+      cout << "bin " << i << ": " << "width: " << w
+	<< " height: " << h << " error: " << e << endl;
+      cout << "    new height: " << h/w << " new error: " << e/w << endl;
+      hin->SetBinContent(i,h/w);
+      hin->SetBinError(i,e/w);
+    }
+  }
+}
 #endif // HISTMATH_H
