dcf9949e8918f39e16a81fbf3fb1a409e9f3743c
frankma <>
2010-04-21 09:52:12 +0000

    add priavte pad margin and hist label offsets

--- pp/dNdEta/ana/Spring2010_7TeV/ana/rootlogon.C
+++ pp/dNdEta/ana/Spring2010_7TeV/ana/rootlogon.C
@@ -2,4 +2,8 @@
 {
   // styles
   gROOT->Macro("$SavedFMa/analysis/root/logon/rootlogon_dNdEtaBase.C");
+  //gStyle->SetTitleXOffset(1.35);
+  gStyle->SetTitleYOffset(1.5);
+  gStyle->SetPadRightMargin(0.05);
+  gStyle->SetPadBottomMargin(0.17);
 }
