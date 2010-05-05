1801964c15756481bccfb7e2a790e883d20e89db
frankma <>
2010-05-05 14:03:46 +0000

    add a mode parameter, in case just want to make list

--- data/scripts/transfer/new_castor_list.sh
+++ data/scripts/transfer/new_castor_list.sh
@@ -1,33 +1,49 @@
 #!/bin/bash -
 if [ $# -lt 2 ]; then
    echo "Usage"
-   echo "  new_castor_list.sh <castor_dir> <flistname> [tag]"
+   echo "  new_castor_list.sh <castor_dir> <flistname> [n_files_to_get] [tag] [mode]"
+   echo "    mode:"
+   echo "    0 - create list don't copy"
+   echo "    1 - create list and copy (default)"
    exit 1
 fi
 
 # set var's
 castordir=$1
 flist=$2
+nfiles=1000000
 tag=root
+mode=1
 
-if [ $# -gt 2 ]; then
-  tag=$3
+if [ $# -ge 3 ]; then
+  nfiles=$3
+fi
+if [ $# -ge 4 ]; then
+  tag=$4
+  echo tag: $tag
+fi
+if [ $# -ge 5 ]; then
+  mode=$5
+  echo mode: $mode
 fi
 
 # check
 if [ -e $flist ]; then
-  echo "$flist already exists. Will no create new one"
+  echo "$flist already exists. Will not create new one"
   exit 1
 fi
 
 # run
 touch $flist
-files=`rfdir $castordir | grep $tag | awk '{print $NF}'`
+files=`rfdir $castordir | grep $tag | head -n $nfiles | awk '{print $NF}'`
 for i in $files; do
    echo "$castordir/$i" >> $flist
 done
 echo $flist created
 
+# check mode
+if [ $mode -eq 0 ]; then exit; fi
+
 # get
 echo Will begin to copy
-get_castor_from_list.sh $flist >& ${flist%.*}.log &
+#get_castor_from_list.sh $flist >& ${flist%.*}.log &
