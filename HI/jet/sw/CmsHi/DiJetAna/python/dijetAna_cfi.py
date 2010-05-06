b09728503f381ad4b6c50346158d73be4699476b
frankma <>
2010-05-06 14:02:49 +0000

    fix trk collection label

--- HI/jet/sw/CmsHi/DiJetAna/python/dijetAna_cfi.py
+++ HI/jet/sw/CmsHi/DiJetAna/python/dijetAna_cfi.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 dijetAna = cms.EDAnalyzer('DiJetAna',
     vtxsrc = cms.untracked.InputTag("hiSelectedVertex"),
     jetsrc = cms.untracked.InputTag("akPu5patJets"),
-    trksrc = cms.untracked.InputTag("hiSelectTracks"),
+    trksrc = cms.untracked.InputTag("hiSelectedTracks"),
     isMC = cms.untracked.bool(True),
     jetEtaMax = cms.untracked.double(5.0),
     nVtxTrkCut = cms.untracked.int32(3),
