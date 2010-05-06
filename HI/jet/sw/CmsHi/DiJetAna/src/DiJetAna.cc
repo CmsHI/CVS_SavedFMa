01daf9eb88d2d9b8f7dc8e99dc90caef8294000b
frankma <>
2010-05-06 12:56:48 +0000

    add vtx

--- HI/jet/sw/CmsHi/DiJetAna/src/DiJetAna.cc
+++ HI/jet/sw/CmsHi/DiJetAna/src/DiJetAna.cc
@@ -5,15 +5,15 @@
 // 
 /**\class DiJetAna DiJetAna.cc ana/DiJetAna/src/DiJetAna.cc
 
- Description: [one line class summary]
+Description: [one line class summary]
 
- Implementation:
-     [Notes on implementation]
-*/
+Implementation:
+[Notes on implementation]
+ */
 //
 // Original Author:  Frank Ma,32 4-A06,+41227676980,
 //         Created:  Thu May  6 10:29:52 CEST 2010
-// $Id$
+// $Id: DiJetAna.cc,v 1.1 2010/05/06 09:41:23 frankma Exp $
 //
 //
 
@@ -43,27 +43,29 @@
 #include "DataFormats/HepMCCandidate/interface/GenParticle.h"
 #include "DataFormats/PatCandidates/interface/Jet.h"
 
+using namespace std;
+
 //
 // constructors and destructor
 //
 DiJetAna::DiJetAna(const edm::ParameterSet& iConfig)
 
 {
-   //now do what ever initialization is needed
-   vtxsrc_ = iConfig.getUntrackedParameter<edm::InputTag>("vtxsrc",edm::InputTag("hiSelectedVertex"));
-   jetsrc_ = iConfig.getUntrackedParameter<edm::InputTag>("jetsrc",edm::InputTag("akPu5patJets"));
-   trksrc_ = iConfig.getUntrackedParameter<edm::InputTag>("vtxsrc",edm::InputTag("hiSelectTracks"));
-   isMC_ = iConfig.getUntrackedParameter<bool>("isMC", true);
-   jetEtaMax_ = iConfig.getUntrackedParameter<double>("jetEtaMax", 2.0);
-   nVtxTrkCut_ = iConfig.getUntrackedParameter<int>("nVtxTrkCut", 3);
+  //now do what ever initialization is needed
+  vtxsrc_ = iConfig.getUntrackedParameter<edm::InputTag>("vtxsrc",edm::InputTag("hiSelectedVertex"));
+  jetsrc_ = iConfig.getUntrackedParameter<edm::InputTag>("jetsrc",edm::InputTag("akPu5patJets"));
+  trksrc_ = iConfig.getUntrackedParameter<edm::InputTag>("vtxsrc",edm::InputTag("hiSelectTracks"));
+  isMC_ = iConfig.getUntrackedParameter<bool>("isMC", true);
+  jetEtaMax_ = iConfig.getUntrackedParameter<double>("jetEtaMax", 2.0);
+  nVtxTrkCut_ = iConfig.getUntrackedParameter<int>("nVtxTrkCut", 3);
 }
 
 
 DiJetAna::~DiJetAna()
 {
- 
-   // do anything here that needs to be done at desctruction time
-   // (e.g. close files, deallocate resources etc.)
+
+  // do anything here that needs to be done at desctruction time
+  // (e.g. close files, deallocate resources etc.)
 
 }
 
@@ -73,29 +75,67 @@ DiJetAna::~DiJetAna()
 //
 
 // ------------ method called to for each event  ------------
-void
+  void
 DiJetAna::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
-   using namespace edm;
+  using namespace edm;
+  using namespace std;
+  using namespace reco;
+  const int nTrigs = 5;
 
+  const string qualityString = "highPurity";
 
+  //-----------------------  Preselection (This part will be in an EDFilter later)  
+  // get vtx collection 
+  Handle<vector<Vertex> > vertices;
+  iEvent.getByLabel(vtxsrc_, vertices);
+  Int_t numVtx = (Int_t)vertices->size();
+  hNumVtx_->Fill(numVtx);
+  if(numVtx<1) return; // at least one vtx
 
-#ifdef THIS_IS_AN_EVENT_EXAMPLE
-   Handle<ExampleData> pIn;
-   iEvent.getByLabel("example",pIn);
-#endif
-   
-#ifdef THIS_IS_AN_EVENTSETUP_EXAMPLE
-   ESHandle<SetupData> pSetup;
-   iSetup.get<SetupRecord>().get(pSetup);
-#endif
+  Int_t numFake=0, maxtracks=0;
+  double bestndof=-999.9,bestvz=-999.9, bestvx=-999.9, bestvy=-999.9, bestNchi2=999.9;
+  for(UInt_t it=0; it<vertices->size(); ++it) {
+    const reco::Vertex & vtx = (*vertices)[it];
+    // check if valid vertex
+    if(vtx.isFake()) {
+      ++numFake;
+      continue;
+    }
+    // update best vertex
+    if(numVtx > maxtracks || (numVtx == maxtracks && vtx.normalizedChi2() < bestNchi2) ) {
+      maxtracks = vtx.tracksSize();
+      bestvz = vtx.z(); bestvx = vtx.x(); bestvy = vtx.y();
+      bestNchi2 = vtx.normalizedChi2();
+      bestndof = vtx.ndof();
+    } 
+  }
+  hVtxNumTrksPreSel_->Fill(maxtracks);
+  if(maxtracks<nVtxTrkCut_) return; // vtx quality selection
+  hVtxNumTrksEvtSel_->Fill(maxtracks);
+  hVtxZEvtSel_->Fill(bestvz);
 }
 
 
 // ------------ method called once each job just before starting event loop  ------------
-void 
+  void 
 DiJetAna::beginJob()
 {
+  // histos
+  hNumVtx_ = fs->make<TH1D>("hNumVtx","; # vtx;",20,0,20);
+  hVtxZEvtSel_ = fs->make<TH1D>("hVtxZEvtSel","z position of best reconstructed hi selected vertex;vz [cm]", 80,-20,20);
+  hVtxNumTrksPreSel_ = fs->make<TH1D>("hVtxNumTrksPreSel",";# trks in best vtx (pre evt sel);",100,0,100);
+  hVtxNumTrksEvtSel_ = fs->make<TH1D>("hVtxNumTrksEvtSel",";# trks in best vtx;",100,0,100);
+  hJetPtPreSel_ = fs->make<TH1D>("hJetPtPreSel",";p_{T}^{trk} [GeV/c]", 200, 0.0, 200.0);
+  hTrkPtPreSel_ = fs->make<TH1D>("hTrkPtPreSel",";p_{T}^{corr. jet} [GeV/c]", 200, 0.0, 200.0);
+  // trees
+  calojTree_ = fs->make<TTree>("calojTree","data: calo dijet tree");
+  if ( isMC_ ) {
+    calojGenjTree_ = fs->make<TTree>("calojGenjTree","mc: calo dijet tree with genjet ref");
+    calojPtnjTree_ = fs->make<TTree>("calojPtnjTree","mc: calo dijet tree with parton ref");
+    genjCalojTree_ = fs->make<TTree>("genjCalojTree","mc: genjet dijet tree with calojet ref");
+    ptnjCalojTree_ = fs->make<TTree>("ptnjCalojTree","mc: parton dijet tree with calojet ref");
+  }
 }
 
 // ------------ method called once each job just after ending the event loop  ------------
