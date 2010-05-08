e27684c6da73508874c70715963c649565cb6624
frankma <>
2010-05-08 17:52:02 +0000

    add genjet/genp anas

--- HI/jet/sw/CmsHi/DiJetAna/python/dijetAna_cfi.py
+++ HI/jet/sw/CmsHi/DiJetAna/python/dijetAna_cfi.py
@@ -11,14 +11,27 @@ dijetAna_mc = cms.EDAnalyzer('DiJetAna',
     doJEC = cms.untracked.int32(3),
     nearJetPtMin = cms.untracked.double(40.0),
     awayJetPtMin = cms.untracked.double(40.0),
+    trkPtMin = cms.untracked.double(0.3),
     anaJetType = cms.untracked.int32(2),
     refJetType = cms.untracked.int32(1),
     anaTrkType = cms.untracked.int32(2)
     )
 
-genDijetAna_mc = dijetAna_mc.clone(
+dijetAna_mc_genjet_trk = dijetAna_mc.clone(
     jetsrc = cms.untracked.InputTag("ak5HiGenJets"),
     anaJetType = cms.untracked.int32(1)
+    )
+
+dijetAna_mc_calojet_genp = dijetAna_mc.clone(
+    trksrc = cms.untracked.InputTag("hiGenParticles"),
+    anaTrkType = cms.untracked.int32(0)
+    )
+
+dijetAna_mc_genjet_genp = dijetAna_mc.clone(
+    jetsrc = cms.untracked.InputTag("ak5HiGenJets"),
+    anaJetType = cms.untracked.int32(1),
+    trksrc = cms.untracked.InputTag("hiGenParticles"),
+    anaTrkType = cms.untracked.int32(0)
     )
 
 dijetAna_data = dijetAna_mc.clone(
@@ -32,5 +45,7 @@ dijetAna_data_seq = cms.Sequence(
 
 dijetAna_mc_seq = cms.Sequence(
     dijetAna_mc *
-    genDijetAna_mc
+    #dijetAna_mc_genjet_trk *
+    dijetAna_mc_calojet_genp *
+    dijetAna_mc_genjet_genp
     )
