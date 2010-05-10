df48035dd8b86bc1885b3a4581ff8e78e0c826cd
frankma <>
2010-05-10 15:58:00 +0000

    add centrality sw for 36X

--- HI/jet/ana/Mc2760GeV/scripts/setup360.sh
+++ HI/jet/ana/Mc2760GeV/scripts/setup360.sh
@@ -8,3 +8,9 @@ addpkg RecoHI/HiJetAlgos
 # skiming code
 cvs co -d ana/Mc2760GeV UserCode/SavedFMa/HI/jet/ana/Mc2760GeV
 
+# For centrality see
+# - https://twiki.cern.ch/twiki/bin/view/CMS/SWGuideHeavyIonCentrality#Using_centrality_in_analysis
+cvs co -r V00-01-04 RecoHI/HiCentralityAlgos
+cvs co -r V00-01-09 DataFormats/HeavyIonEvent
+cvs co -r V00-00-03 CondFormats/HIObjects
+cvs co -r V00-00-00 CondCore/HIPlugins
