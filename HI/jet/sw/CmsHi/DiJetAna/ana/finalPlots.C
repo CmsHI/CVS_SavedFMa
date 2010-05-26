0a58b2fbbeeda6d66ec44ec1d2fbb9d87835c201
frankma <>
2010-05-26 15:40:37 +0000

    temp fix for long path name problem by using concise names. Segmentation Violation occurs if output dir is greater than 50char

--- HI/jet/sw/CmsHi/DiJetAna/ana/finalPlots.C
+++ HI/jet/sw/CmsHi/DiJetAna/ana/finalPlots.C
@@ -13,10 +13,11 @@
 #include <cassert>
 using namespace std;
 
-void finalPlots(const char * inFileName="plots/dijetAna_anaJet_Mc1_try26_10k/Sel1_Nr_80_120_Aw_70_DPhi_2.6/ffana_hists.root")
+void finalPlots(TString inDir="plots/dj_try26_10k/Sel1_N80to120_A70_DPhi26")
 {
+  TString inFileName=inDir+"/ffana_hists.root";
   TFile * inFile = new TFile(inFileName);
-  CPlot::sOutDir = TString("plots/dijetAna_anaJet_Mc1_try26_10k/ana0525");
+  CPlot::sOutDir = inDir+"/ff";
   inFile->ls();
 
   // plot
@@ -28,13 +29,13 @@ void finalPlots(const char * inFileName="plots/dijetAna_anaJet_Mc1_try26_10k/Sel
   cpFFAll.AddHist1D(inFile,"hXiSig_Near_mcj2t0","HI: calojet+genp","E",kBlue,1,kOpenCircle);
   cpFFAll.AddHist1D(inFile,"hXiSig_Near_mcj2t0MatOrder","HI: calojet(MatGen)+genp","E",kBlue,1,kOpenStar);
   cpFFAll.AddHist1D(inFile,"hXiSig_Near_mcGenMatOrder","HI: genjet(MatCalo)+genp","E",kGreen+2,1,kOpenStar);
-  cpFFAll.Draw(cFFAll,true,"gif");
+  cpFFAll.Draw(cFFAll,true,"png");
 
   TCanvas * cFFMatAll = new TCanvas("cFFMatAll","cFFMatAll",500,500);
-  CPlot cpFFMatAll("FFMatAll","FF","#xi=ln(E_{T}^{Jet}/p_{T}^{trk})","#frac{1}{N_{jet}} #frac{dN}{d#xi}");
+  CPlot cpFFMatAll("FFMat","FF","#xi=ln(E_{T}^{Jet}/p_{T}^{trk})","#frac{1}{N_{jet}} #frac{dN}{d#xi}");
   cpFFMatAll.SetYRange(0,6.);
   cpFFMatAll.AddHist1D(inFile,"hXiSig_Near_mcGenTruthMat","Signal: genjet(Matched)+genp","histE",kRed,1,0);
   cpFFMatAll.AddHist1D(inFile,"hXiSig_Near_mcGenMat","HI: genjet(matched)+genp","E",kGreen+2,1,kOpenSquare);
   cpFFMatAll.AddHist1D(inFile,"hXiSig_Near_mcj2t0Mat","HI: calojet(matched)+genp","E",kBlue,1,kOpenCircle);
-  cpFFMatAll.Draw(cFFMatAll,true,"gif");
+  cpFFMatAll.Draw(cFFMatAll,true,"png");
 }
