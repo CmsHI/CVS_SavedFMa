8d58c48d42da4b1e3f9957323cb15a5b4d8e9300
frankma <>
2010-05-26 12:46:09 +0000

    add genjet ordered calojet FF and calojet ordered genjet FF

--- HI/jet/sw/CmsHi/DiJetAna/ana/anaDiJetFF.C
+++ HI/jet/sw/CmsHi/DiJetAna/ana/anaDiJetFF.C
@@ -14,8 +14,8 @@
 using namespace std;
 
 void anaDiJetFF(int doMC=1,
-    const char * inFile0Name="../process_aod/outputs/dijetAna_anaJet_Mc1_try26_2k.root",
-    TString outdir = "plots/dijetAna_anaJet_Mc1_try26_2k",
+    const char * inFile0Name="../process_aod/outputs/dijetAna_anaJet_Mc1_try26_10k.root",
+    TString outdir = "plots/dijetAna_anaJet_Mc1_try26_10k",
     TString title1="MC Calojet (Hyd2.76TeV+dijet)",
     TString title2="MC Genjet (Hyd2.76TeV+dijet)")
 {
@@ -129,6 +129,10 @@ void anaDiJetFF(int doMC=1,
   AnaFrag mcj2t0Aw("mcj2t0","Away",mcj2t0,mcAna.DJ,mcAna.DJTrk,"log(1./za)","padr<0.5","padrbg<0.5");
   AnaFrag mcj2t0NrMat("mcj2t0Mat","Near",mcj2t0,mcMatAna.DJ,mcMatAna.DJTrk,"log(1./zn)","pndr<0.5","pndrbg<0.5");
   AnaFrag mcj2t0AwMat("mcj2t0Mat","Away",mcj2t0,mcMatAna.DJ,mcMatAna.DJTrk,"log(1./za)","padr<0.5","padrbg<0.5");
+  AnaFrag mcj2t0NrMatOrder("mcj2t0MatOrder","Near",mcj2t0,mcMatAna.DJ,mcMatAna.DJTrk,
+      "log(1./zn)*(nlrjet>alrjet)+log(1./za)*(nlrjet<alrjet)",
+      "(pndr<0.5 && nlrjet>alrjet)||(padr<0.5 && nlrjet<alrjet)",
+      "(pndrbg<0.5 && nlrjet>alrjet)||(padrbg<0.5 && nlrjet<alrjet)");
 
   // gen jet smear + genp
   AnaFrag mcGenJetSmearNr("mcj1Smear","Near",mcj1t0,mcAna.DJ,mcAna.DJTrk,"log(nljet*njec[10]/ppt)","pndr<0.5","pndrbg<0.5");
@@ -139,11 +143,18 @@ void anaDiJetFF(int doMC=1,
   AnaFrag mcGenAw("mcGen","Away",mcj1t0,mcAna.DJ,mcAna.DJTrk,"log(1/za)","padr<0.5","padrbg<0.5");
   AnaFrag mcGenNrMat("mcGenMat","Near",mcj2t0,mcMatAna.DJ,mcMatAna.DJTrk,"log(nlrjet/ppt)","pndr<0.5","pndrbg<0.5");
   AnaFrag mcGenAwMat("mcGenMat","Away",mcj2t0,mcMatAna.DJ,mcMatAna.DJTrk,"log(alrjet/ppt)","padr<0.5","padrbg<0.5");
+  AnaFrag mcGenNrMatOrder("mcGenMatOrder","Near",mcj1t0,mcMatAna.DJ,mcMatAna.DJTrk,
+      "log(1./zn)*(nlrjet>alrjet)+log(1./za)*(nlrjet<alrjet)",
+      "(pndr<0.5 && nlrjet>alrjet)||(padr<0.5 && nlrjet<alrjet)",
+      "(pndrbg<0.5 && nlrjet>alrjet)||(padrbg<0.5 && nlrjet<alrjet)");
 
   // Gen-Truth
   TCut djTrkTruthCut = mcAna.DJTrk && "psube==0";
+  TCut djTrkTruthMat = mcMatAna.DJTrk && "psube==0";
   AnaFrag mcGenTruthNr("mcGenTruth","Near",mcj1t0,mcAna.DJ,djTrkTruthCut,"log(1/zn)","pndr<0.5","pndrbg<0.5");
   AnaFrag mcGenTruthAw("mcGenTruth","Away",mcj1t0,mcAna.DJ,djTrkTruthCut,"log(1/za)","padr<0.5","padrbg<0.5");
+  AnaFrag mcGenTruthNrMat("mcGenTruthMat","Near",mcj2t0,mcMatAna.DJ,djTrkTruthMat,"log(nlrjet/ppt)","pndr<0.5","pndrbg<0.5");
+  AnaFrag mcGenTruthAwMat("mcGenTruthMat","Away",mcj2t0,mcMatAna.DJ,djTrkTruthMat,"log(alrjet/ppt)","padr<0.5","padrbg<0.5");
 
   // All done, save and exit
   outf->Write();
--- HI/jet/sw/CmsHi/DiJetAna/ana/finalPlots.C
+++ HI/jet/sw/CmsHi/DiJetAna/ana/finalPlots.C
@@ -13,10 +13,10 @@
 #include <cassert>
 using namespace std;
 
-void finalPlots(const char * inFileName="plots/dijetAna_anaJet_Mc1_try26_2k/Sel1_Nr_80_120_Aw_70_DPhi_2.6/ffana_hists.root")
+void finalPlots(const char * inFileName="plots/dijetAna_anaJet_Mc1_try26_10k/Sel1_Nr_80_120_Aw_70_DPhi_2.6/ffana_hists.root")
 {
   TFile * inFile = new TFile(inFileName);
-  CPlot::sOutDir = TString("plots/dijetAna_anaJet_Mc1_try26_2k/ana0525");
+  CPlot::sOutDir = TString("plots/dijetAna_anaJet_Mc1_try26_10k/ana0525");
   inFile->ls();
 
   // plot
@@ -26,5 +26,15 @@ void finalPlots(const char * inFileName="plots/dijetAna_anaJet_Mc1_try26_2k/Sel1
   cpFFAll.AddHist1D(inFile,"hXiSig_Near_mcGenTruth","Signal: genjet+genp","histE",kRed,1,0);
   cpFFAll.AddHist1D(inFile,"hXiSig_Near_mcGen","HI: genjet+genp","E",kGreen+2,1,kOpenSquare);
   cpFFAll.AddHist1D(inFile,"hXiSig_Near_mcj2t0","HI: calojet+genp","E",kBlue,1,kOpenCircle);
+  cpFFAll.AddHist1D(inFile,"hXiSig_Near_mcj2t0MatOrder","HI: calojet(MatGen)+genp","E",kBlue,1,kOpenStar);
+  cpFFAll.AddHist1D(inFile,"hXiSig_Near_mcGenMatOrder","HI: genjet(MatCalo)+genp","E",kGreen+2,1,kOpenStar);
   cpFFAll.Draw(cFFAll,true,"gif");
+
+  TCanvas * cFFMatAll = new TCanvas("cFFMatAll","cFFMatAll",500,500);
+  CPlot cpFFMatAll("FFMatAll","FF","#xi=ln(E_{T}^{Jet}/p_{T}^{trk})","#frac{1}{N_{jet}} #frac{dN}{d#xi}");
+  cpFFMatAll.SetYRange(0,6.);
+  cpFFMatAll.AddHist1D(inFile,"hXiSig_Near_mcGenTruthMat","Signal: genjet(Matched)+genp","histE",kRed,1,0);
+  cpFFMatAll.AddHist1D(inFile,"hXiSig_Near_mcGenMat","HI: genjet(matched)+genp","E",kGreen+2,1,kOpenSquare);
+  cpFFMatAll.AddHist1D(inFile,"hXiSig_Near_mcj2t0Mat","HI: calojet(matched)+genp","E",kBlue,1,kOpenCircle);
+  cpFFMatAll.Draw(cFFMatAll,true,"gif");
 }
