695e3ae4d7fcfdb63d3dacffe4075810cdf1cbc8
frankma <>
2010-06-09 16:18:14 +0000

    Added: Add th1f into group

--- analysis/root/macros/histogram/HisTGroup.h
+++ analysis/root/macros/histogram/HisTGroup.h
@@ -13,9 +13,9 @@ class HisTGroup
   public:
     // methods
     HisTGroup(TString gname, Int_t n=0, Float_t xmin=0, Float_t xmax=0);
-    void Add(TString iname);
     void Add(TData * t1, TString iname,Float_t sc=-1);
-    void Add(TFile * inFile, TString tname,TString iname, Float_t sc=-1);
+    void Add(TString iname);
+    void Add(TFile * inFile, TString hname,TString iname, Float_t sc=-1);
     void Save();
     TData * Sum();
     TData * Average();
@@ -46,4 +46,32 @@ HisTGroup<TData>::HisTGroup(TString name, Int_t n, Float_t xmin, Float_t xmax) :
 {
   TData::SetDefaultSumw2();
 }
+
+template <typename TData>
+void HisTGroup<TData>::Add(TData * h1, TString iname, Float_t sc)
+{
+  assert(h1);
+  scales_[iname] = sc;
+
+  if (sc<0) {
+    hm_[iname] = h1;
+  }
+
+  if (sc>0) {
+    TString schname = TString("sc")+h1->GetName();
+    TData * hsc = (TData*)h1->Clone(schname);
+    hsc->Scale(sc);
+    hm_[iname] = hsc;
+  }
+}
+
+/*
+template <typename TData>
+void HisTGroup<TData>::Add(TString iname,Float_t sc)
+{
+  TData * h1 = new TData(iname,iname,nbins_,xmin_,xmax_);
+  Add(h1,sc);
+}
+*/
+
 #endif
--- analysis/root/macros/histogram/hisTGroupDriver.C
+++ analysis/root/macros/histogram/hisTGroupDriver.C
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "HisTGroup.h"
 #include "TH1D.h"
+#include "TCanvas.h"
 using namespace std;
 
 void hisTGroupDriver()
@@ -8,4 +9,18 @@ void hisTGroupDriver()
   HisTGroup<TH1D> hgDj1("Dj1");
   cout << "group: " << hgDj1.name_ << endl;
   cout << "nbins: " << hgDj1.nbins_ << "xmin: " << hgDj1.xmin_ << " xmax: " << hgDj1.xmax_ << " " << hgDj1.hSum_ << " " << hgDj1.hAve_ << endl;
+
+  // Test add th1
+  TH1D * h1 = new TH1D("h1","h1",10,0,10);
+  h1->Fill(1);
+  h1->Fill(1);
+  h1->Fill(5);
+  hgDj1.Add(h1,"Nr");
+  hgDj1.Add(h1,"NrSmall",0.5);
+  
+  TCanvas * c2 = new TCanvas("c2","c2",500,500);
+  hgDj1.hm_["Nr"]->Draw();
+
+  TCanvas * c3 = new TCanvas("c3","c3",500,500);
+  hgDj1.hm_["NrSmall"]->Draw();
 }
