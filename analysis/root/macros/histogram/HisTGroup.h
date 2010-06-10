f529a1af59bf563a3c7e5e0b4c8c671aee07f823
frankma <>
2010-06-10 15:21:26 +0000

    prepare to add TH2D. Add a simple print function to printout member data

--- analysis/root/macros/histogram/HisTGroup.h
+++ analysis/root/macros/histogram/HisTGroup.h
@@ -16,6 +16,7 @@ class HisTGroup
     void Add(TData * t1, TString iname,Double_t sc=-1);
     void Add(TString iname);
     void Add(TFile * inFile, TString hname,TString iname, Double_t sc=-1);
+    void Print();
     void Save();
     TData * Sum();
     TData * Average();
@@ -26,9 +27,10 @@ class HisTGroup
     std::map<TString,Double_t> scales_;
 
     // group properties
-    Int_t nbins_;
+    Int_t xnbins_;
     Double_t xmin_;
     Double_t xmax_;
+    Int_t ynbins_;
     Double_t ymin_;
     Double_t ymax_;
 
@@ -40,9 +42,12 @@ class HisTGroup
 template <typename TData>
 HisTGroup<TData>::HisTGroup(TString name, Int_t n, Double_t xmin, Double_t xmax) :
   name_(name),
-  nbins_(n),
+  xnbins_(n),
   xmin_(xmin),
   xmax_(xmax),
+  ynbins_(-1),
+  ymin_(-1),
+  ymax_(-1),
   hSum_(0),
   hAve_(0)
 {
@@ -71,8 +76,17 @@ template <typename TData>
 void HisTGroup<TData>::Add(TString iname)
 {
   TString hname = TString("h")+name_+iname;
-  TData * h1 = new TData(hname,hname,nbins_,xmin_,xmax_);
+  TData * h1 = new TData(hname,hname,xnbins_,xmin_,xmax_);
   Add(h1,iname);
+}
+
+template <typename TData>
+void HisTGroup<TData>::Print()
+{
+  std::cout << "group: " << name_ << std::endl;
+  std::cout << "xnbins: " << xnbins_ << " xmin: " << xmin_ << " xmax: " << xmax_ << " hSum_: " << hSum_ << " hAve_: " << hAve_ << std::endl;
+  if (ynbins_>0)
+    std::cout << "ynbins: " << ynbins_ << " ymin: " << ymin_ << " ymax: " << ymax_ << " hSum_: " << hSum_ << " hAve_: " << hAve_ << std::endl;
 }
 
 #endif
--- analysis/root/macros/histogram/hisTGroupDriver.C
+++ analysis/root/macros/histogram/hisTGroupDriver.C
@@ -8,8 +8,7 @@ using namespace std;
 void hisTGroupDriver()
 {
   HisTGroup<TH1D> hgDj1("Dj1");
-  cout << "group: " << hgDj1.name_ << endl;
-  cout << "nbins: " << hgDj1.nbins_ << " xmin: " << hgDj1.xmin_ << " xmax: " << hgDj1.xmax_ << " " << hgDj1.hSum_ << " " << hgDj1.hAve_ << endl;
+  hgDj1.Print();
 
   // Test add th1
   TH1D * h1 = new TH1D("h1","h1",10,0,10);
@@ -25,9 +24,10 @@ void hisTGroupDriver()
 
   TCanvas * c3 = new TCanvas("c3","c3",500,500);
   hgDj1.hm_["NrSmall"]->Draw();
+  hgDj1.Print();
 
   HisTGroup<TProfile> hgDj0("Dj0",10,0,10);
-  cout << "nbins: " << hgDj0.nbins_ << " xmin: " << hgDj0.xmin_ << " xmax: " << hgDj0.xmax_ << " " << hgDj0.hSum_ << " " << hgDj0.hAve_ << endl;
+  hgDj0.Print();
   hgDj0.Add("Nr");
 
   TCanvas * c4 = new TCanvas("c4","c4",500,500);
