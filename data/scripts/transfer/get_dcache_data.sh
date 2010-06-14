316dcf872d9cc832dde25cd9a9e448cb99b59eb4
frankma <>
2010-06-14 10:38:25 +0000

    allow a tag to filter the original dir

--- data/scripts/transfer/get_dcache_data.sh
+++ data/scripts/transfer/get_dcache_data.sh
@@ -1,15 +1,16 @@
 #!/bin/bash -
 if [ $# -eq 0 ]; then
    echo Usage
-   echo "  $0 <dcache_path> <number_of_files>"
+   echo "  $0 <dcache_path> <tag> <number_of_files>"
    exit 1
 fi
 
 path=$1
-n=$2
-exefile=./getData.sh
+tag=$2
+nfiles=$3
 
+exefile=./getDcache.sh
 echo "# commandline options: $@" > $exefile
-for i in `ls $1/*.root | sort -n | head -n $2`; do echo "dccp $i ." >> $exefile; done
+for i in `ls $path/*.root | grep $tag | sort -n | head -n $nfiles`; do echo "dccp $i ." >> $exefile; done
 
 chmod +x $exefile
