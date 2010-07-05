16cbec4d8ef89cc221c41f65670d39f9c7a3619f
frankma <>
2010-07-05 17:28:31 +0000

    initial cfg for July Exercise

---
+++ Saved/JulyExercise/skim/jetAnaSkim_data_cfg.py
@@ -0,0 +1,37 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('PAT')
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring("reco.root")
+    )
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(10)
+    )
+
+process.load('PhysicsTools.PatAlgos.patHeavyIonSequences_cff')
+from PhysicsTools.PatAlgos.tools.heavyIonTools import *
+configureHeavyIons(process)
+
+from PhysicsTools.PatAlgos.tools.jetTools import *
+#switchJECSet( process, "Spring10") # Spring10 is the new default
+
+process.load("HeavyIonsAnalysis.Configuration.analysisProducers_cff")
+process.load("HeavyIonsAnalysis.Configuration.analysisEventContent_cff")
+
+process.output = cms.OutputModule("PoolOutputModule",
+    process.jetTrkSkimContent,
+    fileName = cms.untracked.string("jetAnaSkim.root")
+    )
+
+process.prod = cms.Path(process.allTracks +
+    #process.heavyIon +
+    process.makeHeavyIonJets
+    )
+
+from Saved.JulyExercise.customise_cfi import *
+disableMC(process)
+
+process.out_step = cms.EndPath(process.output)
+
