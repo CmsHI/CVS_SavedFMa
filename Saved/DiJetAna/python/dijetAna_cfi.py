302824b502c51c04022dc0724d499bbae44d5ab2
frankma <>
2010-07-26 12:07:19 +0000

    update centrality tag, default dj dphi cut to 2.5, default trk pt min to 0.7GeV

--- Saved/DiJetAna/python/dijetAna_cfi.py
+++ Saved/DiJetAna/python/dijetAna_cfi.py
@@ -7,7 +7,7 @@ dijetAna_mc = cms.EDAnalyzer('DiJetAna',
     fillL1Corr = cms.untracked.bool(False),                         
     genOnly = cms.untracked.bool(False),
     centFile = cms.string("CentralityTables.root"),
-    centLabel = cms.string("HFhits20_MC_Hydjet2760GeV_MC_3XY_V24_v0"),
+    centLabel = cms.string("HFhits20_DataJulyExercise_AMPT2760GeV_MC_37Y_V5_ZS_v0"),
     vtxsrc = cms.untracked.InputTag("hiSelectedVertex"),
     jetsrc = cms.untracked.InputTag("patJets"),
     refjetsrc = cms.untracked.InputTag("patJets"),
@@ -20,13 +20,13 @@ dijetAna_mc = cms.EDAnalyzer('DiJetAna',
     # dphi cut
     nearJetPtMin = cms.untracked.double(20.0),
     awayJetPtMin = cms.untracked.double(20.0),
-    djDPhiMin = cms.double(1.),
+    djDPhiMin = cms.double(2.5),
     # trk cut
-    trkPtMin = cms.untracked.double(0.3),
+    trkPtMin = cms.untracked.double(0.7),
     anaJetType = cms.untracked.int32(2),
     refJetType = cms.untracked.int32(11),
     anaTrkType = cms.untracked.int32(3),
-    # HI Event Selection: top 30% centrality
+    # Take all centrality at the moment
     centBinBeg = cms.untracked.int32(0),
     centBinEnd = cms.untracked.int32(20)
     )
@@ -34,7 +34,7 @@ dijetAna_mc = cms.EDAnalyzer('DiJetAna',
 # === Data ===
 dijetAna_data = dijetAna_mc.clone(
     isMC = cms.untracked.bool(False),
-    centLabel = cms.string("HFhits20_DataJulyExercise_Hydjet2760GeV_MC_37Y_V5_NZS_v0"),
+    centLabel = cms.string("HFhits20_DataJulyExercise_AMPT2760GeV_MC_37Y_V5_ZS_v0"),
     trksrc = cms.untracked.InputTag("hiSelectedTracks"),
     anaTrkType = cms.untracked.int32(2),
     refJetType = cms.untracked.int32(-1),
@@ -48,7 +48,8 @@ dijetAna_data = dijetAna_mc.clone(
 
 dijetAna_mc_genjet_trk = dijetAna_mc.clone(
     jetsrc = cms.untracked.InputTag("iterativeCone5HiGenJets"),
-    anaJetType = cms.untracked.int32(1)
+    anaJetType = cms.untracked.int32(1),
+    refJetType = cms.untracked.int32(12)
     )
 
 dijetAna_mc_calojet_genp = dijetAna_mc.clone(
