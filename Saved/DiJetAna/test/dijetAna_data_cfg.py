152a8fcfcc382282448cd6e6bdd1d1cec8a0de05
frankma <>
2010-07-26 13:04:57 +0000

    add ak5 to sequence

--- Saved/DiJetAna/test/dijetAna_data_cfg.py
+++ Saved/DiJetAna/test/dijetAna_data_cfg.py
@@ -18,6 +18,7 @@ process.load("Saved.DiJetAna.dijetAna_cff")
 process.dijetAna_data.trksrc = "allTracks"
 process.dijetAna_data.anaTrkType = 3
 process.dijetAna_data_seq += process.dijetAna_data_kt4
+process.dijetAna_data_seq += process.dijetAna_data_ak5
 print process.dijetAna_data.dumpPython()
 
 anaOutName = "dijetAna_%s.root" % (process.dijetAna_data.jetsrc.value())
