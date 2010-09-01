07294ff14361f85a377604a5633b8382f57de850
frankma <>
2010-09-01 06:54:08 +0000

    mc0827 trk distribution looks nicer ... what changed?

--- Saved/DiJetAna/macros/anaY1J/runAna.sh
+++ Saved/DiJetAna/macros/anaY1J/runAna.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -
-inputHard='~/scratch01/ana/JEx_Hard_GSR_ZS/mc0824_hlt/dijetAna_mc_output_*.root'
-inputMB='~/scratch01/ana/JEx_MB_GSR_ZS/mc0824_hlt/dijetAna_mc_output_*.root'
+inputHard='~/scratch01/ana/JEx_Hard_GSR_ZS/mc0827_hltall/dijetAna_mc_output_*.root'
+inputMB='~/scratch01/ana/JEx_MB_GSR_ZS/mc0827_hltall/dijetAna_mc_output_*.root'
 try=$1
 
 # anaDiJet
-#./runAnaDiJet.sh 0 $inputHard JExHdMc a1 dj JulyData-Hard dijetAna_mc $try
-#./runAnaDiJet.sh 0 $inputMB JExMbMc a1 dj JulyData-MB dijetAna_mc $try
+./runAnaDiJet.sh 0 $inputHard JExHdMc a0_$try dj JulyData-Hard dijetAna_mc $try
+./runAnaDiJet.sh 0 $inputMB JExMbMc a0_$try dj JulyData-MB dijetAna_mc $try
 
 # anaDiJetTrk
-./runAnaDiJetTrk.sh 0 $inputHard JExHdMc j1t0a2 djtrk JulyData-Hard dijetAna_mc_genjet_genp $try
-./runAnaDiJetTrk.sh 0 $inputMB JExMbMc j1t0a2 djtrk JulyData-MB dijetAna_mc_genjet_genp $try
+./runAnaDiJetTrk.sh 0 $inputHard JExHdMc a0_$try djtrk JulyData-Hard dijetAna_mc $try
+./runAnaDiJetTrk.sh 0 $inputMB JExMbMc a0_$try djtrk JulyData-MB dijetAna_mc $try
 
 exit
