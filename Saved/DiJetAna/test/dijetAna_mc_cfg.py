74944327e389c9ce0d3c97c11bab67b45c757a51
frankma <>
2010-09-02 18:39:39 +0000

    test tracking problem in JEx GSR with ChargedCand collection

--- Saved/DiJetAna/test/dijetAna_mc_cfg.py
+++ Saved/DiJetAna/test/dijetAna_mc_cfg.py
@@ -14,20 +14,20 @@ process.source = cms.Source("PoolSource",
 
 # ===== Centrality =====
 from Saved.DiJetAna.customise_cfi import *
-#loadCentralityDB(process,'HFhits40_DataJulyExercise_AMPT2760GeV_MC_37Y_V5_ZS_v0')
-loadCentralityDB(process,'HFhits40_MC_Hydjet2760GeV_MC_3XY_V24_v0')
+loadCentralityDB(process,'HFhits40_DataJulyExercise_AMPT2760GeV_MC_37Y_V5_ZS_v0')
+#loadCentralityDB(process,'HFhits40_MC_Hydjet2760GeV_MC_3XY_V24_v0')
 
 process.load("RecoHI.HiCentralityAlgos.CentralityFilter_cfi")
 process.centralityFilter.centralityBase = "HF"
 process.centralityFilter.selectedBins = range(40)
 
 #================ DiJet Ana ==========================
+process.load("HeavyIonsAnalysis.Configuration.analysisProducers_cff")
 process.load("Saved.DiJetAna.dijetAna_cff")
 # -- sample specific configs --
-#process.dijetAna_mc.hltsrc = cms.InputTag("TriggerResults","","HISIGNAL3")
-process.dijetAna_mc.hltsrc = cms.InputTag("None")
-process.dijetAna_mc_calojet_genp.hltsrc = cms.InputTag("None")
-process.dijetAna_mc_genjet_genp.hltsrc = cms.InputTag("None")
+process.dijetAna_mc.hltsrc = cms.InputTag("TriggerResults","","HISIGNAL3")
+process.dijetAna_mc_calojet_genp.hltsrc = cms.InputTag("TriggerResults","","HISIGNAL3")
+process.dijetAna_mc_genjet_genp.hltsrc = cms.InputTag("TriggerResults","","HISIGNAL3")
 #process.dijetAna_mc.trksrc = "hiSelectedTracks"
 #process.dijetAna_mc.anaTrkType = 2
 print process.dijetAna_mc.dumpPython()
@@ -40,7 +40,8 @@ process.TFileService = cms.Service('TFileService',
     )
 
 # =============== Final Paths =====================
-process.ana = cms.Path(#process.chargedCandidates *
+process.ana = cms.Path(
    process.centralityFilter *
+   process.allTracks *
    process.dijetAna_mc_seq
    )
