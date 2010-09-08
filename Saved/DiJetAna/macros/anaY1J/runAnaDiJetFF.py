cda8576349f2ac640d3efa5c6d51cb52ef492ecf
frankma <>
2010-09-08 01:36:21 +0000

    s0 and s1. Also added log output

--- Saved/DiJetAna/macros/anaY1J/runAnaDiJetFF.py
+++ Saved/DiJetAna/macros/anaY1J/runAnaDiJetFF.py
@@ -1,5 +1,4 @@
-import os
-import sys
+import os,sys
 
 AnaV = "a7"
 inFile0Name="~/scratch01/ana/Hydj_Hard_GSR_NZS/UQDJ80_120_proc0831_10k.root"
@@ -9,15 +8,21 @@ header="Hydjet+UQDJ80"
 AnaType="ff"
 
 SysAnas = {
-    "s0":["dijetAna_mc_genjet_genp",100,200,50,3.,"Ana","Ana","log\(1./zn\)","log\(1./za\)"],
-    "s1":["dijetAna_mc_genjet_genp",100,200,50,2.,"Ana","Ana","log\(1./zn\)","log\(1./za\)"],
-    "s5":["dijetAna_mc_calojet_genp",100,200,50,2.,"Ana","Ana","log\(1./zn\)","log\(1./za\)"]
+    "s0":["dijetAna_mc_genjet_genp",100,200,0,3.,"Ana","Ana","log\(1./zn\)","log\(1./za\)"],
+    "s1":["dijetAna_mc_genjet_genp",100,200,0,2.,"Ana","Ana","log\(1./zn\)","log\(1./za\)"],
+    "s8":["dijetAna_mc_calojet_genp",100,200,50,2.,"Ana","Ana","log\(1./zn\)","log\(1./za\)"]
     }
+
+# prepare ana
+logdir='log/'+SrcName+'/'+AnaV
+os.system("mkdir -p "+logdir)
+logfile=logdir+"/ff_"+sys.argv[1]+'.txt'
+os.system('echo '+logdir+' > '+logfile)
 
 # run ana
 from operator import itemgetter
 for sysana, [mod,NrJEtMin,NrJEtMax,AwJEtMin,AwJEtaMax,DJCutType,TrkCutType,varXiNrJES,varXiAwJES] in sorted(SysAnas.items()):
-  print '--- ',sysana,' ---'
+  os.system('echo --- '+sysana+' --- >> '+logfile)
   cmd='root -l -b -q anaDiJetFF.C+\('
   cmd+=str(doMC)+','
   cmd+='\\"'+AnaV+'\\",'
@@ -34,6 +39,7 @@ for sysana, [mod,NrJEtMin,NrJEtMax,AwJEtMin,AwJEtaMax,DJCutType,TrkCutType,varXi
   cmd+='\\"'+SrcName+'\\",'
   cmd+='\\"'+header+'\\",'
   cmd+='\\"'+AnaType+sysana+'\\"\)'
+  cmd+=' >> '+logfile
   print cmd
   os.system(cmd)
 
