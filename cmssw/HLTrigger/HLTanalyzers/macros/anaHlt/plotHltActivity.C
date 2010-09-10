d7cdc4a912bf672520e24b74790d12bbe3abc768
frankma <>
2010-09-10 18:27:19 +0000

    added L1T 40, 41

--- cmssw/HLTrigger/HLTanalyzers/macros/anaHlt/plotHltActivity.C
+++ cmssw/HLTrigger/HLTanalyzers/macros/anaHlt/plotHltActivity.C
@@ -15,7 +15,7 @@
 using namespace std;
 
 void plotHltActivity(
-    TString inFile0Name="../openhlt/output/openhlt_try1_1k.root",
+    TString inFile0Name="../openhlt/output/openhlt_try2_1k.root",
     TString header="Hydjet 2.76TeV"
     )
 {
@@ -37,21 +37,27 @@ void plotHltActivity(
   hgEffVsCent.Add1D("HFAct3_1Hit");
   hgEffVsCent.Add1D("HFAct3_Coinc1");
   hgEffVsCent.Add1D("HFAct3_Coinc2");
+  hgEffVsCent.Add1D("L1T40");
+  hgEffVsCent.Add1D("L1T41");
 
   // Analyze from tree
   ohTree->Draw(Form("HLT_ActivityHF3:hiBin*2.5>>%s",hgEffVsCent.GetH("HFAct3_1Hit")->GetName()),"","prof goff");
   ohTree->Draw(Form("HLT_ActivityHF3_Coinc1:hiBin*2.5>>%s",hgEffVsCent.GetH("HFAct3_Coinc1")->GetName()),"","prof goff");
   ohTree->Draw(Form("HLT_ActivityHF3_Coinc2:hiBin*2.5>>%s",hgEffVsCent.GetH("HFAct3_Coinc2")->GetName()),"","prof goff");
+  ohTree->Draw(Form("L1Tech_BSC_minBias_threshold1.v0:hiBin*2.5>>%s",hgEffVsCent.GetH("L1T40")->GetName()),"","prof goff");
+  ohTree->Draw(Form("L1Tech_BSC_minBias_threshold2.v0:hiBin*2.5>>%s",hgEffVsCent.GetH("L1T41")->GetName()),"","prof goff");
 
   // Plot
   TCanvas * cHltActEffVsCent = new TCanvas("HltActEffVsCent","HltActEffVsCent",500,500);
   CPlot cpHltActEffVsCent("HltActEffVsCent","HltActEffVsCent","Centrality","HLT Eff.");
   cpHltActEffVsCent.SetXRange(0,100);
   cpHltActEffVsCent.SetYRange(0,1.2);
+  cpHltActEffVsCent.AddHist1D(hgEffVsCent.H("L1T40"),"L1 Bit 40","E",kGreen+2,kOpenCircle);
+  cpHltActEffVsCent.AddHist1D(hgEffVsCent.H("L1T41"),"L1 Bit 41","E",kOrange+2,kOpenDiamond);
   cpHltActEffVsCent.AddHist1D(hgEffVsCent.H("HFAct3_1Hit"),"ActivityHF3 (Any Hit)","E",kBlue,kOpenSquare);
   cpHltActEffVsCent.AddHist1D(hgEffVsCent.H("HFAct3_Coinc1"),"ActivityHF3 (1 Hit Coinc.)","E",kBlack,kFullCircle);
   cpHltActEffVsCent.AddHist1D(hgEffVsCent.H("HFAct3_Coinc2"),"ActivityHF3 (2 Hits Coinc.)","E",kRed,kOpenStar);
-  cpHltActEffVsCent.SetLegend(0.25,0.28,0.58,0.46);
+  cpHltActEffVsCent.SetLegend(0.29,0.21,0.62,0.43);
   cpHltActEffVsCent.SetLegendHeader(header);
   cpHltActEffVsCent.Draw(cHltActEffVsCent,true);
 }
