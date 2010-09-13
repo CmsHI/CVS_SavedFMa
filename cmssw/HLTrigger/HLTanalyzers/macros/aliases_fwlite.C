760ee7ba04fd30511c324d8203691e0a5688d975
frankma <>
2010-09-13 14:17:46 +0000

    initial alias macro for fwlite

---
+++ cmssw/HLTrigger/HLTanalyzers/macros/aliases_fwlite.C
@@ -0,0 +1,7 @@
+void aliases_fwlite(TTree * Events)
+{
+  Events->SetAlias("ct","CaloTowersSorted_towerMaker__RECO.obj.obj");
+
+  Events->SetAlias("NHfN","Sum$(ct.energy()>3 && ct.eta()<3)");
+  Events->SetAlias("NHfP","Sum$(ct.energy()>3 && ct.eta()>3)");
+}
