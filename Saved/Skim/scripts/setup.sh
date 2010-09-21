44c838d50ef2f795a61fa5b54dca5e9c9017e22e
frankma <>
2010-09-21 12:08:46 +0000

    updated for 384

--- Saved/Skim/scripts/setup.sh
+++ Saved/Skim/scripts/setup.sh
@@ -1,25 +1,28 @@
 #!/bin/bash -
-# HI sw
+# - Based on
+# * UserCode/CmsHi/JetAnalysis/scripts
+# * HLTrigger/HLTanalyzers/scripts/setup.sh
+
+# HI Reco
 cvs co RecoHI/Configuration
 cvs co RecoHI/HiCentralityAlgos
 cvs co RecoHI/HiEvtPlaneAlgos
 cvs co RecoHI/HiJetAlgos
+cvs co RecoHI/HiEgammaAlgos
 cvs co RecoHI/HiMuonAlgos
+cvs co RecoHI/HiTracking
 
+cvs co CondFormats/HIObjects
 cvs co GeneratorInterface/HiGenCommon
-
 cvs co DataFormats/HeavyIonEvent
 cvs co SimDataFormats/HiGenData
 
-# jet specific
+# analysis specific
+cvs co RecoJets/JetAlgorithms
 cvs co RecoJets/JetProducers
 cvs co HeavyIonsAnalysis/Configuration
 
-# pat
-cvs co -r V08-00-24 PhysicsTools/PatAlgos
-
-# dependencies
-cvs co -r V00-04-02 RecoLocalCalo/Castor
+cvs co -d Saved UserCode/SavedFMa/Saved
 
 # check dependencies
-checkdeps
+#checkdeps
