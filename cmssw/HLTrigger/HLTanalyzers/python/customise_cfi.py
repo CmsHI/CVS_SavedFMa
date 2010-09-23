ed8df35443836103b08a2ec088053dab67a3cc1c
frankma <>
2010-09-23 18:54:11 +0000

    tried w/o success to add jetana workflow

--- cmssw/HLTrigger/HLTanalyzers/python/customise_cfi.py
+++ cmssw/HLTrigger/HLTanalyzers/python/customise_cfi.py
@@ -3,8 +3,10 @@ import FWCore.ParameterSet.Config as cms
 def defineReco(process):
   process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
   process.load('Configuration.StandardSequences.MixingNoPileUp_cff')
-  process.raw2digi_step = cms.Path(process.RawToDigi)
-  process.reconstruction_step = cms.Path(process.reconstructionHeavyIons)
+  process.load('Configuration.StandardSequences.RawToDigi_cff')
+  process.load('Configuration.StandardSequences.ReconstructionHeavyIons_cff')
+  process.raw2digi_step = cms.EndPath(process.RawToDigi)
+  #process.reconstruction_step = cms.EndPath(process.reconstructionHeavyIons)
 
 def defineExtraHlt(process):
   process.hltHcalSimpleRecHitFilterCoincidence2 = process.hltHcalSimpleRecHitFilterCoincidence.clone(
@@ -16,19 +18,17 @@ def defineExtraHlt(process):
   process.HLT_HIActivityHF_Coincidence3_2Hit.replace(process.hltHcalSimpleRecHitFilterCoincidence,process.hltHcalSimpleRecHitFilterCoincidence2)
 
 def defineExtraAna(process):
+  from Saved.DiJetAna.customise_cfi import enableRECO
+  enableRECO(process,"Data")
+  process.patJets.jetSource = cms.InputTag("hltIterativeCone5PileupSubtractionCaloJets")
   process.load("Saved.DiJetAna.dijetAna_cff")
-  for i,m in enumerate([process.dijetAna_mc,
-    process.dijetAna_mc_calojet_tower,
-    process.dijetAna_mc_calojet_genp,
-    process.dijetAna_mc_genjet_genp]):
+  for i,m in enumerate([process.dijetAna_data_calojet_tower]):
     m.hltsrc = cms.InputTag("TriggerResults","",process.process)
     m.hltNames = ["HLT_HIMinBiasBSC","HLT_HIActivityHF_Single3","HLT_HIActivityHF_Coincidence3","HLT_HIActivityHF_Coincidence3_2Hit"]
     print i, "hlt: ", m.hltsrc
-  process.dijetAna_mc.trksrc = "hiSelectedTracks"
-  process.dijetAna_mc.anaTrkType = cms.int32(2)
-  from Saved.DiJetAna.customise_cfi import enableRECO
-  enableRECO(process)
-  process.reco_extra.remove(process.heavyIon)
-  process.extra_reco_step = cms.EndPath(process.reco_extra)
-  process.extra_ana_step = cms.EndPath(process.dijetAna_mc_seq)
+  process.dijetAna_data_calojet_tower.jetEtaMax = 5
+  process.dijetAna_data_calojet_tower.vtxsrc = cms.InputTag( "hltHIPixelClusterVertices" )
+  process.dijetAna_data_calojet_tower.trksrc = cms.InputTag( "hltTowerMakerForAll" )
+  process.extra_reco_step = cms.Path(process.reco_extra)
+  process.extra_ana_step = cms.EndPath(process.dijetAna_data_calojet_tower)
 
