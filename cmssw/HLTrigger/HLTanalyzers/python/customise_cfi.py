37b5d2338d42d700f00bbcc7ae07629da0e25fe5
frankma <>
2010-09-25 10:31:49 +0000

    return to workflow on reco with FEVTSIM

--- cmssw/HLTrigger/HLTanalyzers/python/customise_cfi.py
+++ cmssw/HLTrigger/HLTanalyzers/python/customise_cfi.py
@@ -20,15 +20,15 @@ def defineExtraHlt(process):
 def defineExtraAna(process):
   from Saved.DiJetAna.customise_cfi import enableRECO
   enableRECO(process,"Data")
-  process.patJets.jetSource = cms.InputTag("hltIterativeCone5PileupSubtractionCaloJets")
   process.load("Saved.DiJetAna.dijetAna_cff")
   for i,m in enumerate([process.dijetAna_data_calojet_tower]):
     m.hltsrc = cms.InputTag("TriggerResults","",process.process)
-    m.hltNames = ["HLT_HIMinBiasBSC","HLT_HIActivityHF_Single3","HLT_HIActivityHF_Coincidence3","HLT_HIActivityHF_Coincidence3_2Hit"]
+    m.hltNames = ["HLT_ZeroBiasPixel_SingleTrack",
+	"HLT_HIMinBiasBSC","HLT_HIMinBiasCalo",
+	"HLT_L1Tech_HCAL_HF","HLT_L1Tech_BSC_minBias",
+	"HLT_HIActivityHF_Single3","HLT_HIActivityHF_Coincidence3","HLT_HIActivityHF_Coincidence3_2Hit"]
     print i, "hlt: ", m.hltsrc
   process.dijetAna_data_calojet_tower.jetEtaMax = 5
-  process.dijetAna_data_calojet_tower.vtxsrc = cms.InputTag( "hltHIPixelClusterVertices" )
-  process.dijetAna_data_calojet_tower.trksrc = cms.InputTag( "hltTowerMakerForAll" )
   process.extra_reco_step = cms.Path(process.reco_extra)
   process.extra_ana_step = cms.EndPath(process.dijetAna_data_calojet_tower)
 
