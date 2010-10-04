71be0669d5d86884f18a17a3d4467ff88644be29
frankma <>
2010-10-04 16:53:43 +0000

    ran on Matt's sample. Need: towers, genparticles

--- Saved/DiJetAna/test/dijetAna_mc_cfg.py
+++ Saved/DiJetAna/test/dijetAna_mc_cfg.py
@@ -15,7 +15,8 @@ process.source = cms.Source("PoolSource",
 # ===== Centrality =====
 from Saved.DiJetAna.customise_cfi import *
 #loadCentralityDB(process,'HFhits40_DataJulyExercise_AMPT2760GeV_MC_37Y_V5_ZS_v0')
-loadCentralityDB(process,'HFhits40_MC_Hydjet2760GeV_MC_3XY_V24_v0')
+#loadCentralityDB(process,'HFhits40_MC_Hydjet2760GeV_MC_3XY_V24_v0')
+loadCentralityDB(process,'CentralityTable_HFhits40_Hydjet2760GeV_v0_mc')
 
 process.load("Saved.DiJetAna.eventSelection_cff")
 process.centralityFilter.centralityBase = "HF"
@@ -26,13 +27,16 @@ process.load("HeavyIonsAnalysis.Configuration.analysisProducers_cff")
 process.load("Saved.DiJetAna.dijetAna_cff")
 # -- sample specific configs --
 for i,m in enumerate([process.dijetAna_mc,
-    process.dijetAna_mc_calojet_tower,
-    process.dijetAna_mc_calojet_genp,
-    process.dijetAna_mc_genjet_genp]):
-  m.hltsrc = cms.InputTag("TriggerResults","","HISIGNAL")
+  process.dijetAna_mc_calojet_tower,
+  process.dijetAna_mc_calojet_genp,
+  process.dijetAna_mc_genjet_genp]):
+  #m.hltsrc = cms.InputTag("TriggerResults","","HISIGNAL")
+  m.jetsrc = "ak5patJets"
+  m.refjetsrc = "ak5patJets"
   print i, "hlt: ", m.hltsrc
 process.dijetAna_mc.trksrc = "hiSelectedTracks"
 process.dijetAna_mc.anaTrkType = 2
+process.dijetAna_mc_genjet_genp.jetsrc = "ak5HiGenJets"
 print process.dijetAna_mc.dumpPython()
 
 anaOutName = "dijetAna_%s.root" % (process.dijetAna_mc.jetsrc.value())
@@ -47,6 +51,9 @@ enableRECO(process)
 enablePp(process)
 
 process.reco = cms.Path( process.eventSelection * process.reco_extra)
+process.dijetAna_mc_seq.remove(process.dijetAna_mc_calojet_tower)
+process.dijetAna_mc_seq.remove(process.dijetAna_mc_calojet_genp)
+process.dijetAna_mc_seq.remove(process.dijetAna_mc_genjet_genp)
 process.ana = cms.Path(
    process.eventSelection *
    #process.allTracks *
