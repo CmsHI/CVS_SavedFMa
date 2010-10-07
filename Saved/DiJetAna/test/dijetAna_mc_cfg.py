59a8b030228fc5305968d9f1ee6df3a8415caf33
frankma <>
2010-10-07 01:33:14 +0000

    add pp mode to enablePp, also clean up somemore

--- Saved/DiJetAna/test/dijetAna_mc_cfg.py
+++ Saved/DiJetAna/test/dijetAna_mc_cfg.py
@@ -16,11 +16,8 @@ process.source = cms.Source("PoolSource",
 from Saved.DiJetAna.customise_cfi import *
 #loadCentralityDB(process,'HFhits40_DataJulyExercise_AMPT2760GeV_MC_37Y_V5_ZS_v0')
 #loadCentralityDB(process,'HFhits40_MC_Hydjet2760GeV_MC_3XY_V24_v0')
-loadCentralityDB(process,'CentralityTable_HFhits40_Hydjet2760GeV_v0_mc')
-
+#loadCentralityDB(process,'CentralityTable_HFhits40_Hydjet2760GeV_v0_mc')
 process.load("Saved.DiJetAna.eventSelection_cff")
-process.centralityFilter.centralityBase = "HF"
-process.centralityFilter.selectedBins = range(40)
 
 #================ DiJet Ana ==========================
 process.load("HeavyIonsAnalysis.Configuration.analysisProducers_cff")
@@ -31,8 +28,6 @@ for i,m in enumerate([process.dijetAna_mc,
   process.dijetAna_mc_calojet_genp,
   process.dijetAna_mc_genjet_genp]):
   #m.hltsrc = cms.InputTag("TriggerResults","","HISIGNAL")
-  m.jetsrc = "ak5patJets"
-  m.refjetsrc = "ak5patJets"
   print i, "hlt: ", m.hltsrc
 process.dijetAna_mc.trksrc = "hiSelectedTracks"
 process.dijetAna_mc.anaTrkType = 2
@@ -47,20 +42,17 @@ process.TFileService = cms.Service('TFileService',
     )
 
 # =============== Final Paths =====================
-enableRECO(process)
+enableRECO(process,"MC","pp")
 enablePp(process)
+#process.dijetAna_mc_seq.remove(process.dijetAna_mc_calojet_tower)
 
 process.reco = cms.Path( process.eventSelection * process.reco_extra)
-process.dijetAna_mc_seq.remove(process.dijetAna_mc_calojet_tower)
-process.dijetAna_mc_seq.remove(process.dijetAna_mc_calojet_genp)
-process.dijetAna_mc_seq.remove(process.dijetAna_mc_genjet_genp)
-process.ana = cms.Path(
-   process.eventSelection *
+process.ana = cms.Path( process.eventSelection *
    #process.allTracks *
    process.dijetAna_mc_seq
    )
 
 process.schedule = cms.Schedule(
-  #process.reco,
+  process.reco,
   process.ana
   )
