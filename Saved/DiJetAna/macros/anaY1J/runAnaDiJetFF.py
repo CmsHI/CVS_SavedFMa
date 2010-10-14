c8ac8b74d600e5ad0d81c2c13767848397ca0773
frankma <>
2010-10-14 16:28:00 +0000

    goes back to standard sys bias

--- Saved/DiJetAna/macros/anaY1J/runAnaDiJetFF.py
+++ Saved/DiJetAna/macros/anaY1J/runAnaDiJetFF.py
@@ -18,24 +18,24 @@ SysAnas = {
     #"s00":["djgen",NrJEtMin,NrJEtMax,0,3.,"Ana","AnaSig","log\(nljet/ppt\)","log\(aljet/ppt\)"],
     #"s01":["djgen",NrJEtMin,NrJEtMax,0,2.,"Ana","AnaSig","log\(nljet/ppt\)","log\(aljet/ppt\)"],
     #"s02":["djgen",NrJEtMin,NrJEtMax,AwJEtMin,2.,"Ana","AnaSig","log\(nljet/ppt\)","log\(aljet/ppt\)"],
-    #"s03":["djgen",NrJEtMin,NrJEtMax,AwJEtMin,2.,"Ana","Ana","log\(nljet/ppt\)","log\(aljet/ppt\)"],
+    "s03":["djgen",NrJEtMin,NrJEtMax,AwJEtMin,2.,"Ana","Ana","log\(nljet/ppt\)","log\(aljet/ppt\)"],
     #"s04":["djgen",NrJEtMin,NrJEtMax,AwJEtMin,2.,"AnaMatRefNr","Ana","log\(nljet/ppt\)","log\(aljet/ppt\)"], # mat Nr
     #"s05":["djgen",NrJEtMin,NrJEtMax,AwJEtMin,2.,"AnaMatRef","Ana","log\(nljet/ppt\)","log\(aljet/ppt\)"], # mat aw
     #"s05b":["djgen",NrJEtMin,NrJEtMax,AwJEtMin,2.,"RefOrderRef","Ana","log\(nljet/ppt\)","log\(aljet/ppt\)"], # mat aw
-    #"s06":["djcalo_genp",NrJEtMin,NrJEtMax,AwJEtMin,2.,"Ref","Ana","log\(nlrjet/ppt\)","log\(alrjet/ppt\)"], # calo order
+    "s06":["djcalo_genp",NrJEtMin,NrJEtMax,AwJEtMin,2.,"Ref","Ana","log\(nlrjet/ppt\)","log\(alrjet/ppt\)"], # calo order
     #"s06b":["djcalo_genp",NrJEtMin,NrJEtMax,AwJEtMin,2.,"Ref","Ana","log\(nljet/ppt\)","log\(aljet/ppt\)"], # calo order, with calo jes in xi
     #"s06b2":["djcalo_genp",NrJEtMin,NrJEtMax,AwJEtMin,2.,"Ref","Ana","log\(nlrjet*1.1/ppt\)","log\(alrjet/ppt\)"], # calo order
     #"s07":["djcalo_genp",NrJEtMin,NrJEtMax,AwJEtMin,2.,"RefAwAnaMatNr","Ana","log\(nlrjet/ppt\)","log\(alrjet/ppt\)"], # cut on nr calo et, still matched
-    #"s08":["djcalo_genp",NrJEtMin,NrJEtMax,AwJEtMin,2.,"AnaMatRef","Ana","log\(nlrjet/ppt\)","log\(alrjet/ppt\)"], # cut on aw calo et, still matched
+    "s08":["djcalo_genp",NrJEtMin,NrJEtMax,AwJEtMin,2.,"AnaMatRef","Ana","log\(nlrjet/ppt\)","log\(alrjet/ppt\)"], # cut on aw calo et, still matched
     #"s09":["djcalo_genp",NrJEtMin,NrJEtMax,AwJEtMin,2.,"AnaMatRefNr","Ana","log\(nlrjet/ppt\)","log\(alrjet/ppt\)"], # no Aw mat
-    #"s10":["djcalo_genp",NrJEtMin,NrJEtMax,AwJEtMin,2.,"Ana","Ana","log\(nlrjet/ppt\)","log\(alrjet/ppt\)"], # no Nr mat
-    #"s11":["djcalo_genp",NrJEtMin,NrJEtMax,AwJEtMin,2.,"Ana","Ana","log\(nljet/ppt\)","log\(aljet/ppt\)"] # xi use Nr JES -- all calojet
-    "sf03a":["djgen",NrJEtMin,NrJEtMax,AwJEtMin,2.,"AnaLz0","Ana","log\(nljet/ppt\)","log\(aljet/ppt\)"],
-    "sf03b":["djgen",NrJEtMin,NrJEtMax,AwJEtMin,2.,"AnaLz1","Ana","log\(nljet/ppt\)","log\(aljet/ppt\)"],
-    "sf03c":["djgen",NrJEtMin,NrJEtMax,AwJEtMin,2.,"AnaLz2","Ana","log\(nljet/ppt\)","log\(aljet/ppt\)"],
-    "sf11a":["djcalo_genp",NrJEtMin,NrJEtMax,AwJEtMin,2.,"AnaLz0","Ana","log\(nljet/ppt\)","log\(aljet/ppt\)"], 
-    "sf11b":["djcalo_genp",NrJEtMin,NrJEtMax,AwJEtMin,2.,"AnaLz1","Ana","log\(nljet/ppt\)","log\(aljet/ppt\)"],
-    "sf11c":["djcalo_genp",NrJEtMin,NrJEtMax,AwJEtMin,2.,"AnaLz2","Ana","log\(nljet/ppt\)","log\(aljet/ppt\)"]
+    "s10":["djcalo_genp",NrJEtMin,NrJEtMax,AwJEtMin,2.,"Ana","Ana","log\(nlrjet/ppt\)","log\(alrjet/ppt\)"], # no Nr mat
+    "s11":["djcalo_genp",NrJEtMin,NrJEtMax,AwJEtMin,2.,"Ana","Ana","log\(nljet/ppt\)","log\(aljet/ppt\)"] # xi use Nr JES -- all calojet
+    #"sf03a":["djgen",NrJEtMin,NrJEtMax,AwJEtMin,2.,"AnaLz0","Ana","log\(nljet/ppt\)","log\(aljet/ppt\)"],
+    #"sf03b":["djgen",NrJEtMin,NrJEtMax,AwJEtMin,2.,"AnaLz1","Ana","log\(nljet/ppt\)","log\(aljet/ppt\)"],
+    #"sf03c":["djgen",NrJEtMin,NrJEtMax,AwJEtMin,2.,"AnaLz2","Ana","log\(nljet/ppt\)","log\(aljet/ppt\)"],
+    #"sf11a":["djcalo_genp",NrJEtMin,NrJEtMax,AwJEtMin,2.,"AnaLz0","Ana","log\(nljet/ppt\)","log\(aljet/ppt\)"], 
+    #"sf11b":["djcalo_genp",NrJEtMin,NrJEtMax,AwJEtMin,2.,"AnaLz1","Ana","log\(nljet/ppt\)","log\(aljet/ppt\)"],
+    #"sf11c":["djcalo_genp",NrJEtMin,NrJEtMax,AwJEtMin,2.,"AnaLz2","Ana","log\(nljet/ppt\)","log\(aljet/ppt\)"]
     }
 
 # prepare ana
