57a178080b2473af35f3f1cdc0cb069d218b82df
frankma <>
2010-10-28 12:30:31 +0000

    cleanup and fix y axis range

--- Saved/DiJetAna/macros/anaY1J/anaJesFF.C
+++ Saved/DiJetAna/macros/anaY1J/anaJesFF.C
@@ -23,24 +23,21 @@ void anaJesFF(int doMC=1,
     TString AnaVersion = "a1028",
     TString CmpVersion = "c0",
     TString modName = "djcalo_genp",
-    Double_t NrJEtMin = 80,
-    Double_t NrJEtMax = 120,
+    Double_t NrJEtMin = 50,
+    Double_t NrJEtMax = 200,
     TString DJCutType = "Ref",
-    //TString fragVar = "lppt[0]/nlrjet",
-    //TString fragVarTag = "Lz",
-    //TString fragVarTitle = "z^{lead} = p_{T}^{trk}/E_{T}^{RefJet}",
-    //Double_t fragVarMin = 0,
-    //Double_t fragVarMax = 1,
-    TString fragVar = "nlrjet",
-    TString fragVarTag = "RefJEt",
-    TString fragVarTitle = "E_{T}^{RefJet}",
+    TString fragVar = "lppt[0]/nlrjet",
+    TString fragVarTag = "Lz",
+    TString fragVarTitle = "z^{lead} = p_{T}^{trk}/E_{T}^{RefJet}",
     Double_t fragVarMin = 0,
-    Double_t fragVarMax = 250,
-    //TString jEtVar = "nljet",
-    //TString jEtVarTag = "JEt",
-    //TString jEtVarTitle = "E_{T}^{CaloJet}",
+    Double_t fragVarMax = 1.5,
+    //TString fragVar = "nlrjet",
+    //TString fragVarTag = "RefJEt",
+    //TString fragVarTitle = "E_{T}^{RefJet}",
+    //Double_t fragVarMin = 0,
+    //Double_t fragVarMax = 250,
     TString jextraCut = "",//"(ljcpt[0]-ljcptbg[0])/nlrjet>0.5",
-    const char * inFile0Name="~/scratch01/mc/QCD/su10-qcd80-startup36v9_f500_proc1022_final/trkhists_9*.root",
+    const char * inFile0Name="~/scratch01/mc/QCD/su10-qcd80-startup36v9_f500_proc1022_final/trkhists_*.root",
     TString header = "QCD-DiJet80",
     TString AnaType = "dj")
 {
@@ -69,16 +66,6 @@ void anaJesFF(int doMC=1,
   gSystem->mkdir(outdir.Data(),kTRUE);
 
   // === ana ===
-  /*
-  TH2F * hLPPtVsGJEt = new TH2F("hLPPtVsGJEt","hLPPtVsJEt",50,0,250,50,0,250);
-  TH2F * hLPPtVsJEt = new TH2F("hLPPtVsJEt","hLPPtVsJEt",50,0,250,50,0,250);
-  TProfile * hLzVsGJEtProf = new TProfile("hLzVsGJEtProf","hLzVsJEt",50,0,250);
-  TProfile * hLzVsJEtProf = new TProfile("hLzVsJEtProf","hLzVsJEt",50,0,250);
-  TProfile * hRespVsLzProf = new TProfile("hRespVsLzProf","hLzVsJEt",50,0,1);
-  TProfile * hRespVsAzProf = new TProfile("hRespVsAzProf","hAzVsJEt",50,0,1);
-  TProfile * hRespVsLJEtProf = new TProfile("hRespVsLJEtProf","hLJEtVsJEt",50,0,250);
-  TProfile * hRespVsAJEtProf = new TProfile("hRespVsAJEtProf","hAJEtVsJEt",50,0,250);
-  */
   // frag var
   Int_t numFragVarBins=40;
   AnaFrag anaFragVarNr(fragVarTag,"Nr",djTree,anaSel.FinLJCut(),"",fragVar,"","",numFragVarBins,fragVarMin,fragVarMax);
@@ -111,61 +98,11 @@ void anaJesFF(int doMC=1,
   anaFragVar_JResp.ytitle = "E_{T}^{RecoJet}/E_{t}^{RefJet}";
   anaFragVar_JResp.PlotCorrelations(anaSel,"nljet/nlrjet",fragVar,numFragVarBins,fragVarMin,fragVarMax);
 
-  /*
-  djTree->Draw("lppt[0]:nlrjet>>hLPPtVsGJEt",anaSel.FinLJCut(),"goff");
-  djTree->Draw("lppt[0]:nljet>>hLPPtVsJEt",anaSel.FinLJCut(),"goff");
-  djTree->Draw("lppt[0]/nlrjet:nlrjet>>hLzVsGJEtProf",anaSel.FinLJCut(),"prof goff");
-  djTree->Draw("lppt[0]/nlrjet:nljet>>hLzVsJEtProf",anaSel.FinLJCut(),"prof goff");
-  djTree->Draw("nljet/nlrjet:lppt[0]/nlrjet>>hRespVsLzProf",anaSel.FinDJCut(),"prof goff");
-  djTree->Draw("aljet/alrjet:lppt[1]/alrjet>>hRespVsAzProf",anaSel.FinDJCut(),"prof goff");
-  djTree->Draw("nljet/nlrjet:nlrjet>>hRespVsLJEtProf",anaSel.FinDJCut(),"prof goff");
-  djTree->Draw("aljet/alrjet:alrjet>>hRespVsAJEtProf",anaSel.FinDJCut(),"prof goff");
-  */
-
   // -- plot frag var--
   TCanvas * cFragVar = new TCanvas("c"+fragVarTag,"c"+fragVarTag,500,500);
   CPlot cpFragVar(fragVarTag+anaSel.Tag2,fragVarTag,fragVarTitle,"unit normalization");
+  cpFragVar.SetLogy();
   cpFragVar.AddHist1D(anaFragVarNr.hRaw,"Leading Jet","E",kRed,kOpenCircle);
   cpFragVar.AddHist1D(anaFragVarAw.hRaw,"Away Jet","E",kBlue,kOpenSquare);
   cpFragVar.Draw(cFragVar,true);
-
-  /*
-  TCanvas * cLPPtVsGJEt = new TCanvas("cLPPtVsGJEt","cLPPtVsGJEt",500,500);
-  cLPPtVsGJEt->SetLogz();
-  CPlot cpLPPtVsGJEt("LPPtVsGJEt"+anaSel.Tag2,"DJ","Leading E_{T}^{genjet} [GeV]","Leading p_{T}^{trk} (in Jet Cone) [GeV]");
-  cpLPPtVsGJEt.SetYRange(0,160);
-  cpLPPtVsGJEt.AddHist2D(hLPPtVsGJEt,"colz");
-  cpLPPtVsGJEt.Draw(cLPPtVsGJEt,true);
-
-  TCanvas * cLzVsGJEtProf = new TCanvas("LzVsGJEtProf","LzVsGJEtProf",500,500);
-  CPlot cpLzVsGJEtProf("LzVsGJEtProf"+anaSel.Tag2,"LzVsGJEtProf","Leading E_{T}^{genjet} [GeV]","z^{lead trk} (in Jet Cone)");
-  cpLzVsGJEtProf.SetYRange(0,0.3);
-  cpLzVsGJEtProf.AddProfile(hLzVsGJEtProf,"j1","E");
-  cpLzVsGJEtProf.SetLegend(0.53,0.31,0.86,0.37);
-  cpLzVsGJEtProf.Draw(cLzVsGJEtProf,true);
-
-  TCanvas * cLzVsJEtProf = new TCanvas("LzVsJEtProf","LzVsJEtProf",500,500);
-  CPlot cpLzVsJEtProf("LzVsJEtProf"+anaSel.Tag2,"LzVsJEtProf","Leading E_{T}^{calojet} [GeV]","z^{lead trk} (in Jet Cone)");
-  cpLzVsJEtProf.SetYRange(0,0.3);
-  cpLzVsJEtProf.AddProfile(hLzVsJEtProf,"j1","E");
-  cpLzVsJEtProf.SetLegend(0.53,0.31,0.86,0.37);
-  cpLzVsJEtProf.Draw(cLzVsJEtProf,true);
-
-  // -- response --
-  TCanvas * cRespVsLzProf = new TCanvas("RespVsLzProf","RespVsLzProf",500,500);
-  CPlot cpRespVsLzProf("RespVsLzProf"+anaSel.Tag2,"RespVsLzProf","z^{lead trk} (in Jet Cone)","E_{T}^{calojet}/E_{T}^{genjet}");
-  cpRespVsLzProf.SetYRange(0,1.5);
-  cpRespVsLzProf.AddProfile(hRespVsLzProf,"j1","E");
-  cpRespVsLzProf.AddProfile(hRespVsAzProf,"j2","E",kBlue,kOpenSquare);
-  cpRespVsLzProf.SetLegend(0.53,0.31,0.86,0.37);
-  cpRespVsLzProf.Draw(cRespVsLzProf,true);
-
-  TCanvas * cRespVsLJEtProf = new TCanvas("RespVsLJEtProf","RespVsLJEtProf",500,500);
-  CPlot cpRespVsLJEtProf("RespVsLJEtProf"+anaSel.Tag2,"RespVsLJEtProf","E_{T}^{genjet}","E_{T}^{calojet}/E_{T}^{genjet}");
-  cpRespVsLJEtProf.SetYRange(0,1.5);
-  cpRespVsLJEtProf.AddProfile(hRespVsLJEtProf,"j1","E");
-  cpRespVsLJEtProf.AddProfile(hRespVsAJEtProf,"j2","E",kBlue,kOpenSquare);
-  cpRespVsLJEtProf.SetLegend(0.53,0.31,0.86,0.37);
-  cpRespVsLJEtProf.Draw(cRespVsLJEtProf,true);
-  */
 }
