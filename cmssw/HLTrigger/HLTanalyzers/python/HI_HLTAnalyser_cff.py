1d4834cb3cd8665728c492500a86c8c0104da7a6
frankma <>
2010-11-02 16:38:38 +0000

    update input tag for photon hlt object

--- cmssw/HLTrigger/HLTanalyzers/python/HI_HLTAnalyser_cff.py
+++ cmssw/HLTrigger/HLTanalyzers/python/HI_HLTAnalyser_cff.py
@@ -5,8 +5,15 @@ from GeneratorInterface.HiGenCommon.HeavyIon_cff import *
 from RecoHI.HiCentralityAlgos.CentralityBin_cfi import *
 from HLTrigger.HLTanalyzers.HLTAnalyser_cfi import * 
 
+# jets
 hltanalysis.genjets           = cms.InputTag("iterativeCone5HiGenJets")
 hltanalysis.recjets           = cms.InputTag("hltIterativeCone5PileupSubtractionCaloJets")
+
+# photons
+hltanalysis.BarrelPhoton      = cms.InputTag("hltCorrectedIslandBarrelSuperClustersHI")
+hltanalysis.EndcapPhoton      = cms.InputTag("hltCorrectedIslandEndcapSuperClustersHI")
+
+# muons
 #hltanalysis.MuCandTag3       = cms.InputTag("hltL3GenMuonCandidates") ### if DoL3Muons is true
 
 hltanalysis.Centrality    = cms.InputTag("hiCentrality")
