68a51a750b29cf91174b5bdc2e462e015b8f1741
frankma <>
2010-11-07 03:05:11 +0000

    a simple macro to plot trigger bx0 vs 5bx

---
+++ Saved/HltAna/macros/plotTrig5Bx.C
@@ -0,0 +1,22 @@
+void plotTrig5Bx(Int_t runNum=150308)
+{
+  TChain * oh = new TChain("hltanalysis/HltTree");
+  oh->Add(Form("/d101/frankma/data/HIExpress/HR10Exp3/r%d/hltana_*.root",runNum));
+  oh->Draw("LumiBlock>>hLumi","L1_BscMinBiasThreshold1_BptxAND","goff");
+  oh->Draw("LumiBlock>>hLumi5Bx","L1_BscMinBiasThreshold1_BptxAND_5bx","goff");
+  hLumi5Bx->SetXTitle("Lumi Section");
+  hLumi5Bx->SetYTitle("L1_BscMinBiasThreshold1_BptxAND");
+  hLumi->SetLineColor(2);
+  hLumi5Bx->SetMinimum(0.1);
+  TCanvas * cLumi = new TCanvas("cLumi","cLumi",500,500);
+  cLumi->SetLogy();
+  hLumi5Bx->Draw();
+  hLumi->Draw("same");
+  TLegend *l= new TLegend(0.185,0.17,0.6,0.35);;
+  l->SetHeader(Form("Run %d",runNum));;
+  l->AddEntry(hLumi,"bx 0","L");;
+  l->AddEntry(hLumi5Bx,"bx 0#pm2","L");;
+  //l->SetFillStyle(0);;
+  l->SetFillColor(0);;
+  l->Draw();
+}
