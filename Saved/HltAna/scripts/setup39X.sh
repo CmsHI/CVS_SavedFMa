78d29826a352ff2049071d1af74658ac1812f662
frankma <>
2010-11-08 13:12:26 +0000

    update with yetkin's script

--- Saved/HltAna/scripts/setup39X.sh
+++ Saved/HltAna/scripts/setup39X.sh
@@ -2,3 +2,5 @@ cvs co HLTrigger/HLTanalyzers
 cvs co RecoHI/HiEgammaAlgos
 cvs co -d Saved UserCode/SavedFMa/Saved
 cvs co -d CmsHi/Analysis2010 UserCode/CmsHi/Analysis2010
+
+./CmsHi/Analysis2010/scripts/setup.sh
