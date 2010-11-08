36f72d2802dd77fb368cd098cb9ab1a5d6304529
frankma <>
2010-11-08 13:20:58 +0000

    automate wide bx enable

--- Saved/HltAna/scripts/setup39X.sh
+++ Saved/HltAna/scripts/setup39X.sh
@@ -1,6 +1,11 @@
+# Basic Openhlt
 cvs co HLTrigger/HLTanalyzers
 cvs co RecoHI/HiEgammaAlgos
 cvs co -d Saved UserCode/SavedFMa/Saved
 cvs co -d CmsHi/Analysis2010 UserCode/CmsHi/Analysis2010
 
+# Enable wide bx for Openhlt
+sed -i 's/_OR_BXes=false/_OR_BXes=true/' HLTrigger/HLTanalyzers/src/HLTInfo.cc
+
+# Centrality
 ./CmsHi/Analysis2010/scripts/setup.sh
