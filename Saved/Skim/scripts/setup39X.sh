a14e5cee49efd0fa8711a62b0869cf10fd7eec64
frankma <>
2010-11-15 14:07:11 +0000

    first HI data analysis

---
+++ Saved/Skim/scripts/setup39X.sh
@@ -0,0 +1,9 @@
+# analyzers
+cvs co HLTrigger/HLTanalyzers
+cvs co RecoHI/HiEgammaAlgos
+cvs co -d Saved UserCode/SavedFMa/Saved
+cvs co -d CmsHi/Analysis2010 UserCode/CmsHi/Analysis2010
+
+# Centrality
+./CmsHi/Analysis2010/scripts/setup.sh
+
