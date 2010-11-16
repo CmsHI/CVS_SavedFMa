f6f806e4c85526707c9484291fa9a6c1dba27141
frankma <>
2010-11-16 22:12:48 +0000

    used for matt's skim of minbias data: HCPR-MB-151020to151076-PAT.txt

---
+++ Saved/DiJetAna/job/submitAll.py
@@ -0,0 +1,18 @@
+import os
+os.system("./create_custom_python_cfg.sh")
+
+scratch="/net/hidsk0001/d00/scratch/frankma"
+jobTag="1116_trigana"
+
+# ================== 362 ===============
+samples  = {
+    #"HCPR-J50U-151020to151076-PAT":"ana"
+    "HCPR-MB-151020to151076-PAT":"ana"
+    }
+
+for sample,PD in samples.items():
+  inputList="~/inputs_data/"+sample+".txt"
+  outputDir=scratch+"/"+PD+"/"+sample+"_"+jobTag
+  cmd="./submitJob.sh "+inputList+" "+outputDir
+  print cmd
+  os.system(cmd)
