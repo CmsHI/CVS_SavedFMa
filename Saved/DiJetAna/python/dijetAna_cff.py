8f7ece6ba7eff0750486b78685ed5ccf102e563b
frankma <>
2010-11-20 20:08:12 +0000

    default threshold is now raised

--- Saved/DiJetAna/python/dijetAna_cff.py
+++ Saved/DiJetAna/python/dijetAna_cff.py
@@ -5,15 +5,13 @@ from Saved.DiJetAna.dijetAna_cfi import *
 # different jet reco
 djcaloic5 = djcalo.clone(
     jetsrc = cms.InputTag("ic5patJets"),
-    refjetsrc = cms.InputTag("ic5patJets"),
-    nearJetPtMin = cms.double(80)
+    refjetsrc = cms.InputTag("ic5patJets")
     )
 
 djcalokt4 = djcalo.clone(
     jetsrc = cms.InputTag("kt4patJets"),
     #doFJL1Corr = cms.bool(True),
-    refjetsrc = cms.InputTag("kt4patJets"),
-    nearJetPtMin = cms.double(80)
+    refjetsrc = cms.InputTag("kt4patJets")
     )
 
 djcaloak5 = djcalokt4.clone(
