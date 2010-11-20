646d1ce36a3f397b721ede9f2af6408735d557b2
frankma <>
2010-11-20 20:08:37 +0000

    make centrality bin by default

--- Saved/DiJetAna/python/ExtraReco_cff.py
+++ Saved/DiJetAna/python/ExtraReco_cff.py
@@ -2,5 +2,9 @@ import FWCore.ParameterSet.Config as cms
 
 from Saved.DiJetAna.TrackSelection_cff import *
 from HeavyIonsAnalysis.Configuration.analysisProducers_cff import *
+from RecoHI.HiCentralityAlgos.CentralityBin_cfi import *
 
-reco_extra = cms.Sequence(highPurityTracks)
+dj_reco_extra = cms.Sequence(centralityBin #*
+    #highPurityTracks
+    #allTracks
+    )
