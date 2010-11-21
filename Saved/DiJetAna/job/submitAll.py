b944cc90c56d2733a5860016c57883caeaea98e7
frankma <>
2010-11-21 00:19:09 +0000

    first look ana on data

--- Saved/DiJetAna/job/submitAll.py
+++ Saved/DiJetAna/job/submitAll.py
@@ -2,12 +2,14 @@ import os
 os.system("./create_custom_python_cfg.sh")
 
 scratch="/net/hidsk0001/d00/scratch/frankma"
-jobTag="1116_trigana_LIGHT"
+jobTag="djlook1120"
 
 # ================== 362 ===============
 samples  = {
-    #"HCPR-J50U-151020to151076-PAT":"ana"
-    "HCPR-MB-151020to151076-PAT":"ana"
+    "HCPR-J50U-150883to150887-PAT":"ana",
+    "HCPR-J50U-151020to151076-PAT":"ana",
+    "HCPR-J50U-151077to151211-PAT":"ana",
+    "HCPR-J50U-151217-PAT":"ana"
     }
 
 for sample,PD in samples.items():
