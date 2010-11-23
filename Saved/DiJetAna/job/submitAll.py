ff333bef66d2394baad040c46bdcd19ead76a5f6
frankma <>
2010-11-23 22:32:33 +0000

    ran on PR mb with good runs and lumi

--- Saved/DiJetAna/job/submitAll.py
+++ Saved/DiJetAna/job/submitAll.py
@@ -2,14 +2,15 @@ import os
 os.system("./create_custom_python_cfg.sh")
 
 scratch="/net/hidsk0001/d00/scratch/frankma"
-jobTag="djlook1120"
+jobTag="mb1123"
 
 # ================== 362 ===============
 samples  = {
-    "HCPR-J50U-150883to150887-PAT":"ana",
-    "HCPR-J50U-151020to151076-PAT":"ana",
-    "HCPR-J50U-151077to151211-PAT":"ana",
-    "HCPR-J50U-151217-PAT":"ana"
+    #"HCPR-J50U-150883to150887-PAT":"ana",
+    #"HCPR-J50U-151020to151076-PAT":"ana",
+    #"HCPR-J50U-151077to151211-PAT":"ana",
+    #"HCPR-J50U-151217-PAT":"ana"
+    "HCPR-GoodRuns-151027and151058":"ana"
     }
 
 for sample,PD in samples.items():
