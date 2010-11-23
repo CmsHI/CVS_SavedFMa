90c67d554b3c2cfcd43d4401f619f80f071cc89e
frankma <>
2010-11-23 11:00:26 +0000

    add good track selection

--- Saved/DiJetAna/python/ExtraReco_cff.py
+++ Saved/DiJetAna/python/ExtraReco_cff.py
@@ -3,8 +3,10 @@ import FWCore.ParameterSet.Config as cms
 from Saved.DiJetAna.TrackSelection_cff import *
 from HeavyIonsAnalysis.Configuration.analysisProducers_cff import *
 from RecoHI.HiCentralityAlgos.CentralityBin_cfi import *
+from edwenger.HiTrkEffAnalyzer.TrackSelections_cff import *
 
-dj_reco_extra = cms.Sequence(centralityBin #*
+dj_reco_extra = cms.Sequence(centralityBin *
+    hiGoodTracksSelection
     #highPurityTracks
     #allTracks
     )
