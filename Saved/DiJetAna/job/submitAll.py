12901f40074a71df6f95fc566a942043efedd15d
frankma <>
2010-11-24 21:34:43 +0000

    added new runs

--- Saved/DiJetAna/job/submitAll.py
+++ Saved/DiJetAna/job/submitAll.py
@@ -2,15 +2,19 @@ import os
 os.system("./create_custom_python_cfg.sh")
 
 scratch="/net/hidsk0001/d00/scratch/frankma"
-jobTag="GoodTrk1123"
+jobTag="JEta5_1124"
 
 # ================== 362 ===============
 samples  = {
     "HCPR-J50U-150883to150887-PAT":"ana",
     "HCPR-J50U-151020to151076-PAT":"ana",
     "HCPR-J50U-151077to151211-PAT":"ana",
-    "HCPR-J50U-151217-PAT":"ana"
+    "HCPR-J50U-151217-PAT":"ana",
     #"HCPR-GoodRuns-151027and151058":"ana"
+    "HCPR-J50U-151238to151240-PAT":"ana",
+    "HCPR-J50U-151350to151353-PAT":"ana",
+    "HCPR-J50U-151878to151975-PAT":"ana",
+    "HCPR-J50U-151935to151969-PAT":"ana"
     }
 
 for sample,PD in samples.items():
