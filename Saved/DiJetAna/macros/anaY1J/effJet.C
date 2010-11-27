012287ca01233a1f19bb02acd89d4a5c401665f7
frankma <>
2010-11-27 14:33:10 +0000

    compare Q vs UQ sample

--- Saved/DiJetAna/macros/anaY1J/effJet.C
+++ Saved/DiJetAna/macros/anaY1J/effJet.C
@@ -41,29 +41,48 @@ TGraphAsymmErrors *eff(TTree * t,TString var="nljet",TCut sel="",TCut cut="",TSt
 }
 
 TChain * effJet(bool doMC=1,
-    TString infile="dj_Data_MinBias0to20_DijetUnquenched50_d20101124_StdJetGoodTrk1126.root",
-    TString header="QuenDiJet-DataBackground"
+    TString infile0="dj_Data_MinBias_DijetUnquenched80_d20101125_StdJetGoodTrk1126.root",
+    TString infile1="dj_Data_MinBias0to20_DijetUnquenched50_d20101124_StdJetGoodTrk1126.root",
+    TString header="McDiJet-DataBackground"
     )
 {
   TChain * dj = new TChain("djgen/djTree");
+  TChain * dj1 = new TChain("djgen/djTree");
 
-  dj->Add(infile);
-  dj->AddFriend("djcalo = djcalo/djTree",infile);
+  dj->Add(infile0);
+  dj->AddFriend("djcalo = djcalo/djTree",infile0);
   aliases_dijet(dj,1.2,doMC,"djcalo");
-  cout << "Total: " << dj->GetEntries() << endl;
+  cout << "dj0 Total: " << dj->GetEntries() << endl;
 
-  TCut evtSel("cent<10 && nljet>0 && abs(nljeta)<2 && aljet>0 && abs(aljeta)<2");
+  dj1->Add(infile1);
+  dj1->AddFriend("djcalo = djcalo/djTree",infile1);
+  aliases_dijet(dj1,1.2,doMC,"djcalo");
+  cout << "dj1 Total: " << dj1->GetEntries() << endl;
+
+  TCut evtSel("cent<10 && nljet>35 && abs(nljeta)<2 && aljet>0 && abs(aljeta)<2 && jdphi>TMath::Pi()*5/6");
   //evtSel = evtSel && "djgen.nljet>0&&djgen.aljet>0" //for now abs eff --- no selection on mc
-  evtSel = evtSel && "jdphi>TMath::Pi()*5/6";
+  TCut evtSelAw = evtSel && "nljet>120";
   //TCut evtSel("HLT_HIJet50U && cent<10 ");
+
+  TCanvas * cEt = new TCanvas("cEt","cEt",500,500);
+  dj->Draw("nljet",evtSel,"hist");
+  dj->Draw("aljet",evtSel,"sameE");
+  dj1->Draw("nljet",evtSel,"hist same");
+  dj1->Draw("aljet",evtSel,"sameE");
 
   TCanvas * cDr = new TCanvas("cDr","cDr",500,500);
   dj->Draw("tr2nljdr",evtSel,"hist");
   dj->Draw("tr2aljdr",evtSel,"sameE");
 
+  TCanvas * cDr1 = new TCanvas("cDr1","cDr1",500,500);
+  dj1->Draw("tr2nljdr",evtSel,"hist");
+  dj1->Draw("tr2aljdr",evtSel,"sameE");
+
   TGraphAsymmErrors *g0=0,*g1=0,*g2=0,*g3=0,*g4=0;
   g0 = eff(dj,"nljet",evtSel,"tr2nljdr<0.3","NrJEt");
-  g1 = eff(dj,"aljet",evtSel,"tr2aljdr<0.3","AwJEt");
+  g1 = eff(dj,"aljet",evtSelAw,"tr2aljdr<0.3","AwJEt");
+  g2 = eff(dj1,"nljet",evtSel,"tr2nljdr<0.3","NrJEt1");
+  g3 = eff(dj1,"aljet",evtSelAw,"tr2aljdr<0.3","AwJEt1");
 
   // Draw
   TCanvas *cTrigEff = new TCanvas("cTrigEff","cTrigEff",600,600);
@@ -72,15 +91,15 @@ TChain * effJet(bool doMC=1,
   const int nBin=25;
   Float_t bin[nBin+1]={0,5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,90,100,110,120,140,160,200,240,300};
   TH1F *hTmp = new TH1F("hTmp","",nBin,bin);
-  g1->SetLineColor(kGreen+2);
-  g1->SetMarkerColor(kGreen+2);
-  g1->SetMarkerStyle(kOpenSquare);
+  g1->SetLineColor(kGreen+2); g1->SetMarkerColor(kGreen+2); g1->SetMarkerStyle(kOpenSquare);
+  g2->SetLineColor(kBlue); g2->SetMarkerColor(kBlue); g2->SetMarkerStyle(kOpenStar);
   hTmp->SetAxisRange(0,1.3,"Y");
-  hTmp->SetXTitle("Leading E_{T}^{Jet,Corr} [GeV]");
+  hTmp->SetXTitle("E_{T}^{GenJet} [GeV]");
   hTmp->SetYTitle("Eff. (Matched Reco/Gen)");
   hTmp->Draw();
   g0->Draw("p");
-  g1->Draw("p");
+  //g1->Draw("p");
+  g2->Draw("p");
   TLine *l = new TLine(0,1,bin[nBin],1);
   l->SetLineStyle(2);
   l->Draw();
@@ -88,8 +107,9 @@ TChain * effJet(bool doMC=1,
   t->SetHeader(header);
   t->SetBorderSize(0);
   t->SetFillStyle(0);
-  t->AddEntry(g0,"Leading Jet","pl");
-  t->AddEntry(g1,"Away Jet","pl");
+  t->AddEntry(g0,"UnQuen MC - Leading Jet","pl");
+  //t->AddEntry(g1,"UnQuen MC - Away Jet","pl");
+  t->AddEntry(g2,"Quen MC - Leading Jet","pl");
   t->Draw();
 
   return dj;
