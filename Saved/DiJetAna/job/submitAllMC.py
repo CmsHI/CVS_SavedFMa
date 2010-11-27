32b1c35514c7b8aedcf205440a1a822f5024727b
frankma <>
2010-11-27 21:09:17 +0000

    reprocess data mix sample with manual matching

--- Saved/DiJetAna/job/submitAllMC.py
+++ Saved/DiJetAna/job/submitAllMC.py
@@ -2,14 +2,14 @@ import os
 os.system("./create_custom_python_cfg.sh")
 
 scratch="/net/hidsk0001/d00/scratch/frankma"
-jobTag="StdJetGoodTrk1126"
+jobTag="MatchedJetGoodTrk1127"
 
 # ================== 362 ===============
 samples  = {
     #"HydjetQ_DJQ80_F10GSR":"ana",
     #"HydjetQ_DJUQ80_F10GSR":"ana"
-    #"Data_MinBias0to20_DijetUnquenched50_d20101124":"ana",
-    "Data_MinBias_DijetUnquenched80_d20101125":"ana"
+    "Data_MinBias_DijetUnquenched80_d20101125":"ana",
+    "Data_MinBias0to20_DijetUnquenched50_d20101124":"ana"
     }
 
 for sample,PD in samples.items():
