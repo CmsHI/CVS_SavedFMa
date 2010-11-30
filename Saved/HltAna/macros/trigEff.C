94606a499b9ad81de9bd51ff82cf615d80afd345
frankma <>
2010-11-30 22:57:49 +0000

    cleanup for AN

--- Saved/HltAna/macros/trigEff.C
+++ Saved/HltAna/macros/trigEff.C
@@ -8,7 +8,16 @@
 #include <TLine.h>
 #include <TCanvas.h>
 #include <iostream>
+
+#include "Saved/DiJetAna/macros/commonUtility.h"
+#include "Saved/Utilities/macros/graph/tgraphTools.C"
+
 using namespace std;
+
+//const int nBin=11;
+//Float_t bin[nBin+1]={0,5,10,15,30,60,80,100,120,160,200,300};
+const int nBin=25;
+Float_t bin[nBin+1]={0,5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,90,100,110,120,140,160,200,240,300};
 
 TGraphAsymmErrors *calcEff(TH1* h1, TH1* h2,char *hName="hEff")
 {
@@ -23,15 +32,12 @@ TGraphAsymmErrors *eff(TTree * t,TString var="nljet",TCut sel="",TCut cut="",TSt
   cout << "Evt Sel: " << TString(sel) << ": " << t->GetEntries(sel) << endl;
   cout << "Trigger: " << TString(sel&&cut) << ": " << t->GetEntries(sel&&cut) << endl;
 
-  //const int nBin=11;
-  //Float_t bin[nBin+1]={0,5,10,15,30,60,80,100,120,160,200,300};
-  const int nBin=25;
-  Float_t bin[nBin+1]={0,5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,90,100,110,120,140,160,200,240,300};
   TH1F *h = new TH1F("h"+tag,"",nBin,bin);
   TH1F *hCut = new TH1F("hCut"+tag,"",nBin,bin);
   t->Draw(var+">>h"+tag,sel);
   t->Draw(var+">>hCut"+tag,sel&&cut);
   TGraphAsymmErrors *g = calcEff(h,hCut);
+  clearXErrorBar(g);
   //g->Draw("ap");
   return g;
 }
@@ -39,7 +45,9 @@ TGraphAsymmErrors *eff(TTree * t,TString var="nljet",TCut sel="",TCut cut="",TSt
 TChain * trigEff(
     //TString infile="../process_aod/dj_HCPR-MB-151020to151076_trigana1116.root",
     TString infile="~/scratch01/ana/HCPR-GoodRuns-151027and151058_mb1123/merge/dj_HCPR-MB-151027and151058_mb1123.root",
-    TString header="HLT_HIJet50U_Core"
+    TString header="HLT_HIJet50U_Core, |#eta|<3",
+    TString var="nljet",
+    TString Tag="Corr"
     )
 {
   TChain * dj = new TChain("djcalo/djTree");
@@ -56,26 +64,23 @@ TChain * trigEff(
 
   TGraphAsymmErrors *g0=0,*g1=0,*g2=0,*g3=0,*g4=0;
   //g0 = eff(dj,"nljet",evtSel,"hlt[2]","ic5pu");
-  g0 = eff(dj,"nljrawet",evtSel,"hlt[2]","ic5pu");
+  g0 = eff(dj,var,evtSel,"hlt[2]","ic5pu");
   //g1 = eff(dj,"joc.nljet",evtSel,"hlt[2]","joc");
   //g2 = eff(dj,"ak5.nljet",evtSel,"hlt[2]","ak5");
   //g3 = eff(dj,"kt4.nljet",evtSel,"hlt[2]","kt4");
   //g4 = eff(dj,"ic5.nljet",evtSel,"hlt[2]","ic5fj");
 
   // Draw
-  TCanvas *cTrigEff = new TCanvas("cTrigEff","cTrigEff",500,500);
-  //const int nBin=11;
-  //Float_t bin[nBin+1]={0,5,10,15,30,60,80,100,120,160,200,300};
-  const int nBin=25;
-  Float_t bin[nBin+1]={0,5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,90,100,110,120,140,160,200,240,300};
+  TCanvas *cTrigEff = new TCanvas("cTrigEff","cTrigEff",500,550);
   TH1F *hTmp = new TH1F("hTmp","",nBin,bin);
   //g1->SetLineColor(kGreen+2); g1->SetMarkerColor(kGreen+2); g1->SetMarkerStyle(kOpenSquare);
   //g2->SetLineColor(kRed); g2->SetMarkerColor(kRed); g2->SetMarkerStyle(kOpenCircle);
   //g3->SetMarkerColor(kBlue); g3->SetLineColor(kBlue); g3->SetMarkerStyle(kOpenStar);
   //g4->SetMarkerStyle(kOrange); g4->SetLineStyle(kOrange); g4->SetMarkerStyle(kOpenDiamond);
   hTmp->SetAxisRange(0,1.3,"Y");
-  hTmp->SetXTitle("Leading E_{T}^{Jet,UnCorr} [GeV]");
+  hTmp->SetXTitle("Leading E_{T}^{Jet,"+Tag+"} [GeV]");
   hTmp->SetYTitle("Eff. (Trigger/MB)");
+  handsomeTH1(hTmp);
   hTmp->Draw();
   g0->Draw("p");
   //g1->Draw("p");
@@ -89,12 +94,18 @@ TChain * trigEff(
   t->SetHeader(header);
   t->SetBorderSize(0);
   t->SetFillStyle(0);
-  t->AddEntry(g0,"Ic5pu","pl");
+  //t->AddEntry("","|#eta|<3","");
+  //t->AddEntry(g0,"Ic5pu","pl");
   //t->AddEntry(g1,"Ic5pu online","pl");
   //t->AddEntry(g2,"Ak5","pl");
   //t->AddEntry(g3,"Kt4","pl");
   //t->AddEntry(g4,"Ic5Fj","pl");
   t->Draw();
+  addCms(cTrigEff);
+  cTrigEff->Print("TrigEff"+Tag+"ected151058and151025.gif");
+  cTrigEff->Print("TrigEff"+Tag+"ected151058and151025.eps");
+  cTrigEff->Print("TrigEff"+Tag+"ected151058and151025.pdf");
+  cTrigEff->Print("TrigEff"+Tag+"ected151058and151025.C");
 
   return dj;
 }
