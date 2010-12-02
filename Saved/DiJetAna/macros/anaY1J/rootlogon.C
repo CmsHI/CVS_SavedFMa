553e026c19d04e8fe0f3f8c7b1005a8850d91bac
frankma <>
2010-12-02 19:43:06 +0000

    sync

--- Saved/DiJetAna/macros/anaY1J/rootlogon.C
+++ Saved/DiJetAna/macros/anaY1J/rootlogon.C
@@ -4,7 +4,6 @@
   // in your rootlogon.C, the CMS setup is executed only if the CMS
   // environment is set up.
   //
-  /*
   TString cmsswbase = getenv("CMSSW_BASE");
   if (cmsswbase.Length() > 0) {
     //
@@ -13,25 +12,32 @@
     //
     cout << "Loading FW Lite setup." << endl;
     gSystem->Load("libFWCoreFWLite.so");
-    gSystem->Load("libDataFormatsFWLite.so");
-    gSystem->Load("libDataFormatsPatCandidates.so");
-    gSystem->Load("libDataFormatsCommon.so");
+    //gSystem->Load("libDataFormatsFWLite.so");
+    //gSystem->Load("libDataFormatsPatCandidates.so");
+    //gSystem->Load("libDataFormatsCommon.so");
+    gSystem->Load("libDataFormatsMath.so");
+    gSystem->Load("pluginSavedDiJetAna.so");
 
     // JEC
-    gSystem->Load("libCondFormatsJetMETObjects.so");
+    //gSystem->Load("libCondFormatsJetMETObjects.so");
 
     AutoLibraryLoader::enable();
   }
-  */
 
   // include paths
   gSystem->AddIncludePath("-I$CMSSW_BASE/src/");
 
   // styles
-  gROOT->Macro("$CMSSW_BASE/src/Saved/Utilities/macros/logon/rootlogon_dNdEtaBase.C+");
+  gROOT->Macro("$CMSSW_BASE/src/MNguyen/JetAnalysisMacros/InclusiveJetAnalyzer/rootlogon.C+");
+  //gROOT->Macro("$CMSSW_BASE/src/Saved/Utilities/macros/logon/rootlogon_dNdEtaBase.C+");
+  //gROOT->Macro("$CMSSW_BASE/src/Saved/Utilities/macros/logon/rootcolors.C+");
+  gStyle->SetOptStat(0);   
   //gStyle->SetOptStat(1111);
-  gStyle->SetPadLeftMargin(0.2);
+  //gStyle->SetPadLeftMargin(0.2);
+  gStyle->SetOptFit(0);
+
 
   // helper classes
+  gROOT->Macro("$CMSSW_BASE/src/Saved/DiJetAna/macros/anaY1J/JetFragAna.C+");
   gROOT->Macro("$CMSSW_BASE/src/Saved/Utilities/macros/cplot/CPlot.cc+");
 }
