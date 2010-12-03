8642c6eb3ed1d49389a914b1bbce2cfef1ddaa0f
frankma <>
2010-12-03 07:59:25 +0000

    added ana commandline parameters

--- Saved/DiJetAna/macros/anaY1J/anaJetFrag.C
+++ Saved/DiJetAna/macros/anaY1J/anaJetFrag.C
@@ -1,25 +1,44 @@
+#include <iostream>
+#include "Saved/DiJetAna/macros/aliases_dijet.C"
 #include "Saved/DiJetAna/macros/anaY1J/JetFragAna.h"
 #include <TCanvas.h>
-#include <iostream>
 using namespace std;
 
-void anaJetFrag(TString infile="dj_HCPR-GoodTrkAndPixel_CleanEvt1130.root")
+void anaJetFrag(int doMC=0,
+    TString evtBase="S1",
+    TString AnaVersion = "a1006",
+    TString modName = "djcalo",
+    Double_t NrJEtMin = 120,
+    Double_t NrJEtMax = 500,
+    Double_t AwJEtMin = 50,
+    Double_t AwJEtMax = 500,
+    Double_t AwJEtaMax = 2.,
+    Double_t JDPhiMin = 2.5,
+    TString DJCutType = "Ana",
+    TString TrkCutType = "Ana",
+    const char * inFile0Name="dj_HCPR-GoodTrkAndPixel_CleanEvt1130.root",
+    TString SrcName = "HydjUQDJ80")
 {
-  TChain * dj = new TChain("djcalo/djTree");
-  dj->Add(infile);
+  // Define Inputs
+  cout << "======= Inputs: ========" << endl;
+  cout << inFile0Name << endl;
+  cout << "Analyze: " << modName << endl;
+  TChain * djTree = new TChain(modName+"/djTree","dijet Tree");
+  djTree->Add(inFile0Name);
+  aliases_dijet(djTree,doMC);
+  cout << " # entries: " << djTree->GetEntries() << endl;
 
-  JetFragAna jana(dj);
-  jana.cut.SetDJEt(200,500,50,500,2.5);
-  jana.cut.BaseCutType="S1";
-  jana.cut.DJCutType = "Ana";
-  jana.cut.TrkCutType = "Ana";
+  JetFragAna jana(djTree,SrcName,doMC);
+  jana.cut.SetDJEt(NrJEtMin,NrJEtMax,AwJEtMin,AwJEtMax,JDPhiMin);
+  jana.cut.BaseCutType=evtBase;
+  jana.cut.DJCutType = DJCutType;
+  jana.cut.TrkCutType = TrkCutType;
   jana.cut.SetCut();
 
   // -- analysis selections --
   cout << endl << "====== DJ Selection: " << jana.cut.DJCutType << " ======" << endl;
   cout << " DJ selection: " << TString(jana.cut.FinDJCut()) << endl;
-  cout << "# DJ events passed: " << dj->GetEntries(jana.cut.FinDJCut()) << endl;
-
+  cout << "# DJ events passed: " << djTree->GetEntries(jana.cut.FinDJCut()) << endl;
 
   // do ana
   jana.Loop();
