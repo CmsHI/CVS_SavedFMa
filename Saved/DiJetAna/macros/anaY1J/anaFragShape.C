a1911c6744b3709c9b31b3e1419615741fb973c0
frankma <>
2010-12-03 14:46:10 +0000

    look at different Aj, update label

--- Saved/DiJetAna/macros/anaY1J/anaFragShape.C
+++ Saved/DiJetAna/macros/anaY1J/anaFragShape.C
@@ -1,10 +1,19 @@
+#include "Saved/DiJetAna/macros/aliases_dijet.C"
+
 void anaFragShape(TString infile="dj_HCPR-GoodTrkAndPixel_CleanEvt1130.root")
 {
   TChain * djcalo = new TChain("djcalo/djTree");
   djcalo->Add(infile);
+  aliases_dijet(djcalo);
   TChain * djcalopix = new TChain("djcalo_pxl/djTree");
   djcalopix->Add(infile);
-  TString evtSel("(cent<10 && nljet>120 && abs(nljeta)<2 && aljet>50 && abs(aljeta)<2 && jdphi>2.5)");
+  aliases_dijet(djcalopix);
+  TString evtSel("(cent<10 && nljet>120 && abs(nljeta)<2 && aljet>50 && abs(aljeta)<2 && jdphi>2.5 && Aj>0.24)");
+  TH1::SetDefaultSumw2();
+
+  TCanvas * c0 = new TCanvas("c0","c0",500,500);
+  djcalo->Draw("Aj>>hAj(20,0,1)",evtSel);
+  djcalopix->Draw("Aj",evtSel,"Esame");
 
   TCanvas * c2 = new TCanvas("c2","c2",500,500);
   TH1D * hJDPhi = new TH1D("hJDPhi","hJDPhi",50,0,3.1416);
@@ -13,10 +22,10 @@ void anaFragShape(TString infile="dj_HCPR-GoodTrkAndPixel_CleanEvt1130.root")
   Float_t numDJ2 = djcalopix->Draw("jdphi>>hJDPhi2",evtSel);
   cout << "num djs: " << numDJ << "  djs(pix)" << numDJ2 << endl;
 
-  TH1D * hPNDRTrk = new TH1D("hPNDRTrk",";#DeltaR(trk,jet);1/N_{DJ} dp_{T}^{Trk}/d(#Delta#phi#Delta#eta)",10,0,TMath::PiOver2());
-  TH1D * hPNDRPix = new TH1D("hPNDRPix",";#DeltaR(trk,jet);1/N_{DJ} dp_{T}^{Trk}/d(#Delta#phi#Delta#eta)",10,0,TMath::PiOver2());
-  TH1D * hPADRTrk = new TH1D("hPADRTrk",";#DeltaR(trk,jet);1/N_{DJ} dp_{T}^{Trk}/d(#Delta#phi#Delta#eta)",10,0,TMath::PiOver2());
-  TH1D * hPADRPix = new TH1D("hPADRPix",";#DeltaR(trk,jet);1/N_{DJ} dp_{T}^{Trk}/d(#Delta#phi#Delta#eta)",10,0,TMath::PiOver2());
+  TH1D * hPNDRTrk = new TH1D("hPNDRTrk",";R(trk,jet);1/(N_{DJ} 2#piR) dp_{T}^{Trk}/dR",10,0,TMath::PiOver2());
+  TH1D * hPNDRPix = new TH1D("hPNDRPix",";R(trk,jet);1/(N_{DJ} 2#piR) dp_{T}^{Trk}/dR",10,0,TMath::PiOver2());
+  TH1D * hPADRTrk = new TH1D("hPADRTrk",";R(trk,jet);1/(N_{DJ} 2#piR) dp_{T}^{Trk}/dR",10,0,TMath::PiOver2());
+  TH1D * hPADRPix = new TH1D("hPADRPix",";R(trk,jet);1/(N_{DJ} 2#piR) dp_{T}^{Trk}/dR",10,0,TMath::PiOver2());
   djcalo->Draw("pndr>>hPNDRTrk",Form("(%s&&ppt>=1.2)*(ppt/(TMath::TwoPi()*pndr))",evtSel.Data()));
   djcalopix->Draw("pndr>>hPNDRPix",Form("(%s&&ppt>=0.3&&ppt<1.2)*(ppt/(TMath::TwoPi()*pndr))",evtSel.Data()));
   djcalo->Draw("padr>>hPADRTrk",Form("(%s&&ppt>=1.2)*(ppt/(TMath::TwoPi()*padr))",evtSel.Data()));
@@ -25,7 +34,7 @@ void anaFragShape(TString infile="dj_HCPR-GoodTrkAndPixel_CleanEvt1130.root")
   hPNDRPix->Scale(1./(numDJ*hPNDRPix->GetBinWidth(1)));
   hPADRTrk->Scale(1./(numDJ*hPADRTrk->GetBinWidth(1)));
   hPADRPix->Scale(1./(numDJ*hPADRPix->GetBinWidth(1)));
-  hPNDRPix->SetMinimum(10);
+  hPNDRPix->SetMinimum(50);
   hPNDRPix->SetMaximum(100);
   hPNDRTrk->SetMinimum(10);
   hPNDRTrk->SetMaximum(2000);
@@ -38,7 +47,6 @@ void anaFragShape(TString infile="dj_HCPR-GoodTrkAndPixel_CleanEvt1130.root")
   hPADRPix->SetMarkerColor(kBlue);
   hPADRTrk->SetMarkerColor(kBlue);
   TCanvas * c3 = new TCanvas("c3","c3",500,500);
-  c3->SetLogy();
   hPNDRPix->Draw("E");
   hPADRPix->Draw("Esame");
   TCanvas * c3_2 = new TCanvas("c3_2","c3_2",500,500);
