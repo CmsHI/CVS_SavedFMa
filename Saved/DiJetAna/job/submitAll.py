f468a5fc838a9dcf6b6e7976159428bc94d235d9
frankma <>
2010-12-04 12:33:35 +0000

    process matt's new clean pat files with hiGoodMergedTracks

--- Saved/DiJetAna/job/submitAll.py
+++ Saved/DiJetAna/job/submitAll.py
@@ -2,19 +2,20 @@ import os
 os.system("./create_custom_python_cfg.sh")
 
 scratch="/net/hidsk0001/d00/scratch/frankma"
-jobTag="DPhi26_1124"
+jobTag="StdAna_1204"
 
 # ================== 362 ===============
 samples  = {
     #"HCPR-GoodRuns-151027and151058":"ana"
-    "HCPR-J50U-150883to150887-PAT":"ana",
-    "HCPR-J50U-151020to151076-PAT":"ana",
-    "HCPR-J50U-151077to151211-PAT":"ana",
-    "HCPR-J50U-151217-PAT":"ana",
-    "HCPR-J50U-151238to151240-PAT":"ana",
-    "HCPR-J50U-151350to151353-PAT":"ana",
-    "HCPR-J50U-151878to151975-PAT":"ana",
-    "HCPR-J50U-151935to151969-PAT":"ana"
+    #"HCPR-J50U-150883to150887-PAT":"ana",
+    #"HCPR-J50U-151020to151076-PAT":"ana",
+    #"HCPR-J50U-151077to151211-PAT":"ana",
+    #"HCPR-J50U-151217-PAT":"ana",
+    #"HCPR-J50U-151238to151240-PAT":"ana",
+    #"HCPR-J50U-151350to151353-PAT":"ana",
+    #"HCPR-J50U-151878to151975-PAT":"ana",
+    #"HCPR-J50U-151935to151969-PAT":"ana"
+    "HCPR-J50U-OfficialJSON_hiGoodMergedTracks_Runs_152561_to_152643_RECOPAT-v1":"ana"
     }
 
 for sample,PD in samples.items():
