9706948027a5f6eb15df716e2999a9e962f6f517
frankma <>
2010-12-08 22:06:39 +0000

    script to run trk energy anas

---
+++ Saved/DiJetAna/macros/anaY1J/runAnaJetFrag.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -
+infileData="dj_HCPR-J50U-hiGoodMergedTracks_OfficialSelv2.root"
+infilePyquen="dj_PyquenUQ80_hiGoodMergedTracks_OfficialSelv2.root"
+module="djcalo_tower"
+BkgSub="EtaRefl"
+root -b -q 'anaJetFrag.C+(0,"S1","1208","'$module'",0,10,120,500,50,500,2.,2.094,0.,1.0,"Ana","'$BkgSub'","'$infileData'","HCPR_J50U")'
+root -b -q 'anaJetFrag.C+(0,"S1","1208","'$module'",10,30,120,500,50,500,2.,2.094,0.,1.0,"Ana","'$BkgSub'","'$infileData'","HCPR_J50U")'
+root -b -q 'anaJetFrag.C+(0,"S1","1208","'$module'",30,100,120,500,50,500,2.,2.094,0.,1.0,"Ana","'$BkgSub'","'$infileData'","HCPR_J50U")'
+root -b -q 'anaJetFrag.C+(0,"S1","1208","'$module'",0,30,120,500,50,500,2.,2.094,0.24,1.0,"Ana","'$BkgSub'","'$infileData'","HCPR_J50U")'
+root -b -q 'anaJetFrag.C+(0,"S1","1208","'$module'",0,30,120,500,50,500,2.,2.094,0.,0.24,"Ana","'$BkgSub'","'$infileData'","HCPR_J50U")'
+root -b -q 'anaJetFrag.C+(0,"S1","1208","'$module'",0,100,120,500,50,500,2.,2.094,0.,1.0,"Ana","'$BkgSub'","'$infileData'","PyquenUQ80")'
+root -b -q 'anaJetFrag.C+(0,"S1","1208","'$module'",0,100,120,500,50,500,2.,2.094,0.,1.0,"Ana","'$BkgSub'","'$infilePyquen'","PyquenUQ80")'
---
+++ Saved/DiJetAna/macros/anaY1J/runDrawJetFragBalance.sh
@@ -0,0 +1,6 @@
+root -b -q 'drawJetFragBalance.C("jfh_HCPR_J50U_djcalo_tower_Cent0to10_Aj0to100_SubEtaRefl")'
+root -b -q 'drawJetFragBalance.C("jfh_HCPR_J50U_djcalo_tower_Cent10to30_Aj0to100_SubEtaRefl")'
+root -b -q 'drawJetFragBalance.C("jfh_HCPR_J50U_djcalo_tower_Cent30to100_Aj0to100_SubEtaRefl")'
+root -b -q 'drawJetFragBalance.C("jfh_HCPR_J50U_djcalo_tower_Cent0to30_Aj24to100_SubEtaRefl")'
+root -b -q 'drawJetFragBalance.C("jfh_HCPR_J50U_djcalo_tower_Cent0to30_Aj0to24_SubEtaRefl")'
+root -b -q 'drawJetFragBalance.C("jfh_PyquenUQ80_djcalo_tower_Cent0to100_Aj0to100_SubEtaRefl")'
