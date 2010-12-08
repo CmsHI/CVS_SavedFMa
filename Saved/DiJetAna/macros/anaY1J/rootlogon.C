2c49d7f10eea7382cc7dcc9f7684d822ac9c8b4d
frankma <>
2010-12-08 13:48:02 +0000

    simplify

--- Saved/DiJetAna/macros/anaY1J/rootlogon.C
+++ Saved/DiJetAna/macros/anaY1J/rootlogon.C
@@ -40,6 +40,6 @@
 
 
   // helper classes
-  gROOT->Macro("$CMSSW_BASE/src/Saved/DiJetAna/macros/anaY1J/JetFragAna.C+");
+  //gROOT->Macro("$CMSSW_BASE/src/Saved/DiJetAna/macros/anaY1J/JetFragAna.C+");
   gROOT->Macro("$CMSSW_BASE/src/Saved/Utilities/macros/cplot/CPlot.cc+");
 }
