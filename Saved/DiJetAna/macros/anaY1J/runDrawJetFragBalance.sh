5f8ee059992d3374d5f282cfab121e4ce290ca5e
frankma <>
2010-12-08 22:28:55 +0000

    add parameters to histogram names

--- Saved/DiJetAna/macros/anaY1J/runDrawJetFragBalance.sh
+++ Saved/DiJetAna/macros/anaY1J/runDrawJetFragBalance.sh
@@ -1,6 +1,10 @@
-root -b -q 'drawJetFragBalance.C("jfh_HCPR_J50U_djcalo_tower_Cent0to10_Aj0to100_SubEtaRefl")'
-root -b -q 'drawJetFragBalance.C("jfh_HCPR_J50U_djcalo_tower_Cent10to30_Aj0to100_SubEtaRefl")'
-root -b -q 'drawJetFragBalance.C("jfh_HCPR_J50U_djcalo_tower_Cent30to100_Aj0to100_SubEtaRefl")'
-root -b -q 'drawJetFragBalance.C("jfh_HCPR_J50U_djcalo_tower_Cent0to30_Aj24to100_SubEtaRefl")'
-root -b -q 'drawJetFragBalance.C("jfh_HCPR_J50U_djcalo_tower_Cent0to30_Aj0to24_SubEtaRefl")'
-root -b -q 'drawJetFragBalance.C("jfh_PyquenUQ80_djcalo_tower_Cent0to100_Aj0to100_SubEtaRefl")'
+#!/bin/bash -
+anaV=1208
+module="djcalo_tower"
+BkgSub="SubEtaRefl"
+root -b -q 'drawJetFragBalance.C("jfh'$anaV'_HCPR_J50U_'$module'_Cent0to10_Aj0to100_'$BkgSub'")'
+root -b -q 'drawJetFragBalance.C("jfh'$anaV'_HCPR_J50U_'$module'_Cent10to30_Aj0to100_'$BkgSub'")'
+root -b -q 'drawJetFragBalance.C("jfh'$anaV'_HCPR_J50U_'$module'_Cent30to100_Aj0to100_'$BkgSub'")'
+root -b -q 'drawJetFragBalance.C("jfh'$anaV'_HCPR_J50U_'$module'_Cent0to30_Aj24to100_'$BkgSub'")'
+root -b -q 'drawJetFragBalance.C("jfh'$anaV'_HCPR_J50U_'$module'_Cent0to30_Aj0to24_'$BkgSub'")'
+root -b -q 'drawJetFragBalance.C("jfh'$anaV'_PyquenUQ80_'$module'_Cent0to100_Aj0to100_'$BkgSub'")'
