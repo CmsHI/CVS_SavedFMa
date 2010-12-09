ecea37356a342463588b7e4685618b6818444ea3
frankma <>
2010-12-09 09:25:11 +0000

    new 6 plots

--- Saved/DiJetAna/macros/anaY1J/runAnaJetFrag.sh
+++ Saved/DiJetAna/macros/anaY1J/runAnaJetFrag.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -
-anaV=1208
+anaV=1208b
 infileData="dj_HCPR-J50U-hiGoodMergedTracks_OfficialSelv2.root"
 infilePyquen="dj_PyquenUQ80_hiGoodMergedTracks_OfficialSelv2.root"
-module="djcalo_tower"
+module="djcalo"
 BkgSub="EtaRefl"
-root -b -q 'anaJetFrag.C+(0,"S1","'$anaV'","'$module'",0,10,120,500,50,500,2.,2.094,0.,1.0,"Ana","'$BkgSub'","'$infileData'","HCPR_J50U")'
-root -b -q 'anaJetFrag.C+(0,"S1","'$anaV'","'$module'",10,30,120,500,50,500,2.,2.094,0.,1.0,"Ana","'$BkgSub'","'$infileData'","HCPR_J50U")'
-root -b -q 'anaJetFrag.C+(0,"S1","'$anaV'","'$module'",30,100,120,500,50,500,2.,2.094,0.,1.0,"Ana","'$BkgSub'","'$infileData'","HCPR_J50U")'
-root -b -q 'anaJetFrag.C+(0,"S1","'$anaV'","'$module'",0,30,120,500,50,500,2.,2.094,0.24,1.0,"Ana","'$BkgSub'","'$infileData'","HCPR_J50U")'
-root -b -q 'anaJetFrag.C+(0,"S1","'$anaV'","'$module'",0,30,120,500,50,500,2.,2.094,0.,0.24,"Ana","'$BkgSub'","'$infileData'","HCPR_J50U")'
-root -b -q 'anaJetFrag.C+(0,"S1","'$anaV'","'$module'",0,100,120,500,50,500,2.,2.094,0.,1.0,"Ana","'$BkgSub'","'$infileData'","PyquenUQ80")'
 root -b -q 'anaJetFrag.C+(0,"S1","'$anaV'","'$module'",0,100,120,500,50,500,2.,2.094,0.,1.0,"Ana","'$BkgSub'","'$infilePyquen'","PyquenUQ80")'
+root -b -q 'anaJetFrag.C+(0,"S1","'$anaV'","'$module'",0,30,120,500,50,500,2.,2.094,0.,1.0,"Ana","'$BkgSub'","'$infileData'","HCPR_J50U")'
+root -b -q 'anaJetFrag.C+(0,"S1","'$anaV'","'$module'",30,100,120,500,50,500,2.,2.094,0.,1.0,"Ana","'$BkgSub'","'$infileData'","HCPR_J50U")'
+
+root -b -q 'anaJetFrag.C+(0,"S1","'$anaV'","'$module'",0,100,120,500,50,500,2.,2.094,0.24,1.0,"Ana","'$BkgSub'","'$infilePyquen'","PyquenUQ80")'
+root -b -q 'anaJetFrag.C+(0,"S1","'$anaV'","'$module'",0,30,120,500,50,500,2.,2.094,0.,0.24,"Ana","'$BkgSub'","'$infileData'","HCPR_J50U")'
+root -b -q 'anaJetFrag.C+(0,"S1","'$anaV'","'$module'",0,30,120,500,50,500,2.,2.094,0.24,1.0,"Ana","'$BkgSub'","'$infileData'","HCPR_J50U")'
--- Saved/DiJetAna/macros/anaY1J/runDrawJetFragBalance.sh
+++ Saved/DiJetAna/macros/anaY1J/runDrawJetFragBalance.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -
-anaV=1208
-module="djcalo_tower"
+anaV=1208b
+module="djcalo"
 BkgSub="SubEtaRefl"
-root -b -q 'drawJetFragBalance.C("jfh'$anaV'_HCPR_J50U_'$module'_Cent0to10_Aj0to100_'$BkgSub'")'
-root -b -q 'drawJetFragBalance.C("jfh'$anaV'_HCPR_J50U_'$module'_Cent10to30_Aj0to100_'$BkgSub'")'
+root -b -q 'drawJetFragBalance.C("jfh'$anaV'_PyquenUQ80_'$module'_Cent0to100_Aj0to100_'$BkgSub'")'
+root -b -q 'drawJetFragBalance.C("jfh'$anaV'_HCPR_J50U_'$module'_Cent0to30_Aj0to100_'$BkgSub'")'
 root -b -q 'drawJetFragBalance.C("jfh'$anaV'_HCPR_J50U_'$module'_Cent30to100_Aj0to100_'$BkgSub'")'
+
+root -b -q 'drawJetFragBalance.C("jfh'$anaV'_PyquenUQ80_'$module'_Cent0to100_Aj24to100_'$BkgSub'")'
 root -b -q 'drawJetFragBalance.C("jfh'$anaV'_HCPR_J50U_'$module'_Cent0to30_Aj24to100_'$BkgSub'")'
 root -b -q 'drawJetFragBalance.C("jfh'$anaV'_HCPR_J50U_'$module'_Cent0to30_Aj0to24_'$BkgSub'")'
-root -b -q 'drawJetFragBalance.C("jfh'$anaV'_PyquenUQ80_'$module'_Cent0to100_Aj0to100_'$BkgSub'")'
