12aee92250e7bd3cf72bfee211a2aa3a000e548b
frankma <>
2010-12-09 16:40:47 +0000

    add case for genp, add cent bings

--- Saved/DiJetAna/macros/anaY1J/runAnaJetFrag.sh
+++ Saved/DiJetAna/macros/anaY1J/runAnaJetFrag.sh
@@ -4,7 +4,10 @@ infileData="dj_HCPR-J50U-hiGoodMergedTracks_OfficialSelv2.root"
 infilePyquen="dj_PyquenUQ80_hiGoodMergedTracks_OfficialSelv2.root"
 module="djcalo"
 BkgSub="EtaRefl"
+root -b -q 'anaJetFrag.C+(0,"S1","'$anaV'","djcalo_genp",0,100,120,500,50,500,2.,2.094,0.,1.0,"Ana","'$BkgSub'","'$infilePyquen'","PyquenUQ80")'
 root -b -q 'anaJetFrag.C+(0,"S1","'$anaV'","'$module'",0,100,120,500,50,500,2.,2.094,0.,1.0,"Ana","'$BkgSub'","'$infilePyquen'","PyquenUQ80")'
+root -b -q 'anaJetFrag.C+(0,"S1","'$anaV'","'$module'",0,10,120,500,50,500,2.,2.094,0.,1.0,"Ana","'$BkgSub'","'$infileData'","HCPR_J50U")'
+root -b -q 'anaJetFrag.C+(0,"S1","'$anaV'","'$module'",10,30,120,500,50,500,2.,2.094,0.,1.0,"Ana","'$BkgSub'","'$infileData'","HCPR_J50U")'
 root -b -q 'anaJetFrag.C+(0,"S1","'$anaV'","'$module'",0,30,120,500,50,500,2.,2.094,0.,1.0,"Ana","'$BkgSub'","'$infileData'","HCPR_J50U")'
 root -b -q 'anaJetFrag.C+(0,"S1","'$anaV'","'$module'",30,100,120,500,50,500,2.,2.094,0.,1.0,"Ana","'$BkgSub'","'$infileData'","HCPR_J50U")'
 
