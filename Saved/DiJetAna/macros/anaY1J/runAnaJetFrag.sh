bbc360399bb220b3b1baadae875c5db8a4cd14fb
frankma <>
2010-12-10 13:36:39 +0000

    plots for first paper

--- Saved/DiJetAna/macros/anaY1J/runAnaJetFrag.sh
+++ Saved/DiJetAna/macros/anaY1J/runAnaJetFrag.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -
-anaV=CorrEtaLowPt
-infileData="dj_HCPR-J50U-hiGoodMergedTracks_OfficialSelv2.root"
+anaV=CorrEtaHighPt
+infileData="dj_HCPR-J50U-hiGoodMergedTracks_OfficialSelv2_Final0.root"
 #infilePyquen="dj_PyquenUQ80_hiGoodMergedTracks_OfficialSelv2.root"
 infilePyquen="dj_PyquenUQ80_hiGoodMergedTracks_VtxPatch_v1_OfficialSelv2GenAll.root"
 module="djcalo"
@@ -12,6 +12,7 @@ root -b -q 'anaJetFrag.C+(0,"S1","'$anaV'","'$module'",10,30,120,500,50,500,2.,2
 root -b -q 'anaJetFrag.C+(0,"S1","'$anaV'","'$module'",0,30,120,500,50,500,2.,2.094,0.,1.0,"Ana","'$BkgSub'","'$infileData'","HCPR_J50U")'
 root -b -q 'anaJetFrag.C+(0,"S1","'$anaV'","'$module'",30,100,120,500,50,500,2.,2.094,0.,1.0,"Ana","'$BkgSub'","'$infileData'","HCPR_J50U")'
 
+root -b -q 'anaJetFrag.C+(0,"S1","'$anaV'","'$module'",0,100,120,500,50,500,2.,2.094,0.,0.24,"Ana","'$BkgSub'","'$infilePyquen'","PyquenUQ80")'
 root -b -q 'anaJetFrag.C+(0,"S1","'$anaV'","'$module'",0,100,120,500,50,500,2.,2.094,0.24,1.0,"Ana","'$BkgSub'","'$infilePyquen'","PyquenUQ80")'
 root -b -q 'anaJetFrag.C+(0,"S1","'$anaV'","'$module'",0,30,120,500,50,500,2.,2.094,0.,0.24,"Ana","'$BkgSub'","'$infileData'","HCPR_J50U")'
 root -b -q 'anaJetFrag.C+(0,"S1","'$anaV'","'$module'",0,30,120,500,50,500,2.,2.094,0.24,1.0,"Ana","'$BkgSub'","'$infileData'","HCPR_J50U")'
