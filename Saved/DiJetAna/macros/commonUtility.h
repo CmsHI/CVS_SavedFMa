6f6462cb8ed0acce3f188f3ffe5da028862d20ed
frankma <>
2010-12-13 15:29:16 +0000

    more legible text

--- Saved/DiJetAna/macros/commonUtility.h
+++ Saved/DiJetAna/macros/commonUtility.h
@@ -213,7 +213,7 @@ void addCms(TCanvas *c)
 void drawText(const char *text, float xp, float yp){
    TLatex *tex = new TLatex(xp,yp,text);
    tex->SetTextFont(63);
-   tex->SetTextSize(16);
+   tex->SetTextSize(20);
    //tex->SetTextSize(0.05);                                                                   
    tex->SetTextColor(kBlack);
    tex->SetLineWidth(1);
