1be4bf59b9ed0eaf0a68dedd4cc73a05c0ab79c2
frankma <>
2010-12-14 10:47:28 +0000

    draw also pythia+hydjet

--- Saved/DiJetAna/macros/draw/drawPPt.C
+++ Saved/DiJetAna/macros/draw/drawPPt.C
@@ -8,7 +8,9 @@
 using namespace std;
 
 void drawPPt(TString infileData="",
-    TString infileMc="", bool doLeg=false)
+    TString infilePy="",
+    TString infileHyPy="",
+    bool doLeg=true)
 {
   TFile * infData = new TFile(infileData);
   TH1D * hNrBgSubData = (TH1D*)infData->Get("hNrCPPtBgSub");
@@ -21,12 +23,24 @@ void drawPPt(TString infileData="",
   hAwBgSubData->SetMarkerColor(2);
   hAwBgSubData->SetLineColor(2);
 
-  TFile * infMc = new TFile(infileMc);
-  TH1D * hNrBgSubMc = (TH1D*)infMc->Get("hNrCPPtBgSub");
-  TH1D * hAwBgSubMc = (TH1D*)infMc->Get("hAwCPPtBgSub");
-  normHist(hNrBgSubMc,0,true);
-  hNrBgSubMc->SetLineStyle(7);
-  normHist(hAwBgSubMc,0,true);
+  TFile * infPy = new TFile(infilePy);
+  TH1D * hNrBgSubPy = (TH1D*)infPy->Get("hNrCPPtBgSub");
+  TH1D * hAwBgSubPy = (TH1D*)infPy->Get("hAwCPPtBgSub");
+  normHist(hNrBgSubPy,0,true);
+  hNrBgSubPy->SetLineStyle(7);
+  normHist(hAwBgSubPy,0,true);
+
+  TFile * infHyPy = new TFile(infileHyPy);
+  TH1D * hNrBgSubHyPy = (TH1D*)infHyPy->Get("hNrCPPtBgSub");
+  TH1D * hAwBgSubHyPy = (TH1D*)infHyPy->Get("hAwCPPtBgSub");
+  normHist(hNrBgSubHyPy,0,true);
+  normHist(hAwBgSubHyPy,0,true);
+  hNrBgSubHyPy->SetLineColor(kBlue);
+  hNrBgSubHyPy->SetMarkerColor(kBlue);
+  hNrBgSubHyPy->SetMarkerStyle(kOpenSquare);
+  hAwBgSubHyPy->SetLineColor(kBlue);
+  hAwBgSubHyPy->SetMarkerColor(kBlue);
+  hAwBgSubHyPy->SetMarkerStyle(kFullSquare);
 
   //TCanvas * c2 = new TCanvas("c2","c2",500,500);
   gPad->SetLogy();
@@ -36,37 +50,49 @@ void drawPPt(TString infileData="",
   hNrBgSubData->SetAxisRange(1e-4,2e2,"Y");
   hNrBgSubData->Draw("E");
   hAwBgSubData->Draw("sameE");
-  hNrBgSubMc->Draw("same hist");
-  hAwBgSubMc->Draw("same hist");
+  hNrBgSubPy->Draw("same hist");
+  hAwBgSubPy->Draw("same hist");
+  hNrBgSubHyPy->Draw("same E");
+  hAwBgSubHyPy->Draw("same E");
 
-  Float_t nrConePtData=0, awConePtData=0, nrConePtMc=0, awConePtMc=0;
+  Float_t nrConePtData=0, awConePtData=0, nrConePtPy=0, awConePtPy=0,nrConePtHyPy=0, awConePtHyPy=0;
   for (Int_t i=0; i<hNrBgSubData->GetNbinsX(); ++i) {
     nrConePtData+=(hNrBgSubData->GetBinContent(i)*hNrBgSubData->GetBinCenter(i)*hNrBgSubData->GetBinWidth(i));
   }
   for (Int_t i=0; i<hAwBgSubData->GetNbinsX(); ++i) {
     awConePtData+=(hAwBgSubData->GetBinContent(i)*hAwBgSubData->GetBinCenter(i)*hAwBgSubData->GetBinWidth(i));
   }
-  for (Int_t i=0; i<hNrBgSubMc->GetNbinsX(); ++i) {
-    nrConePtMc+=(hNrBgSubMc->GetBinContent(i)*hNrBgSubMc->GetBinCenter(i)*hNrBgSubMc->GetBinWidth(i));
+  for (Int_t i=0; i<hNrBgSubPy->GetNbinsX(); ++i) {
+    nrConePtPy+=(hNrBgSubPy->GetBinContent(i)*hNrBgSubPy->GetBinCenter(i)*hNrBgSubPy->GetBinWidth(i));
   }
-  for (Int_t i=0; i<hAwBgSubMc->GetNbinsX(); ++i) {
-    awConePtMc+=(hAwBgSubMc->GetBinContent(i)*hAwBgSubMc->GetBinCenter(i)*hAwBgSubMc->GetBinWidth(i));
+  for (Int_t i=0; i<hAwBgSubPy->GetNbinsX(); ++i) {
+    awConePtPy+=(hAwBgSubPy->GetBinContent(i)*hAwBgSubPy->GetBinCenter(i)*hAwBgSubPy->GetBinWidth(i));
+  }
+  for (Int_t i=0; i<hNrBgSubHyPy->GetNbinsX(); ++i) {
+    nrConePtHyPy+=(hNrBgSubHyPy->GetBinContent(i)*hNrBgSubHyPy->GetBinCenter(i)*hNrBgSubHyPy->GetBinWidth(i));
+  }
+  for (Int_t i=0; i<hAwBgSubHyPy->GetNbinsX(); ++i) {
+    awConePtHyPy+=(hAwBgSubHyPy->GetBinContent(i)*hAwBgSubHyPy->GetBinCenter(i)*hAwBgSubHyPy->GetBinWidth(i));
   }
   cout << "Data cone pt sum - Nr: " << nrConePtData
     << " Aw: " << awConePtData << endl;
-  cout << "Mc cone pt sum - Nr: " << nrConePtMc
-    << " Aw: " << awConePtMc << endl;
+  cout << "Mc cone pt sum - Nr: " << nrConePtPy
+    << " Aw: " << awConePtPy << endl;
+  cout << "Mc cone pt sum - Nr: " << nrConePtHyPy
+    << " Aw: " << awConePtHyPy << endl;
 
   if (doLeg) {
-    TLegend * l = new TLegend(0.56,0.62,0.86,0.92);
+    TLegend * l = new TLegend(0.42,0.62,0.71,0.92);
     l->SetBorderSize(0);
     l->SetFillStyle(0);
     l->SetTextSize(0.03);
     l->AddEntry(hNrBgSubData,"Pb+Pb #sqrt{s_{NN}}=2.76TeV","");
     l->AddEntry(hNrBgSubData,"Leading Jet Data","pl");
     l->AddEntry(hAwBgSubData,"Sub-Leading Jet Data","pl");
-    l->AddEntry(hNrBgSubMc,"Leading Jet Pythia","l");
-    l->AddEntry(hAwBgSubMc,"Sub-Leading Jet Pythia","l");
+    l->AddEntry(hNrBgSubPy,"Leading Jet Pythia","l");
+    l->AddEntry(hAwBgSubPy,"Sub-Leading Jet Pythia","l");
+    l->AddEntry(hNrBgSubHyPy,"Leading Jet Pythia+Hydjet","pl");
+    l->AddEntry(hAwBgSubHyPy,"Sub-Leading Jet Pythia+Hydjet","pl");
     l->Draw();
   }
 }
--- Saved/DiJetAna/macros/draw/drawPPtCentAll.C
+++ Saved/DiJetAna/macros/draw/drawPPtCentAll.C
@@ -3,15 +3,19 @@
 void drawPPtCentAll()
 {
   TCanvas * cAll = new TCanvas("cAll","cAll",1000,500);
-  cAll->Divide(2,1);
+  cAll->Divide(3,1);
 
   cAll->cd(1);
-  drawPPt("jfhCorrEtaHighPtv3WideRBin_HCPR_J50U_djcalo_Cent30to100_Aj0to100_SubEtaRefl.root","jfhCorrEtaHighPtv3WideRBin_PyquenUQ80_djcalo_Cent0to100_Aj0to100_SubEtaRefl.root");
+  drawPPt("jfhCorrEtaHighPtv3WideRBin_HCPR_J50U_djcalo_Cent30to100_Aj0to100_SubEtaRefl.root","jfhCorrEtaHighPtv3WideRBin_PyquenUQ80_djcalo_Cent0to100_Aj0to100_SubEtaRefl.root","jfhCorrEtaHighPtv3WideRBin_Hydjet_djcalo_Cent30to100_Aj0to100_SubEtaRefl.root",false);
   drawText("30-100%",0.22,0.90);
 
   cAll->cd(2);
-  drawPPt("jfhCorrEtaHighPtv3WideRBin_HCPR_J50U_djcalo_Cent0to30_Aj0to100_SubEtaRefl.root","jfhCorrEtaHighPtv3WideRBin_PyquenUQ80_djcalo_Cent0to100_Aj0to100_SubEtaRefl.root",true);
-  drawText("0-30%",0.22,0.90);
+  drawPPt("jfhCorrEtaHighPtv3WideRBin_HCPR_J50U_djcalo_Cent10to30_Aj0to100_SubEtaRefl.root","jfhCorrEtaHighPtv3WideRBin_PyquenUQ80_djcalo_Cent0to100_Aj0to100_SubEtaRefl.root","jfhCorrEtaHighPtv3WideRBin_Hydjet_djcalo_Cent10to30_Aj0to100_SubEtaRefl.root",true);
+  drawText("10-30%",0.22,0.90);
+
+  cAll->cd(3);
+  drawPPt("jfhCorrEtaHighPtv3WideRBin_HCPR_J50U_djcalo_Cent0to10_Aj0to100_SubEtaRefl.root","jfhCorrEtaHighPtv3WideRBin_PyquenUQ80_djcalo_Cent0to100_Aj0to100_SubEtaRefl.root","jfhCorrEtaHighPtv3WideRBin_Hydjet_djcalo_Cent0to10_Aj0to100_SubEtaRefl.root",false);
+  drawText("0-10%",0.22,0.90);
 
   cAll->Print("PPt_CentAll.gif");
 }
