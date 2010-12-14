5acb91ad1385f9a17ed57781605bc56b2365ff8a
frankma <>
2010-12-14 00:59:31 +0000

    3by2 panel plots now with pythia+hydjet

---
+++ Saved/DiJetAna/macros/anaY1J/drawTrkEnergyAjAll32.C
@@ -0,0 +1,70 @@
+#include <iostream>
+#include "TH1.h"
+#include "TFile.h"
+#include "TStyle.h"
+#include "TCanvas.h"
+#include "TGaxis.h"
+#include "TLegend.h"
+#include "TPad.h"
+#include "TMath.h"
+#include "TLatex.h"
+#include "TString.h"
+#include "drawTrkEnergy.h"
+using namespace std;
+
+void drawTrkEnergyAjAll32(
+    TString anaV="CorrEtaHighPtv3WideRBin",
+    TString module="djcalo",
+    TString BckSub="SubEtaRefl",
+    TString title="_Track",
+    TString drawV="d4",
+    Int_t logScale=0,
+    Int_t normType=0)
+{
+  TCanvas *c1 = new TCanvas("c1","",1100,650);
+
+  Float_t leftMargin=0.28,bottomMargin=0.18;
+  makeMultiPanelCanvas(c1,3,2,0.0,0.0,leftMargin,bottomMargin,0.02);
+
+  Float_t leftSpace=leftMargin/2., downSpace=bottomMargin/2.;
+  c1->cd(1);
+  drawTrkEnergy("plot/drawn_jfh"+anaV+"_PyquenUQ80_"+module+"_Cent0to100_Aj0to24_"+BckSub+title+".root",false,true,logScale,normType);
+  drawText("PYTHIA",leftSpace+0.43,0.51);
+  drawText("A_{J}<0.24",leftSpace+0.43,0.43);
+
+  c1->cd(2);
+  drawTrkEnergy("plot/drawn_jfh"+anaV+"_Hydjet_"+module+"_Cent0to30_Aj0to24_"+BckSub+title+".root",false,false,logScale,normType);
+  drawText("Hydjet",0.43,0.51);
+  drawText("+PYTHIA",0.43,0.43);
+  drawText("0-30%",0.43,0.35);
+  drawText("A_{J}<0.24",0.43,0.27);
+
+  c1->cd(3);
+  drawTrkEnergy("plot/drawn_jfh"+anaV+"_HCPR_J50U_"+module+"_Cent0to30_Aj0to24_"+BckSub+title+".root",false,false,logScale,normType);
+  drawText("0-30%",0.43,0.51);
+  drawText("A_{J}<0.24",0.43,0.43);
+  drawText("CMS",0.43,0.90);
+  drawText("Pb+Pb  #sqrt{s}_{_{NN}}=2.76 TeV",0.43,0.82);
+  drawText("#intL dt = 6.7 #mub^{-1}",0.43,0.7);
+
+  c1->cd(4);
+  drawTrkEnergy("plot/drawn_jfh"+anaV+"_PyquenUQ80_"+module+"_Cent0to100_Aj24to100_"+BckSub+title+".root",false,true,logScale,normType);
+  drawText("PYTHIA",leftSpace+0.43,0.51);
+  drawText("A_{J}>0.24",leftSpace+0.43,0.43);
+
+  c1->cd(5);
+  drawTrkEnergy("plot/drawn_jfh"+anaV+"_Hydjet_"+module+"_Cent0to30_Aj24to100_"+BckSub+title+".root",true,false,logScale,normType);
+  drawText("Hydjet",0.43,downSpace+0.51);
+  drawText("+PYTHIA",0.43,downSpace+0.43);
+  drawText("0-30%",0.43,downSpace+0.35);
+  drawText("A_{J}>0.24",0.43,downSpace+0.27);
+
+  c1->cd(6);
+  drawTrkEnergy("plot/drawn_jfh"+anaV+"_HCPR_J50U_"+module+"_Cent0to30_Aj24to100_"+BckSub+title+".root",false,false,logScale,normType);
+  drawText("0-30%",0.43,downSpace+0.51);
+  drawText("A_{J}>0.24",0.43,downSpace+0.43);
+
+  c1->Print(anaV+"_"+BckSub+title+"TrackEnergyPtRBkgSubAjAll32_"+drawV+Form("_norm%d.gif",normType));
+  c1->Print(anaV+"_"+BckSub+title+"TrackEnergyPtRBkgSubAjAll32_"+drawV+Form("_norm%d.eps",normType));
+  c1->Print(anaV+"_"+BckSub+title+"TrackEnergyPtRBkgSubAjAll32_"+drawV+Form("_norm%d.C",normType));
+}
---
+++ Saved/DiJetAna/macros/anaY1J/drawTrkEnergyCentAll32.C
@@ -0,0 +1,68 @@
+#include <iostream>
+#include "TH1.h"
+#include "TFile.h"
+#include "TStyle.h"
+#include "TCanvas.h"
+#include "TGaxis.h"
+#include "TLegend.h"
+#include "TPad.h"
+#include "TMath.h"
+#include "TLatex.h"
+#include "TString.h"
+#include "drawTrkEnergy.h"
+using namespace std;
+
+void drawTrkEnergyCentAll32(
+    TString anaV="CorrEtaHighPtv3WideRBin",
+    TString module="djcalo",
+    TString BckSub="SubEtaRefl",
+    TString title="_Track",
+    TString drawV="d4",
+    Int_t logScale=0,
+    Int_t normType=0)
+{
+  TCanvas *c1 = new TCanvas("c1","",1100,650);
+
+  Float_t leftMargin=0.28,bottomMargin=0.18;
+  makeMultiPanelCanvas(c1,3,2,0.0,0.0,leftMargin,bottomMargin,0.02);
+
+  Float_t leftSpace=leftMargin/2., downSpace=bottomMargin/2.;
+  c1->cd(1);
+  drawTrkEnergy("plot/drawn_jfh"+anaV+"_Hydjet_"+module+"_Cent30to100_Aj0to100_"+BckSub+title+".root",false,true,logScale,normType);
+  drawText("Hydjet",leftSpace+0.43,0.51);
+  drawText("+PYTHIA",leftSpace+0.43,0.43);
+  drawText("30-100%",leftSpace+0.43,0.35);
+
+  c1->cd(2);
+  drawTrkEnergy("plot/drawn_jfh"+anaV+"_Hydjet_"+module+"_Cent10to30_Aj0to100_"+BckSub+title+".root",false,true,logScale,normType);
+  drawText("Hydjet",0.43,0.51);
+  drawText("+PYTHIA",0.43,0.43);
+  drawText("10-30%",0.43,0.35);
+
+  c1->cd(3);
+  drawTrkEnergy("plot/drawn_jfh"+anaV+"_Hydjet_"+module+"_Cent0to10_Aj0to100_"+BckSub+title+".root",false,true,logScale,normType);
+  drawText("Hydjet",0.43,0.51);
+  drawText("+PYTHIA",0.43,0.43);
+  drawText("0-10%",0.43,0.35);
+
+  c1->cd(4);
+  drawTrkEnergy("plot/drawn_jfh"+anaV+"_HCPR_J50U_"+module+"_Cent30to100_Aj0to100_"+BckSub+title+".root",false,false,logScale,normType);
+  drawText("30-100%",leftSpace+0.43,downSpace+0.51);
+  drawText("CMS",0.56,downSpace+0.90);
+
+  c1->cd(5);
+  drawTrkEnergy("plot/drawn_jfh"+anaV+"_HCPR_J50U_"+module+"_Cent10to30_Aj0to100_"+BckSub+title+".root",true,true,logScale,normType);
+  drawText("10-30%",0.43,downSpace+0.51);
+
+  c1->cd(6);
+  drawTrkEnergy("plot/drawn_jfh"+anaV+"_HCPR_J50U_"+module+"_Cent0to10_Aj0to100_"+BckSub+title+".root",false,false,logScale,normType);
+  drawText("0-10%",0.44,downSpace+0.51);
+  drawText("Pb+Pb  #sqrt{s}_{_{NN}}=2.76 TeV",0.43,0.82);
+  drawText("#intL dt = 6.7 #mub^{-1}",0.43,0.74);
+
+  c1->Print(anaV+"_"+BckSub+title+"TrackEnergyPtRBkgSubCentAll"+drawV+Form("_norm%d.gif",normType));
+  c1->Print(anaV+"_"+BckSub+title+"TrackEnergyPtRBkgSubCentAll"+drawV+Form("_norm%d.gif",normType));
+  c1->Print(anaV+"_"+BckSub+title+"TrackEnergyPtRBkgSubCentAll"+drawV+Form("_norm%d.gif",normType));
+}
+
+
