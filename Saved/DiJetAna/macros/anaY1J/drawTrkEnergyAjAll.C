cad965d55d2505c07aea2a82d2657cc07eba8869
frankma <>
2010-12-16 19:27:10 +0000

    make 3by2

--- Saved/DiJetAna/macros/anaY1J/drawTrkEnergyAjAll.C
+++ Saved/DiJetAna/macros/anaY1J/drawTrkEnergyAjAll.C
@@ -13,7 +13,7 @@
 using namespace std;
 
 void drawTrkEnergyAjAll(
-    TString anaV="CorrEtaHighPtv3WideRBin",
+    TString anaV="CorrEtaPtBin4RBin20v1",
     TString module="djcalo",
     TString BckSub="SubEtaRefl",
     TString title="_Track",
@@ -21,10 +21,10 @@ void drawTrkEnergyAjAll(
     Int_t logScale=0,
     Int_t normType=0)
 {
-  TCanvas *c1 = new TCanvas("c1","",700,650);
+  TCanvas *c1 = new TCanvas("c1","",1100,650);
 
   Float_t leftMargin=0.28,bottomMargin=0.18;
-  makeMultiPanelCanvas(c1,2,2,0.0,0.0,leftMargin,bottomMargin,0.02);
+  makeMultiPanelCanvas(c1,3,2,0.0,0.0,leftMargin,bottomMargin,0.02);
 
   Float_t leftSpace=leftMargin/2., downSpace=bottomMargin/2.;
   c1->cd(1);
@@ -48,17 +48,27 @@ void drawTrkEnergyAjAll(
   drawText("A_{J}>0.24",0.43,0.27);
 
   c1->cd(3);
+  drawTrkEnergy("plot/drawn_jfh"+anaV+"_Hydjet_"+module+"_Cent0to30_Aj0to100_"+BckSub+title+".root",true,true,logScale,normType);
+  drawText("Hydjet",0.43,0.51);
+  drawText("+PYTHIA",0.43,0.43);
+  drawText("0-30%",0.43,0.35);
+
+  c1->cd(4);
   drawTrkEnergy("plot/drawn_jfh"+anaV+"_HCPR_J50U_"+module+"_Cent0to30_Aj0to24_"+BckSub+title+".root",false,true,logScale,normType);
   drawText("0-30%",leftSpace+0.43,downSpace+0.51);
   drawText("A_{J}<0.24",leftSpace+0.43,downSpace+0.43);
 
-  c1->cd(4);
+  c1->cd(5);
+  drawTrkEnergy("plot/drawn_jfh"+anaV+"_HCPR_J50U_"+module+"_Cent0to30_Aj24to100_"+BckSub+title+".root",false,false,logScale,normType);
+  drawText("0-30%",0.43,downSpace+0.51);
+  drawText("A_{J}>0.24",0.43,downSpace+0.43);
+
+  c1->cd(6);
   drawTrkEnergy("plot/drawn_jfh"+anaV+"_HCPR_J50U_"+module+"_Cent0to30_Aj24to100_"+BckSub+title+".root",false,false,logScale,normType);
   drawText("CMS",0.41,0.90);
   drawText("Pb+Pb  #sqrt{s}_{_{NN}}=2.76 TeV",0.41,0.82);
   drawText("#intL dt = 6.7 #mub^{-1}",0.41,0.74);
   drawText("0-30%",0.43,downSpace+0.51);
-  drawText("A_{J}>0.24",0.43,downSpace+0.43);
 
   c1->Print(anaV+"_"+BckSub+title+"TrackEnergyPtRBkgSubAjAll"+drawV+Form("_norm%d.gif",normType));
   c1->Print(anaV+"_"+BckSub+title+"TrackEnergyPtRBkgSubAjAll"+drawV+Form("_norm%d.eps",normType));
