e51145a9add73a59f7bea6b23338c1e34acb8b6f
frankma <>
2010-12-16 21:16:19 +0000

    enable full root file input and 4pt bins

--- Saved/DiJetAna/macros/anaY1J/runDrawJetFragBalance.sh
+++ Saved/DiJetAna/macros/anaY1J/runDrawJetFragBalance.sh
@@ -1,29 +1,29 @@
 #!/bin/bash -
 #macro="drawMeanAJ.C"
 macro="drawJetFragBalance_DR.C"
-anaV=CorrEtaHighPtv3RBin20
+anaV=CorrEtaPtBin4RBin20v3
 module="djcalo"
 BkgSub="SubEtaRefl"
 Title="Track"
 
-root -b -q $macro'+("jfh'$anaV'_PyquenUQ80_djcalo_genp_Cent0to100_Aj0to100_'$BkgSub'","'$Title'")'
-root -b -q $macro'+("jfh'$anaV'_PyquenUQ80_'$module'_Cent0to100_Aj0to100_'$BkgSub'","'$Title'")'
+root -b -q $macro'+("jfh'$anaV'_PyquenUQ80_djcalo_genp_Cent0to100_Aj0to100_'$BkgSub'.root","'$Title'")'
+root -b -q $macro'+("jfh'$anaV'_PyquenUQ80_'$module'_Cent0to100_Aj0to100_'$BkgSub'.root","'$Title'")'
 
-root -b -q $macro'+("jfh'$anaV'_HCPR_J50U_'$module'_Cent0to10_Aj0to100_'$BkgSub'","'$Title'")'
-root -b -q $macro'+("jfh'$anaV'_HCPR_J50U_'$module'_Cent0to30_Aj0to100_'$BkgSub'","'$Title'")'
-root -b -q $macro'+("jfh'$anaV'_HCPR_J50U_'$module'_Cent10to30_Aj0to100_'$BkgSub'","'$Title'")'
-root -b -q $macro'+("jfh'$anaV'_HCPR_J50U_'$module'_Cent30to100_Aj0to100_'$BkgSub'","'$Title'")'
+root -b -q $macro'+("jfh'$anaV'_HCPR_J50U_'$module'_Cent0to10_Aj0to100_'$BkgSub'.root","'$Title'")'
+root -b -q $macro'+("jfh'$anaV'_HCPR_J50U_'$module'_Cent0to30_Aj0to100_'$BkgSub'.root","'$Title'")'
+root -b -q $macro'+("jfh'$anaV'_HCPR_J50U_'$module'_Cent10to30_Aj0to100_'$BkgSub'.root","'$Title'")'
+root -b -q $macro'+("jfh'$anaV'_HCPR_J50U_'$module'_Cent30to100_Aj0to100_'$BkgSub'.root","'$Title'")'
 
-root -b -q $macro'+("jfh'$anaV'_Hydjet_'$module'_Cent0to10_Aj0to100_'$BkgSub'","'$Title'")'
-root -b -q $macro'+("jfh'$anaV'_Hydjet_'$module'_Cent0to30_Aj0to100_'$BkgSub'","'$Title'")'
-root -b -q $macro'+("jfh'$anaV'_Hydjet_'$module'_Cent10to30_Aj0to100_'$BkgSub'","'$Title'")'
-root -b -q $macro'+("jfh'$anaV'_Hydjet_'$module'_Cent30to100_Aj0to100_'$BkgSub'","'$Title'")'
+root -b -q $macro'+("jfh'$anaV'_Hydjet_'$module'_Cent0to10_Aj0to100_'$BkgSub'.root","'$Title'")'
+root -b -q $macro'+("jfh'$anaV'_Hydjet_'$module'_Cent0to30_Aj0to100_'$BkgSub'.root","'$Title'")'
+root -b -q $macro'+("jfh'$anaV'_Hydjet_'$module'_Cent10to30_Aj0to100_'$BkgSub'.root","'$Title'")'
+root -b -q $macro'+("jfh'$anaV'_Hydjet_'$module'_Cent30to100_Aj0to100_'$BkgSub'.root","'$Title'")'
 
-root -b -q $macro'+("jfh'$anaV'_PyquenUQ80_'$module'_Cent0to100_Aj0to24_'$BkgSub'","'$Title'")'
-root -b -q $macro'+("jfh'$anaV'_PyquenUQ80_'$module'_Cent0to100_Aj24to100_'$BkgSub'","'$Title'")'
+root -b -q $macro'+("jfh'$anaV'_PyquenUQ80_'$module'_Cent0to100_Aj0to24_'$BkgSub'.root","'$Title'")'
+root -b -q $macro'+("jfh'$anaV'_PyquenUQ80_'$module'_Cent0to100_Aj24to100_'$BkgSub'.root","'$Title'")'
 
-root -b -q $macro'+("jfh'$anaV'_HCPR_J50U_'$module'_Cent0to30_Aj0to24_'$BkgSub'","'$Title'")'
-root -b -q $macro'+("jfh'$anaV'_HCPR_J50U_'$module'_Cent0to30_Aj24to100_'$BkgSub'","'$Title'")'
+root -b -q $macro'+("jfh'$anaV'_HCPR_J50U_'$module'_Cent0to30_Aj0to24_'$BkgSub'.root","'$Title'")'
+root -b -q $macro'+("jfh'$anaV'_HCPR_J50U_'$module'_Cent0to30_Aj24to100_'$BkgSub'.root","'$Title'")'
 
-root -b -q $macro'+("jfh'$anaV'_Hydjet_'$module'_Cent0to30_Aj0to24_'$BkgSub'","'$Title'")'
-root -b -q $macro'+("jfh'$anaV'_Hydjet_'$module'_Cent0to30_Aj24to100_'$BkgSub'","'$Title'")'
+root -b -q $macro'+("jfh'$anaV'_Hydjet_'$module'_Cent0to30_Aj0to24_'$BkgSub'.root","'$Title'")'
+root -b -q $macro'+("jfh'$anaV'_Hydjet_'$module'_Cent0to30_Aj24to100_'$BkgSub'.root","'$Title'")'
