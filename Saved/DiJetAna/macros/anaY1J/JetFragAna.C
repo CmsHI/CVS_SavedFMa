c0c5b28402656e27905b042e8916632750d7fd4b
frankma <>
2010-12-17 17:37:38 +0000

    fine pt binning to find quadrants, also change AJ divide to 0.22 (the median Aj for 0to30)

--- Saved/DiJetAna/macros/anaY1J/JetFragAna.C
+++ Saved/DiJetAna/macros/anaY1J/JetFragAna.C
@@ -31,11 +31,14 @@ JetFragAna::JetFragAna(TTree *tree,TString tag,Int_t doMC) :
    //const Int_t numPtBins = 7;
    //Double_t ptBins[numPtBins+1]={0.5,1,2,4,8,16,64,200};
    //Double_t ptBins[numPtBins+1]={1.5,3,6,9,18,36,72,200};
-   const Int_t numPtBins = 4;
+   //const Int_t numPtBins = 4;
    //Double_t ptBins[numPtBins+1]={1.5,4,10,22,200}; // v0
    //Double_t ptBins[numPtBins+1]={1.5,4,8,12,200}; // v1
    //Double_t ptBins[numPtBins+1]={1.5,4,8,24,200}; // v2
-   Double_t ptBins[numPtBins+1]={1.5,4,12,20,200}; // v3
+   //Double_t ptBins[numPtBins+1]={1.5,4,12,20,200}; // v3
+   const Int_t numPtBins = 200;
+   Double_t ptBins[numPtBins+1];
+   for (int i=0;i<numPtBins+1;++i) { ptBins[i]=200/numPtBins*i; }
    const Int_t numDPhiBins = 20;
    Double_t dPhiBins[numDPhiBins+1];
    for (int i=0;i<numDPhiBins+1;i++)   { dPhiBins[i] = PI/2./((double)numDPhiBins)*i; }
--- Saved/DiJetAna/macros/anaY1J/runAnaJetFrag.sh
+++ Saved/DiJetAna/macros/anaY1J/runAnaJetFrag.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -
-anaV=CorrEtaPtBin4RBin20v3
+anaV=CorrEtaPtBin200RBin20
 infileData="dj_HCPR-J50U-hiGoodMergedTracks_OfficialSelv2_Final0.root"
 #infilePyquen="dj_PyquenUQ80_hiGoodMergedTracks_OfficialSelv2.root"
 infilePyquen="dj_PyquenUQ80_hiGoodMergedTracks_VtxPatch_v1_OfficialSelv2GenAll.root"
@@ -9,6 +9,7 @@ module="djcalo"
 BkgSub="EtaRefl"
 NrJEtMin=120
 TrkPtMin=1.5
+AjMed0to30=0.22
 
 # Pyquen
 root -b -q 'anaJetFrag.C+(0,"S1","'$anaV'","djcalo_genp",0,100,'$NrJEtMin',500,50,500,0.,1.0,'$TrkPtMin',"Ana","'$BkgSub'","'$infilePyquen'","PyquenUQ80")'
@@ -26,11 +27,12 @@ root -b -q 'anaJetFrag.C+(0,"S1","'$anaV'","'$module'",10,30,'$NrJEtMin',500,50,
 root -b -q 'anaJetFrag.C+(0,"S1","'$anaV'","'$module'",0,30,'$NrJEtMin',500,50,500,0.,1.0,'$TrkPtMin',"Ana","'$BkgSub'","'$infileHyd'","Hydjet")'
 root -b -q 'anaJetFrag.C+(0,"S1","'$anaV'","'$module'",30,100,'$NrJEtMin',500,50,500,0.,1.0,'$TrkPtMin',"Ana","'$BkgSub'","'$infileHyd'","Hydjet")'
 
-root -b -q 'anaJetFrag.C+(0,"S1","'$anaV'","'$module'",0,100,'$NrJEtMin',500,50,500,0.,0.24,'$TrkPtMin',"Ana","'$BkgSub'","'$infilePyquen'","PyquenUQ80")'
-root -b -q 'anaJetFrag.C+(0,"S1","'$anaV'","'$module'",0,100,'$NrJEtMin',500,50,500,0.24,1.0,'$TrkPtMin',"Ana","'$BkgSub'","'$infilePyquen'","PyquenUQ80")'
+# Aj
+root -b -q 'anaJetFrag.C+(0,"S1","'$anaV'","'$module'",0,100,'$NrJEtMin',500,50,500,0.,'$AjMed0to30','$TrkPtMin',"Ana","'$BkgSub'","'$infilePyquen'","PyquenUQ80")'
+root -b -q 'anaJetFrag.C+(0,"S1","'$anaV'","'$module'",0,100,'$NrJEtMin',500,50,500,'$AjMed0to30',1.0,'$TrkPtMin',"Ana","'$BkgSub'","'$infilePyquen'","PyquenUQ80")'
 
-root -b -q 'anaJetFrag.C+(0,"S1","'$anaV'","'$module'",0,30,'$NrJEtMin',500,50,500,0.,0.24,'$TrkPtMin',"Ana","'$BkgSub'","'$infileData'","HCPR_J50U")'
-root -b -q 'anaJetFrag.C+(0,"S1","'$anaV'","'$module'",0,30,'$NrJEtMin',500,50,500,0.24,1.0,'$TrkPtMin',"Ana","'$BkgSub'","'$infileData'","HCPR_J50U")'
+root -b -q 'anaJetFrag.C+(0,"S1","'$anaV'","'$module'",0,30,'$NrJEtMin',500,50,500,0.,'$AjMed0to30','$TrkPtMin',"Ana","'$BkgSub'","'$infileData'","HCPR_J50U")'
+root -b -q 'anaJetFrag.C+(0,"S1","'$anaV'","'$module'",0,30,'$NrJEtMin',500,50,500,'$AjMed0to30',1.0,'$TrkPtMin',"Ana","'$BkgSub'","'$infileData'","HCPR_J50U")'
 
-root -b -q 'anaJetFrag.C+(0,"S1","'$anaV'","'$module'",0,30,'$NrJEtMin',500,50,500,0.,0.24,'$TrkPtMin',"Ana","'$BkgSub'","'$infileHyd'","Hydjet")'
-root -b -q 'anaJetFrag.C+(0,"S1","'$anaV'","'$module'",0,30,'$NrJEtMin',500,50,500,0.24,1.0,'$TrkPtMin',"Ana","'$BkgSub'","'$infileHyd'","Hydjet")'
+root -b -q 'anaJetFrag.C+(0,"S1","'$anaV'","'$module'",0,30,'$NrJEtMin',500,50,500,0.,'$AjMed0to30','$TrkPtMin',"Ana","'$BkgSub'","'$infileHyd'","Hydjet")'
+root -b -q 'anaJetFrag.C+(0,"S1","'$anaV'","'$module'",0,30,'$NrJEtMin',500,50,500,'$AjMed0to30',1.0,'$TrkPtMin',"Ana","'$BkgSub'","'$infileHyd'","Hydjet")'
