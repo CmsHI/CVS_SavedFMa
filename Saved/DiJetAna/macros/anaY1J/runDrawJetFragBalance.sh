03786fd6fa9c7c5c8a0ae7c066509b680db991ac
frankma <>
2010-12-18 17:42:00 +0000

    Aj 0.22 median for 0-30

--- Saved/DiJetAna/macros/anaY1J/runDrawJetFragBalance.sh
+++ Saved/DiJetAna/macros/anaY1J/runDrawJetFragBalance.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -
 #macro="drawMeanAJ.C"
 macro="drawJetFragBalance_DR.C"
-anaV=CorrEtaPtBin4RBin20v3
+anaV=CorrEtaPtBin5RBin20v0
 module="djcalo"
 BkgSub="SubEtaRefl"
 Title="Track"
@@ -19,11 +19,11 @@ root -b -q $macro'+("jfh'$anaV'_Hydjet_'$module'_Cent0to30_Aj0to100_'$BkgSub'.ro
 root -b -q $macro'+("jfh'$anaV'_Hydjet_'$module'_Cent10to30_Aj0to100_'$BkgSub'.root","'$Title'")'
 root -b -q $macro'+("jfh'$anaV'_Hydjet_'$module'_Cent30to100_Aj0to100_'$BkgSub'.root","'$Title'")'
 
-root -b -q $macro'+("jfh'$anaV'_PyquenUQ80_'$module'_Cent0to100_Aj0to24_'$BkgSub'.root","'$Title'")'
-root -b -q $macro'+("jfh'$anaV'_PyquenUQ80_'$module'_Cent0to100_Aj24to100_'$BkgSub'.root","'$Title'")'
+root -b -q $macro'+("jfh'$anaV'_PyquenUQ80_'$module'_Cent0to100_Aj0to22_'$BkgSub'.root","'$Title'")'
+root -b -q $macro'+("jfh'$anaV'_PyquenUQ80_'$module'_Cent0to100_Aj22to100_'$BkgSub'.root","'$Title'")'
 
-root -b -q $macro'+("jfh'$anaV'_HCPR_J50U_'$module'_Cent0to30_Aj0to24_'$BkgSub'.root","'$Title'")'
-root -b -q $macro'+("jfh'$anaV'_HCPR_J50U_'$module'_Cent0to30_Aj24to100_'$BkgSub'.root","'$Title'")'
+root -b -q $macro'+("jfh'$anaV'_HCPR_J50U_'$module'_Cent0to30_Aj0to22_'$BkgSub'.root","'$Title'")'
+root -b -q $macro'+("jfh'$anaV'_HCPR_J50U_'$module'_Cent0to30_Aj22to100_'$BkgSub'.root","'$Title'")'
 
-root -b -q $macro'+("jfh'$anaV'_Hydjet_'$module'_Cent0to30_Aj0to24_'$BkgSub'.root","'$Title'")'
-root -b -q $macro'+("jfh'$anaV'_Hydjet_'$module'_Cent0to30_Aj24to100_'$BkgSub'.root","'$Title'")'
+root -b -q $macro'+("jfh'$anaV'_Hydjet_'$module'_Cent0to30_Aj0to22_'$BkgSub'.root","'$Title'")'
+root -b -q $macro'+("jfh'$anaV'_Hydjet_'$module'_Cent0to30_Aj22to100_'$BkgSub'.root","'$Title'")'
