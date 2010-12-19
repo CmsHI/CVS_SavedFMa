c1ec76bb673b0990bd3c0bcf59c5d257529d33ce
frankma <>
2010-12-19 21:16:47 +0000

    add trk corr from yenjie

--- Saved/DiJetAna/macros/anaY1J/anaJetFrag.C
+++ Saved/DiJetAna/macros/anaY1J/anaJetFrag.C
@@ -3,17 +3,18 @@
 #include <TCanvas.h>
 #include "TH1.h"
 #include "TF1.h"
-#include "Saved/DiJetAna/macros/aliases_dijet.C"
-#include "Saved/DiJetAna/macros/anaY1J/JetFragAna.h"
+#include "aliases_dijet.C"
+#include "JetFragAna.C"
+
 using namespace std;
 
 void anaJetFrag(
     // Data
-    const char * inFile0Name="dj_HCPR-J50U-hiGoodMergedTracks_OfficialSelv2_Final0.root",
+    const char * inFile0Name="dj_HCPR-J50U-hiGoodMergedTracks_OfficialSelv2_Final0_djcalo_120_50.root",
     TString SrcName = "HCPR_J50U",
     // MC
     //const char * inFile0Name="dj_PyquenUQ80_hiGoodMergedTracks_VtxPatch_v1_OfficialSelv2GenAll.root",
-    //TString SrcName = "PyquenUQ80")
+    //TString SrcName = "PyquenUQ80" 
     int doMC=0,
     TString AnaVersion = "test",
     TString modName = "djcalo",
@@ -42,7 +43,9 @@ void anaJetFrag(
   aliases_dijet(djTree,doMC);
   cout << " # entries: " << djTree->GetEntries() << endl;
 
-  // Corrections
+  //=======================================================================================================================
+  // Jet Eta Corrections
+  //=======================================================================================================================
   TFile *fetacorr = new TFile("etacorr.root");
   std::map<TString,TF1*> jetaCorr;
   jetaCorr["ec1"] = (TF1 *) fetacorr->Get("f1");
@@ -50,16 +53,33 @@ void anaJetFrag(
   jetaCorr["ec3"] = (TF1 *) fetacorr->Get("f3");
   jetaCorr["ec4"] = (TF1 *) fetacorr->Get("f4");
   jetaCorr["ec5"] = (TF1 *) fetacorr->Get("f5");
+ 
+  //=======================================================================================================================
+  // tracking efficiency correction
+  //=======================================================================================================================
+  TFile *fTrackingEffCorr = new TFile("trkCorrJet_bass10k.root");
+  TH2D *hTrackingEff[10];
+  TH2D *hTrackingFake[10];
+  for (int i=0;i<5;i++)
+  {
+     hTrackingEff[i]=(TH2D*)fTrackingEffCorr->FindObjectAny(Form("rEff_cbin%d",i));
+     hTrackingFake[i]=(TH2D*)fTrackingEffCorr->FindObjectAny(Form("rFak_cbin%d",i));
+  }
 
+  //=======================================================================================================================
+  // tracking efficiency correction
+  //=======================================================================================================================
   TString outName(Form("draw/jfh%s_%s_%s_Cent%.0fto%.0f_Aj%.0fto%.0f_Sub%s.root",AnaVersion.Data(),SrcName.Data(),modName.Data(),CentMin,CentMax,AjMin*100,AjMax*100,BkgSubType.Data()));
   if (!doEvtSel) {
-   outName=TString("nt_")+inFile0Name;
+    outName=TString("nt_")+inFile0Name;
   }
   cout << "Output: " << outName << endl;
   TFile * outf = new TFile(outName,"RECREATE");
+
   JetFragAna jana(djTree,SrcName,doMC);
   jana.doEvtSel_ = doEvtSel;
   jana.doEtaCorr_ = true;
+  jana.doTrackingEffFakeCorr_ = true;
   jana.doJetOnly_ = false;
   jana.cut.CentMin = CentMin;
   jana.cut.CentMax = CentMax;
@@ -76,6 +96,15 @@ void anaJetFrag(
   jana.cut.DJAnd(TCut(Form("Aj>=%.2f&&Aj<%.2f",AjMin,AjMax)));
   jana.cut.Print(1);
   jana.jetaCorr_ = jetaCorr;
+
+  jana.trackingEtaBin_ = (TH1D*)hTrackingEff[0]->ProjectionX();
+  jana.trackingPtBin_ = (TH1D*)hTrackingEff[0]->ProjectionY();
+
+  for (int i=0;i<5;i++) {
+     jana.trackingEffCorr_[i] = hTrackingEff[i];
+     jana.trackingFakeCorr_[i] = hTrackingFake[i];
+
+  }
 
   // -- analysis selections --
   cout << endl << "====== DJ Selection: " << jana.cut.DJCutType << " ======" << endl;
