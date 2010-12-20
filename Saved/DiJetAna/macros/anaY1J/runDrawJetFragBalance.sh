dfa35e41de28816db9f2a1c08b846754d6cb4d94
frankma <>
2010-12-20 13:18:30 +0000

    3 aj bins

--- Saved/DiJetAna/macros/anaY1J/runDrawJetFragBalance.sh
+++ Saved/DiJetAna/macros/anaY1J/runDrawJetFragBalance.sh
@@ -1,12 +1,11 @@
 #!/bin/bash -
 #macro="drawMeanAJ.C"
 macro="drawJetFragBalance_DR.C"
-anaV=CorrEtaTrkEffPtBin5RBin20v1
+anaV=CorrEtaTrkEffPtBin5RBin20v2
 module="djcalo"
 BkgSub="SubEtaRefl"
 Title="Track"
 
-root -b -q $macro'+("jfh'$anaV'_PyquenUQ80_djcalo_genp_Cent0to100_Aj0to100_'$BkgSub'.root","'$Title'")'
 root -b -q $macro'+("jfh'$anaV'_PyquenUQ80_'$module'_Cent0to100_Aj0to100_'$BkgSub'.root","'$Title'")'
 
 root -b -q $macro'+("jfh'$anaV'_HCPR_J50U_'$module'_Cent0to10_Aj0to100_'$BkgSub'.root","'$Title'")'
@@ -19,11 +18,14 @@ root -b -q $macro'+("jfh'$anaV'_Hydjet_'$module'_Cent0to30_Aj0to100_'$BkgSub'.ro
 root -b -q $macro'+("jfh'$anaV'_Hydjet_'$module'_Cent10to30_Aj0to100_'$BkgSub'.root","'$Title'")'
 root -b -q $macro'+("jfh'$anaV'_Hydjet_'$module'_Cent30to100_Aj0to100_'$BkgSub'.root","'$Title'")'
 
-root -b -q $macro'+("jfh'$anaV'_PyquenUQ80_'$module'_Cent0to100_Aj0to22_'$BkgSub'.root","'$Title'")'
-root -b -q $macro'+("jfh'$anaV'_PyquenUQ80_'$module'_Cent0to100_Aj22to100_'$BkgSub'.root","'$Title'")'
+root -b -q $macro'+("jfh'$anaV'_PyquenUQ80_'$module'_Cent0to100_Aj0to15_'$BkgSub'.root","'$Title'")'
+root -b -q $macro'+("jfh'$anaV'_PyquenUQ80_'$module'_Cent0to100_Aj15to29_'$BkgSub'.root","'$Title'")'
+root -b -q $macro'+("jfh'$anaV'_PyquenUQ80_'$module'_Cent0to100_Aj29to100_'$BkgSub'.root","'$Title'")'
 
-root -b -q $macro'+("jfh'$anaV'_HCPR_J50U_'$module'_Cent0to30_Aj0to22_'$BkgSub'.root","'$Title'")'
-root -b -q $macro'+("jfh'$anaV'_HCPR_J50U_'$module'_Cent0to30_Aj22to100_'$BkgSub'.root","'$Title'")'
+root -b -q $macro'+("jfh'$anaV'_HCPR_J50U_'$module'_Cent0to30_Aj0to15_'$BkgSub'.root","'$Title'")'
+root -b -q $macro'+("jfh'$anaV'_HCPR_J50U_'$module'_Cent0to30_Aj15to29_'$BkgSub'.root","'$Title'")'
+root -b -q $macro'+("jfh'$anaV'_HCPR_J50U_'$module'_Cent0to30_Aj29to100_'$BkgSub'.root","'$Title'")'
 
-root -b -q $macro'+("jfh'$anaV'_Hydjet_'$module'_Cent0to30_Aj0to22_'$BkgSub'.root","'$Title'")'
-root -b -q $macro'+("jfh'$anaV'_Hydjet_'$module'_Cent0to30_Aj22to100_'$BkgSub'.root","'$Title'")'
+root -b -q $macro'+("jfh'$anaV'_Hydjet_'$module'_Cent0to30_Aj0to15_'$BkgSub'.root","'$Title'")'
+root -b -q $macro'+("jfh'$anaV'_Hydjet_'$module'_Cent0to30_Aj15to29_'$BkgSub'.root","'$Title'")'
+root -b -q $macro'+("jfh'$anaV'_Hydjet_'$module'_Cent0to30_Aj29to100_'$BkgSub'.root","'$Title'")'
