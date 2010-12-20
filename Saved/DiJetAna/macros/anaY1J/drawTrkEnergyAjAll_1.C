cd44ae226d8145d82cab72f8d1f55bec195ce4d2
kimy <>
2010-12-20 13:54:06 +0000

    aj bin 0 - 0.15 - 0.29

--- Saved/DiJetAna/macros/anaY1J/drawTrkEnergyAjAll_1.C
+++ Saved/DiJetAna/macros/anaY1J/drawTrkEnergyAjAll_1.C
@@ -13,24 +13,24 @@
 using namespace std;
 
 void drawTrkEnergyAjAll_1(
-    TString anaV="CorrEtaPtBin5RBin20v0",
-    //TString anaV="CorrEtaTrkEffPtBin5RBin20v1",
-    TString module="djcalo",
-    TString BckSub="SubEtaRefl",
-    TString title="_Track",
-    TString drawV="d5",
-    Int_t logScale=0,
-    Int_t normType=0)
+			  TString anaV="CorrEtaTrkEffPtBin5RBin20v2",
+			  //TString anaV="CorrEtaTrkEffPtBin5RBin20v1",
+			  TString module="djcalo",
+			  TString BckSub="SubEtaRefl",
+			  TString title="_Track",
+			  TString drawV="d5",
+			  Int_t logScale=0,
+			  Int_t normType=0)
 {
-  TCanvas *c1 = new TCanvas("c1","",780,650);
+  TCanvas *c1 = new TCanvas("c1","",1180,650);
 
   Float_t leftMargin=0.28,bottomMargin=0.18;
-  makeMultiPanelCanvas(c1,2,2,0.0,0.0,leftMargin,bottomMargin,0.02);
+  makeMultiPanelCanvas(c1,3,2,0.0,0.0,leftMargin,bottomMargin,0.02);
 
   Float_t leftSpace=leftMargin/2., downSpace=bottomMargin/2.;
   c1->cd(1);
-  drawTrkEnergy("plot/drawn_jfh"+anaV+"_Hydjet_"+module+"_Cent0to30_Aj0to22_"+BckSub+title+".root",false,true,logScale,normType);
-  drawText("A_{J} < 0.22",leftSpace+0.63,0.52);
+  drawTrkEnergy("plot/drawn_jfh"+anaV+"_Hydjet_"+module+"_Cent0to30_Aj0to15_"+BckSub+title+".root",false,true,logScale,normType);
+  drawText("A_{J} < 0.15",leftSpace+0.63,0.52);
   drawText("Leading jet",0.4,downSpace+0.15,15);
   drawText("Sub-leading jet",0.72,downSpace+0.15,15);
   drawText("Hydjet",0.33,0.81);
@@ -38,15 +38,21 @@ void drawTrkEnergyAjAll_1(
   drawText("0-30%",0.33,0.60);
 
   c1->cd(2);
-  drawTrkEnergy("plot/drawn_jfh"+anaV+"_Hydjet_"+module+"_Cent0to30_Aj22to100_"+BckSub+title+".root",true,true,logScale,normType);
-  drawText("A_{J} > 0.22",0.63,0.52);
+  drawTrkEnergy("plot/drawn_jfh"+anaV+"_Hydjet_"+module+"_Cent0to30_Aj15to29_"+BckSub+title+".root",true,true,logScale,normType);
+  drawText("0.15 < A_{J} < 0.29",0.63,0.52);
   drawText("Leading jet",0.15,downSpace+0.15,15);
   drawText("Sub-leading jet",0.6,downSpace+0.15,15);
  
-
   c1->cd(3);
-  drawTrkEnergy("plot/drawn_jfh"+anaV+"_HCPR_J50U_"+module+"_Cent0to30_Aj0to22_"+BckSub+title+".root",false,true,logScale,normType);
-  drawText("A_{J} < 0.22",leftSpace+0.63,downSpace+0.43);
+  drawTrkEnergy("plot/drawn_jfh"+anaV+"_Hydjet_"+module+"_Cent0to30_Aj29to100_"+BckSub+title+".root",false,true,logScale,normType);
+  drawText("A_{J} > 0.29",0.63,0.52);
+  drawText("Leading jet",0.15,downSpace+0.15,15);
+  drawText("Sub-leading jet",0.6,downSpace+0.15,15);
+
+
+  c1->cd(4);
+  drawTrkEnergy("plot/drawn_jfh"+anaV+"_HCPR_J50U_"+module+"_Cent0to30_Aj0to15_"+BckSub+title+".root",false,true,logScale,normType);
+  drawText("A_{J} > 0.15",leftSpace+0.63,downSpace+0.43);
   drawText("Leading jet",0.4,3*downSpace+0.1,15);
   drawText("Sub-leading jet",0.72,3*downSpace+0.1,15);
   drawText("CMS",0.33,downSpace+0.80);
@@ -54,13 +60,20 @@ void drawTrkEnergyAjAll_1(
   drawText("#intL dt = 6.7 #mub^{-1}",0.31,0.74,15);
   drawText("0-30%",0.33,downSpace+0.51);
 
-  c1->cd(4);
-  drawTrkEnergy("plot/drawn_jfh"+anaV+"_HCPR_J50U_"+module+"_Cent0to30_Aj22to100_"+BckSub+title+".root",false,false,logScale,normType);
-  drawText("A_{J} > 0.22",0.63,downSpace+0.43);
+  c1->cd(5);
+  drawTrkEnergy("plot/drawn_jfh"+anaV+"_HCPR_J50U_"+module+"_Cent0to30_Aj15to29_"+BckSub+title+".root",false,false,logScale,normType);
+  drawText("0.15 < A_{J} < 0.29",0.63,downSpace+0.43);
+  drawText("Leading jet",0.15,3*downSpace+0.1,15);
+  drawText("Sub-leading jet",0.63,3*downSpace+0.1,15);
+
+  c1->cd(6);
+  drawTrkEnergy("plot/drawn_jfh"+anaV+"_HCPR_J50U_"+module+"_Cent0to30_Aj29to100_"+BckSub+title+".root",false,false,logScale,normType);
+  drawText("A_{J} > 0.29",0.63,downSpace+0.43);
   drawText("Leading jet",0.15,3*downSpace+0.1,15);
   drawText("Sub-leading jet",0.6,3*downSpace+0.1,15);
 
   c1->Print(anaV+"_"+BckSub+title+"TrackEnergyPtRBkgSubAjAll_leadSub"+drawV+Form("_norm%d.gif",normType));
   c1->Print(anaV+"_"+BckSub+title+"TrackEnergyPtRBkgSubAjAll_leadSub"+drawV+Form("_norm%d.eps",normType));
   c1->Print(anaV+"_"+BckSub+title+"TrackEnergyPtRBkgSubAjAll_leadSub"+drawV+Form("_norm%d.C",normType));
+  c1->Print("CorrEtaTrkEffPtBin5RBin20v1_SubEtaRefl_TrackTrackEnergyPtRBkgSubAjAll_leadSubd5_norm0.eps");
 }
