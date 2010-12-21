1077efd2c2447015a81d4ed79bf3d6a5e8979020
frankma <>
2010-12-21 14:22:57 +0000

    run all aj and centrality bins

--- Saved/DiJetAna/macros/anaY1J/runDrawJetFragBalance.sh
+++ Saved/DiJetAna/macros/anaY1J/runDrawJetFragBalance.sh
@@ -1,43 +1,24 @@
 #!/bin/bash -
 #macro="drawMeanAJ.C"
 macro="drawJetFragBalance_DR.C"
-anaV=CorrEtaTrkEffPtBin5RBin20v5
+anaV=CorrEtaTrkEffPtBin5RBin20v6
 module="djcalo"
 BkgSub="SubEtaRefl"
 Title="Track"
 
 # No Weight
-root -b -q $macro'+("jfh'$anaV'_PyquenUQ80_'$module'_Cent0to100_Aj0to100_'$BkgSub'.root","'$Title'")'
+for c in 'Cent0to100'; do
+  for a in 'Aj0to100' 'Aj0to11' 'Aj11to22' 'Aj22to33' 'Aj33to100'; do
+    echo cent: $c  Aj: $a
+    root -b -q $macro'+("jfh'$anaV'_PyquenUQ80_'$module'_'$c'_'$a'_'$BkgSub'.root","'$Title'")'
+  done
+done
 
-root -b -q $macro'+("jfh'$anaV'_HCPR_J50U_'$module'_Cent0to10_Aj0to100_'$BkgSub'.root","'$Title'")'
-root -b -q $macro'+("jfh'$anaV'_HCPR_J50U_'$module'_Cent0to30_Aj0to100_'$BkgSub'.root","'$Title'")'
-root -b -q $macro'+("jfh'$anaV'_HCPR_J50U_'$module'_Cent10to30_Aj0to100_'$BkgSub'.root","'$Title'")'
-root -b -q $macro'+("jfh'$anaV'_HCPR_J50U_'$module'_Cent30to100_Aj0to100_'$BkgSub'.root","'$Title'")'
-
-root -b -q $macro'+("jfh'$anaV'_Hydjet_'$module'_Cent0to10_Aj0to100_'$BkgSub'.root","'$Title'")'
-root -b -q $macro'+("jfh'$anaV'_Hydjet_'$module'_Cent0to30_Aj0to100_'$BkgSub'.root","'$Title'")'
-root -b -q $macro'+("jfh'$anaV'_Hydjet_'$module'_Cent10to30_Aj0to100_'$BkgSub'.root","'$Title'")'
-root -b -q $macro'+("jfh'$anaV'_Hydjet_'$module'_Cent30to100_Aj0to100_'$BkgSub'.root","'$Title'")'
-
-root -b -q $macro'+("jfh'$anaV'_PyquenUQ80_'$module'_Cent0to100_Aj0to15_'$BkgSub'.root","'$Title'")'
-root -b -q $macro'+("jfh'$anaV'_PyquenUQ80_'$module'_Cent0to100_Aj15to29_'$BkgSub'.root","'$Title'")'
-root -b -q $macro'+("jfh'$anaV'_PyquenUQ80_'$module'_Cent0to100_Aj29to100_'$BkgSub'.root","'$Title'")'
-
-root -b -q $macro'+("jfh'$anaV'_HCPR_J50U_'$module'_Cent0to30_Aj0to15_'$BkgSub'.root","'$Title'")'
-root -b -q $macro'+("jfh'$anaV'_HCPR_J50U_'$module'_Cent0to30_Aj15to29_'$BkgSub'.root","'$Title'")'
-root -b -q $macro'+("jfh'$anaV'_HCPR_J50U_'$module'_Cent0to30_Aj29to100_'$BkgSub'.root","'$Title'")'
-
-root -b -q $macro'+("jfh'$anaV'_Hydjet_'$module'_Cent0to30_Aj0to15_'$BkgSub'.root","'$Title'")'
-root -b -q $macro'+("jfh'$anaV'_Hydjet_'$module'_Cent0to30_Aj15to29_'$BkgSub'.root","'$Title'")'
-root -b -q $macro'+("jfh'$anaV'_Hydjet_'$module'_Cent0to30_Aj29to100_'$BkgSub'.root","'$Title'")'
-
-# ReWeighted
-root -b -q $macro'+("jfh'$anaV'ReWt_Hydjet_'$module'_Cent0to10_Aj0to100_'$BkgSub'.root","'$Title'")'
-root -b -q $macro'+("jfh'$anaV'ReWt_Hydjet_'$module'_Cent0to30_Aj0to100_'$BkgSub'.root","'$Title'")'
-root -b -q $macro'+("jfh'$anaV'ReWt_Hydjet_'$module'_Cent10to30_Aj0to100_'$BkgSub'.root","'$Title'")'
-root -b -q $macro'+("jfh'$anaV'ReWt_Hydjet_'$module'_Cent30to100_Aj0to100_'$BkgSub'.root","'$Title'")'
-
-root -b -q $macro'+("jfh'$anaV'ReWt_Hydjet_'$module'_Cent0to30_Aj0to15_'$BkgSub'.root","'$Title'")'
-root -b -q $macro'+("jfh'$anaV'ReWt_Hydjet_'$module'_Cent0to30_Aj15to29_'$BkgSub'.root","'$Title'")'
-root -b -q $macro'+("jfh'$anaV'ReWt_Hydjet_'$module'_Cent0to30_Aj29to100_'$BkgSub'.root","'$Title'")'
-
+for c in 'Cent0to10' 'Cent0to30' 'Cent10to30' 'Cent30to100'; do
+  for a in 'Aj0to100' 'Aj0to11' 'Aj11to22' 'Aj22to33' 'Aj33to100'; do
+    echo cent: $c  Aj: $a
+    root -b -q $macro'+("jfh'$anaV'_HCPR_J50U_'$module'_'$c'_'$a'_'$BkgSub'.root","'$Title'")'
+    root -b -q $macro'+("jfh'$anaV'_Hydjet_'$module'_'$c'_'$a'_'$BkgSub'.root","'$Title'")'
+    root -b -q $macro'+("jfh'$anaV'ReWt_Hydjet_'$module'_'$c'_'$a'_'$BkgSub'.root","'$Title'")'
+  done
+done
