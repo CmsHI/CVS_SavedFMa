f467465d2975c194c368925cb1aa0027c0248e46
frankma <>
2010-12-21 14:24:21 +0000

    with trk eff fudge factor for the highest pt tracks

--- Saved/DiJetAna/macros/anaY1J/runAnaJetFragAll.sh
+++ Saved/DiJetAna/macros/anaY1J/runAnaJetFragAll.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -
-anaV=CorrEtaTrkEffPtBin5RBin20v6
+anaV=CorrEtaTrkEffPtBin5RBin20v7
 module=djcalo
 ./runAnaJetFragHI.sh dj_HCPR-J50U-hiGoodMergedTracks_OfficialSelv2_Final0_${module}_120_50.root HCPR_J50U $anaV $module 0
 ./runAnaJetFragHI.sh dj_HydQ_DJUQ80_hiGoodMergedTracks_VtxPatch_v1_OfficialSelv3_${module}_120_50.root Hydjet $anaV $module 0
