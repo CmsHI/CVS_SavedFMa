a11c5c74685f5daa073631bef6cce323864fec88
frankma <>
2010-12-21 11:57:13 +0000

    add option to do reweight

--- Saved/DiJetAna/macros/anaY1J/runAnaJetFragHI.sh
+++ Saved/DiJetAna/macros/anaY1J/runAnaJetFragHI.sh
@@ -3,20 +3,21 @@ infile=$1
 srcName=$2
 anaV=$3
 module=$4
+doReWeight=$5
 BkgSub="EtaRefl"
 AjMed0to30=0.22
 
 AjLow0to30=0.15
 AjHigh0to30=0.29
 
-root -b -q 'anaJetFrag.C+("'$infile'","'$srcName'",0,"'$anaV'","'$module'",1,"'$BkgSub'",0,10,0,1)'
-root -b -q 'anaJetFrag.C+("'$infile'","'$srcName'",0,"'$anaV'","'$module'",1,"'$BkgSub'",0,30,0,1)'
-root -b -q 'anaJetFrag.C+("'$infile'","'$srcName'",0,"'$anaV'","'$module'",1,"'$BkgSub'",10,30,0,1)'
-root -b -q 'anaJetFrag.C+("'$infile'","'$srcName'",0,"'$anaV'","'$module'",1,"'$BkgSub'",30,100,0,1)'
+root -b -q 'anaJetFrag.C+("'$infile'","'$srcName'",0,"'$anaV'","'$module'",1,'$doReWeight',"'$BkgSub'",0,10,0,1)'
+root -b -q 'anaJetFrag.C+("'$infile'","'$srcName'",0,"'$anaV'","'$module'",1,'$doReWeight',"'$BkgSub'",0,30,0,1)'
+root -b -q 'anaJetFrag.C+("'$infile'","'$srcName'",0,"'$anaV'","'$module'",1,'$doReWeight',"'$BkgSub'",10,30,0,1)'
+root -b -q 'anaJetFrag.C+("'$infile'","'$srcName'",0,"'$anaV'","'$module'",1,'$doReWeight',"'$BkgSub'",30,100,0,1)'
 
-root -b -q 'anaJetFrag.C+("'$infile'","'$srcName'",0,"'$anaV'","'$module'",1,"'$BkgSub'",0,30,0,'$AjLow0to30')'
-root -b -q 'anaJetFrag.C+("'$infile'","'$srcName'",0,"'$anaV'","'$module'",1,"'$BkgSub'",0,30,'$AjLow0to30','$AjHigh0to30')'
-root -b -q 'anaJetFrag.C+("'$infile'","'$srcName'",0,"'$anaV'","'$module'",1,"'$BkgSub'",0,30,'$AjHigh0to30',1)'
+root -b -q 'anaJetFrag.C+("'$infile'","'$srcName'",0,"'$anaV'","'$module'",1,'$doReWeight',"'$BkgSub'",0,30,0,'$AjLow0to30')'
+root -b -q 'anaJetFrag.C+("'$infile'","'$srcName'",0,"'$anaV'","'$module'",1,'$doReWeight',"'$BkgSub'",0,30,'$AjLow0to30','$AjHigh0to30')'
+root -b -q 'anaJetFrag.C+("'$infile'","'$srcName'",0,"'$anaV'","'$module'",1,'$doReWeight',"'$BkgSub'",0,30,'$AjHigh0to30',1)'
 
 #root -b -q 'anaJetFrag.C+("'$infile'","'$srcName'",0,"'$anaV'","'$module'",1,"'$BkgSub'",0,30,0,0.11)'
 #root -b -q 'anaJetFrag.C+("'$infile'","'$srcName'",0,"'$anaV'","'$module'",1,"'$BkgSub'",0,30,0.11,0.22)'
