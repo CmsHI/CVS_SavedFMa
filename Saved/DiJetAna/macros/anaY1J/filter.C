94bfd812d0952070ee9c572ef4a5a23135045ea4
frankma <>
2011-01-05 17:24:35 +0000

    cleanup

--- Saved/DiJetAna/macros/anaY1J/filter.C
+++ Saved/DiJetAna/macros/anaY1J/filter.C
@@ -1,7 +1,7 @@
 #include <TH2.h>
 #include <TStyle.h>
 #include <TCanvas.h>
-#include <TTree.h>
+#include <TChain.h>
 #include <TFile.h>
 #include <iostream>
 #include <iomanip>
@@ -9,51 +9,48 @@
 
 using namespace std;
 
-void filter(std::string inputFile_,
+void filter(TString inputFile,
     Double_t nearMinEt=120,
     Double_t awayMinEt=50,
     TString module="djcalo")
 {
   // get the correct tree from input file
-   TFile *inf = new TFile(inputFile_.data());
-   TTree *fChain = (TTree*)inf->Get(module+"/djTree");
-   cout << "Input file is " << inputFile_ << endl;
+  cout << "Input file is " << inputFile << endl;
+  TChain *fChain = new TChain(module+"/djTree");
+  fChain->Add(inputFile);
 
-   // rename the output file
-   std::string remword=".root";
-   size_t pos = inputFile_.find(remword);
-   std::string forOutput = inputFile_;  
-   if(pos!= std::string::npos)
-     forOutput.swap(forOutput.erase(pos,remword.length()));   
-   std::string endfix = Form("_%s_%.0f_%.0f.root",module.Data(),nearMinEt,awayMinEt);
-   std::string outputFile = forOutput + endfix;
+  // rename the output file
+  TString inputFileName(inputFile);
+  inputFileName.ReplaceAll(".root","");
+  TString endfix = Form("_%s_%.0f_%.0f.root",module.Data(),nearMinEt,awayMinEt);
+  TString outputFile = inputFileName + endfix;
 
-   // now open new root file
-   TFile* newfile_data = new TFile(outputFile.data(),"recreate");
-   cout << "Output file " << outputFile << endl;
+  // now open new root file
+  TFile* newfile_data = new TFile(outputFile,"recreate");
+  cout << "Output file " << outputFile << endl;
 
-   // clone tree
-   TTree* newtree = fChain->CloneTree(0);
-   newtree->SetMaxTreeSize(4000000000);
-   cout << "Saving "  << endfix << " tree" << endl;
+  // clone tree
+  TTree* newtree = fChain->CloneTree(0);
+  newtree->SetMaxTreeSize(4000000000);
+  cout << "Saving "  << endfix << " tree" << endl;
 
-   Long64_t nentries = fChain->GetEntries();
-   cout << "nentries = " << nentries << endl;
+  Long64_t nentries = fChain->GetEntries();
+  cout << "nentries = " << nentries << endl;
 
-   Float_t nljet;
-   Float_t aljet;
-   fChain->SetBranchAddress("nljet",&nljet);
-   fChain->SetBranchAddress("aljet",&aljet);
+  Float_t nljet;
+  Float_t aljet;
+  fChain->SetBranchAddress("nljet",&nljet);
+  fChain->SetBranchAddress("aljet",&aljet);
 
-   for (Long64_t jentry=0; jentry<nentries;jentry++) {
-     if (jentry% 10000 == 0) cout <<jentry<<" / "<<nentries<<" "<<setprecision(2)<<(double)jentry/nentries*100<<endl;
-     fChain->GetEntry(jentry);
-     if(nljet>nearMinEt&&aljet>awayMinEt) {
-       newtree->Fill();
-     }
-   }
- 
-   newtree->Print();
-   newtree->AutoSave();
-   delete newfile_data;
+  for (Long64_t jentry=0; jentry<nentries;jentry++) {
+    if (jentry% 10000 == 0) cout <<jentry<<" / "<<nentries<<" "<<setprecision(2)<<(double)jentry/nentries*100<<endl;
+    fChain->GetEntry(jentry);
+    if(nljet>nearMinEt&&aljet>awayMinEt) {
+      newtree->Fill();
+    }
+  }
+
+  newtree->Print();
+  newtree->AutoSave();
+  delete newfile_data;
 }
