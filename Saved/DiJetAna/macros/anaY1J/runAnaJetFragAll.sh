600c0f7b355d38c721742441cba263fb166e48e5
frankma <>
2011-01-05 18:39:38 +0000

    v16 with hydjetv4 (all genp)

--- Saved/DiJetAna/macros/anaY1J/runAnaJetFragAll.sh
+++ Saved/DiJetAna/macros/anaY1J/runAnaJetFragAll.sh
@@ -1,10 +1,17 @@
 #!/bin/bash -
 anaV=CorrEtaTrkEffv16
-BkgSub="EtaRefl" # EtaRefl, EtaReflSingle, PhiRot
 
-for module in djcalo djcalo_genp; do
-  ./runAnaJetFragHI.sh dj_HCPR-J50U-hiGoodMergedTracks_OfficialSelv2_Final0_${module}_120_50.root HCPR_J50U $anaV $module 0 $BkgSub
-  ./runAnaJetFragHI.sh dj_HydQ_DJUQ80_hiGoodMergedTracks_VtxPatch_v1_OfficialSelv3_${module}_120_50.root Hydjet $anaV $module 0 $BkgSub
-  ./runAnaJetFragHI.sh dj_HydQ_DJUQ80_hiGoodMergedTracks_VtxPatch_v1_OfficialSelv3_${module}_120_50.root Hydjet ${anaV}ReWt $module 1 $BkgSub
-  ./runAnaJetFragPp.sh dj_PyquenUQ80_hiGoodMergedTracks_VtxPatch_v1_OfficialSelv3_${module}_120_50.root PyquenUQ80 $anaV $module 0 $BkgSub
+#for BkgSub in "EtaReflSingle" "EtaRefl"; do
+for BkgSub in "EtaRefl"; do
+  for module in djcalo; do
+    #./runAnaJetFragHI.sh dj_HCPR-J50U-hiGoodMergedTracks_OfficialSelv2_Final0_${module}_120_50.root HCPR_J50U $anaV $module 0 $BkgSub
+    ./runAnaJetFragHI.sh dj_HyUQ80v4_${module}_120_50.root Hydjetv4 ${anaV}ReWt $module 1 $BkgSub
+    #./runAnaJetFragPp.sh dj_PyquenUQ80_hiGoodMergedTracks_VtxPatch_v1_OfficialSelv3_${module}_120_50.root PyquenUQ80 $anaV $module 0 $BkgSub
+  done
+
+  for module in djcalo_genp djgen; do
+    #./runAnaJetFragHI.sh dj_HyUQ80v4_${module}_120_50.root Hydjet $anaV $module 0 $BkgSub
+    ./runAnaJetFragHI.sh dj_HyUQ80v4_${module}_120_50.root Hydjetv4 ${anaV}ReWt $module 1 $BkgSub
+    #./runAnaJetFragPp.sh dj_PyquenUQ80_hiGoodMergedTracks_VtxPatch_v1_OfficialSelv3_${module}_120_50.root PyquenUQ80 $anaV $module 0 $BkgSub
+  done
 done
