f7131603d3da9d7f2598d38983a77e3744d42b89
frankma <>
2011-01-11 17:55:38 +0000

    For Cone MPt: no bkg sub

--- Saved/DiJetAna/macros/anaY1J/runAnaJetFragAll.sh
+++ Saved/DiJetAna/macros/anaY1J/runAnaJetFragAll.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -
 anaV=v18
 
-for BkgSub in "EtaReflSingle" "EtaRefl"; do
+#for BkgSub in "EtaReflSingle" "EtaRefl"; do
+for BkgSub in "None"; do
   for module in djcalo; do
     ./runAnaJetFragHI.sh dj_HCPR-J50U-hiGoodMergedTracks_OfficialSelv2_Final0_${module}_120_50.root HCPR_J50U $anaV $module 0 $BkgSub 1
     ./runAnaJetFragHI.sh dj_HyUQ80v4_${module}_120_50.root HydjetAll ${anaV}ReWt $module 1 $BkgSub 1
@@ -17,9 +18,3 @@ for BkgSub in "EtaReflSingle" "EtaRefl"; do
     ./runAnaJetFragHI.sh dj_HyUQ80v4_${module}_120_50.root HydjetAll ${anaV}ReWt $module 1 $BkgSub 0
   done
 done
-
-#for BkgSub in "None"; do
-#  for module in djcalo_genp; do
-#    ./runAnaJetFragHI.sh dj_HyUQ80v3_${module}_120_50.root HydjetSig ${anaV}ReWt $module 1 $BkgSub 1
-#  done
-#done
--- Saved/DiJetAna/macros/anaY1J/runAnaJetFragHI.sh
+++ Saved/DiJetAna/macros/anaY1J/runAnaJetFragHI.sh
@@ -9,18 +9,19 @@ EtaCorr=$7
 
 # Note: AjMed0to30=0.22
 #for c in '0,10' '0,30' '10,30' '30,100'; do
-#  alist='0,1'
-#  for a in $alist; do
-#    echo cent: $c  Aj: $a
-#    root -b -q 'anaJetFrag.C+("'$infile'",1,'$EtaCorr','$doReWeight',"'$BkgSub'",0,"'$srcName'","'$anaV'","'$module'",'$c','$a')'
-#  done
-#done
-
-#for c in '0,30'; do
-for c in '30,100'; do
-  alist='0,0.11 0.11,0.22 0.22,0.33 0.33,1'
+for c in '0,30' '30,100'; do
+  alist='0,1'
   for a in $alist; do
     echo cent: $c  Aj: $a
     root -b -q 'anaJetFrag.C+("'$infile'",1,'$EtaCorr','$doReWeight',"'$BkgSub'",0,"'$srcName'","'$anaV'","'$module'",'$c','$a')'
   done
 done
+
+#for c in '0,30'; do
+#for c in '30,100'; do
+#  alist='0,0.11 0.11,0.22 0.22,0.33 0.33,1'
+#  for a in $alist; do
+#    echo cent: $c  Aj: $a
+#    root -b -q 'anaJetFrag.C+("'$infile'",1,'$EtaCorr','$doReWeight',"'$BkgSub'",0,"'$srcName'","'$anaV'","'$module'",'$c','$a')'
+#  done
+#done
