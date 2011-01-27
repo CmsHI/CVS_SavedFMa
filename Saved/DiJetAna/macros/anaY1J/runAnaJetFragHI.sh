d928fb819786749d7b39a981cf7b1d06afd4da9d
frankma <>
2011-01-27 17:09:20 +0000

    ran for v19DeadForPix

--- Saved/DiJetAna/macros/anaY1J/runAnaJetFragHI.sh
+++ Saved/DiJetAna/macros/anaY1J/runAnaJetFragHI.sh
@@ -9,13 +9,13 @@ EtaCorr=$7
 
 # Note: AjMed0to30=0.22
 #for c in '0,10' '0,30' '10,30' '30,100'; do
-for c in '0,30' '30,100'; do
-  alist='0,1'
-  for a in $alist; do
-    echo cent: $c  Aj: $a
-    root -b -q 'anaJetFrag.C+("'$infile'",1,'$EtaCorr','$doReWeight',"'$BkgSub'",0,"'$srcName'","'$anaV'","'$module'",'$c','$a')'
-  done
-done
+#for c in '0,30' '30,100'; do
+#  alist='0,1'
+#  for a in $alist; do
+#    echo cent: $c  Aj: $a
+#    root -b -q 'anaJetFrag.C+("'$infile'",1,'$EtaCorr','$doReWeight',"'$BkgSub'",0,"'$srcName'","'$anaV'","'$module'",'$c','$a')'
+#  done
+#done
 
 for c in '0,30' '30,100'; do
   alist='0,0.11 0.11,0.22 0.22,0.33 0.33,1'
