b022ba7c709761c1d65dbf7fde61dc5b08d345be
frankma <>
2011-02-01 19:04:04 +0000

    normalize to unity

--- Saved/DiJetAna/macros/anaY1J/RandomConePull.C
+++ Saved/DiJetAna/macros/anaY1J/RandomConePull.C
@@ -11,6 +11,7 @@
 #include <TCut.h>
 #include <TProfile.h>
 #include <TText.h>
+#include "TStyle.h"
 #include "commonUtility.h"
 #include "DrawTick.C"
 #include "colorScheme.h"
@@ -21,13 +22,14 @@ void RandomConePull(TString infname = "datambv3mix1_djcalo_c0to30",
                     bool drawSys = true
 		    )
 {
+  gStyle->SetOptStat(1111);
   // ===========================================================
   // Get Input
   // ===========================================================
   vector<TChain*> t;
   vector<TH1D*> vhMetx;
   vector<TH1D*> vhJPhi;
-  for (Int_t i=0; i<=20; ++i) {
+  for (Int_t i=0; i<=35; ++i) {
     t.push_back(new TChain("ntjt"));
     TString infile(Form("../ntv5_%s_offset%d.root",infname.Data(),i*10));
     t[i]->Add(infile);
@@ -40,40 +42,39 @@ void RandomConePull(TString infname = "datambv3mix1_djcalo_c0to30",
   // ===========================================================
   // Analysis Setup
   // ===========================================================
-  const int nBin = 5;
-  double bins[nBin+1] = {0.5,1.0,2,4,8,1000};  
-
-  const int nBinAj = 4;
-  double ajBins[nBinAj+1] = {0.0001,0.11,0.22,0.33,0.49999};
   // Selection cut
   TCut evtCut = "nljet>120&&abs(nljetacorr)<1.6&&aljet>50&&abs(aljetacorr)<1.6&&jdphi>2./3*TMath::Pi()&&!maskEvt";
 
-  TH1D * hMean = new TH1D("hMean",";<#slash{p}_{T}^{#parallel}>;sub experiment",40,-10,10);
+  TH1D * hMean = new TH1D("hMean",";<#slash{p}_{T}^{#parallel}>;sub experiment",30,-10,10);
   for (Int_t i=0; i<t.size(); ++i) {
     cout << "Sel evt: " << t[i]->GetEntries(evtCut&&myCut) << endl;
-    t[i]->Project(vhMetx[i]->GetName(),"-metx",evtCut*"weight");
-    t[i]->Project(vhJPhi[i]->GetName(),"nljphi",evtCut*"weight");
+    t[i]->Project(vhMetx[i]->GetName(),"-metx",(evtCut&&myCut)*"weight");
+    t[i]->Project(vhJPhi[i]->GetName(),"nljphi",(evtCut&&myCut)*"weight");
     //vhMetx[i]->Fit("gaus");
     Float_t mean = vhMetx[i]->GetMean();
     cout << "metx mean: " << mean << endl;
     hMean->Fill(mean);
+    vhMetx[i]->Scale(1./vhMetx[i]->GetEntries());
+    vhJPhi[i]->Scale(1./vhJPhi[i]->GetEntries());
   }
 
   TCanvas * c2 = new TCanvas("c2","c2",600,600);
-  vhMetx[0]->SetAxisRange(0,500,"Y");
+  vhMetx[0]->SetAxisRange(0,0.1,"Y");
   vhMetx[0]->Draw("");
   for (Int_t i=1; i<t.size(); ++i) {
-    vhMetx[i]->Draw("same");
+    vhMetx[i]->Draw("same hist");
   }
 
   TCanvas * c3 = new TCanvas("c3","c3",600,600);
   vhJPhi[0]->Draw("");
-  vhJPhi[0]->SetAxisRange(0,500,"Y");
+  vhJPhi[0]->SetAxisRange(0,0.1,"Y");
   for (Int_t i=1; i<t.size(); ++i) {
     vhJPhi[i]->Draw("same");
   }
 
   TCanvas * c13 = new TCanvas("c13","c13",600,600);
+  hMean->Scale(1./hMean->GetEntries());
+  hMean->SetAxisRange(0,0.4,"Y");
   hMean->Fit("gaus");
   hMean->Draw();
 }
