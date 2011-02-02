a39b147332358980a2bcbc4b893b70cc890b01f3
frankma <>
2011-02-02 15:10:55 +0000

    for now define jdphi as the abs value

--- Saved/DiJetAna/macros/anaY1J/JetFragAna.C
+++ Saved/DiJetAna/macros/anaY1J/JetFragAna.C
@@ -350,7 +350,7 @@ Int_t JetFragAna::GetJetEntry(TChain * t, AnaJet & jet, Long64_t entry)
 	//cout << "entry: " << entry << " old eta: " << etain << "  new eta: " << etaout << " " << anaJets_[i] << endl;
       }
     }
-    anaJetDPhi_ = reco::deltaPhi(anaJets_[0].phi(),anaJets_[1].phi());
+    anaJetDPhi_ = fabs(reco::deltaPhi(anaJets_[0].phi(),anaJets_[1].phi()));
     //anaJets_[0].SetPhi(r3->Rndm()*TMath::TwoPi()-TMath::Pi());
     //anaJets_[1].SetPhi(anaJets_[0].phi()+(anaJets_[1].phi()-anaJets_[0].phi()));
   } // finished with entry
