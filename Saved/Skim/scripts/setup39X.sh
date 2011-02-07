3c4288c4ff4c68211bec848475d2ab8aa1ba2d2a
frankma <>
2011-02-07 12:52:39 +0000

    tagged HiEgamma

--- Saved/Skim/scripts/setup39X.sh
+++ Saved/Skim/scripts/setup39X.sh
@@ -1,6 +1,6 @@
 # analyzers
 cvs co -r V03-03-08 HLTrigger/HLTanalyzers
-cvs co RecoHI/HiEgammaAlgos
+cvs co -r HI_HLT_2010_11_20 RecoHI/HiEgammaAlgos
 cvs co -d Saved UserCode/SavedFMa/Saved
 cvs co -d CmsHi/Analysis2010 UserCode/CmsHi/Analysis2010
 cvs co -d edwenger/HiTrkEffAnalyzer UserCode/edwenger/HiTrkEffAnalyzer
