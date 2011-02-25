727be74bffd4c1fb341975ed714ad40fada1aefd
frankma <>
2011-02-25 12:39:09 +0000

    Finished Init

--- Saved/DiJetAna/macros/highPtFragAna/Corrector.h
+++ Saved/DiJetAna/macros/highPtFragAna/Corrector.h
@@ -29,11 +29,12 @@ class Corrector
     Int_t numJEtBins_;
     Int_t numLevels_;
 
-    vector<vector<vector<vector<vector<TH2F*> > > > > correction_;
+    vector<vector<vector<vector<vector<TH2D*> > > > > correction_;
 
     Corrector();
     void Init();
     void GetCorr(Float_t pt, Float_t eta, Float_t jet, Float_t cent, Float_t * corr);
+    TH2D * ProjectPtEta(TH3F * h3, Int_t zbinbeg, Int_t zbinend);
 };
 
 Corrector::Corrector() :
@@ -70,7 +71,7 @@ Corrector::Corrector() :
   for (Int_t i=0; i<ptHatMin_.size(); ++i) {
     TString fname=Form("djtrkhist_hydjetBassv2_djuq%.0f.root",ptHatMin_[i]);
     sample_.push_back(new TFile(fname));
-    cout << fname << endl;
+    cout << sample_[i]->GetName() << endl;
   }
   numSamples_ = sample_.size();
 
@@ -88,15 +89,15 @@ Corrector::Corrector() :
   numPtBins_ = ptBin_->GetNbinsX();
   numEtaBins_ = etaBin_->GetNbinsX();
   //numJEtBins_ = jetBin_->GetNbinsX();
-  numJEtBins_ = jetBin_->FindBin(400);
+  numJEtBins_ = jetBin_->FindBin(399);
 
   cout << "nbins pt,eta,jet: " << numPtBins_ << " " << numEtaBins_ << " " << numJEtBins_ << endl;
 
-  vector<vector<vector<vector<vector<TH2F*> > > > > v(numLevels_,
-      vector<vector<vector<vector<TH2F*> > > > (numSamples_,
-	vector<vector<vector<TH2F*> > > (numCentBins_,
-	  vector<vector<TH2F*> > (numJEtBins_,
-	    vector<TH2F*>(2)
+  vector<vector<vector<vector<vector<TH2D*> > > > > v(numLevels_,
+      vector<vector<vector<vector<TH2D*> > > > (numSamples_,
+	vector<vector<vector<TH2D*> > > (numCentBins_,
+	  vector<vector<TH2D*> > (numJEtBins_,
+	    vector<TH2D*>(2)
 	    )
 	  )
 	)
@@ -107,16 +108,29 @@ Corrector::Corrector() :
 void Corrector::Init()
 {
   for (Int_t lv=0; lv<numLevels_; ++lv) {
+    cout << "Load " << levelName_[lv] << " Histograms for ptHatMin";
     for (Int_t s=0; s<numSamples_; ++s) {
+      cout << " " << ptHatMin_[s] <<;
       for (Int_t c=0; c<numCentBins_; ++c) {
 	for (Int_t m=0; m<2; ++m) {
-	  TString hname(Form("%s/%s_cbin%d",trkCorrModule_.Data(),levelInput_[lv][m].Data(),c));
-	  cout << hname << endl;
-	  for (Int_t j=0; j<numJEtBins_; ++j) {
-	    //cout << correction_[lv][s][c][j][m] << endl;
+	  TString hname(Form("%s/%s_cbin%s",trkCorrModule_.Data(),levelInput_[lv][m].Data(),centBin_[c].Data()));
+	  TH3F * h3 = (TH3F*)sample_[s]->Get(hname);
+	  //cout << hname << " " << h3->GetEntries() << endl;
+	  for (Int_t j=1; j<=numJEtBins_; ++j) {
+	    TH2D * h2 = ProjectPtEta(h3,j,j);
+	    h2->SetName(Form("h%s_f%.0f_c%d_j%d_%d",levelName_[lv].Data(),ptHatMin_[s],c,j,m));
+	    //cout << h2->GetName() << endl;
 	  }
 	}
       }
     }
+    cout << endl;
   }
+}
+
+TH2D* Corrector::ProjectPtEta(TH3F * h3, Int_t zbinbeg, Int_t zbinend)
+{
+  h3->GetZaxis()->SetRange(zbinbeg,zbinend);
+  TH2D * h2 = (TH2D*)h3->Project3D(Form("yx_%d_%d",zbinbeg,zbinend));
+  return h2;
 }
