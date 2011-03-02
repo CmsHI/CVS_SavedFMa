c7b68622b7d24577083b740468331beb01c66e08
frankma <>
2011-03-02 16:28:15 +0000

    plot also the trk distribution from samples

--- Saved/DiJetAna/macros/highPtFragAna/plotJetSamples.C
+++ Saved/DiJetAna/macros/highPtFragAna/plotJetSamples.C
@@ -34,22 +34,48 @@ void plotJetSamples(
     cout << fname << " cut " << TString(evtCut) << ": " << vt[i]->GetEntries() << endl;
   }
 
+  // ============================================
+  // Book Histograms
+  // ============================================
   vector<TH1D*> vhJEt;
+  vector<TH1D*> vhPPt;
   for (Int_t i=0; i<vt.size(); ++i) {
     vhJEt.push_back(new TH1D(Form("hJEt_f%d",i),";Jet p_{T} (GeV/c);count",100,0,400));
+    vhPPt.push_back(new TH1D(Form("hPPt_f%d",i),";Trk p_{T} (GeV/c);count",30,0,120));
   }
 
+  // ============================================
+  // Analyze
+  // ============================================
   for (Int_t i=0; i<vt.size(); ++i) {
     vt[i]->Project(vhJEt[i]->GetName(),"jtpt",evtCut);
+    TCut jetCut(Form("jtpt[0]>%f",ptHatMin[i]));
+    cout << "Sample " << i << ", Jet cut: " << TString(jetCut) << endl;
+    vt[i]->Project(vhPPt[i]->GetName(),"ppt",evtCut&&jetCut);
   }
 
-  TCanvas *c2 = new TCanvas("c2","closure",500,500);
+  // ============================================
+  // Plot
+  // ============================================
+  Int_t colors[10] = {kGray+2,kViolet,kBlue+1,kGreen+2,kOrange+2,kMagenta,kRed};
+  TCanvas *c2 = new TCanvas("c2","Jet",500,500);
   c2->SetLogy();
   vhJEt[0]->SetAxisRange(0,250,"X");
   vhJEt[0]->SetAxisRange(3e-1,3e4,"Y");
   vhJEt[0]->Draw();
   for (Int_t i=1; i<vt.size(); ++i) {
+    vhJEt[i]->SetLineColor(colors[i]);
     vhJEt[i]->Draw("same hist");
+  }
+
+  TCanvas *c3 = new TCanvas("c3","Jet",500,500);
+  c3->SetLogy();
+  vhPPt[0]->SetAxisRange(0,120,"X");
+  vhPPt[0]->SetAxisRange(3e-1,1e6,"Y");
+  vhPPt[0]->Draw();
+  for (Int_t i=1; i<vt.size(); ++i) {
+    vhPPt[i]->SetLineColor(colors[i]);
+    vhPPt[i]->Draw("same hist");
   }
 
   // ====================
