9fca157ca98f7910c449ae02ec370bf27ea4efd2
frankma <>
2011-03-07 15:29:33 +0000

    high pt tracking jobs for high pt jet FF

--- Saved/DiJetAna/job/submitAllMC.py
+++ Saved/DiJetAna/job/submitAllMC.py
@@ -2,7 +2,7 @@ import os
 os.system("./create_custom_python_cfg.sh")
 
 scratch="/net/hidsk0001/d00/scratch/frankma"
-jobTag="OfficialSelv5"
+jobTag="HighPtv0"
 
 # ================== 362 ===============
 samples  = {
@@ -14,12 +14,22 @@ samples  = {
     # Final MC
     #"Pyquen_DJUQ80_Pat_hiGoodMergedTracks_VtxPatch_v1":"ana",
     #"Pyquen_DJUQ50_Pat_hiGoodMergedTracks_VtxPatch_v1":"ana"
-    "HydjetQ_DJUQ80_Pat_hiGoodMergedTracks_VtxPatch_v1":"ana",
+    #"HydjetQ_DJUQ80_Pat_hiGoodMergedTracks_VtxPatch_v1":"ana",
     #"HydjetQ_DJUQ50_Pat_hiGoodMergedTracks_VtxPatch_v1":"ana"
     # Embedded in Data
     #"Data_MinBias_DijetUnquenched80_d20101125and1126":"ana",
     #"Data_MinBias_DijetUnquenched50_d20101127":"ana"
     #"Data_MinBias_DijetUnquenched50_d20101211":"ana"
+    # Tracking
+    #"HydjetBass_DJUQ30_GSR_v2":"ana",
+    #"HydjetBass_DJUQ50_GSR_v2":"ana",
+    #"HydjetBass_DJUQ80_GSR_v2":"ana"
+    #"HydjetBass_DJUQ110_GSR_v2":"ana"
+    #"HydjetBass_DJUQ170_GSR_v2":"ana"
+    # Tracking high_pt high stat
+    "HydjetBass_DJUQ170_GSR_393_s1":"ana",
+    "HydjetBass_DJUQ170_GSR_393_s2":"ana",
+    "HydjetBass_DJUQ170_GSR_393_s3":"ana"
     }
 
 for sample,PD in samples.items():
