3264acd541e490b4f32d7fd21317a5924d9c015e
frankma <>
2011-03-14 09:21:59 +0000

    declared histograms

--- Saved/DiJetAna/macros/highPtFragAna/MakeCorr.C
+++ Saved/DiJetAna/macros/highPtFragAna/MakeCorr.C
@@ -14,9 +14,52 @@ void MakeCorr()
   trec->Add("~/scratch01/ana/HydjetBass_DJUQ110_GSR_v2_HighPtv0EffNt/mergeAll/all_nt.root");
   trec->Print();
 
+  // =========
+  // A0
+  // =========
+  // pt bins
+  const double small = 1e-3;
+  double pt;
+  vector<Double_t> ptBinsA0;
+  for(pt =   0  ; pt <   1.2-small; pt +=  0.05) ptBinsA0.push_back(pt); // 24 bins
+  for(pt =   1.2; pt <   2.4-small; pt +=  0.1 ) ptBinsA0.push_back(pt); // 12 bins
+  for(pt =   2.4; pt <   7.2-small; pt +=  0.2 ) ptBinsA0.push_back(pt); // 24 bins
+  for(pt =   7.2; pt <  13.2-small; pt +=  0.5 ) ptBinsA0.push_back(pt); // 12 bins
+  for(pt =  13.2; pt <  25.2-small; pt +=  1.0 ) ptBinsA0.push_back(pt); // 12 bins
+  for(pt =  25.2; pt <  61.2-small; pt +=  3.0 ) ptBinsA0.push_back(pt); // 12 bins
+  for(pt =  61.2; pt < 181.2-small; pt +=  5.0 ) ptBinsA0.push_back(pt); // 24 bins
+  ptBinsA0.push_back(181.2);
+  // eta bins
+  double etaMin   = -2.6;
+  double etaMax   =  2.6;
+  double etaWidth =  0.4;
+  vector<Double_t> etaBins;
+  for(double eta = etaMin; eta < etaMax + etaWidth/2; eta += etaWidth)
+    etaBins.push_back(eta);
+  //jet bins
+  float jetMin = 0.0;
+  float jetMax = 1000; // good to be matched with ana 
+  float jetWidth = 20;
+  vector<Double_t> jetBins;
+  for(double jet = jetMin; jet < jetMax + jetWidth/2; jet += jetWidth)
+    jetBins.push_back(jet);
+  //centrality bins
+  vector<Int_t> centBins;
+  centBins.push_back(0);
+  centBins.push_back(1);
+  centBins.push_back(3);
+  centBins.push_back(11);
+  centBins.push_back(19);
+  centBins.push_back(39);
+
   TrkCorrHisAna hisana("A0");
   hisana.tsim_ = tsim;
   hisana.trec_ = trec;
+  hisana.ptBins = ptBinsA0;
+  hisana.etaBins = etaBins;
+  hisana.jetBins = jetBins;
+  hisana.centBins = centBins;
+  hisana.DeclareHistograms();
   hisana.LoopSim();
   hisana.LoopRec();
 }
--- Saved/DiJetAna/macros/highPtFragAna/TrkCorrHisAna.h
+++ Saved/DiJetAna/macros/highPtFragAna/TrkCorrHisAna.h
@@ -59,12 +59,42 @@ class TrkCorrHisAna
   public:
     TChain * tsim_;
     TChain * trec_;
-    vector<Double_t> ptBins_;
-    vector<Double_t> etaBins_;
-    vector<Double_t> jetBins_;
-    vector<Int_t> centBins_;
+    vector<Double_t> ptBins;
+    vector<Double_t> etaBins;
+    vector<Double_t> jetBins;
+    vector<Int_t> centBins;
 
     TString name_;
+    // SimTrack
+    TH2F* hsim;
+    TH2F* hacc;
+    TH2F* heff;
+    TH2F* hmul;
+
+    TH3F* hsim3D;
+    TH3F* heff3D;
+    TH3F* hmul3D;
+    TH3F* hresStoR3D;
+
+    // RecTrack
+    TH2F* hrec;
+    TH2F* hfak;
+    TH2F* hsec;
+
+    TH3F* hrec3D;
+    TH3F* hfak3D;
+    TH3F* hsec3D;
+
+    // vector of histograms
+    std::vector<TH3F*> vhsim3D;
+    std::vector<TH3F*> vheff3D;
+    std::vector<TH3F*> vhmul3D;
+
+    std::vector<TH3F*> vhrec3D;
+    std::vector<TH3F*> vhfak3D;
+    std::vector<TH3F*> vhsec3D;
+
+    std::vector<TH3F*> vhresStoR3D;
 
     // methods
     TrkCorrHisAna(TString name);
@@ -80,6 +110,122 @@ class TrkCorrHisAna
 TrkCorrHisAna::TrkCorrHisAna(TString name) :
   name_(name)
 {
+}
+
+void TrkCorrHisAna::DeclareHistograms()
+{
+  // simulated
+  hsim = new TH2F("hsim","Sim Tracks;#eta;p_{T} (GeV/c)",
+      etaBins.size()-1, &etaBins[0],
+      ptBins.size()-1, &ptBins[0]);
+
+  // accepted
+  hacc = new TH2F("hacc","Accepted Tracks;#eta;p_{T} (GeV/c)",
+      etaBins.size()-1, &etaBins[0],
+      ptBins.size()-1, &ptBins[0]);
+
+  // efficiency
+  heff = new TH2F("heff","Effic Rec Tracks;#eta;p_{T} (GeV/c)",
+      etaBins.size()-1, &etaBins[0],
+      ptBins.size()-1, &ptBins[0]);
+
+  // multiply reconstructed
+  hmul = new TH2F("hmul","Mult Rec Tracks;#eta;p_{T} (GeV/c)",
+      etaBins.size()-1, &etaBins[0],
+      ptBins.size()-1, &ptBins[0]);
+
+  // reconstructed
+  hrec = new TH2F("hrec","Rec Tracks;#eta;p_{T} (GeV/c)",
+      etaBins.size()-1, &etaBins[0],
+      ptBins.size()-1, &ptBins[0]);
+
+  // fakes
+  hfak = new TH2F("hfak","Fake Tracks;#eta;p_{T} (GeV/c)",
+      etaBins.size()-1, &etaBins[0],
+      ptBins.size()-1, &ptBins[0]);
+
+  // secondary
+  hsec = new TH2F("hsec","Secondary Tracks;#eta;p_{T} (GeV/c)",
+      etaBins.size()-1, &etaBins[0],
+      ptBins.size()-1, &ptBins[0]);
+
+
+  // simulated 3D 
+  hsim3D = new TH3F("hsim3D","Sim Tracks;#eta;p_{T} (GeV/c);jet E_{T} (GeV/c)",
+      etaBins.size()-1, &etaBins[0],
+      ptBins.size()-1, &ptBins[0],
+      jetBins.size()-1, &jetBins[0]);
+
+  // efficiency  3D 
+  heff3D = new TH3F("heff3D","Effic Rec Tracks;#eta;p_{T} (GeV/c);jet E_{T} (GeV/c)",
+      etaBins.size()-1, &etaBins[0],
+      ptBins.size()-1, &ptBins[0],
+      jetBins.size()-1, &jetBins[0]);
+
+  // multiply reconstructed 3D 
+  hmul3D = new TH3F("hmul3D","Mult Rec Tracks;#eta;p_{T} (GeV/c);jet E_{T} (GeV/c)",
+      etaBins.size()-1, &etaBins[0],
+      ptBins.size()-1, &ptBins[0],
+      jetBins.size()-1, &jetBins[0]);
+
+
+  // reconstructed 3D 
+  hrec3D = new TH3F("hrec3D","Rec Tracks;#eta;p_{T} (GeV/c);jet E_{T} (GeV/c)",
+      etaBins.size()-1, &etaBins[0],
+      ptBins.size()-1, &ptBins[0],
+      jetBins.size()-1, &jetBins[0]);
+
+  // fakes 3D 
+  hfak3D = new TH3F("hfak3D","Fake Tracks;#eta;p_{T} (GeV/c);jet E_{T} (GeV/c)",
+      etaBins.size()-1, &etaBins[0],
+      ptBins.size()-1, &ptBins[0],
+      jetBins.size()-1, &jetBins[0]);
+
+  // secondary
+  hsec3D = new TH3F("hsec3D","Secondary Tracks;#eta;p_{T} (GeV/c);jet E_{T} (GeV/c)",
+      etaBins.size()-1, &etaBins[0],
+      ptBins.size()-1, &ptBins[0],
+      jetBins.size()-1, &jetBins[0]);
+
+  // mom resolution (Sim to Rec) 
+  hresStoR3D = new TH3F("hresStoR3D","Momentum resolution (sim to rec);#eta;sim p_{T} (GeV/c);rec p_{T} (GeV/c)",
+      etaBins.size()-1, &etaBins[0],
+      ptBins.size()-1, &ptBins[0],
+      ptBins.size()-1, &ptBins[0]);
+
+  for(unsigned i=0;i<centBins.size()-1;i++){
+    vhsim3D.push_back(new TH3F("","Sim Tracks;#eta;p_{T} (GeV/c);jet E_{T} (GeV/c)",
+	  etaBins.size()-1, &etaBins[0], ptBins.size()-1, &ptBins[0], jetBins.size()-1, &jetBins[0]) );
+    vheff3D.push_back(new TH3F("","Effic Rec Tracks;#eta;p_{T} (GeV/c);jet E_{T} (GeV/c)",
+	  etaBins.size()-1, &etaBins[0], ptBins.size()-1, &ptBins[0], jetBins.size()-1, &jetBins[0]) );
+    vhmul3D.push_back(new TH3F("","Mult Rec Tracks;#eta;p_{T} (GeV/c);jet E_{T} (GeV/c)",
+	  etaBins.size()-1, &etaBins[0], ptBins.size()-1, &ptBins[0], jetBins.size()-1, &jetBins[0]) );
+    vhrec3D.push_back(new TH3F("","Rec Rec Tracks;#eta;p_{T} (GeV/c);jet E_{T} (GeV/c)",
+	  etaBins.size()-1, &etaBins[0], ptBins.size()-1, &ptBins[0], jetBins.size()-1, &jetBins[0]) );
+    vhfak3D.push_back(new TH3F("","Fake Rec Tracks;#eta;p_{T} (GeV/c);jet E_{T} (GeV/c)",
+	  etaBins.size()-1, &etaBins[0], ptBins.size()-1, &ptBins[0], jetBins.size()-1, &jetBins[0]) );
+    vhsec3D.push_back(new TH3F("","Secondary Tracks;#eta;p_{T} (GeV/c);jet E_{T} (GeV/c)",
+	  etaBins.size()-1, &etaBins[0], ptBins.size()-1, &ptBins[0], jetBins.size()-1, &jetBins[0]) );
+    vhresStoR3D.push_back(new TH3F("","Momentum resolution (sim to rec);#eta;sim p_{T} (GeV/c);rec p_{T} (GeV/c)",
+	  etaBins.size()-1, &etaBins[0], ptBins.size()-1, &ptBins[0], ptBins.size()-1, &ptBins[0]) );
+    if(i==0) {
+      vhsim3D[i]->SetName(Form("hsim3D_cbin%dto%d",centBins[i],centBins[i+1]));
+      vheff3D[i]->SetName(Form("heff3D_cbin%dto%d",centBins[i],centBins[i+1]));
+      vhrec3D[i]->SetName(Form("hrec3D_cbin%dto%d",centBins[i],centBins[i+1]));
+      vhfak3D[i]->SetName(Form("hfak3D_cbin%dto%d",centBins[i],centBins[i+1]));
+      vhmul3D[i]->SetName(Form("hmul3D_cbin%dto%d",centBins[i],centBins[i+1]));
+      vhsec3D[i]->SetName(Form("hsec3D_cbin%dto%d",centBins[i],centBins[i+1]));
+      vhresStoR3D[i]->SetName(Form("hresStoR3D_cbin%dto%d",centBins[i],centBins[i+1]));
+    }else{
+      vhsim3D[i]->SetName(Form("hsim3D_cbin%dto%d",centBins[i]+1,centBins[i+1]));
+      vheff3D[i]->SetName(Form("heff3D_cbin%dto%d",centBins[i]+1,centBins[i+1]));
+      vhrec3D[i]->SetName(Form("hrec3D_cbin%dto%d",centBins[i]+1,centBins[i+1]));
+      vhfak3D[i]->SetName(Form("hfak3D_cbin%dto%d",centBins[i]+1,centBins[i+1]));
+      vhmul3D[i]->SetName(Form("hmul3D_cbin%dto%d",centBins[i]+1,centBins[i+1]));
+      vhsec3D[i]->SetName(Form("hsec3D_cbin%dto%d",centBins[i]+1,centBins[i+1]));
+      vhresStoR3D[i]->SetName(Form("hresStoR3D_cbin%dto%d",centBins[i]+1,centBins[i+1]));
+    }
+  }
 }
 
 void TrkCorrHisAna::LoopSim()
