cff538c9498704aba704ec09cc8b6e3ac7c68bb6
frankma <>
2011-03-31 23:05:52 +0000

    === Version with PFTower and no muons === - checked that hiGoodTracks are consistent as before   * Main lesson: global tag matters in track reco and selection - Added eff ana - checked genp   * added subevent info

---
+++ Saved/QM11Ana/test/HiJetTrk_mc_2760_PFTowers_noMuons_cfg.py
@@ -0,0 +1,78 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('HIJETS')
+
+#process.Timing = cms.Service("Timing")
+
+# Input source
+process.source = cms.Source("PoolSource",
+                            fileNames = cms.untracked.vstring(
+    #'/store/relval/CMSSW_3_10_0/RelValQCD_Pt_80_120/GEN-SIM-RECO/START310_V3-v1/0050/54ED7C66-680E-E011-BD9F-001A92810ADE.root'
+    #'/store/relval/CMSSW_3_9_9/RelValHydjetQ_B0_2760GeV/GEN-SIM-RECO/START39_V7HI-v1/0001/0618A00D-E23D-E011-A7FE-001A9281173E.root'                                                              
+    '/store/user/davidlw/Hydjet_Bass_MinBias_2760GeV/Pyquen_UnquenchedDiJet_Pt50_GEN-SIM-RECO_393_v2/1243c1b8707a4e7eb28eae64e1474920/hiReco_RAW2DIGI_RECO_9_1_ZVb.root'
+    )
+                            )
+
+process.maxEvents = cms.untracked.PSet(
+            input = cms.untracked.int32(-1)
+    )
+
+#load some general stuff
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+process.GlobalTag.globaltag = 'START39_V9::All'
+
+process.load('Configuration.StandardSequences.GeometryExtended_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+
+process.load('FWCore.MessageService.MessageLogger_cfi')
+
+# load centrality
+process.HeavyIonGlobalParameters = cms.PSet(
+           centralityVariable = cms.string("HFhits"),
+           nonDefaultGlauberModel = cms.string("Hydjet_2760GeV"),
+           centralitySrc = cms.InputTag("hiCentrality")
+                     )
+
+process.makeCentralityTableTFile = cms.EDAnalyzer('CentralityTableProducer',
+                                                  isMC = cms.untracked.bool(True),
+                                                  makeDBFromTFile = cms.untracked.bool(False),
+                                                  makeTFileFromDB = cms.untracked.bool(True)
+                                                  )
+
+
+#  --- Track and muon reconstruction ---
+# pixel triplet tracking (HI Tracking)
+process.load("RecoLocalTracker.Configuration.RecoLocalTracker_cff")
+process.load("RecoHI.Configuration.Reconstruction_HI_cff")
+process.load("RecoHI.HiTracking.LowPtTracking_PbPb_cff")
+# Needed to produce "HcalSeverityLevelComputerRcd" used by CaloTowersCreator/towerMakerPF
+process.load("RecoLocalCalo.Configuration.hcalLocalReco_cff")
+#Track Reco
+process.rechits = cms.Sequence(process.siPixelRecHits * process.siStripMatchedRecHits)
+process.hiTrackReco = cms.Sequence(process.rechits * process.heavyIonTracking)
+
+# for PF
+process.load("RecoHI.Configuration.Reconstruction_hiPF_cff")
+process.HiParticleFlowRecoNoJets = cms.Sequence(
+    process.particleFlowCluster
+    * process.trackerDrivenElectronSeeds
+    * process.particleFlowReco
+    )
+process.trackerDrivenElectronSeeds.TkColList = cms.VInputTag("hiGoodTracks")
+
+#for tree output
+process.TFileService = cms.Service("TFileService",
+                                  fileName=cms.string("JetAnalysisTTrees_hiGoodTracks_v1.root"))
+
+# ----- jet reco extra and analyzers -----
+process.load("Saved.QM11Ana.ExtraReco_cff")
+process.load("Saved.QM11Ana.Analyzers_cff")
+
+# =============== Final Paths =====================
+#process.extraHiReco = cms.Path( process.makeCentralityTableTFile )
+process.extraTrkReco = cms.Path( process.hiTrackReco * process.hiextraTrackReco )
+process.extraPfReco = cms.Path( process.HiParticleFlowRecoNoJets )
+process.extraJetReco = cms.Path( process.hiGen * process.runAllJets )
+process.ana_step = cms.Path( process.jetana_seq * process.trkana_seq )
+
+process.schedule = cms.Schedule(process.extraTrkReco,process.extraPfReco,process.extraJetReco,process.ana_step)
