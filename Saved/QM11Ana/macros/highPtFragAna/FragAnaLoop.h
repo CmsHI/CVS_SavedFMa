01cc56c1ec4aa4671a18d783eb4eee3156f1ec00
frankma <>
2011-04-04 13:01:28 +0000

    macros for trk closure

---
+++ Saved/QM11Ana/macros/highPtFragAna/FragAnaLoop.h
@@ -0,0 +1,259 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include "TChain.h"
+#include "TNtuple.h"
+#include "TH1.h"
+#include "TH2.h"
+#include "TString.h"
+#include "Corrector3D.h"
+#include "HisMath.C"
+#include "selectionCut.h"
+#include "../helpers.h"
+using namespace std;
+const Int_t MAXNP = 100000;
+
+struct JetFragRel
+{
+  Float_t cent;
+  Float_t jtpt[2];
+  Float_t jteta[2];
+  Float_t jtphi[2];
+  Int_t np;
+  Float_t ppt[MAXNP];
+  Float_t peta[MAXNP];
+  Float_t pphi[MAXNP];
+  // relations
+  Float_t jdphi;
+  Float_t pjdr[2][MAXNP];
+  Float_t pjdrbg[2][MAXNP];
+  void SetBranches(TTree * t) {
+    t->Branch("cent",&(this->cent),"cent/F");
+    t->Branch("jtpt",this->jtpt,"jtpt[2]/F");
+    t->Branch("jteta",this->jteta,"jteta[2]/F");
+    t->Branch("jtphi",this->jtphi,"jtphi[2]/F");
+    t->Branch("np",&(this->np),"np/I");
+    t->Branch("ppt",this->ppt,"ppt[np]/F");
+    t->Branch("peta",this->peta,"peta[np]/F");
+    t->Branch("pphi",this->pphi,"pphi[np]/F");
+  }
+  void LoadBranches(TTree * t) {
+    t->SetBranchAddress("cent",&(this->cent));
+    t->SetBranchAddress("jtpt",this->jtpt);
+    t->SetBranchAddress("jteta",this->jteta);
+    t->SetBranchAddress("jtphi",this->jtphi);
+    t->SetBranchAddress("np",&(this->np));
+    t->SetBranchAddress("ppt",this->ppt);
+    t->SetBranchAddress("peta",this->peta);
+    t->SetBranchAddress("pphi",this->pphi);
+  }
+  void clear()
+  {
+    for (Int_t j=0; j<2; ++j) {
+      jtpt[j]=-99;
+    }
+    np=0;
+  }
+};
+
+// =====================================================
+// Class Definition
+// =====================================================
+class FragAnaLoop
+{
+  public:
+    TChain * t_;
+    JetFragRel jfr_;
+    Corrector3D * trkCorr_;
+    vector<Corrector3D *> vtrkCorr_;
+    selectionCut * cut_;
+
+    TString name_;
+    Double_t ptHatMin_;
+    Int_t anaTrkType_;
+
+    vector<Double_t> numJet_;
+
+    // analysis histograms
+    vector<Double_t> ptBin_;
+    vector<vector<TH1D*> > vhPPtCorr_;
+    vector<vector<TH1D*> > vhPPtRat_;
+
+    // monitor histograms
+    vector<vector<TH2D*> > vhTrkCorrPPt_;
+    vector<vector<TH2D*> > vhTrkCorrJEt_;
+    vector<vector<TH2D*> > vhTrkCorrCent_;
+
+    // ntuples
+    TNtuple * ntCorr_;
+
+    // Methods
+    FragAnaLoop(TString name);
+    void Init();
+    void Loop();
+
+    Bool_t SelEvt(const JetFragRel & jfr) {
+      return (jfr.cent>=cut_->CentMin && jfr.cent<cut_->CentMax);
+    }
+    Bool_t SelJet(const JetFragRel & jfr, Int_t j) {
+      if (j==0
+	  && jfr.jtpt[j]>=cut_->JEtMin[j] && jfr.jtpt[j]<cut_->JEtMax[j] && fabs(jfr.jteta[j])<cut_->JEtaMax[j]
+	 ) return true;
+      else if (j==1
+	  && jfr.jtpt[0]>=cut_->JEtMin[0] && jfr.jtpt[0]<cut_->JEtMax[0] && fabs(jfr.jteta[0])<cut_->JEtaMax[0]
+	  && jfr.jtpt[j]>=cut_->JEtMin[j] && jfr.jtpt[j]<cut_->JEtMax[j] && fabs(jfr.jteta[j])<cut_->JEtaMax[j]
+	 ) return true;
+      return false;
+    }
+    Bool_t SelFrag(const JetFragRel & jfr, Int_t ip, Int_t j) {
+	//return ( fabs(jfr.peta[ip])<cut_->TrkEtaMax );
+	return ( jfr.pjdr[j][ip]<cut_->ConeSize && jfr.ppt[ip]>3);
+    }
+};
+
+FragAnaLoop::FragAnaLoop(TString name) :
+  name_(name),
+  vtrkCorr_(2),
+  anaTrkType_(2),
+  numJet_(2),
+  vhPPtCorr_(2),
+  vhPPtRat_(2),
+  vhTrkCorrPPt_(2),
+  vhTrkCorrJEt_(2),
+  vhTrkCorrCent_(2)
+{
+}
+
+void FragAnaLoop::Init()
+{
+  cout << "================ Ana Loop ==================" << endl;
+  cout << " anaTrkType: " << anaTrkType_ << endl;
+  cout << " LeadingJet: " << cut_->JEtMin[0] << "," << cut_->JEtMax[0] << " SubLeadingJet: " << cut_->JEtMin[1] << " " << cut_->JEtMax[1] << endl;
+  cout << "============================================" << endl;
+
+  jfr_.LoadBranches(t_);
+
+  // Book Histograms
+  for (Int_t j=0; j<2; ++j) {
+    for (Int_t lv=0; lv<5; ++lv) {
+      if (anaTrkType_==0 && lv>0) continue;
+      vhPPtCorr_[j].push_back(new TH1D(Form("h%sPPtCorr%d_j%d",name_.Data(),lv,j),";p_{T} (GeV/c);",ptBin_.size()-1,&ptBin_[0]));
+      vhPPtCorr_[j][lv]->Sumw2();
+      vhPPtRat_[j].push_back(new TH1D(Form("h%sPPtRat%d_j%d",name_.Data(),lv,j),";p_{T} (GeV/c);",ptBin_.size()-1,&ptBin_[0]));
+      vhPPtRat_[j][lv]->Sumw2();
+
+      vhTrkCorrPPt_[j].push_back(new TH2D(Form("h%sTrkCorr%dPPt_j%d",name_.Data(),lv,j),";p_{T} (GeV/c);",50,0,100,50,-0.2,1.2));
+      vhTrkCorrJEt_[j].push_back(new TH2D(Form("h%sTrkCorr%dJEt_j%d",name_.Data(),lv,j),";Jet p_{T} (GeV/c);",100,0,400,50,-0.2,1.2));
+      vhTrkCorrCent_[j].push_back(new TH2D(Form("h%sTrkCorr%dCent_j%d",name_.Data(),lv,j),";Centrality (%);",10,0,100,50,-0.2,1.2));
+    }
+  }
+
+  // Book ntuples
+  ntCorr_ = new TNtuple("ntCorr","nt of trk corrections",
+      "cent:pthatmin:"
+      "jtpt1:jteta1:jtpt2:jteta2:"
+      "ppt:peta:pj1dr:pj2dr:"
+      "eff:fak");
+}
+
+void FragAnaLoop::Loop()
+{
+  // Initialize counters
+  for (Int_t j=0; j<2; ++j) {
+    numJet_[j]=0;
+  }
+
+  // Main Loop
+  Int_t numEntries = t_->GetEntries();
+  cout << t_->GetName() << ": " << numEntries << endl;
+  for (Int_t i=0; i<numEntries; ++i) {
+    t_->GetEntry(i);
+    // Calc Dijet properties
+    jfr_.jdphi = deltaPhi(jfr_.jtphi[0],jfr_.jtphi[1]);
+    // ===========================
+    // Basic Event preselection
+    // ===========================
+    if (jfr_.jtpt[0]<50) continue;
+
+    for (Int_t j=0; j<2; ++j) {
+      if (!cut_->doSel||(SelEvt(jfr_)&&SelJet(jfr_,j))) {
+	++numJet_[j];
+	//cout << jfr_.jtpt[0] << " " << jfr_.jteta[0] << " " << jfr_.jtphi[0] << endl;
+      }
+    }
+
+    for (Int_t ip=0; ip<jfr_.np; ++ip) {
+      Float_t trkEnergy = jfr_.ppt[ip];
+      Float_t trkEta = jfr_.peta[ip];
+      // Correction for this trk
+      Double_t corr[4];
+      Double_t eff=1,fak=1,mul=1,sec=1,trkwt=1;
+      /*
+      if (anaTrkType_>=2) {
+	trkCorr_->GetCorr(trkEnergy,trkEta,(*jfr_.jtpt)[0],jfr_.cent,corr);
+	//trkCorr_->GetCorr(trkEnergy,trkEta,50,jfr_.cent,corr);
+	eff = corr[0];
+	fak = corr[1];
+	mul = corr[2];
+	sec = corr[3];
+	ntCorr_->Fill(jfr_.cent,ptHatMin_,
+	    (*jfr_.jtpt)[0],(*jfr_.jteta)[0],(*jfr_.jtpt)[1],(*jfr_.jteta)[1],
+	    trkEnergy,trkEta,(*jfr_.pdr)[0][ip],(*jfr_.pdr)[1][ip],
+	    eff,fak
+	    );
+	if (eff<1e-5) { eff=1; }
+	trkwt = (1-fak)*(1-sec)/(eff*(1+mul));
+	//if (trkwt<0||trkwt>20) {
+	//  cout << trkEnergy << " " << eff << " " << fak << " " << mul << " " << sec << " " << trkwt << endl;
+	//}
+      }
+      */
+      // Fill
+      for (Int_t j=0; j<2; ++j) {
+	// =======================
+	// Calc jet-particle relations
+	// =======================
+	jfr_.pjdr[j][ip] = deltaR(jfr_.peta[ip],jfr_.pphi[ip],jfr_.jteta[j],jfr_.jtphi[j]);
+	//cout << "jet " << j << " dr: " << jfr_.pjdr[j][ip] << endl;
+	jfr_.pjdrbg[j][ip] = deltaR(jfr_.peta[ip],jfr_.pphi[ip],jfr_.jteta[j],jfr_.jtphi[j]+TMath::PiOver2());
+	// =======================
+	// Get Corrections
+	// =======================
+	//cout << (*jfr_.pdr)[j][ip] << endl;
+	if (!cut_->doSel||(SelEvt(jfr_)&&SelJet(jfr_,j)&&SelFrag(jfr_,ip,j))) {
+	  if (anaTrkType_>=2) {
+	    vtrkCorr_[j]->GetCorr(trkEnergy,trkEta,jfr_.jtpt[j],jfr_.cent,corr);
+	    eff = corr[0];
+	    fak = corr[1];
+	    mul = corr[2];
+	    sec = corr[3];
+	    if (eff<1e-5) { eff=1; }
+	    trkwt = (1-fak)*(1-sec)/(eff*(1+mul));
+	  }
+	  // fill
+	  vhPPtCorr_[j][0]->Fill(trkEnergy);
+	  if (anaTrkType_==0) continue;
+	  vhPPtCorr_[j][1]->Fill(trkEnergy,1./eff);
+	  vhPPtCorr_[j][2]->Fill(trkEnergy,(1-fak)/eff);
+	  vhPPtCorr_[j][3]->Fill(trkEnergy,(1-fak)/(eff*(1+mul)));
+	  vhPPtCorr_[j][4]->Fill(trkEnergy,trkwt);
+	  // study correction
+	  for (Int_t lv=1; lv<=4; ++lv) {
+	    vhTrkCorrPPt_[j][lv]->Fill(trkEnergy,corr[lv-1]);
+	    vhTrkCorrJEt_[j][lv]->Fill(jfr_.jtpt[j],corr[lv-1]);
+	    vhTrkCorrCent_[j][lv]->Fill(jfr_.cent,corr[lv-1]);
+	  }
+	}
+      }
+    }
+  } // end of main loop
+
+  // Normalize
+  for (Int_t j=0; j<2; ++j) {
+    cout << "jet " << j << " count: " << numJet_[j] << endl;
+    for (Int_t lv=0; lv<5; ++lv) {
+      if (anaTrkType_==0 && lv>0) continue;
+      normHist(vhPPtCorr_[j][lv],0,true,1./numJet_[j]);
+    }
+  }
+}
---
+++ Saved/QM11Ana/macros/highPtFragAna/loopTrkClosure.C
@@ -0,0 +1,228 @@
+#include "TChain.h"
+#include "TCut.h"
+#include "TCanvas.h"
+#include "TLine.h"
+#include "TLegend.h"
+#include "TProfile.h"
+#include "FragAnaLoop.h"
+
+void loopTrkClosure(Double_t ptHatMin=110,
+    Bool_t doSel=true)
+{
+  // ===================================
+  // Inputs
+  // ===================================
+  //TString infrec="nt_djhp_HyUQ110v0_djcalo_100_50_offset0.root";
+  //TString infgen="nt_djhp_HyUQ110v0_djcalo_genp_100_50_offset0.root";
+  TString infrec=Form("trana_hydjuq%.0f_mc_akpu3pf_t2.root",ptHatMin);
+  TString infgen=Form("trana_hydjuq%.0f_mc_akpu3pf_t0.root",ptHatMin);
+  if (ptHatMin<0) {
+    infrec="nt_djhp_HyUQAllv0_djcalo.root";
+    infgen="nt_djhp_HyUQAllv0_djcalo_genp.root";
+  }
+  TChain * trec = new TChain("tjf");
+  trec->Add(infrec);
+  TChain * tgen = new TChain("tjf");
+  tgen->Add(infgen);
+  TCut evtCut="cent<30";
+  if (trec->GetEntries()>0) cout << infrec << " cut " << TString(evtCut) << ": " << trec->GetEntries(evtCut) << endl;
+  else { cout << infrec << " has 0 entries" << endl; exit(1); }
+  if (tgen->GetEntries()>0) cout << infgen << " cut " << TString(evtCut) << ": " << tgen->GetEntries(evtCut) << endl;
+  else { cout << infgen << " has 0 entries" << endl; exit(1); }
+
+  TString tag("CorrUqB2Jet");
+  // ===================================
+  // Cuts
+  // ===================================
+  selectionCut cut;
+  cut.doSel = doSel;
+  cut.CentMin=0;
+  cut.CentMax=30;
+  cut.JEtMin[0] = 150;
+  cut.JEtMax[0] = 250;
+  cut.JEtMin[1] = 50;
+  cut.JEtMax[1] = 250;
+  cut.JEtaMax[0] = 0.8;
+  cut.JEtaMax[1] = 0.8;
+  cut.TrkEtaMax = 1;
+  cut.ConeSize = 0.5;
+
+  // ===================================
+  // Correction
+  // ===================================
+  Corrector3D trkCorr("trkCorrHisAna_djuq","_jtv2_2_cv0","B2");
+  trkCorr.ptRebinFactor_ = 1;
+  trkCorr.sampleMode_ = 1; // 0 for choosing individual sample, 1 for merge samples
+  trkCorr.smoothLevel_ = 1; // 0: no smooth, 1: smooth jet, 2: smooth jet,eta
+  trkCorr.Init(0);
+  //TH1D * hPtBinUnRebin = (TH1D*)trkCorr.ptBin_->Clone("hPtBinUnRebin");
+  //trkCorr.Init(1,"TrkCorr2DB0.root");
+  Corrector3D trkCorrJ1("trkCorrHisAna_djuq","_jtv2_2_cv0","B2InConeJ1");
+  trkCorrJ1.sampleMode_ = 1; // 0 for choosing individual sample, 1 for merge samples
+  trkCorrJ1.smoothLevel_ = 1; // 0: no smooth, 1: smooth jet, 2: smooth jet,eta
+  trkCorrJ1.Init();
+  Corrector3D trkCorrJ2("trkCorrHisAna_djuq","_jtv2_2_cv0","B2InConeJ2");
+  trkCorrJ2.sampleMode_ = 1; // 0 for choosing individual sample, 1 for merge samples
+  trkCorrJ2.smoothLevel_ = 1; // 0: no smooth, 1: smooth jet, 2: smooth jet,eta
+  trkCorrJ2.Init();
+
+  // ===================================
+  // Setup
+  // ===================================
+  // bins
+  const Int_t numPPtBins=18;
+  Float_t pptBins[numPPtBins+1] = {0.5,1,1.5,2,2.5,3,4,5,7.5,10,12,15,20,25,30,45,60,90,120};
+  vector<Double_t> ptBin(pptBins,pptBins+numPPtBins+1);
+  //TH1D * hxbin = new TH1D("hxbin","",numPPtBins,pptBins);
+  //TH1D * hxbin = (TH1D*)hPtBinUnRebin->Clone("hxbin");
+  //TH1D * hxbin = (TH1D*)trkCorr.ptBin_->Clone("hxbin");
+  //hxbin->Rebin(2);
+  //vector<Double_t> ptBin;
+  //for (Int_t i=1; i<=hxbin->GetNbinsX()+1; ++i) {
+  //  ptBin.push_back(hxbin->GetBinLowEdge(i));
+  //}
+  cout << ptBin.size()-1 << " pt bins: ";
+  for (Int_t i=0; i<ptBin.size(); ++i) {cout << ptBin[i] << " ";}
+  cout << endl;
+
+  // ===================================
+  // Analyze
+  // ===================================
+  TString outName(Form("Ana%.0f_%s",ptHatMin,tag.Data()));
+  if (!cut.doSel) outName+="Nt";
+  TString outFileName(Form("histsFragLoop_%s.root",outName.Data()));
+  TFile * outf = new TFile(outFileName,"RECREATE");
+  // rec trk
+  FragAnaLoop recfana("Rec");
+  recfana.ptHatMin_ = ptHatMin;
+  recfana.anaTrkType_=2;
+  recfana.t_ = trec;
+  recfana.cut_ = &cut;
+  //recfana.vtrkCorr_[0] = &trkCorr;
+  recfana.vtrkCorr_[0] = &trkCorrJ1;
+  recfana.vtrkCorr_[1] = &trkCorrJ2;
+  recfana.ptBin_ = ptBin;
+  recfana.Init();
+  recfana.Loop();
+  // gen partilce
+  FragAnaLoop genfana("Gen");
+  genfana.ptHatMin_ = ptHatMin;
+  genfana.anaTrkType_=0;
+  genfana.t_ = tgen;
+  genfana.cut_ = &cut;
+  //genfana.vtrkCorr_[0] = &trkCorr;
+  genfana.vtrkCorr_[0] = &trkCorrJ1;
+  genfana.vtrkCorr_[1] = &trkCorrJ2;
+  genfana.ptBin_ = ptBin;
+  genfana.Init();
+  genfana.Loop();
+
+  // get ratio
+  for (Int_t j=0; j<2; ++j) {
+    for (Int_t lv=0; lv<=4; ++lv) {
+      recfana.vhPPtRat_[j][lv]->Divide(recfana.vhPPtCorr_[j][lv],genfana.vhPPtCorr_[j][0]);
+    }
+  }
+
+  // ===================================
+  // Plot
+  // ===================================
+  Int_t colors[10] = {kGray+2,kViolet,kBlue+1,kGreen+2,kOrange+2,kMagenta,kRed};
+  for (Int_t j=0; j<2; ++j) {
+    TString append(Form("_j%d_%s",j,outName.Data()));
+    // prepare histogram frame
+    genfana.vhPPtCorr_[j][0]->SetAxisRange(4,100,"X");
+    genfana.vhPPtCorr_[j][0]->SetAxisRange(1e-3,5e1,"Y");
+    genfana.vhPPtCorr_[j][0]->SetLineColor(kRed);
+    genfana.vhPPtCorr_[j][0]->SetMarkerColor(kRed);
+    genfana.vhPPtCorr_[j][0]->SetMarkerStyle(kOpenCircle);
+    for (Int_t lv=0; lv<=3; ++lv) {
+      recfana.vhPPtCorr_[j][lv]->SetMarkerStyle(kOpenSquare);
+      recfana.vhPPtCorr_[j][lv]->SetMarkerColor(colors[lv]);
+      recfana.vhPPtCorr_[j][lv]->SetLineColor(colors[lv]);
+      recfana.vhPPtRat_[j][lv]->SetMarkerStyle(kOpenSquare);
+      recfana.vhPPtRat_[j][lv]->SetMarkerColor(colors[lv]);
+      recfana.vhPPtRat_[j][lv]->SetLineColor(colors[lv]);
+    }
+
+    // draw spectrum
+    TCanvas *c2 = new TCanvas("c2"+append,"closure"+append,500,900);
+    c2->Divide(1,2);
+    c2->cd(1);
+    c2->GetPad(1)->SetLogy();
+    c2->GetPad(1)->SetLogx();
+    genfana.vhPPtCorr_[j][0]->Draw("hist");
+    recfana.vhPPtCorr_[j][0]->Draw("sameE");
+    for (Int_t lv=1; lv<=4; ++lv) {
+      recfana.vhPPtCorr_[j][lv]->Draw("sameE");
+    }
+    // draw ratio
+    c2->cd(2);
+    c2->GetPad(2)->SetLogx();
+    recfana.vhPPtRat_[j][0]->Draw("E");
+    recfana.vhPPtRat_[j][0]->SetAxisRange(4,100,"X");
+    recfana.vhPPtRat_[j][0]->SetAxisRange(0,1.5,"Y");
+    for (Int_t lv=1; lv<=4; ++lv) {
+      recfana.vhPPtRat_[j][lv]->Draw("sameE");
+    }
+
+    // ====================
+    TLine *l = new TLine(4,1,100,1);
+    l->SetLineStyle(2);
+    l->Draw();
+
+    TString corrLevelName[5] = { "Raw","Eff","Fake","Mul. Rec","Full" };
+    c2->cd(1);
+    TLegend *leg = new TLegend(0.61,0.57,0.91,0.91);
+    leg->SetFillStyle(0);
+    leg->SetBorderSize(0);
+    leg->SetTextSize(0.035);
+    //leg->AddEntry(genfana.vhPPtCorr_[0][0],"#DeltaR(jet,trk)<0.5","");
+    leg->AddEntry(genfana.vhPPtCorr_[0][0],"Gen. Particle","pl");
+    for (Int_t lv=0; lv<=4; ++lv) {
+      leg->AddEntry(recfana.vhPPtCorr_[j][lv],"Trk Corr. "+corrLevelName[lv],"pl");
+    }
+    leg->Draw();
+
+    c2->Print(Form("ClosureTrkPt%s.gif",append.Data()));
+
+    // ====================
+    TCanvas * chk0 = new TCanvas("chk0"+append,"eff_vs_pt"+append,500,500);
+    chk0->SetLogz();
+    chk0->SetRightMargin(0.15);
+    recfana.vhTrkCorrPPt_[j][1]->Draw("colz");
+    recfana.vhTrkCorrPPt_[j][1]->ProfileX()->Draw("same");
+    //trkCorr.InspectCorr(0,-1,0,-1,2,7-2,7+2,"histsame");
+    chk0->Print(Form("AppliedTrkEffPPt%s.gif",append.Data()));
+
+    TCanvas * chk1 = new TCanvas("chk1"+append,"fake_vs_pt"+append,500,500);
+    chk1->SetLogz();
+    chk1->SetRightMargin(0.15);
+    recfana.vhTrkCorrPPt_[j][2]->Draw("colz");
+    recfana.vhTrkCorrPPt_[j][2]->ProfileX()->Draw("same");
+    //trkCorr.InspectCorr(1,-1,0,-1,2,7-2,7+2,"histsame");
+    chk1->Print(Form("AppliedTrkFakPPt%s.gif",append.Data()));
+
+    TCanvas * cCorrJEt = new TCanvas("cCorrJEt"+append,"eff_vs_jet"+append,500,500);
+    cCorrJEt->SetLogz();
+    cCorrJEt->SetRightMargin(0.15);
+    recfana.vhTrkCorrJEt_[j][1]->Draw("colz");
+    recfana.vhTrkCorrJEt_[j][1]->ProfileX()->Draw("same");
+    //trkCorr.InspectCorr(0,-1,0,-1,2,7-2,7+2,"histsame");
+    cCorrJEt->Print(Form("AppliedTrkEffJEt%s.gif",append.Data()));
+
+    TCanvas * cCorrCent = new TCanvas("cCorrCent"+append,"eff_vs_cent"+append,500,500);
+    cCorrCent->SetLogz();
+    cCorrCent->SetRightMargin(0.15);
+    recfana.vhTrkCorrCent_[j][1]->Draw("colz");
+    recfana.vhTrkCorrCent_[j][1]->ProfileX()->Draw("same");
+    //trkCorr.InspectCorr(0,-1,0,-1,2,7-2,7+2,"histsame");
+    cCorrCent->Print(Form("AppliedTrkEffCent%s.gif",append.Data()));
+  }
+
+  // ===================================
+  // All Done, Write output if choose
+  // ===================================
+  if (!cut.doSel) recfana.ntCorr_->Write();
+  else outf->Clear();
+}
