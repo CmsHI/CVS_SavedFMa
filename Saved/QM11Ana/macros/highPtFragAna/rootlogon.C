e765218f6f2b1133a40228ee4d41ba0941b7271f
frankma <>
2011-04-07 17:24:25 +0000

    fwlite needed for jec correction

--- Saved/QM11Ana/macros/highPtFragAna/rootlogon.C
+++ Saved/QM11Ana/macros/highPtFragAna/rootlogon.C
@@ -1,11 +1,18 @@
 {
-  // styles
-  gROOT->Macro("../rootlogon_base.C");
-  gStyle->SetOptStat(0);   
-  //gStyle->SetOptStat(1111);
-  //gStyle->SetPadLeftMargin(0.2);
-  gStyle->SetOptFit(0);
-  gStyle->SetMarkerSize(1.);
-  gStyle->SetHistLineWidth(2);
-  gStyle->SetErrorX(0); 
+  // Set up FW Lite for automatic loading of CMS libraries
+  // and data formats.   As you may have other user-defined setup
+  // in your rootlogon.C, the CMS setup is executed only if the CMS
+  // environment is set up.
+  //
+  TString cmsswbase = getenv("CMSSW_BASE");
+  if (cmsswbase.Length() > 0) {
+    //
+    // The CMSSW environment is defined (this is true even for FW Lite)
+    // so set up the rest.
+    //
+    cout << "Loading FW Lite setup." << endl;
+    gSystem->Load("libFWCoreFWLite.so");
+    AutoLibraryLoader::enable();
+    gROOT->ProcessLine(".x load_JEC.C");
+  }
 }
