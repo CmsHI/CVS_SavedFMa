a309b11b005d655a44caeb4289d6305086e62f73
frankma <>
2011-04-28 19:22:52 +0000

    check pp trk corr

--- Saved/QM11Ana/macros/highPtFragAna/final/drawTrkCorrPt.C
+++ Saved/QM11Ana/macros/highPtFragAna/final/drawTrkCorrPt.C
@@ -7,13 +7,17 @@
 void drawTrkCorrPt(Int_t corrLevel=0,
     TString mod="hitrkEffAnalyzer_akpu3pf_j2",
     TString modref="hitrkEffAnalyzer_akpu3pf_j1",
+    //TString mod="hiHighPtTrkEffAnalyzer_akpu3pf_j2",
+    //TString modref="hiHighPtTrkEffAnalyzer_akpu3pf_j1",
     //TString mod="B2InConeJ2",
     //TString modref="B2InConeJ1",
     //TString app="_mattgmv1",
-    //TString app="_tev6",
-    //TString appref="_tev6",
-    TString app="_ppv1",
-    TString appref="_ppv1",
+    //TString app="_tev7hpt",
+    //TString appref="_tev7hpt",
+    //TString app="_ppv1",
+    //TString appref="_ppv1",
+    TString app="_pphptv1",
+    TString appref="_pphptv1",
     //TString app="_jtv5_cv6",
     //TString appref="_jtv5_cv6",
     Int_t isample=0, // -1 for all samples
@@ -23,17 +27,20 @@ void drawTrkCorrPt(Int_t corrLevel=0,
   Int_t mode=1; // 0 for write, 1 for read
   //Int_t centBeg=0, centEnd=2; // HI: 0-30%
   Int_t centBeg=0, centEnd=4; // pp: all cent
-  Float_t ptHatMin=-1;
 
   Corrector3D trkCorr("trkCorrHisAna_djuq",app,mod);
   //trkCorr.ptHatMin_.clear();
-  //trkCorr.ptHatMin_.push_back(ptHatMin);
+  //trkCorr.ptHatMin_.push_back(50);
+  //trkCorr.ptHatMin_.push_back(80);
+  //trkCorr.ptHatMin_.push_back(110);
   trkCorr.sampleMode_ = 1; // 0 for choosing individual sample, 1 for merge samples
   trkCorr.Init();
 
   Corrector3D trkCorrRef("trkCorrHisAna_djuq",appref,modref);
   //trkCorrRef.ptHatMin_.clear();
-  //trkCorrRef.ptHatMin_.push_back(ptHatMin);
+  //trkCorrRef.ptHatMin_.push_back(50);
+  //trkCorrRef.ptHatMin_.push_back(80);
+  //trkCorrRef.ptHatMin_.push_back(110);
   trkCorrRef.sampleMode_ = 1; // 0 for choosing individual sample, 1 for merge samples
   trkCorrRef.Init();
 
@@ -51,8 +58,6 @@ void drawTrkCorrPt(Int_t corrLevel=0,
 
   // fake
   TH1D * hCorr1PtRefJet100 = (TH1D*) trkCorrRef.InspectCorr(1,isample,centBeg,centEnd,6,16,2,7-etaPM,7+etaPM);
-  hCorr1PtRefJet100->SetAxisRange(0,89.9,"X");
-  hCorr1PtRefJet100->SetAxisRange(0,1,"Y");
   hCorr1PtRefJet100->SetMarkerStyle(kOpenCircle);
   hCorr1PtRefJet100->Draw("sameE");
   TH1D * hCorr1PtJet40 = (TH1D*) trkCorr.InspectCorr(1,isample,centBeg,centEnd,3,16,2,7-etaPM,7+etaPM);
@@ -62,7 +67,8 @@ void drawTrkCorrPt(Int_t corrLevel=0,
   leg->SetFillStyle(0);
   leg->SetBorderSize(0);
   leg->SetTextSize(0.035);
-  leg->AddEntry(hCorrPtRefJet100,"0-30%, |#eta^{Trk}|<1","");
+  //leg->AddEntry(hCorrPtRefJet100,"0-30%, |#eta^{Trk}|<1","");
+  leg->AddEntry(hCorrPtRefJet100,"pp, |#eta^{Trk}|<1","");
   leg->AddEntry(hCorrPtRefJet100,"Leading Jet, p_{T} > 100 GeV/c","p");
   leg->AddEntry(hCorrPtJet40,"Subleading Jet, p_{T} > 40 GeV/c","p");
   leg->Draw();
