5c2e7a709a001d51314104a7664faf77ec4b987e
frankma <>
2011-06-20 16:40:45 +0000

    added openhlt for 399

--- Saved/JeAna11/scripts/setup39X.sh
+++ Saved/JeAna11/scripts/setup39X.sh
@@ -36,11 +36,13 @@ cvs co -r V00-07-21 RecoLocalCalo/HcalRecAlgos/interface/HBHETimingShapedFlag.h
 cvs co -r V00-00-18 JetMETAnalysis/HcalReflagging
 cvs co -d HcalFilter UserCode/MTonjes/HcalFilter
 
-#ecal requires some code from the photon analyzer, 'rm' commands are to truncate the endless chain of packages
- obviously, this should be cleaned up
+#ecal requires some code from the photon analyzer, 'rm' commands are to truncate the endless chain of packages obviously, this should be cleaned up
 cvs co -d CmsHi/PhotonAnalysis UserCode/CmsHi/PhotonAnalysis
 rm CmsHi/PhotonAnalysis/plugins/MultiPhotonAnalyzer.*
 rm CmsHi/PhotonAnalysis/plugins/SinglePhotonAnalyzer.*
 cvs co -d UserCode/HafHistogram UserCode/CmsHi/HafHistogram
+
+# openhlt
+cvs co -r V03-03-08 HLTrigger/HLTanalyzers
 
 scram b -j4
