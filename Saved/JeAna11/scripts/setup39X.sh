bdff6e7e11beefe3c6d9146d1135cf12bfb27f38
frankma <>
2011-06-21 15:41:41 +0000

    update to run with pftowers and clean up openhlt output

--- Saved/JeAna11/scripts/setup39X.sh
+++ Saved/JeAna11/scripts/setup39X.sh
@@ -1,9 +1,7 @@
 ## hiGoodMergedTracks and the track analyzer
 cvs co -d       edwenger/HiVertexAnalyzer UserCode/edwenger/HiVertexAnalyzer
 cvs co -d       edwenger/HiTrkEffAnalyzer UserCode/edwenger/HiTrkEffAnalyzer
-#cvs co -d      Appeltel/PixelTrackAnalysis  UserCode/Appeltel/PixelTrackAnalysis
-#cvs co -d      Appeltel/PixelTracksRun2010  UserCode/Appeltel/PixelTracksRun2010
-cvs co -d      edwenger/HiTrkEffAnalyzer/python/ UserCode/edwenger/HiTrkEffAnalyzer/python/
+cvs co -d       edwenger/HiTrkEffAnalyzer/python/ UserCode/edwenger/HiTrkEffAnalyzer/python/
 
 # pat macros
 cvs co -d      MNguyen/patMacrosForMC  UserCode/MNguyen/patMacrosForMC
@@ -24,11 +22,13 @@ cvs co          DataFormats/HeavyIonEvent
 cvs co          RecoHI/HiCentralityAlgos
 cvs co -d       CmsHi/Analysis2010 UserCode/CmsHi/Analysis2010
 
-# Jet plus tracks
-cvs co -r       V00-02-00 RecoJets/JetPlusTracks  
+# needed to get PFTowers to work
+cvs co -d Saved/JeAna11 UserCode/SavedFMa/Saved/JeAna11
+cp Saved/JeAna11/modifiedFiles/VirtualJetProducer.cc RecoJets/JetProducers/plugins/
+cp Saved/JeAna11/modifiedFiles/PileUpSubtractor.cc RecoJets/JetProducers/src/
+cp Saved/JeAna11/modifiedFiles/PileUpSubtractor.h RecoJets/JetProducers/interface/
 
 #ecal and hcal cleaning
-
 #hcal
 addpkg RecoLocalCalo/HcalRecAlgos
 cvs co -r V00-07-21 RecoLocalCalo/HcalRecAlgos/src/HBHETimingShapedFlag.cc
@@ -44,5 +44,8 @@ cvs co -d UserCode/HafHistogram UserCode/CmsHi/HafHistogram
 
 # openhlt
 cvs co -r V03-03-08 HLTrigger/HLTanalyzers
+# private code: clean up output
+cp Saved/JeAna11/modifiedFiles/HLTAnalyzer.h HLTrigger/HLTanalyzers/interface/HLTAnalyzer.h
+cp Saved/JeAna11/modifiedFiles/HLTAnalyzer.cc HLTrigger/HLTanalyzers/src/HLTAnalyzer.cc
 
 scram b -j4
