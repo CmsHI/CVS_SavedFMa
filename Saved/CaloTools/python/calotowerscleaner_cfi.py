3e5462f19c160c7946db8900406d2f7e72af954d
frankma <>
2011-09-15 21:30:49 +0000

    parameters mean the exclusion region

--- Saved/CaloTools/python/calotowerscleaner_cfi.py
+++ Saved/CaloTools/python/calotowerscleaner_cfi.py
@@ -2,8 +2,8 @@ import FWCore.ParameterSet.Config as cms
 
 cleanedTowers = cms.EDProducer("CaloTowersCleaner",
    regularTowerTag = cms.InputTag("towerMaker"),
-   excludeEtaMin = cms.double(-999),
-   excludeEtaMax = cms.double(999),
-   excludePhiMin = cms.double(-999),
-   excludePhiMax = cms.double(999)
+   excludeEtaMin = cms.double(0),
+   excludeEtaMax = cms.double(0),
+   excludePhiMin = cms.double(0),
+   excludePhiMax = cms.double(0)
    )
