94b3a2f444c486c0bc66f86c4e93998e4922f34f
frankma <>
2011-10-11 06:56:20 +0000

    fix the forgotten turning on of storing baseline and baselinpoints

--- Saved/TrackerZS/test/hiReco_data.py
+++ Saved/TrackerZS/test/hiReco_data.py
@@ -35,7 +35,7 @@ process.options = cms.untracked.PSet(
 
 # Production Info
 process.configurationMetadata = cms.untracked.PSet(
-    version = cms.untracked.string('$Revision: 1.2 $'),
+    version = cms.untracked.string('$Revision: 1.3 $'),
     annotation = cms.untracked.string('hiReco nevts:1'),
     name = cms.untracked.string('PyReleaseValidation')
 )
@@ -46,13 +46,13 @@ process.FEVTDEBUGHLToutput = cms.OutputModule("PoolOutputModule",
     splitLevel = cms.untracked.int32(0),
     eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
     outputCommands = process.FEVTDEBUGHLTEventContent.outputCommands,
-    fileName = cms.untracked.string('hiReco_RAW2DIGI_RECO_ZSHEAD10_05_1150pm_ALLVR_DistrotionThresh0MeanCM128GTV4.root'),
+    fileName = cms.untracked.string('hiReco_RAW2DIGI_RECO_ZSHEAD10_07_1148am_ALLVR_GTV4_hi2010Pedestal.root'),
     dataset = cms.untracked.PSet(
         filterName = cms.untracked.string(''),
         dataTier = cms.untracked.string('GEN-SIM-FEVTDEBUGHLT')
     )
 )
-process.FEVTDEBUGHLToutput.outputCommands.append("keep *_*_VirginRaw_*")
+process.FEVTDEBUGHLToutput.outputCommands.append("keep *_siStripZeroSuppression_*_*")
 
 # Additional output definition
 
@@ -60,9 +60,13 @@ process.FEVTDEBUGHLToutput.outputCommands.append("keep *_*_VirginRaw_*")
 process.GlobalTag.globaltag = 'GR_R_44_V4::All'
 
 # ZS Settings
-process.siStripZeroSuppression.Algorithms.DeltaCMThreshold = cms.uint32(0)       # for BaselineFollower inspect
-process.siStripZeroSuppression.Algorithms.distortionThreshold = cms.uint32(0)
-process.siStripZeroSuppression.Algorithms.MeanCM = cms.int32(158)
+process.siStripZeroSuppression.produceRawDigis = cms.bool(True)
+process.siStripZeroSuppression.produceCalculatedBaseline = cms.bool(True)
+process.siStripZeroSuppression.produceBaselinePoints = cms.bool(True)
+process.siStripZeroSuppression.storeInZScollBadAPV = cms.bool(True)
+#process.siStripZeroSuppression.Algorithms.DeltaCMThreshold = cms.uint32(0)       # for BaselineFollower inspect
+#process.siStripZeroSuppression.Algorithms.distortionThreshold = cms.uint32(0)
+#process.siStripZeroSuppression.Algorithms.MeanCM = cms.int32(158)
 
 # Path and EndPath definitions
 process.raw2digi_step = cms.Path(process.RawToDigi)
@@ -73,3 +77,62 @@ process.FEVTDEBUGHLToutput_step = cms.EndPath(process.FEVTDEBUGHLToutput)
 # Schedule definition
 process.schedule = cms.Schedule(process.raw2digi_step,process.reconstruction_step,process.endjob_step,process.FEVTDEBUGHLToutput_step)
 
+def customiseSiStripConditions(process):
+    process.stripConditions = cms.ESSource("PoolDBESSource",
+      process.CondDBSetup,
+      timetype = cms.untracked.string('runnumber'),
+      connect = cms.string('frontier://FrontierProd/CMS_COND_31X_STRIP'),
+      toGet = cms.VPSet(
+        cms.PSet(
+          record = cms.string('SiStripNoisesRcd'),
+          tag = cms.string('SiStripNoise_GR10_v1_hlt')
+          ),
+          cms.PSet(
+          record = cms.string('SiStripPedestalsRcd'),
+          tag = cms.string('SiStripPedestals_GR10_v1_hlt')
+          ),
+          cms.PSet(
+          record = cms.string('SiStripFedCablingRcd'),
+          tag = cms.string('SiStripFedCabling_GR10_v1_hlt')
+          ),
+          cms.PSet(
+          record = cms.string('SiStripBadChannelRcd'),
+          tag = cms.string('SiStripBadChannel_FromOnline_GR10_v1_hlt')
+          ),
+          cms.PSet(
+          record = cms.string('SiStripLatencyRcd'),
+          tag = cms.string('SiStripLatency_GR10_v2_hlt')
+          ),
+          cms.PSet(
+          record = cms.string('SiStripThresholdRcd'),
+          tag = cms.string('SiStripThreshold_GR10_v1_hlt')
+          ),
+          cms.PSet(
+          record = cms.string('SiStripBadFiberRcd'),
+          tag = cms.string('SiStripBadChannel_FromOfflineCalibration_GR10_v2_offline')
+          ),
+          cms.PSet(
+          record = cms.string('SiStripBadModuleRcd'),
+          tag = cms.string('SiStripBadChannel_FromEfficiencyAnalysis_GR10_v1_offline')
+          ),
+          cms.PSet(
+          record = cms.string('SiStripConfObjectRcd'),
+          tag = cms.string('SiStripShiftAndCrosstalk_GR10_v1_offline')
+          ),
+          cms.PSet(
+          record = cms.string('SiStripLorentzAngleRcd'),
+          tag = cms.string('SiStripLorentzAngle_GR10_v1_offline')
+          ),
+          cms.PSet(
+          record = cms.string('SiStripApvGain2Rcd'),
+          tag = cms.string('SiStripApvGain_FromParticles_GR10_v2_offline')
+          ),
+          cms.PSet(
+          record = cms.string('SiStripApvGainRcd'),
+          tag = cms.string('SiStripApvGain_GR10_v1_hlt')
+          )
+        )
+     )    
+    process.es_prefer_strips = cms.ESPrefer("PoolDBESSource","stripConditions")
+
+customiseSiStripConditions(process)
--- Saved/TrackerZS/test/siStripAna_cfg.py
+++ Saved/TrackerZS/test/siStripAna_cfg.py
@@ -17,15 +17,27 @@ process.source = cms.Source("PoolSource",
         #'file:hiReco_RAW2DIGI_RECO_ZSHEAD10_05_13pm_ALLVR.root'
         #'file:hiReco_RAW2DIGI_RECO_ZSHEAD10_05_1150pm_ALLVR.root'
         #'file:hiReco_RAW2DIGI_RECO_ZSHEAD10_05_1150pm_ALLVR_DistrotionThresh0.root'
-        'file:hiReco_RAW2DIGI_RECO_ZSHEAD10_05_1150pm_ALLVR_DistrotionThresh0MeanCM128GTV4.root'
+        'file:hiReco_RAW2DIGI_RECO_ZSHEAD10_07_1148am_ALLVR_DistrotionThresh0MeanCM158GTV4_hi2010Pedestal.root'
+        # wei's jet 50u
+        #Events in new ZS but not old ZS:
+        #'file:/net/hisrv0001/home/davidlw/scratch1/HLTStudies/CMSSW_4_4_0_OldZS/src/RECO_highptInNewNotOld_OldZS_Jet50U.root'
+        #'file:/net/hisrv0001/home/davidlw/scratch1/HLTStudies/CMSSW_4_4_0_NewZS/src/RECO_highptInNewNotOld_NewZS_Jet50U.root'
+        #Events in old ZS but not new ZS:
+        #'file:/net/hisrv0001/home/davidlw/scratch1/HLTStudies/CMSSW_4_4_0_NewZS/src/RECO_highptInOldNotNew_NewZS_Jet50U.root'
+        #'file:/net/hisrv0001/home/davidlw/scratch1/HLTStudies/CMSSW_4_4_0_OldZS/src/RECO_highptInOldNotNew_OldZS_Jet50U.root'
+
     ),
     #eventsToProcess = cms.untracked.VEventRange('151088:107:646838-151088:107:646838')
 )
 
 process.GlobalTag.globaltag = 'GR_R_44_V5::All'
 
+#proc = "RECOZSHEAD"
+#tag="10_07_1148am"
+proc = "RECO1"
+tag=""
 process.TFileService = cms.Service("TFileService",
-                                  fileName=cms.string("sstr_zshead10_05_23pm_pedsub_allvrzscl_restoreThresh0MeanCM128GTV4.root"))
+                                  fileName=cms.string("sstr_"+proc+tag+"_restoreThresh0MeanCM128GTV4_hi2010ped.root"))
                                   
 process.load("Saved.TrackerZS.trackrechitscanner_cfi")
 process.load("RecoLocalTracker.SiStripZeroSuppression.SiStripBaselineAnalyzer_cfi")
@@ -34,9 +46,9 @@ process.trkRecHitScanner.trkSrc = "hiGlobalPrimTracks"
 process.trkRecHitScanner.trkPtMin = 20
 
 process.sshis = process.SiStripBaselineAnalyzer.clone(
-  srcBaseline =  cms.InputTag('siStripZeroSuppression','BADAPVBASELINE','RECO1'),
-  srcAPVCM  =  cms.InputTag('siStripZeroSuppression','APVCM','RECO1'),
-  srcProcessedRawDigi =  cms.InputTag('siStripZeroSuppression','VirginRaw','RECO1'),
+  srcBaseline =  cms.InputTag('siStripZeroSuppression','BADAPVBASELINE'),
+  srcAPVCM  =  cms.InputTag('siStripZeroSuppression','APVCM'),
+  srcProcessedRawDigi =  cms.InputTag('siStripZeroSuppression','VirginRaw'),
   nModuletoDisplay = cms.uint32(500),
   plotClusters = cms.bool(True),
   plotBaseline = cms.bool(True),
@@ -48,29 +60,40 @@ from RecoLocalTracker.SiStripZeroSuppression.DefaultAlgorithms_cff import *
 
 process.ssnt = cms.EDAnalyzer("StripAnalyzer",
   Algorithms = DefaultAlgorithms,
-  vr = cms.InputTag("siStripDigis","VirginRaw"),
-  zs = cms.InputTag("siStripZeroSuppression","VirginRaw"),
-  pr = cms.InputTag("siStripZeroSuppression","BADAPVBASELINE"),
-  cl = cms.InputTag("siStripClusters"),
+  vr = cms.InputTag("siStripDigis","VirginRaw",proc),
+  zs = cms.InputTag("siStripZeroSuppression","VirginRaw",proc),
+  pr = cms.InputTag("siStripZeroSuppression","BADAPVBASELINE",proc),
+  cl = cms.InputTag("siStripClusters","",proc),
   doVR = cms.bool(True),
   doZS = cms.bool(True),
   doPR = cms.bool(True),
   doCL = cms.bool(True)
   )
 
-process.ssntRRZS = process.ssnt.clone(
-  vr=cms.InputTag("siStripDigis","VirginRaw","RECO1"),
-  zs = cms.InputTag("siStripZeroSuppression","VirginRaw","RECO1"),
-  pr = cms.InputTag("siStripZeroSuppression","BADAPVBASELINE","RECO1"),
-  cl = cms.InputTag("siStripClusters","","RECO1"),
-)
-
 process.ssntBad = process.ssnt.clone(
-  vr = cms.InputTag("siStripZeroSuppression","VirginRaw")
+  vr = cms.InputTag("siStripZeroSuppression","VirginRaw",proc),
+  doZS = False,
+  doPR = False,
+  doCL = False
   )
 
-process.ssntBadRRZS = process.ssntRRZS.clone(
-  vr = cms.InputTag("siStripZeroSuppression","VirginRaw","RECO1")
+process.ssntBadBLPts = process.ssnt.clone(
+  pr = cms.InputTag("siStripZeroSuppression","BADAPVBASELINEPOINTS",proc),
+  doVR = False,
+  doZS = False,
+  doCL = False
   )
 
-process.p = cms.Path(process.sshis*process.ssnt*process.ssntRRZS*process.ssntBad*process.ssntBadRRZS)
+#process.ssntBadRRZS = process.ssntRRZS.clone(
+#  vr = cms.InputTag("siStripZeroSuppression","VirginRaw",proc)
+#  )
+
+#process.ssntRRZS = process.ssnt.clone(
+#  vr=cms.InputTag("siStripDigis","VirginRaw",proc),
+#  zs = cms.InputTag("siStripZeroSuppression","VirginRaw",proc),
+#  pr = cms.InputTag("siStripZeroSuppression","BADAPVBASELINE",proc),
+#  cl = cms.InputTag("siStripClusters","",proc),
+#)
+
+#process.p = cms.Path(process.sshis*process.ssnt*process.ssntRRZS*process.ssntBad*process.ssntBadRRZS)
+process.p = cms.Path(process.sshis*process.ssnt*process.ssntBad*process.ssntBadBLPts)
