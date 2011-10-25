13b591f1a10879db7667b082df31bcf5fceab244
frankma <>
2011-10-25 22:05:13 +0000

    tested yueshi apv tagger

--- Saved/Tracking/macros/zsstudy/PlotAllMod.C
+++ Saved/Tracking/macros/zsstudy/PlotAllMod.C
@@ -1,33 +1,37 @@
 #include "ZSCluster.C"
 
+struct StripEvent{
+  UInt_t run;
+  UInt_t evt;
+  UInt_t id;
+  UInt_t mod;
+  UInt_t nstrip;
+};
+
+
 void PlotAllMod(
-                TString outdir="out/2011.10.07/badall_andsavedVR"
+                TString infname="~/scratch01/work/HLT/hi2011zsyueshi_441/src/Saved/TrackerZS/test/sstr_RECOZSHEADyueshi10195pm_.root",
+                TString outdir="out/2011.10.20/zsyueshi_badall"
                 )
 {
-  TFile * inf1 = new TFile("../ntout/sstr_zshead10_05_23pm_pedsub_allvrzscl_restoreThresh0MeanCM128GTV4_hi2010ped.root");
+  TFile * inf1 = new TFile(infname);
   
-  TTree * tbase1 = (TTree*)inf1->Get("ssntRRZS/pr");
+  //TTree * tbase1 = (TTree*)inf1->Get("ssntRRZS/pr");
+  TTree * tbase1 = (TTree*)inf1->Get("ssntBad/vr");
   
   //Declaration of leaves types
-  UInt_t          module_run;
-  UInt_t          module_evt;
-  UInt_t          module_id;
-  UInt_t          module_mod;
-  UInt_t          module_nstrip;
+  StripEvent se;
   
   // Set branch addresses.
-  tbase1->SetBranchAddress("module",&module_run);
-  tbase1->SetBranchAddress("module",&module_evt);
-  tbase1->SetBranchAddress("module",&module_id);
-  tbase1->SetBranchAddress("module",&module_mod);
-  tbase1->SetBranchAddress("module",&module_nstrip);
+  tbase1->SetBranchAddress("module",&se.run);
   
   Long64_t nentries = tbase1->GetEntries();
+  Long64_t maxPlot = 1000;
   
   Long64_t nbytes = 0;
-  for (Long64_t i=0; i<nentries;i++) {
+  for (Long64_t i=0; i<nentries&&i<maxPlot;++i) {
     nbytes += tbase1->GetEntry(i);
-    cout << module_id << endl;
-    ZSCluster(module_id,outdir);
+    cout << "run" << se.run << " evt: " << se.evt << " id: " << se.id << " nstrip: " << se.nstrip << endl;
+    ZSCluster(infname,se.evt,se.id,0,768,outdir,"zsyueshi");
   }
 }
--- Saved/Tracking/macros/zsstudy/ZSCluster.C
+++ Saved/Tracking/macros/zsstudy/ZSCluster.C
@@ -6,6 +6,7 @@
 #include "TString.h"
 #include "TLine.h"
 #include "TCanvas.h"
+#include "TSystem.h"
 using namespace std;
 
 void RemoveHistZeroPoints(TH1 * h)
@@ -17,21 +18,30 @@ void RemoveHistZeroPoints(TH1 * h)
 }
 
 void ZSCluster(
+               //TString infname="ntout/sstr_RECOZSHEAD10_07_1148am_GTV4_hi2010ped.root",
+               //TString infname="ntout/sstr_RECOZSHEAD10_07_1148am_restoreThresh0MeanCM128GTV4_hi2010ped.root",
+               TString infname="~/scratch01/work/HLT/hi2011zsyueshi_441/src/Saved/TrackerZS/test/sstr_RECOZSHEADyueshi10195pm_.root",
+               int evt=646838,
                int id=436294128,
-               TString outdir="out/2011.10.11_fixblpts"
+               int cluster_beg=0,
+               int cluster_size=768,
+               TString outdir="out/test",
+               TString title="PromptRecoZS",
+               bool useBLF=true,
+               bool newInterface=true
                 )
 {
-  //TFile * inf1 = new TFile("../ntout/sstr_RECOZSHEAD10_07_1148am_GTV4_hi2010ped.root");
-  //TFile * inf1 = new TFile("../ntout/sstr_RECOZSHEAD10_07_1148am_restoreThresh0MeanCM128GTV4_hi2010ped.root");
-  TFile * inf1 = new TFile("../ntout/sstr_RECO1_GTV4_hi2010ped.root");
+  TFile * inf1 = new TFile(infname);
+  cout << infname << " " << title << endl;
   
   TTree * tvr1   = (TTree*)inf1->Get("ssnt/vr");
   TTree * tzs1   = (TTree*)inf1->Get("ssnt/zs");
   TTree * tbase1 = (TTree*)inf1->Get("ssnt/pr");
   TTree * tclus  = (TTree*)inf1->Get("ssnt/cl");
   TTree * tbadvr1 = (TTree*)inf1->Get("ssntBad/vr");
-  //TTree * tblpts1 = (TTree*)inf1->Get("ssntBadBLPts/zs");
-  TTree * tblpts1 = (TTree*)inf1->Get("ssntBadBLPts/pr");
+  TTree * tblpts1 = 0;
+  if (newInterface) tblpts1 = (TTree*)inf1->Get("ssntBadBLPts/zs");
+  else tblpts1 = (TTree*)inf1->Get("ssntBadBLPts/pr");
     
   TH1D * hRef = new TH1D("hRef","",768,0,768);
   TH1D * hPR1 = new TH1D("hPR1","",768,0,768);
@@ -41,69 +51,78 @@ void ZSCluster(
   TH1D * hClus = new TH1D("hClus","",768,0,768);
   TH1D * hBadVR1 = new TH1D("hBadVR1","",768,0,768);
 
-  cout << "booked hists" << endl;
+  //cout << "booked hists" << endl;
+  TString sel=Form("evt==%d&&id==%d",evt,id);
+  TString selclus=Form("evt==%d&&id==%d&&strip>=%d&&strip<=%d",evt,id,cluster_beg,cluster_beg+cluster_size);
+  cout << sel << endl;
+  cout << selclus << endl;
+  tvr1->Project("hRef","strip",Form("adc*(%s)",sel.Data()));
+  tvr1->Project("hPR1","strip",Form("padc*(%s)",sel.Data()));
+  tzs1->Project("hZS1","strip",Form("adc*(%s)",sel.Data()));
+  tclus->Project("hClus","strip",Form("50*(%s)",selclus.Data()));
+  tbase1->Project("hBase1","strip",Form("adc*(%s)",sel.Data()));
+  if (useBLF) {
+    if (newInterface) tblpts1->Project("hBlPts1","strip",Form("(adc-128)*(%s)",sel.Data()));
+    else tblpts1->Project("hBlPts1","strip",Form("(adc)*(%s)",sel.Data()));
+    tbadvr1->Project("hBadVR1","strip",Form("adc*(%s)",sel.Data()));
+  }
   
-  tvr1->Project("hRef","strip",Form("adc*(run==151088&&evt==646838&&id==%d)",id));
-  tvr1->Project("hPR1","strip",Form("padc*(run==151088&&evt==646838&&id==%d)",id));
-  tzs1->Project("hZS1","strip",Form("adc*(run==151088&&evt==646838&&id==%d)",id));
-  tbase1->Project("hBase1","strip",Form("adc*(run==151088&&evt==646838&&id==%d)",id));
-  //tblpts1->Project("hBlPts1","strip",Form("(adc-128)*(run==151088&&evt==646838&&id==%d)",id));
-  tblpts1->Project("hBlPts1","strip",Form("(adc)*(run==151088&&evt==646838&&id==%d)",id));
-  tclus->Project("hClus","strip",Form("50*(run==151088&&evt==646838&&id==%d&&adc>0)",id));
-  tbadvr1->Project("hBadVR1","strip",Form("adc*(run==151088&&evt==646838&&id==%d)",id));
-  
-  cout << "drawn from tree" << endl;
+  //cout << "drawn from tree" << endl;
   RemoveHistZeroPoints(hBlPts1);
   //RemoveHistZeroPoints(hZS1);
   RemoveHistZeroPoints(hClus);
   RemoveHistZeroPoints(hBadVR1);
 
   TCanvas * c2 = new TCanvas("c2","",1000,500);
-  hRef->SetTitle(";strip # ; ADC");
-  //hRef->SetAxisRange(0,128,"X");
-  hRef->SetAxisRange(-200,1400,"Y");
+  TH1D * hFrame = (TH1D*)hRef->Clone("hFrame");
+  hFrame->Reset();
+  hFrame->SetTitle(";strip # ; ADC");
+  //hFrame->SetAxisRange(0,128,"X");
+  hFrame->SetAxisRange(-150,1000,"Y");
   hRef->SetLineStyle(2);
   //hRef->SetLineWidth(2);
   hClus->SetLineColor(kGreen-8);
   hClus->SetMarkerColor(kGreen-9);
   hClus->SetLineWidth(2);
+  hClus->SetMarkerSize(1.);
   hBase1->SetLineColor(kBlue);
   hBlPts1->SetLineColor(kBlue+1);
   hBlPts1->SetMarkerColor(kBlue+1);
-  hBlPts1->SetMarkerSize(0.6);
+  hBlPts1->SetMarkerSize(0.7);
   hBlPts1->SetMarkerStyle(kOpenCircle);
   hZS1->SetLineColor(kRed);
-  hZS1->SetLineWidth(2);
+  //hZS1->SetLineWidth(2);
   hBadVR1->SetLineColor(kYellow);
   hBadVR1->SetMarkerColor(kYellow);
   hBadVR1->SetLineWidth(3);
-  hRef->Draw("hist");
-  hBadVR1->Draw("same l");
-  hClus->Draw("same p-");
+  hBadVR1->SetMarkerSize(1.);
+  hFrame->Draw("hist");
+  if (useBLF) hBadVR1->Draw("same p");
+  hClus->Draw("same p");
   hRef->Draw("same hist");
   hPR1->Draw("same hist");
   hBase1->Draw("same hist");
   hZS1->Draw("same hist");
-  hBlPts1->Draw("samep");
+  if (useBLF) hBlPts1->Draw("samep");
   
-  TLegend *leg = new TLegend(0.60,0.6,0.91,0.90);
+  TLegend *leg = new TLegend(0.61,0.64,0.92,0.94);
   leg->SetFillStyle(0);
   leg->SetBorderSize(0);
   leg->SetTextSize(0.035);
-  leg->AddEntry(hRef,"Run 151088, Evt 646838","");
+  leg->AddEntry(hRef,Form("Evt %d",evt),"");
   leg->AddEntry(hRef,Form("DetID %d",id),"");
   //leg->AddEntry(hRef,"HEAD src 2011.10.07 11am","");
-  leg->AddEntry(hRef,"AllPhysics ReReco ZS","");
+  //leg->AddEntry(hRef,"AllPhysics ReReco ZS","");
+  leg->AddEntry(hRef,title,"");
   leg->AddEntry(hRef,"Unpacked VR Digi","l");
   leg->AddEntry(hPR1,"Processed Raw","l");
   leg->AddEntry(hBase1,"Baseline","l");
-  leg->AddEntry(hBlPts1,"Flat Regions","p");
+  if (useBLF) leg->AddEntry(hBlPts1,"Flat Regions","p");
   leg->AddEntry(hZS1,"ZS Digi","l");
   leg->AddEntry(hClus,"Hit Cluster","p");
-  leg->AddEntry(hBadVR1,"Bad Module (VR Saved)","p");
+  if (useBLF) leg->AddEntry(hBadVR1,"Bad Module (VR Saved)","p");
   leg->Draw();
   
-  //c2->Print(Form("%s/Det%d.gif",outdir.Data(),id));
-  //c2->Print(Form("%s/Det%d_ForceBaseLineFollower.gif",outdir.Data(),id));
-  c2->Print(Form("%s/Det%d_RRZS.gif",outdir.Data(),id));
+  gSystem->mkdir(outdir,true);
+  c2->Print(Form("%s/Det%d_%s.gif",outdir.Data(),id,title.Data()));
 }
