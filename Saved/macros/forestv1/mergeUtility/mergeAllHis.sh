8c017f8438a7605b7e0fd3e61561580f478dc492
frankma <>
2012-06-09 15:12:37 +0000

    bug fixes

--- Saved/macros/forestv1/mergeUtility/mergeAllHis.sh
+++ Saved/macros/forestv1/mergeUtility/mergeAllHis.sh
@@ -1,9 +1,9 @@
 outdir=../trkcorr/Forest2_v19
-mkdir $outdir
+mkdir -p $outdir
 for pthat in 170 120 80 50 30; do
-   hadd_large.sh /mnt/hadoop/cms/store/user/yetkin/MC_Production/Pythia${pthat}_HydjetDrum_mix01/HiForest2_v19 $outdir/trkcorr_hy18dj${pthat}_Forest2_v19.root
+   ./hadd_large.sh /mnt/hadoop/cms/store/user/yetkin/MC_Production/Pythia${pthat}_HydjetDrum_mix01/HiForest2_v19 $outdir/trkcorr_hy18dj${pthat}_Forest2_v19.root
 done
 
 for pthat in 170 120 80 50 30; do
-   hadd_large.sh /mnt/hadoop/cms/store/user/yetkin/MC_Production/Pythia{pthat}_signal01/HiForest2_v19/ $outdir/trkcorr_sigdj${pthat}_Forest2_v19.root
+   ./hadd_large.sh /mnt/hadoop/cms/store/user/yetkin/MC_Production/Pythia${pthat}_signal01/HiForest2_v19/ $outdir/trkcorr_sigdj${pthat}_Forest2_v19.root
 done
