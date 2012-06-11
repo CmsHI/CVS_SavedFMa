d7bd760a8768fcfa846ae8383c804432d4205198
frankma <>
2012-06-11 13:07:21 +0000

    updated to be more flexible

--- data/scripts/transfer/lcgcp_castor2mit.sh
+++ data/scripts/transfer/lcgcp_castor2mit.sh
@@ -1,3 +1,10 @@
-cmd='lcg-cp -b --vo cms -D srmv2 -T srmv2 -v -n 10 srm://srm-cms.cern.ch:8443/srm/managerv2?SFN=/castor/cern.ch/user/f/frankma/forest/Hi2011ForestDiJet_v7_noDuplicate.root srm://se01.cmsaf.mit.edu:8443/srm/v2/server?SFN=/mnt/hadoop/cms/store/user/frankmalocal/forest/Hi2011ForestDiJet_v7_noDuplicate.root'
-echo $cmd
-eval $cmd
+for i in \
+/castor/cern.ch/user/y/yjlee/HiForest/merged_pp2760_AllPhysics_Part_Prod03.root
+do
+   src="srm://srm-cms.cern.ch:8443/srm/managerv2?SFN="${i}
+   out="srm://se01.cmsaf.mit.edu:8443/srm/v2/server?SFN=/mnt/hadoop/cms/store/user/frankmalocal/"${i##*/}
+   
+   cmd='lcg-cp -b --vo cms -D srmv2 -T srmv2 -v -n 10 '${src}' '${out}
+   echo $cmd
+   eval $cmd
+done
