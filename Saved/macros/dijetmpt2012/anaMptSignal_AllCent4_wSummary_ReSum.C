b54e78def4ebe689167df4653ea639e87bb675ee
frankma <>
2012-06-14 05:53:26 +0000

    run on forest2

--- Saved/macros/dijetmpt2012/anaMptSignal_AllCent4_wSummary_ReSum.C
+++ Saved/macros/dijetmpt2012/anaMptSignal_AllCent4_wSummary_ReSum.C
@@ -16,14 +16,18 @@
 #include "TLatex.h"
 #include "TString.h"
 #include "TCut.h"
+#include "TSystem.h"
 
 #include "commonUtility.h"
 #include "SignalCorrector.h"
 #endif
 
 // MPT Ranges
-const int nptrange = 5;
-float ptranges[nptrange+1]={0.5,1.0,2,4,8,180};
+const int nptrange = 6;
+float ptranges[nptrange+1]={0.5,1.0,2,4,8,20,200};
+
+const int nAjBin = 4;
+float AjBins[nAjBin+1] = {0.0001,0.11,0.22,0.33,0.49999};
 
 class AnaMpt {
 public:
@@ -96,7 +100,7 @@ public:
          TFile* hout = new TFile(outfname,"update");
          hout->cd();
          if (verbosity>0) vana[ib]->SetVerbosity(1);
-         float nxbins=800, xmin=-800, xmax=800;
+         float nxbins=1600, xmin=-800, xmax=800;
 //         vana[ib]->rSigAllLeading.Init(nt,250,0,500);
          vana[ib]->rSigAll.Init(nt,nxbins,xmin,xmax);
 //         vana[ib]->rBkgDPhi.Init(nt,nxbins,xmin,xmax);
@@ -122,25 +126,26 @@ void anaMptSignal_AllCent4_wSummary_ReSum(
                                     float minPt1=120,
                                     float minPt2=50,
                                     float sigDPhi=3.1415926*2./3,
-                                    TString outdir = "./fig/05.16_hgtcorr"
+                                    TString outdir = "./fig/06.13MptClos"
                                     )
 {
    TH1::SetDefaultSumw2();
+   gSystem->mkdir(outdir,kTRUE);
    
-   const int nBin = 4;
-   float m[nBin+1] = {0.0001,0.11,0.22,0.33,0.49999};
    const int nCentBin = 2;
    int centBins[3] = {0,12,40};
 
 //    TString inputTree_data="../ntout/output-data-DiJetTrk_v0_v6_icPu5.root";
 //    TString inputTree_data="../ntout/output-data-DiJetTrk_v0_v7_trkAlgo45_icPu5.root";
 //    TString inputTree_data="../ntout/output-data-DiJetTrk_v0_v9_hgt_icPu5.root";
-   TString inputTree_data="../ntout/output-data-DiJetTrk_v0_v9_hgtcorr_icPu5.root";
-   TString inputTree_mc="../ntout/output-hy18dj80_IterPixTrkv1_v6_xsec_icPu5.root";
+//    TString inputTree_data="../ntout/output-data-Forest2v2v3_saveTrks_v0_icPu5.root";
+//    TString inputTree_data = "../ntout/output-hy18dj80_forest2_v0_xsec_icPu5.root";
+   TString inputTree_data="../ntout/output-data-Forest2v2v3_saveTrks_v0_icPu5.root";
+   TString inputTree_mc = "../ntout/output-hy18dj80_forest2_v0_xsec_icPu5.root";
 
    vector<TCut> vcutAnaBin,vcutAnaBinPp;
-   for (int ib=0; ib<nBin; ++ib) {
-      vcutAnaBin.push_back(Form("Aj>=%f&&Aj<%f",m[ib],m[ib+1]));
+   for (int ib=0; ib<nAjBin; ++ib) {
+      vcutAnaBin.push_back(Form("Aj>=%f&&Aj<%f",AjBins[ib],AjBins[ib+1]));
       cout << "anaBin cut: " << vcutAnaBin[ib] << endl;
    }
    
@@ -150,25 +155,29 @@ void anaMptSignal_AllCent4_wSummary_ReSum(
    AnaMpt::minPt1 = minPt1;
    AnaMpt::minPt2 = minPt2;
    AnaMpt::sigDPhi = sigDPhi;
+   float etamax=2.4;
 
-   bool isMC=false;
+   bool isMC=true;
+   TString tag = Form("%s/HisOutput_f2_icPu5_trkHPCorr_%.0f_%.0f_%.0f_eta%.0f",outdir.Data(),minPt1,minPt2,sigDPhi*1000,etamax);
+   if (isMC) tag =Form("%s/HisOutput_v2_icPu5_trkHPCorr_%.0f_%.0f_%.0f_eta%.0f",outdir.Data(),minPt1,minPt2,sigDPhi*1000,etamax);
 
-   TString selTrk = "trkAlgo<5&&trkHP";
-   TString tag = Form("%s/HisOutput_DiJetTrkv0_v9_icPu5_trkAlgo4HPCorr_%.0f_%.0f_%.0f",outdir.Data(),minPt1,minPt2,sigDPhi*1000);
-   if (isMC) tag =Form("%s/HisOutput_Hy18DJv1_v6_icPu5_trkAlgo4HPCorr_%.0f_%.0f_%.0f",outdir.Data(),minPt1,minPt2,sigDPhi*1000);
+   TString recMptVar = "-trkPt*cos(trkPhi-phi1)";
+   TString trkSel = Form("abs(trkEta)<%.1f&&(trkNHit<8||vtrkQual[][0])",etamax);
+   TString trkWt = "vtrkWt[][0]";
 
    // MPT
    TString mcweight;
 //   TString mcweight = "((samplePtHat==50&&pthat<80)||(samplePtHat==80&&pthat<120)||(samplePtHat==120&&pthat<170)||(samplePtHat==170))*weight*sampleWeight";
 //    TString mcweight = "((samplePtHat==80&&pthat<120)||(samplePtHat==120&&pthat<170)||(samplePtHat==170))*weight*sampleWeight"; // not enough statistics for lower pt hat when we have a 120 pt1 cut
 
-   TString mpttag=Form("mptx%s%s","trk","AllAcc");
+   TString mpttag=Form("mptx%s%s","trkCorr","AllAcc");
 
    vector<TString> xmptObs;
+   TString mpt = Form("%s*%s*%s",recMptVar.Data(),trkSel.Data(),trkWt.Data());
    for (int i=0; i<nptrange; ++i) {
-      xmptObs.push_back(Form("Sum$(-trkPt*cos(trkPhi-phi1)*(trkPt>=%.2f&&trkPt<%.2f&&%s)*trkWt)",ptranges[i],ptranges[i+1],selTrk.Data()));
+      xmptObs.push_back(Form("Sum$(%s*(trkPt>=%.2f&&trkPt<%.2f))",mpt.Data(),ptranges[i],ptranges[i+1]));
    }
-   xmptObs.push_back(Form("Sum$(-trkPt*cos(trkPhi-phi1)*(trkPt>=%.2f&&%s))*trkWt",ptranges[0],selTrk.Data()));
+   xmptObs.push_back(Form("Sum$(%s*(trkPt>=%.2f&&trkPt<%.2f))",mpt.Data(),ptranges[0],ptranges[nptrange]));
    
    ////////////////////////////////////////////////////////////////////////////////////////////////
    // Analysis
