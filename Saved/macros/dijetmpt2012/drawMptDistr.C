b44781e73b56c281a8cac8c65b7c93e4f6810045
frankma <>
2012-06-15 02:45:37 +0000

    investigating mpt closure

--- Saved/macros/dijetmpt2012/drawMptDistr.C
+++ Saved/macros/dijetmpt2012/drawMptDistr.C
@@ -19,40 +19,66 @@
 void drawMptDistr(
                                     )
 {
-   TH1::SetDefaultSumw2();
-   float minPt1=100,minPt2=50,sigDPhi=2./3*3.14159;
+   const int nptrange = 6;
+   float ptranges[nptrange+1]={0.5,1.0,2,4,8,20,180};
    
+   TH1::SetDefaultSumw2();
+
+
 //    TString infname="../ntout/output-hy18dj80_IterPixTrkv1_v6_xsec_icPu5.root";
-   TString infname="/net/hidsk0001/d00/scratch/frankma/work/jet/macros/forest/output-hy18dj80_IterPixTrkv1_v7_xsec_icPu5.root";
+//    TString infname="/net/hidsk0001/d00/scratch/frankma/work/jet/macros/forest/output-hy18dj80_IterPixTrkv1_v7_xsec_icPu5.root";
 //    TString infname="/net/hidsk0001/d00/scratch/frankma/work/jet/macros/ntout/output-hy18dj80_IterPixTrkv1_v9hgtOnly_hgtcorr_xsec_icPu5.root";
+   TString infname="/net/hidsk0001/d00/scratch/frankma/work/jet/macros/ntout/output-hy18dj200_iterTrkv1_v11hgtOnly_hgtcorr_mptOnly_xsec_icPu5.root";
    TFile * inf = new TFile(infname);
    TTree * tgj = (TTree*)inf->Get("tgj");
 
-//    TCut mycut="cBin>=0&&cBin<12";
-   TCut mycut="cBin>=12&&cBin<40";
+   TCut mycut="cBin>=0&&cBin<12";
+//    TCut mycut="cBin>=12&&cBin<40";
 
+   float minPt1=200,minPt2=50,sigDPhi=2./3*3.14159;
    TCut leadingSel  = Form("abs(eta1)<2&&pt1>%.3f",minPt1);
    TCut awaySel     = Form("abs(eta2)<2&&pt2>%.3f",minPt2);
    TCut sigSel      = Form("acos(cos(phi2-phi1))>%.3f",sigDPhi);
 
-   TH1D *vmptxRec[7], *vmptxRaw[7], *vmptxGen[7];
+   TString mcweight = "(((samplePtHat==50&&pthat<120)||(samplePtHat==120&&pthat<170)||(samplePtHat==170&&pthat<200)||(samplePtHat==200))*weight*sampleWeight)";
+
+   TH1D *vmptxRec[7], *vmptxRaw[7], *vmptxGen[7], *vwtsum[7];
    for (int i=0; i<7; ++i) {
-      vmptxRaw[i] = new TH1D(Form("hmptxRaw_%d",i),"",200,-200,200);
+      float mptmax=400;
+      vmptxRaw[i] = new TH1D(Form("hmptxRaw_%d",i),"",200,-mptmax,mptmax);
       vmptxRaw[i]->SetMarkerStyle(kOpenCircle);
-      vmptxRec[i] = new TH1D(Form("hmptxRec_%d",i),"",200,-200,200);
-      vmptxGen[i] = new TH1D(Form("hmptxGen_%d",i),"",200,-200,200);
+      vmptxRaw[i]->SetMarkerColor(kBlue);
+      vmptxRec[i] = new TH1D(Form("hmptxRec_%d",i),"",200,-mptmax,mptmax);
+      vmptxGen[i] = new TH1D(Form("hmptxGen_%d",i),"",200,-mptmax,mptmax);
       vmptxGen[i]->SetLineColor(kRed);
+      vwtsum[i] = new TH1D(Form("hmptxWt_%d",i),"",200,-mptmax,mptmax);
    }
    
+   TCut finalCut = mycut&&leadingSel&&awaySel&&sigSel;
+//       finalCut*=mcweight;
+   cout << finalCut << endl;
    for (int i=0; i<7; ++i) {
       if (i<6) {
-         tgj->Draw(Form("-mptxgenpAllAllAcc_pt[%d]>>hmptxGen_%d",i,i),mycut&&leadingSel&&awaySel&&sigSel);
-         tgj->Draw(Form("-mptxtrkCorrAllAcc_pt[%d]>>hmptxRec_%d",i,i),mycut&&leadingSel&&awaySel&&sigSel);
-         tgj->Draw(Form("-mptxtrkAllAcc_pt[%d]>>hmptxRaw_%d",i,i),mycut&&leadingSel&&awaySel&&sigSel);
+//          TCut trkCut = Form("trkPt>%.1f&&trkPt<%.1f",ptranges[i],ptranges[i+1]);
+//          TCut genCut = Form("genpPt>%.1f&&genpPt<%.1f",ptranges[i],ptranges[i+1]);
+//          cout << finalCut*genCut << endl;
+//          cout << finalCut*trkCut << endl;
+//          tgj->Draw(Form("Sum$(-genpPt*cos(genpPhi-phi1)*(genpPt>0.5&&abs(genpEta)<2.2&&genpCh!=0))>>hmptxGen_%d",i),finalCut*genCut);
+//          tgj->Draw(Form("mptxtrkCorrAllAcc_pt[%d]>>hmptxRec_%d",i,i),finalCut*trkCut);
+//          tgj->Draw(Form("-mptxtrkAllAcc_pt[%d]>>hmptxRaw_%d",i,i),finalCut*trkCut);
+         tgj->Draw(Form("mptxgenpAllAllAcc_pt[%d]>>hmptxGen_%d",i,i),finalCut);
+         tgj->Draw(Form("-mptxtrkAllAcc_pt[%d]/0.72>>hmptxRaw_%d",i,i),finalCut);
+         tgj->Draw(Form("-mptxtrkAllAcc_pt[%d]>>hmptxRaw_%d",i,i),finalCut);
       } else {
-         tgj->Draw(Form("-mptxgenpAllAllAcc>>hmptxGen_%d",i),mycut&&leadingSel&&awaySel&&sigSel);
-         tgj->Draw(Form("-mptxtrkCorrAllAcc>>hmptxRec_%d",i),mycut&&leadingSel&&awaySel&&sigSel);
-         tgj->Draw(Form("-mptxtrkAllAcc>>hmptxRaw_%d",i),mycut&&leadingSel&&awaySel&&sigSel);
+//       finalCut*=mcweight;
+//          tgj->Draw(Form("Sum$(-genpPt*cos(genpPhi-phi1)*(genpPt>0.5&&abs(genpEta)<2.2&&genpCh!=0))>>hmptxGen_%d",i),finalCut);
+         tgj->Draw(Form("mptxgenpAllAllAcc>>hmptxGen_%d",i),finalCut);
+         tgj->Draw(Form("mptxtrkCorrAllAcc>>hmptxRec_%d",i),finalCut);
+         tgj->Draw(Form("-mptxtrkAllAcc>>hmptxRaw_%d",i),finalCut);
+//          tgj->Draw(Form("-mptxtrkAllAcc/0.72>>hmptxRaw_%d",i),finalCut);
+//          tgj->Draw(Form("Sum$(-trkPt*cos(trkPhi-phi1)*(trkPt>0.5&&abs(trkEta)<2.2)*trkWt)>>hmptxRaw_%d",i),finalCut);
+//           tgj->Draw(Form("-mptxtrkAllAcc*Sum$( (trkPt>0.5&&abs(trkEta)<2.2)*trkWt )/Sum$( (trkPt>0.5&&abs(trkEta)<2.2) )>>hmptxRaw_%d",i),finalCut);
+//           tgj->Draw(Form("1>>hmptxWt_%d",i),finalCut*"Sum$( (trkPt>0.5&&abs(trkEta)<2.2)*trkWt )");
       }
    }
 
@@ -68,9 +94,10 @@ void drawMptDistr(
    }
 
    TCanvas * c3 = new TCanvas("c3","c3",500,500);
-      gPad->SetLogy();
+   gPad->SetLogy();
    vmptxRec[6]->SetTitle(";mptx (GeV/c);");
    vmptxRec[6]->Draw("E");
+//    vmptxRaw[6]->Divide(vwtsum[6]);
    vmptxRaw[6]->Draw("sameE");
    vmptxGen[6]->Draw("same hist");
 }
--- Saved/macros/dijetmpt2012/drawMptvAj.C
+++ Saved/macros/dijetmpt2012/drawMptvAj.C
@@ -22,22 +22,30 @@ void drawMptvAj(
    TH1::SetDefaultSumw2();
    const int nBin = 4;
    float AjBins[nBin+1] = {0.0001,0.11,0.22,0.33,0.49999};
-   float minPt1=100,minPt2=50,sigDPhi=2./3*3.14159;
    
 //    TString infname="../ntout/output-hy18dj80_IterPixTrkv1_v6_xsec_icPu5.root";
 //    TString infname="/net/hidsk0001/d00/scratch/frankma/work/jet/macros/forest/output-hy18dj80_IterPixTrkv1_v7_xsec_icPu5.root";
-   TString infname="/net/hidsk0001/d00/scratch/frankma/work/jet/macros/ntout/output-hy18dj80_IterPixTrkv1_v9hgtOnly_hgtcorr_xsec_icPu5.root";
+//    TString infname="/net/hidsk0001/d00/scratch/frankma/work/jet/macros/ntout/output-hy18dj80_IterPixTrkv1_v9hgtOnly_hgtcorr_xsec_icPu5.root";
+//    TString infname="/net/hidsk0001/d00/scratch/frankma/work/jet/macros/ntout/output-hy18djallpthats_iterTrkv1_v9hgtOnly_hgtcorr_xsec_icPu5.root";
+//    TString infname="/net/hidsk0001/d00/scratch/frankma/work/jet/macros/ntout/output-hy18dj200_iterTrkv1_v9hgtOnly_hgtcorr_xsec_icPu5.root";
+   TString infname="/net/hidsk0001/d00/scratch/frankma/work/jet/macros/ntout/output-hy18dj200_iterTrkv1_v11hgtOnly_hgtcorr_mptOnly_xsec_icPu5.root";
    TFile * inf = new TFile(infname);
    TTree * tgj = (TTree*)inf->Get("tgj");
 
    TCut mycut="cBin>=0&&cBin<12";
 //    TCut mycut="cBin>=12&&cBin<40";
 
+   float minPt1=200,minPt2=50,sigDPhi=2./3*3.14159;
 //    TCut leadingSel  = Form("abs(eta1)<2&&pt1>%.3f&&(pt1/ref1pt)>0.9&&(pt1/ref1pt)<1.1",minPt1);
+//    TCut leadingSel  = Form("abs(eta1)<2&&pt1>%.3f&&pt1<200",minPt1);
    TCut leadingSel  = Form("abs(eta1)<2&&pt1>%.3f",minPt1);
    TCut awaySel     = Form("abs(eta2)<2&&pt2>%.3f",minPt2);
    TCut sigSel      = Form("acos(cos(phi2-phi1))>%.3f",sigDPhi);
 
+   TString mcweight = "(((samplePtHat==50&&pthat<120)||(samplePtHat==120&&pthat<170)||(samplePtHat==170&&pthat<200)||(samplePtHat==200))*weight*sampleWeight)";
+   
+   TCut finalCut = mycut&&leadingSel&&awaySel&&sigSel;
+//    finalCut*=mcweight;
 
    TProfile *vmptxRec[7], *vmptxRaw[7], *vmptxGen[7];
    for (int i=0; i<7; ++i) {
@@ -48,15 +56,16 @@ void drawMptvAj(
       vmptxGen[i]->SetLineColor(kRed);
    }
    
+   cout << finalCut << endl;
    for (int i=0; i<7; ++i) {
       if (i<6) {
-         tgj->Draw(Form("-mptxgenpAllAllAcc_pt[%d]:Aj>>hmptxGen_%d",i,i),mycut&&leadingSel&&awaySel&&sigSel);
-         tgj->Draw(Form("-mptxtrkCorrAllAcc_pt[%d]:Aj>>hmptxRec_%d",i,i),mycut&&leadingSel&&awaySel&&sigSel);
-         tgj->Draw(Form("-mptxtrkAllAcc_pt[%d]:Aj>>hmptxRaw_%d",i,i),mycut&&leadingSel&&awaySel&&sigSel);
+         tgj->Draw(Form("-mptxgenpAllAllAcc_pt[%d]:Aj>>hmptxGen_%d",i,i),finalCut);
+         tgj->Draw(Form("-mptxtrkCorrAllAcc_pt[%d]:Aj>>hmptxRec_%d",i,i),finalCut);
+         tgj->Draw(Form("-mptxtrkAllAcc_pt[%d]/0.72:Aj>>hmptxRaw_%d",i,i),finalCut);
       } else {
-         tgj->Draw(Form("-mptxgenpAllAllAcc:Aj>>hmptxGen_%d",i),mycut&&leadingSel&&awaySel&&sigSel);
-         tgj->Draw(Form("-mptxtrkCorrAllAcc:Aj>>hmptxRec_%d",i),mycut&&leadingSel&&awaySel&&sigSel);
-         tgj->Draw(Form("-mptxtrkAllAcc:Aj>>hmptxRaw_%d",i),mycut&&leadingSel&&awaySel&&sigSel);
+         tgj->Draw(Form("-mptxgenpAllAllAcc:Aj>>hmptxGen_%d",i),finalCut);
+         tgj->Draw(Form("-mptxtrkCorrAllAcc:Aj>>hmptxRec_%d",i),finalCut);
+         tgj->Draw(Form("-mptxtrkAllAcc/0.72:Aj>>hmptxRaw_%d",i),finalCut);
       }
    }
       
