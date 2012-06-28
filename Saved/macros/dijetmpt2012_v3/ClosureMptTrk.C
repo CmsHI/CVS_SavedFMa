9487a93c6bc52ba122cee895895596051d6a5543
frankma <>
2012-06-28 19:44:29 +0000

    checked new mc with high statistics

--- Saved/macros/dijetmpt2012_v3/ClosureMptTrk.C
+++ Saved/macros/dijetmpt2012_v3/ClosureMptTrk.C
@@ -11,31 +11,18 @@
 #include "commonUtility.h"
 #include "HisMath.C"
 #include "compare.h"
-
-// MPT Ranges
-const int nptrange = 6;
-float ptranges[nptrange+1]={0.5,1.0,2,4,8,20,200};
-// const int nptrange = 12;
-// //                           1   2   3  4 5 6 7 8  9 10 11 12
-// float ptranges[nptrange+1]={0.5,1.0,1.5,2,3,4,6,8,14,20,50,100,200};
+#include "loopMpt.h"
 
 TString ClosureMptTrk(
    int anaMode=0, // 0=pt, 1=eta, 2=dphi
    int idraw=-1,
    TCanvas ** cOut=0,
-   TString outdir = "06.13MptClos"
+   TString outdir = "06.26_TrkClos"
 )
 {
    TH1::SetDefaultSumw2();
    gSystem->mkdir(outdir,kTRUE);
 
-   bool doCorr=true;
-   float maxEta=2.4;
-   float ptmin=8,ptmax=200;
-   if (idraw>=0) {
-      ptmin=ptranges[idraw];
-      ptmax=ptranges[idraw+1];
-   }
    TString mod="hitrkEffAnalyzer_MergedGeneral";
    TrackingCorrections trkCorr("Forest2_v19",mod);
    trkCorr.AddSample("trkcorr/Forest2_v19/trkcorr_hy18dj30_Forest2_v19.root",30);
@@ -45,30 +32,42 @@ TString ClosureMptTrk(
    trkCorr.AddSample("trkcorr/Forest2_v19/trkcorr_hy18dj170_Forest2_v19.root",170);
    trkCorr.smoothLevel_ = 1; 	 
    trkCorr.Init();
-   
-   TFile * inf = new TFile("~/scratch01/work/jet/macros/ntout/output-hy18dj80_forest2_v0_xsec_icPu5.root");
+
+//    TFile * inf = new TFile("~/scratch01/work/jet/macros/ntout/output-hy18dj80_forest2_v0_xsec_icPu5.root");
+   TFile * inf = new TFile("../ntout/output-hy18dj80_Forest2v21_v3_allTrks_simtrk_jpt120_xsec_icPu5.root");
    TTree * t = (TTree*)inf->Get("tgj");
    
    //////////////////////////////////////////
    // Analysis Setup
    //////////////////////////////////////////
-   TString tag=Form("trkClos%d_Aj3_Coarse",anaMode);
+   bool doCorr=true;
+   float maxEta=2.4;
+   float ptmin=20,ptmax=200;
+   if (idraw>=0) {
+      ptmin=ptranges[idraw];
+      ptmax=ptranges[idraw+1];
+   }
+   
+   TString tag=Form("trkClos%d",anaMode);
 
-   TCut sel = "cBin<12&&pt1>120&&pt2>50&&abs(dphi)>2.1&&Aj>0.33";
-   TCut genpSel = "genpCh!=0";
-   TCut trkSel = "trkNHit<8||vtrkQual[][0]";
-   TString trkWt = "vtrkWt[][0]";
-//    TCut trkSel = "trkNHit<8||(vtrkQual[][0]&&trkAlgo==4)";
-//    TString trkWt = "vtrkWt[][1]";
-//    TCut trkSel = "";
-//    TString trkWt = "vtrkWt[][2]";
+   TCut sel = "cBin<12&&pt1>120&&abs(eta1)<1.6&&pt2>50&&abs(eta2)<1.6&&abs(dphi)>2.1";
+//    sel = sel && "Aj>0.2";
+
+   TCut genpSel = "";
+   TCut trkSel = "(trkAlgo<4||vtrkQual[][0])";
+   TString trkWt = "trkWt";
+
+//    TCut trkSel = "(trkAlgo<4||vtrkQual[][0])&&!trkIsFake";
+//    TString trkWt = "trkWt/(1-trkFak)";
+//    TCut genpSel = "simHasRec";
+//    TString trkWt = "trkEff";
 
    float nEvt = t->GetEntries(sel);
    cout << sel << ": " << nEvt << " events" << endl;
    
    Compare cmp("cmp","");
    TH1D * hGenp, *hTrk, *hTrkCorr;
-   float xmin=0, xmax=0;
+   float xmin=0, xmax=0, ymin=0, ymax=0;
    bool doLogx=true, doLogy=true;
    TString genVar, trkVar;
    TCut finalGenSel,finalTrkSel;
@@ -80,8 +79,8 @@ TString ClosureMptTrk(
       cmp.Legend(0.24,0.23,0.7,0.4);
       cmp.leg->AddEntry(hTrk,Form("|#eta| < %.1f",maxEta),"");
       tag += Form("_vs_pt_eta%.0f",maxEta*10);
-      genVar="genpPt"; trkVar="trkPt";
-      finalGenSel = sel&&genpSel&&Form("abs(genpEta)<%.2f",maxEta);
+      genVar="simPt"; trkVar="trkPt";
+      finalGenSel = sel&&genpSel&&Form("abs(simEta)<%.2f",maxEta);
       finalTrkSel = sel&&trkSel&&Form("abs(trkEta)<%.2f",maxEta);
       xmin=0.5; xmax=179.9;
    } else if (anaMode==1) {
@@ -93,9 +92,10 @@ TString ClosureMptTrk(
       cmp.Legend(0.19,0.68,0.65,0.93);
       cmp.leg->AddEntry(hTrk,Form("%.1f<p_{T}<%.1f GeV/c",ptmin,ptmax),"");
       tag += Form("vs_eta_pt%.0f",ptmin);
-      genVar="genpEta"; trkVar="trkEta";
-      finalGenSel = sel&&genpSel&&Form("genpPt>=%.2f&&genpPt<%.2f",ptmin,ptmax);
+      genVar="simEta"; trkVar="trkEta";
+      finalGenSel = sel&&genpSel&&Form("simPt>=%.2f&&simPt<%.2f",ptmin,ptmax);
       finalTrkSel = sel&&trkSel&&Form("trkPt>=%.2f&&trkPt<%.2f",ptmin,ptmax);
+      ymin=0; ymax=1;
    } else if (anaMode==2) {
       TH1D * hBin = new TH1D("hBin",";#Delta#phi(trk,jet1); # per evt",25,0,3.14159);
       hGenp = (TH1D*)hBin->Clone("hGenp");
@@ -105,8 +105,8 @@ TString ClosureMptTrk(
       cmp.Legend(0.19,0.68,0.65,0.93);
       cmp.leg->AddEntry(hTrk,Form("%.1f<p_{T}<%.1f GeV/c",ptmin,ptmax),"");
       tag += Form("_vs_dphi_pt%.0f",ptmin);
-      genVar="acos(cos(genpPhi-phi1))"; trkVar="acos(cos(trkPhi-phi1))";
-      finalGenSel = sel&&genpSel&&Form("genpPt>=%.2f&&genpPt<%.2f",ptmin,ptmax);
+      genVar="acos(cos(simPhi-phi1))"; trkVar="acos(cos(trkPhi-phi1))";
+      finalGenSel = sel&&genpSel&&Form("simPt>=%.2f&&simPt<%.2f",ptmin,ptmax);
       finalTrkSel = sel&&trkSel&&Form("trkPt>=%.2f&&trkPt<%.2f",ptmin,ptmax);
    }
 
@@ -145,6 +145,7 @@ TString ClosureMptTrk(
    if (doLogx) gPad->SetLogx();
    if (doLogy) gPad->SetLogy();
    if (xmax>xmin) hGenp->SetAxisRange(xmin,xmax,"X");
+   if (ymax>ymin) hGenp->SetAxisRange(ymin,ymax,"Y");
    hGenp->Draw("hist");
    hTrk->Draw("sameE");
    if (doCorr) hTrkCorr->Draw("sameE");
@@ -168,7 +169,7 @@ TString ClosureMptTrk(
 
    c2->Print(Form("%s/%s.gif",outdir.Data(),tag.Data()));
    
-   delete c2;
+   if (idraw>=0) delete c2;
    
    // Summary
    if (idraw>=0) {
