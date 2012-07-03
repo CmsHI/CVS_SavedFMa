4473f85b74f71e71a2e334e253cc352b24594536
frankma <>
2012-07-03 23:15:04 +0000

    add difference mode

--- Saved/macros/dijetmpt2012_v3/CompareMptMC.C
+++ Saved/macros/dijetmpt2012_v3/CompareMptMC.C
@@ -18,12 +18,13 @@ using namespace std;
 void CompareMptMC()
 {
    TH1::SetDefaultSumw2();
-//    TString infdataname = "fig/06.29_MPT0StudyDataMC/HisMc_icPu5_trkHPCorr_120_50_2749_eta24_prec4_JetSel2HemSum.root";
-//    TString infrefname  = "fig/06.29_MPT0StudyDataMC/HisMc_icPu5_trkHPCorr_120_50_2749_eta24_prec0_JetSel2HemSum.root";
+   TString infdataname = "fig/06.29_MPT0StudyDataMC/HisMc_icPu5_trkHPCorr_120_50_2749_eta24_prec4_JetSel2HemSum.root";
+   TString infrefname  = "fig/06.29_MPT0StudyDataMC/HisMc_icPu5_trkHPCorr_120_50_2749_eta24_prec0_JetSel2HemSum.root";
 //    TString infdataname = "fig/06.29_MPT0StudyDataMC/HisMc_icPu5_trkHPCorr_120_50_2749_eta24_prec4_GenJetSelEta3HemSum.root";
 //    TString infrefname  = "fig/06.29_MPT0StudyDataMC/HisMc_icPu5_trkHPCorr_120_50_2749_eta24_prec0_GenJetSelEta3HemSum.root";
-   TString infdataname = "fig/06.29_MPT0StudyDataMC/HisMc_icPu5_trkHPCorr_120_50_2749_eta24_prec4_JetSelEta3HemSum.root";
-   TString infrefname  = "fig/06.29_MPT0StudyDataMC/HisMc_icPu5_trkHPCorr_120_50_2749_eta24_prec0_JetSelEta3HemSum.root";
+//    TString infdataname = "fig/06.29_MPT0StudyDataMC/HisMc_icPu5_trkHPCorr_120_50_2749_eta24_prec4_JetSelEta3HemSum.root";
+//    TString infrefname  = "fig/06.29_MPT0StudyDataMC/HisMc_icPu5_trkHPCorr_120_50_2749_eta24_prec0_JetSelEta3HemSum.root";
+//    TString infdataname = "fig/06.30_MPT0StudyDataMC/HisMc_icPu5_trkHPCorr_120_50_2749_eta24_prec4_JetSelEta3TrkCorrDiffLSL.root";
 
    TString insrc="0to12";
 
@@ -35,17 +36,19 @@ void CompareMptMC()
    TTree * tref = (TTree*)infref->Get("t"+insrc);
    cout << "Ref: " << infref->GetName() << " " << tref->GetEntries() << endl;
    
-   TString tag = Form("compMptMc_JetSelEta3_Aj00_"+insrc);
+   TString tag = Form("compMptMc_JetSelEta3_"+insrc);
    float minPt1=120,minPt2=50,sigDPhi=7./8*3.14159;
    TCut leadingSel  = Form("abs(eta1)<1.6&&pt1>%.3f",minPt1);
    TCut awaySel     = Form("abs(eta2)<1.6&&pt2>%.3f",minPt2);
    TCut sigSel      = Form("acos(cos(phi2-phi1))>%.3f",sigDPhi);
-   TCut ajSel       = "Aj<0.11";
+   TCut ajSel       = "Aj>0.";
+   int compMode=0; // 0=ratio, 1=diff
+   if (compMode==1) tag.ReplaceAll("compMpt","compMptDiff");
 
    TString titleJet = "icPu5 Calo Jet";
 //    TString titleJet = "Gen. Jet";
    TString titleSig = "|#eta_{Jet}|<1.6  #Delta#phi_{1,2} > #frac{7}{8}#pi";
-   TString titleAj = "A_{J} < 0.11";
+   TString titleAj = "A_{J} > 0.0";
 
    TCut sel = leadingSel&&awaySel;
    
@@ -57,7 +60,7 @@ void CompareMptMC()
    gSystem->mkdir(outdir,kTRUE);
 
    // histograms
-   for (int i=0; i<6; ++i) {
+   for (int i=0; i<7; ++i) {
       // Legend
       TString titledata = "Rec. Trk.";
       TString titleref  = "Gen. Partl.";
@@ -82,6 +85,17 @@ void CompareMptMC()
          cmp.Draw(";A_{J};<#slash{p}_{T}^{#parallel}> (GeV/c)",-20,20);
          TLine * l0 = new TLine(0,0,AjBins[nAjBin],0);
          l0->Draw();
+         c2->cd(2);
+         TLine * l1;
+         if (compMode==0) {
+            cmp.Ratio()->Draw("hist");
+            l1 = new TLine(0,0,AjBins[nAjBin],0);
+         } else if (compMode==1) {
+            cmp.Difference()->Draw("hist");
+            l1 = new TLine(0,0,AjBins[nAjBin],0);
+         }
+         l1->SetLineStyle(2);
+         l1->Draw();
       } else if (i==1) {
          c2->cd(1);
          cmp.vsel.push_back(sigSel&&ajSel);
@@ -95,20 +109,28 @@ void CompareMptMC()
          c2->cd(1);
          cmp.vsel.push_back(sigSel&&ajSel);
          cmp.vsel.push_back(sigSel&&ajSel);
-         cmp.Init(40,50,250);
+         cmp.Init(20,50,250);
          if (i==2) {
-            cmp.Project("xhem1incone8:pt1>>");
+//             cmp.Project("xhem1incone8:pt1>>");
+            cmp.Project("xhem1incone8-xhem1incone8pt0-xhem1incone8pt1-xhem1incone8pt2:pt1>>");
             cmp.Draw(";Leading Jet p_{T} (GeV/c);<#Sigma p_{T}^{#parallel}(incone)> (GeV/c)",0,500);
          }
          if (i==3) {
-            cmp.Project("xhem2incone8:pt2>>");
+//             cmp.Project("xhem2incone8:pt2>>");
+            cmp.Project("xhem2incone8-xhem2incone8pt0-xhem2incone8pt1-xhem2incone8pt2:pt2>>");
             cmp.Draw(";SubLeading Jet p_{T} (GeV/c);<#Sigma p_{T}^{#parallel}(incone)> (GeV/c)",0,500);
          }
          lex1=0.094; ley1=0.65; lex2=0.594; ley2=0.95;
          lx1=0.19, ly1=0.16, lx2=0.54, ly2=0.34;
          c2->cd(2);
-         cmp.Ratio()->Draw("hist");
-         TLine * l1 = new TLine(50,1,250,1);
+         TLine * l1;
+         if (compMode==0) {
+            cmp.Ratio()->Draw("hist");
+            l1 = new TLine(50,1,250,1);
+         } else if (compMode==1) {
+            cmp.Difference()->Draw("hist");
+            l1 = new TLine(50,0,250,0);
+         }
          l1->SetLineStyle(2);
          l1->Draw();
       } else if (i==4||i==5) {
@@ -128,9 +150,34 @@ void CompareMptMC()
          lx1=0.53, ly1=0.45, lx2=0.87, ly2=0.64;
          c2->cd(2);
          gPad->SetLogx(1);
-         cmp.Ratio()->Draw("hist");
-//          cmp.Difference()->Draw("hist");
-         TLine * l1 = new TLine(ptranges[0],1,ptranges[nptrange],1);
+         TLine * l1;
+         if (compMode==0) {
+            cmp.Ratio()->Draw("hist");
+            l1 = new TLine(ptranges[0],1,ptranges[nptrange],1);
+         } else if (compMode==1) {
+            cmp.Difference()->Draw("hist");
+            l1 = new TLine(ptranges[0],0,ptranges[nptrange],0);
+         }
+         l1->SetLineStyle(2);
+         l1->Draw();
+      } else if (i==6) {
+         c2->cd(1);
+         cmp.vsel.push_back(sigSel&&ajSel);
+         cmp.vsel.push_back(sigSel&&ajSel);
+         cmp.Init(15,50,200);
+         cmp.Project("x:pt2>>");
+         cmp.Draw(";subleading jet p_{T} (GeV/c);<#slash{p}_{T}^{#parallel}> (GeV/c)",-40,40);
+         TLine * l0 = new TLine(50,0,200,0);
+         l0->Draw();
+         c2->cd(2);
+         TLine * l1;
+         if (compMode==0) {
+            cmp.Ratio()->Draw("hist");
+            l1 = new TLine(50,1,250,1);
+         } else if (compMode==1) {
+            cmp.Difference()->Draw("hist");
+            l1 = new TLine(50,0,250,0);
+         }
          l1->SetLineStyle(2);
          l1->Draw();
       }
