ccbecf230c97319357dd48af922704e627659203
frankma <>
2012-07-13 04:24:10 +0000

    simple macro to quickly validate tracking

---
+++ Saved/macros/dijetmpt2012_v4/loopTracks.C
@@ -0,0 +1,90 @@
+#include <TFile.h>
+#include <TH1.h>
+#include <TNtuple.h>
+#include <iostream>
+#include <algorithm>
+#include <utility>
+#include <vector>
+#include "TTree.h"
+#include "TSystem.h"
+#include "TCanvas.h"
+#include "loopTracks.h"
+using namespace std;
+
+void loopTracks(
+               TString outdir = "./fig/07.12_loopTrks_F2STATrkCorrv8"
+             )
+{
+   TH1::SetDefaultSumw2();
+   gSystem->mkdir(outdir,kTRUE);
+
+   const int nCentBin = 1;
+   int centBins[2] = {0,12};
+
+   TString infname = "../ntout/output-hy18dj80_Forest2v21_v3_allTrks_Eta8_jpt50eta2_xsec_icPu5.root";
+   
+   int particleRecLevel = 2; // 0 gen, 1 sim, 2 sim mat, 3 rec mat, 4 rec
+   
+   float minJetPt1=120;
+   float minJetPt2=50;
+   float sigDPhi=3.1415926*7./8;
+   float etamax=2.4;
+   TString tag = Form("%s/HisMc_icPu5_trkHPCorr_%.0f_%.0f_%.0f_eta%.0f_prec%d",outdir.Data(),minJetPt1,minJetPt2,sigDPhi*1000,etamax*10,particleRecLevel);
+   tag+="SelfCorrLowPt";
+
+   TFile *inf = TFile::Open(infname);
+   TTree *nt =(TTree*)inf->FindObjectAny("tgj");
+   cout << endl << "# " << endl;
+   cout << "# " << infname << ": " << nt->GetEntries() << " events." << endl;
+   cout << "# " << endl;
+
+   TString outfname = tag+".root";   
+   cout << "Output file: " << outfname << endl;
+   TFile* hout = new TFile(outfname,"RECREATE");
+
+   for (int c=0; c<nCentBin; ++c) {
+      TString name = Form("%dto%d",centBins[c],centBins[c+1]);
+      AnaTrk ana(name);
+      ana.maxEntry = 10000;
+      ana.isMC = true;
+      ana.particleRecLevel = particleRecLevel;
+      ana.cMin=centBins[c];
+      ana.cMax=centBins[c+1];
+      ana.minJetPt1 = minJetPt1;
+      ana.minJetPt2 = minJetPt2;
+      ana.maxJetEta = 1.6;
+      ana.sigDPhi = sigDPhi;
+      ana.minPt = 0.5;
+      ana.maxEta = etamax;
+      ana.outf = hout;
+      ana.Init(nt);
+      ana.Loop();
+      // Inspect
+      TCanvas * c2 = new TCanvas("c"+name,"c"+name,800,400);
+//       TH1D * hGenp = ana.hGenpPt;
+//       TH1D * hTrk = ana.hTrkPt;
+//       TH1D * hCorr = ana.hTrkCorrPt;
+      TH1D * hGenp = ana.hGenpEta;
+      TH1D * hTrk = ana.hTrkEta;
+      TH1D * hCorr = ana.hTrkCorrEta;
+      c2->Divide(2,1);
+      c2->cd(1);
+      hGenp->SetLineColor(2);
+      hTrk->SetMarkerStyle(kOpenCircle);
+      hGenp->Draw("hist");
+      hTrk->Draw("sameE");
+      hCorr->Draw("sameE");
+      c2->cd(2);
+      TH1D * hGenpRat = (TH1D*)hGenp->Clone("hGenpRat");
+      hGenpRat->Divide(hGenp);
+      TH1D * hTrkRat = (TH1D*)hTrk->Clone("hTrkRat");
+      hTrkRat->Divide(hGenp);
+      TH1D * hTrkCorrRat = (TH1D*)hCorr->Clone("hTrkCorrRat");
+      hTrkCorrRat->Divide(hGenp);
+      hGenpRat->Draw("hist");
+      hTrkRat->Draw("sameE");
+      hTrkCorrRat->Draw("sameE");
+   }
+   hout->Write();   
+}
+
