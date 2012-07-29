172b00f0f3ccaf4df5ab6d6ac5add2a7f271e6cf
frankma <>
2012-07-29 09:36:58 +0000

    new samples

--- Saved/macros/forestv3/runFullTrackCorrection_one.sh
+++ Saved/macros/forestv3/runFullTrackCorrection_one.sh
@@ -19,12 +19,10 @@ trkPtMin=0.5
 echo "sample pthat: $pthat, pthatMax = $pthatMax, trkPtMin = $trkPtMin"
 # samples
 outputFile=trkcorr/$corrset"/"$corrset"_hy18dj${pthat}to${pthatMax}.root"
-if [[ ($pthat -eq 100) ]];
-then inputFile="/mnt/hadoop/cms/store/user/yenjie/HiForest_v27/Dijet${pthat}_HydjetDrum_v27_mergedV1.root"; fi
-if [[ ($pthat -eq 170) ]];
-then inputFile="/net/hidsk0001/d00/scratch/yjlee/merge/v27/pthat${pthat}/Dijet170_HydjetDrum_v27_mergedV1.root"; fi
-if [[ ($pthat -eq 200) ]];
-then inputFile="/net/hidsk0001/d00/scratch/yjlee/merge/v27/pthat${pthat}/Dijet200_HydjetDrum_v28_mergedV1.root"; fi
+if [[ ($pthat -ge 50) && ($pthat -le 170) ]];
+then inputFile="/net/hidsk0001/d00/scratch/yjlee/merge/v27/pthat${pthat}/Dijet${pthat}_HydjetDrum_v27_mergedV1.root"; fi
+if [[ ($pthat -ge 200) && ($pthat -le 300) ]];
+then inputFile="/net/hidsk0001/d00/scratch/yjlee/merge/v27/pthat${pthat}/Dijet${pthat}_HydjetDrum_v28_mergedV1.root"; fi
 # final checks
 echo "input:  "$inputFile
 echo "output: "$outputFile
