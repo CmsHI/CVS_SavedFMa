217be9705c395f610dcb7e8d0a549a063eae236d
frankma <>
2012-11-28 19:33:22 +0000

    fix typo

--- Saved/macros/ff2012_v2/runAddReweight.sh
+++ Saved/macros/ff2012_v2/runAddReweight.sh
@@ -1,3 +1,3 @@
 for i in {0..4}; do
-  root -l -b -q addJetWeight.C+'("jskim_pp-full_ak3PF_Nov14_jetPt_50_jetEtaCut_2.00_noPbin_sm'$i'_ak3PF_gj0.root",'$i',"fig/Nov20/hisSmear1_Rewt0.root")'
+  root -l -b -q addJetWeight.C+'("jskim_pp-full_ak3PF_Nov14_jetPt_50_jetEtaCut_2.00_noPbin_sm'$i'_ak3PF_gj0.root",'$i',"fig/Nov20/hisSmear1_Rewt1.root")'
 done
