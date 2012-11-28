2d0660fdd234b3ed8dfc4fc9b8b945fc16b82103
frankma <>
2012-11-28 19:27:57 +0000

    demo run scripts

---
+++ Saved/macros/ff2012_v2/runForest2jSkim_Nov20.sh
@@ -0,0 +1,13 @@
+subName='Nov20'
+
+# pbpb data
+jetAlgo='akPu3PF'
+root -l -b -q 'forest2jetSkim.C+("/net/hidsk0001/d00/scratch/yjlee/merge/pbpbDijet_v20/promptskim-hihighpt-hltjet80-pt90-v20.root","jskim_hltjet80-pt90-v20_'$jetAlgo'_'$subName'",false,-1,"",false,"'$jetAlgo'",0,0,0)'
+
+# pp
+jetAlgo='ak3PF'
+for i in {0..4}; do # 0 = no centrality weight 
+   root -l -b -q 'forest2jetSkim.C+("/net/hisrv0001/home/yenjie/scratch/hiForest/prod/productionPP/CMSSW_4_4_2_patch6/test/ppForest2/pp_merged_full.root",  "jskim_pp-full_'$jetAlgo'_'$subName'",false,-1,"",false,"'$jetAlgo'",1,'$i',0)'
+
+# old archive
+#root -l -b -q 'forest2jetSkim.C+("/mnt/hadoop/cms/store/user/frankmalocal/forest/HiForest-promptskim-hiForest2_v21_HLTFix.root",  "jskim_hltjet80-v21_'$jetAlgo'_'$subName'",false,1000,"",false,"'$jetAlgo'",0,0,0)'
