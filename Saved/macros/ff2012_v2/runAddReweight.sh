c2641be61ddaecf900b06bc5123aa6f4224d51f6
frankma <>
2013-01-18 04:23:04 +0000

    Jan 16 ntuples

--- Saved/macros/ff2012_v2/runAddReweight.sh
+++ Saved/macros/ff2012_v2/runAddReweight.sh
@@ -1,3 +1,4 @@
 for i in {1..4}; do
-  root -l -b -q addJetWeight.C+'("jskim_pp-full_ak3PF_Dec20_4bin_sm18_jetPt_60_jetEtaCut_2.00_noPbin_sm2bin'$i'_ak3PF_gj0.root",'$i',"fig/Dec18/hisCmp1_Smear2_Rewt0.root")'
+  # Jan 16 Report
+  root -l -b -q addJetWeight.C+'("../ntout/jskim_pp-full_ak3PF_Jan16_4bin_sm18_jetPt_60_jetEtaCut_2.00_noPbin_sm2bin'$i'_ak3PF_gj0.root",'$i',"fig/Jan16Report/jet_spectrum_cmp1_sm2_rewt0_pbpbJan16_ppJan16.root")'
 done
