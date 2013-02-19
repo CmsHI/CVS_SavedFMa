b03d4e1c5d28f015cda43428fabc8bdbef1b5549
frankma <>
2013-02-19 00:05:21 +0000

    checked pthat 50, too much fluctuation for jet100 cut. use pthatmin=80 for jet triggered analysis

--- Saved/macros/ff2012_v2/checkPtHats.C
+++ Saved/macros/ff2012_v2/checkPtHats.C
@@ -35,22 +35,27 @@ void checkPtHats() {
   multiTreeUtil* mt  = new multiTreeUtil();
   TCut jetSelCut = "jetPt>100";
   TCut centCut = "";
+  // TCut centCut = "cBin>=0&&cBin<4";
   // // pp
-  // mt->addFile("../ntout/jskim_dj50_ak3PF_Jan17_4bin_sm18_jetPt_60_jetEtaCut_2.00_noPbin_sm0bin0_ak3PF_gj0.root", "tdj", jetSelCut && centCut && "pthat>=50&&pthat<80",1.021e-3*1.e9);
   // mt->addFile("../ntout/jskim_dj80_ak3PF_Jan17_4bin_sm18_jetPt_60_jetEtaCut_2.00_noPbin_sm0bin0_ak3PF_gj0.root", "tdj", jetSelCut && centCut && "pthat>=80&&pthat<120",9.913e-5*1.e9);
   // mt->addFile("../ntout/jskim_dj120_ak3PF_Jan17_4bin_sm18_jetPt_60_jetEtaCut_2.00_noPbin_sm0bin0_ak3PF_gj0.root", "tdj", jetSelCut && centCut && "pthat>=120&&pthat<9999",1.128e-5*1.e9);
+  // mt->addFile("../ntout/jskim_dj50_ak3PF_Feb15_4bin_sm18_jetPt_60_jetEtaCut_2.00_noPbin_sm0bin0_ak3PF_gj0.root", "tdj", jetSelCut && centCut && "pthat>=50&&pthat<80",1.021e-03*1.e9);
+  // mt->addFile("../ntout/jskim_dj80_ak3PF_Feb15_4bin_sm18_jetPt_60_jetEtaCut_2.00_noPbin_sm0bin0_ak3PF_gj0.root", "tdj", jetSelCut && centCut && "pthat>=80&&pthat<120",9.913e-5*1.e9);
+  // mt->addFile("../ntout/jskim_dj120_ak3PF_Feb15_4bin_sm18_jetPt_60_jetEtaCut_2.00_noPbin_sm0bin0_ak3PF_gj0.root", "tdj", jetSelCut && centCut && "pthat>=120&&pthat<170",1.128e-5*1.e9);
+  // mt->addFile("../ntout/jskim_dj170_ak3PF_Feb15_4bin_sm18_jetPt_60_jetEtaCut_2.00_noPbin_sm0bin0_ak3PF_gj0.root", "tdj", jetSelCut && centCut && "pthat>=170&&pthat<9999",1.470e-6*1.e9);
   // hi
-  // mt->addFile("../ntout/jskim_hydj50_akPu3PF_Jan17_4bin_sm18_jetPt_60_jetEtaCut_2.00_noPbin_sm1bin0_akPu3PF_gj0.root", "tdj", jetSelCut && centCut,1.);
-  // mt->addFile("../ntout/jskim_hydj80_akPu3PF_Jan17_4bin_sm18_jetPt_60_jetEtaCut_2.00_noPbin_sm1bin0_akPu3PF_gj0.root", "tdj", jetSelCut && centCut,1.);
-  // mt->addFile("../ntout/jskim_hydj100_akPu3PF_Jan17_4bin_sm18_jetPt_60_jetEtaCut_2.00_noPbin_sm1bin0_akPu3PF_gj0.root", "tdj", jetSelCut && centCut,1.);
-  // mt->addFile("../ntout/jskim_hydj120_akPu3PF_Jan17_4bin_sm18_jetPt_60_jetEtaCut_2.00_noPbin_sm1bin0_akPu3PF_gj0.root", "tdj", jetSelCut && centCut,1.);
-  // mt->addFile("../ntout/jskim_hydj50_akPu3PF_Jan17_4bin_sm18_jetPt_60_jetEtaCut_2.00_noPbin_sm1bin0_akPu3PF_gj0.root", "tdj", jetSelCut && centCut && "pthat>=50&&pthat<80",1.021e-3*1.e9);
-  mt->addFile("../ntout/jskim_hydj80_akPu3PF_Jan17_4bin_sm18_jetPt_60_jetEtaCut_2.00_noPbin_sm1bin0_akPu3PF_gj0.root", "tdj", jetSelCut && centCut && "pthat>=80&&pthat<100",9.913e-5*1.e9);
-  mt->addFile("../ntout/jskim_hydj100_akPu3PF_Jan17_4bin_sm18_jetPt_60_jetEtaCut_2.00_noPbin_sm1bin0_akPu3PF_gj0.root", "tdj", jetSelCut && centCut && "pthat>=100&&pthat<120",3.069e-5*1.e9);
-  mt->addFile("../ntout/jskim_hydj120_akPu3PF_Jan17_4bin_sm18_jetPt_60_jetEtaCut_2.00_noPbin_sm1bin0_akPu3PF_gj0.root", "tdj", jetSelCut && centCut && "pthat>=120&&pthat<9999",1.128e-5*1.e9);
   // mt->addFile("../ntout/jskim_hydj80_akPu3PF_Jan17_4bin_sm18_jetPt_60_jetEtaCut_2.00_noPbin_sm1bin0_akPu3PF_gj0.root", "tdj", jetSelCut && centCut && "pthat>=80&&pthat<100",9.913e-5*1.e9);
   // mt->addFile("../ntout/jskim_hydj100_akPu3PF_Jan17_4bin_sm18_jetPt_60_jetEtaCut_2.00_noPbin_sm1bin0_akPu3PF_gj0.root", "tdj", jetSelCut && centCut && "pthat>=100&&pthat<9999",3.069e-5*1.e9);
+  mt->addFile("../ntout/jskim_hydj50_akPu3PF_Feb15_4bin_sm18_jetPt_60_jetEtaCut_2.00_noPbin_sm1bin0_akPu3PF_gj0.root", "tdj", jetSelCut && centCut && "pthat>=50&&pthat<80",1.021e-03*1.e9);
+  mt->addFile("../ntout/jskim_hydj80_akPu3PF_Feb15_4bin_sm18_jetPt_60_jetEtaCut_2.00_noPbin_sm1bin0_akPu3PF_gj0.root", "tdj", jetSelCut && centCut && "pthat>=80&&pthat<100",9.913e-5*1.e9);
+  mt->addFile("../ntout/jskim_hydj100_akPu3PF_Feb15_4bin_sm18_jetPt_60_jetEtaCut_2.00_noPbin_sm1bin0_akPu3PF_gj0.root", "tdj", jetSelCut && centCut && "pthat>=100&&pthat<170",3.069e-5*1.e9);
+  mt->addFile("../ntout/jskim_hydj170_akPu3PF_Feb15_4bin_sm18_jetPt_60_jetEtaCut_2.00_noPbin_sm1bin0_akPu3PF_gj0.root", "tdj", jetSelCut && centCut && "pthat>=170&&pthat<9999",1.470e-6*1.e9);
   mt->NormalizeScales(2,"","hEvtCentNoSkim");
+
+  TString outdir="fig/Feb15";
+  // TString tag="dj";
+  TString tag="hydj";
+  // TString tag="test";
 
   TCanvas * c2 = new TCanvas("c2","c2",500,500);
   c2->SetLogy();
@@ -58,19 +63,19 @@ void checkPtHats() {
   mt->Draw(hAll,"pthat","abs(jetEta)<2","1","E");
   hAll->SetAxisRange(2e-2,3e4,"Y");
   hAll->Draw();
-  c2->Print("fig/pthat_mergedsample_hydj.gif");
+  c2->Print(Form("%s/pthat_mergedsample_%s.gif",outdir.Data(),tag.Data()));
 
   TCanvas * c3 = new TCanvas("c3","c3",500,500);
   c3->SetLogy();
   TH1D * hAll2 = new TH1D("hAll2",";#hat{p}_{T} (GeV/c);",100,0,500);
   hAll2->SetAxisRange(2e-2,3e4,"Y");
   mt->Draw2(hAll2,"pthat","abs(jetEta)<2","1","E");
-  c3->Print("fig/pthat_mergedsamplecomp_hydj.gif");
+  c3->Print(Form("%s/pthat_mergedsamplecomp_%s.gif",outdir.Data(),tag.Data()));
 
   TCanvas * c4 = new TCanvas("c4","c4",500,500);
   c4->SetLogy();
   TH1D * hAll3 = new TH1D("hAll3",";jet p_{T} (GeV/c);",100,0,500);
   mt->Draw2(hAll3,"jetPt","abs(jetEta)<2","1","E");
   hAll3->SetAxisRange(2e-2,3e4,"Y");
-  c4->Print("fig/jetpt_mergedsamplecomp_hydj.gif");
+  c4->Print(Form("%s/jetpt_mergedsamplecomp_%s.gif",outdir.Data(),tag.Data()));
 }
