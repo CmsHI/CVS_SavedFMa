02b7562d940c0d210d17f4faad9144b139091b65
frankma <>
2013-04-22 20:25:16 +0000

    check variations of weight of jet pt

--- Saved/macros/ff2012_v2/compareReweight.C
+++ Saved/macros/ff2012_v2/compareReweight.C
@@ -3,6 +3,7 @@
   TFile *_file1 = TFile::Open("../ntout/jskim_pp-full_ak3PF_Jan17_4bin_sm18_jetPt_60_jetEtaCut_2.00_noPbin_sm2bin1_ak3PF_gj0_addedFitRewt.root");
   t0=(TTree*)_file0->Get("tdj");
   t1=(TTree*)_file1->Get("tdj");
-  t0.Draw("rewtPt:jetPt","jetPt>100","colz");
+  // t0.Draw("rewtPt:jetPt","jetPt>100","colz");
+  t1.Draw("1+2*(rewtPt-1):jetPt","jetPt>100","colz");
   t1.Draw("rewtPt:jetPt","jetPt>100","box same");
 }
